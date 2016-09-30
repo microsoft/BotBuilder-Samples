05aa69f9e90818f4165c27fe897a81c60070ccff
JuanAr <juan.arguello@southworks.com>
2016-09-30 20:20:52 -0300

    C# Templates using FromFlow

---
+++ CSharp/template-Form/FormModel.cs
@@ -0,0 +1,29 @@
+﻿namespace FormTemplate
+{
+    using System;
+    using Microsoft.Bot.Builder.FormFlow;
+
+    // POCO model used to build FormFlow Dialog
+    // This class represents the Formto be filled
+    [Serializable]
+    public class FormModel
+    {
+        [Prompt("Hi! What is your {&}?")]
+        public string Name { get; set; }
+
+        [Prompt("Please say something and I'll repeat it")]
+        public string Message { get; set; }
+
+        public static IForm<FormModel> BuildForm()
+        {
+            // Builds an IForm<T> based on FormModel POCO
+            return new FormBuilder<FormModel>().Build();
+        }
+
+        public static IFormDialog<FormModel> BuildFormDialog()
+        {
+            // Generated a new FormDialog<T> based on IForm<FormModel>
+            return FormDialog.FromForm(FormModel.BuildForm);
+        }
+    }
+}
