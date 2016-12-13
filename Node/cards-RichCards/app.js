a42655f7cf2fc22c69629fec6e902cf3d3dfa839
minan05 <minda.aguhob@gmail.com>
2016-12-12 16:43:46 -0800

    Update app.js

--- Node/cards-RichCards/app.js
+++ Node/cards-RichCards/app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.Microsoft_ID,
+    appPassword: process.env.Microsoft_PW
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
