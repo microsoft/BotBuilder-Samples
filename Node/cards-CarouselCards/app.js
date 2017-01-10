13b70002a2ad57c6defd84fc8b0e95de9663a06c
Pablo Costantini <pcostantini@users.noreply.github.com>
2017-01-10 11:03:52 -0300

    Fix typo in code comment

--- Node/cards-CarouselCards/app.js
+++ Node/cards-CarouselCards/app.js
@@ -7,7 +7,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat bot and list for messages
+// Create chat bot and listen to messages
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
