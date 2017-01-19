5d5f989524e24d13f490d37843a421141a7870de
MIDDLEEAST\mmoussa <mmoussa@microsoft.com>
2017-01-19 22:02:50 +0200

    Change Assembly title and assembly product names in the AssemblyInfo.cs to "NewsieBot"

--- CSharp/intelligence-Newsie/Properties/AssemblyInfo.cs
+++ CSharp/intelligence-Newsie/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Newsie")]
+[assembly: AssemblyTitle("NewsieBot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Newsie")]
+[assembly: AssemblyProduct("NewsieBot")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
