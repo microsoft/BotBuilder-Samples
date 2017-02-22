996ea0dbba314bf326c5a3eeb9de927e78ec25e7
Pablo Costantini <pablo.costantini@southworks.com>
2017-02-22 12:03:26 -0300

    MultiDialogs refactor
    - Each submodule exports only a dialog object or waterfall dialog
    - Support triggered with actionTrigger
    - Global bot error logging
    - Main dialog moved to bot constructor

--- Node/core-MultiDialogs/support.js
+++ Node/core-MultiDialogs/support.js
@@ -1,13 +1,13 @@
-module.exports = {
-    Label: 'Support',
-    Dialog: function (session) {
-        // Generate ticket
-        var tickerNumber = Math.ceil(Math.random() * 20000);
+module.exports = function (session) {
+    // Generate ticket
+    var tickerNumber = Math.ceil(Math.random() * 20000);
 
-        // Reply and return to parent dialog
-        session.send('Your message \'%s\' was registered. Once we resolve it; we will get back to you.', session.message.text);
-        session.endDialogWithResult({
-            response: tickerNumber
-        });
-    }
+    // Reply and return to parent dialog
+    session.send('Your message \'%s\' was registered. Once we resolve it; we will get back to you.', session.message.text);
+    
+    session.send('Thanks for contacting our support team. Your ticket number is %s.', tickerNumber);
+
+    session.endDialogWithResult({
+        response: tickerNumber
+    });
 };
