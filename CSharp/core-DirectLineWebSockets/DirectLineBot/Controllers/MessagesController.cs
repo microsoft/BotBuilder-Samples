76ccf52f7a893ae064b481ce12596e14369934dd
Ezequiel Jadib <ezequieljadib@hotmail.com>
2017-03-09 10:24:52 -0300

    [CSharp] DirectLine + WebSockets sample

---
+++ CSharp/core-DirectLineWebSockets/DirectLineBot/Controllers/MessagesController.cs
@@ -0,0 +1,68 @@
+﻿namespace DirectLineBot
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new DirectLineBotDialog());
+            }
+            else
+            {
+                await this.HandleSystemMessage(activity);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (message.MembersAdded.Any(o => o.Id == message.Recipient.Id))
+                {
+                    ConnectorClient client = new ConnectorClient(new Uri(message.ServiceUrl));
+
+                    var reply = message.CreateReply();
+
+                    reply.Text = "Welcome to the Bot to showcase the DirectLine API. Send 'Show me a hero card' or 'Send me a BotFramework image' to see how the DirectLine client supports custom channel data. Any other message will be echoed.";
+
+                    await client.Conversations.ReplyToActivityAsync(reply);
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+        }
+    }
+}
---
+++ CSharp/core-DirectLineWebSockets/DirectLineClient/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace DirectLineSampleClient
+{
+    using System;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Connector.DirectLine;
+    using Newtonsoft.Json;
+    using System.Threading;
+    using System.Text;
+    using WebSocketSharp;
+
+    public class Program
+    {
+        private static string directLineSecret = ConfigurationManager.AppSettings["DirectLineSecret"];
+        private static string botId = ConfigurationManager.AppSettings["BotId"];
+        private static string fromUser = "DirectLineSampleClientUser";
+
+        public static void Main(string[] args)
+        {
+            StartBotConversation().Wait();
+        }
+
+        private static async Task StartBotConversation()
+        {
+            var directLineClient = new DirectLineClient(directLineSecret);
+            
+            var conversation = await directLineClient.Conversations.StartConversationAsync();
+
+            using(var webSocketClient = new WebSocket(conversation.StreamUrl))
+            {
+                webSocketClient.OnMessage += WebSocketClient_OnMessage;
+                webSocketClient.Connect();
+
+                while (true)
+                {
+                    string input = Console.ReadLine().Trim();
+
+                    if (input.ToLower() == "exit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (input.Length > 0)
+                        {
+                            Activity userMessage = new Activity
+                            {
+                                From = new ChannelAccount(fromUser),
+                                Text = input,
+                                Type = ActivityTypes.Message
+                            };
+
+                            await directLineClient.Conversations.PostActivityAsync(conversation.ConversationId, userMessage);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void WebSocketClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            // avoid null reference exception when no data received
+            if (string.IsNullOrWhiteSpace(e.Data))
+            {
+                return;
+            }
+
+            var activitySet = JsonConvert.DeserializeObject<ActivitySet>(e.Data);
+            var activities = from x in activitySet.Activities
+                             where x.From.Id == botId
+                             select x;
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.Text);
+
+                if (activity.Attachments != null)
+                {
+                    foreach (Attachment attachment in activity.Attachments)
+                    {
+                        switch (attachment.ContentType)
+                        {
+                            case "application/vnd.microsoft.card.hero":
+                                RenderHeroCard(attachment);
+                                break;
+
+                            case "image/png":
+                                Console.WriteLine($"Opening the requested image '{attachment.ContentUrl}'");
+
+                                Process.Start(attachment.ContentUrl);
+                                break;
+                        }
+                    }
+                }
+                Console.Write("Command> ");
+            }
+        }
+        
+        private static void RenderHeroCard(Attachment attachment)
+        {
+            const int Width = 70;
+            Func<string, string> contentLine = (content) => string.Format($"{{0, -{Width}}}", string.Format("{0," + ((Width + content.Length) / 2).ToString() + "}", content));
+
+            var heroCard = JsonConvert.DeserializeObject<HeroCard>(attachment.Content.ToString());
+
+            if (heroCard != null)
+            {
+                Console.WriteLine("/{0}", new string('*', Width + 1));
+                Console.WriteLine("*{0}*", contentLine(heroCard.Title));
+                Console.WriteLine("*{0}*", new string(' ', Width));
+                Console.WriteLine("*{0}*", contentLine(heroCard.Text));
+                Console.WriteLine("{0}/", new string('*', Width + 1));
+            }
+        }
+    }
+}
