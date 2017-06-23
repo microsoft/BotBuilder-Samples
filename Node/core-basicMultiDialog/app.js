f9def6d6745976ff7e3f80401ee6069641cd4a4f
Pablo Costantini <sixdemons@gmail.com>
2017-06-23 18:39:41 -0300

    Add default port

--- Node/core-basicMultiDialog/app.js
+++ Node/core-basicMultiDialog/app.js
@@ -4,6 +4,6 @@ const bot = require('./bot.js');
 
 const server = restify.createServer();
 server.post('/api/messages', bot.connector('*').listen());
-server.listen(process.env.PORT, () => {
+server.listen(process.env.PORT || 3978, () => {
     console.log(`${server.name} listening to ${server.url}`);
 });
