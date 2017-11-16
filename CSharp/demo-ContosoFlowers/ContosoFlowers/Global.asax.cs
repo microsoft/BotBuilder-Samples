763338f79a24bf3138f606a9be241fb448e22821
Carlos Castro <ccastro@microsoft.com>
2017-11-16 13:31:54 -0800

    State API deprecation: moving samples to InMemoryStore and providing comments for easy migration to azure table or cosmos db

--- CSharp/demo-ContosoFlowers/ContosoFlowers/Global.asax.cs
+++ CSharp/demo-ContosoFlowers/ContosoFlowers/Global.asax.cs
@@ -1,13 +1,18 @@
 ﻿namespace ContosoFlowers
 {
+    using System.Reflection;
     using System.Web.Http;
     using System.Web.Mvc;
     using System.Web.Routing;
     using Autofac;
     using Autofac.Integration.Mvc;
     using AutoMapper;
+
+    using Microsoft.Bot.Builder.Azure;
     using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Dialogs.Internals;
     using Microsoft.Bot.Builder.Internals.Fibers;
+    using Microsoft.Bot.Connector;
 
     public class WebApiApplication : System.Web.HttpApplication
     {
@@ -29,6 +34,24 @@
                 builder.RegisterModule(new ReflectionSurrogateModule());
                 builder.RegisterModule<ContosoFlowersModule>();
                 builder.RegisterControllers(typeof(WebApiApplication).Assembly);
+
+
+                builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+
+                // Bot Storage: Here we register the state storage for your bot. 
+                // Default store: volatile in-memory store - Only for prototyping!
+                // We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+                // For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+                var store = new InMemoryDataStore();
+
+                // Other storage options
+                // var store = new TableBotDataStore("...DataStorageConnectionString..."); // requires Microsoft.BotBuilder.Azure Nuget package 
+                // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
+
+                builder.Register(c => store)
+                    .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                    .AsSelf()
+                    .SingleInstance();
             });
 
             DependencyResolver.SetResolver(new AutofacDependencyResolver(Conversation.Container));
