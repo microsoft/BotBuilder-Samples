79f8229561f586994318d5b67e26705b7d552bbc
Dave Taniguchi <daveta@microsoft.com>
2018-08-30 10:55:59 -0700

    Simple QnA bot with Application Insights

---
+++ csharp_dotnetcore/11.QnAMaker/README.md
@@ -0,0 +1,54 @@
+﻿This sample shows how to integrate QnA Maker in a simple bot with ASP.Net Core 2 with Application Insights. 
+
+# Concepts introduced in this sample
+The [QnA Maker Service](https://www.qnamaker.a) enables you to build, train and publish a simple question and answer bot based on FAQ URLs, structured documents or editorial content in minutes.
+
+The [Application Insights](https://azure.microsoft.com/en-us/services/application-insights/) enables you to discover actionable insights through application performance management and instant analytics.
+
+In this sample, we demonstrate how to use the QnA Maker service to answer questions based on a FAQ text file as input.
+
+# To try this sample
+
+- Clone the samples repository
+```bash
+git clone https://github.com/Microsoft/botbuilder-samples.git
+```
+
+
+### Prerequisites
+- Follow instructions [here](https://docs.microsoft.com/en-us/azure/cognitive-services/qnamaker/how-to/set-up-qnamaker-service-azure) to create a QnA Maker service.
+- Follow instructions [here](https://docs.microsoft.com/en-us/azure/cognitive-services/qnamaker/how-to/migrate-knowledge-base) to import the [smartLightFAQ.tsv](smartLightFAQ.tsv) to your newly created QnA Maker service.
+- Update [appsettings.json](appsettings.json) with your QnAMaker-Host, QnAMaker-KnowledgeBaseId and QnAMaker-EndpointKey. You can find this information under "Settings" tab for your QnA Maker Knowledge Base at [QnAMaker.ai](https://qnamaker.ai)
+- Follow instructions [here](https://docs.microsoft.com/en-us/azure/application-insights/app-insights-asp-net-core) to set up your Application Insights service.
+  - Note: The Application Insights will automatically update the [appsettings.json](appsettings.json) file.
+
+## Visual studio
+- Navigate to the samples folder (BotBuilder-Samples\11.QnAMaker) and open AspNetCore-QnA-Bot.csproj in Visual studio 
+- Hit F5
+
+## Visual studio code
+- Open BotBuilder-Samples\11.QnAMaker sample folder.
+- Bring up a terminal, navigate to BotBuilder-Samples\11.QnAMaker folder
+- type 'dotnet run'
+
+## Testing the bot using Bot Framework Emulator
+[Microsoft Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
+
+- Install the Bot Framework emulator from [here](https://github.com/Microsoft/BotFramework-Emulator/releases)
+
+### Connect to bot using Bot Framework Emulator **V4**
+- Launch Bot Framework Emulator
+- File -> Open bot and navigate to BotBuilder-Samples\11.QnAMaker folder
+- Select AspNetCore-QnA-Bot.bot file
+
+
+# QnA Maker service
+QnA Maker enables you to power a question and answer service from your semi-structured content. 
+
+One of the basic requirements in writing your own Bot service is to seed it with questions and answers. In many cases, the questions and answers already exist in content like FAQ URLs/documents, product manuals, etc. With QnA Maker, users can query your application in a natural, conversational manner. QnA Maker uses machine learning to extract relevant question-answer pairs from your content. It also uses powerful matching and ranking algorithms to provide the best possible match between the user query and the questions.
+
+# Further reading
+
+- [Azure Bot Service Introduction](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
+- [Bot State](https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-storage-concept?view=azure-bot-service-4.0)
+- [QnA Maker documentation](https://docs.microsoft.com/en-us/azure/cognitive-services/qnamaker/overview/overview)
---
+++ csharp_dotnetcore/11.QnAMaker/Startup.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AspNetCore_QnA_Bot.AppInsights;
+
+namespace AspNetCore_QnA_Bot
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBot<QnABot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+
+                // Add MyAppInsightsLoggerMiddleware (logs activity messages into Application Insights)
+                var instrumentationKey = Configuration.GetSection("ApplicationInsights")?.GetSection("InstrumentationKey").Value;
+                if (null == instrumentationKey)
+                {
+                    throw new InvalidOperationException("Application Insights instrumentation key must be configured in the appsettings.json file.");
+                }
+                var appInsightsLogger = new MyAppInsightsLoggerMiddleware(instrumentationKey, logUserName: true, logOriginalMessage: true);
+                options.Middleware.Add(appInsightsLogger);
+            });
+
+            // Create a QNA Maker that is initialized and suitable for passing
+            // into the IBot-derived class (QnABot). 
+            // This custom class also logs results to Application Insights.
+            services.AddSingleton<MyAppInsightsQnaMaker>(sp =>
+            {
+                var knowledgeBaseId = Configuration.GetSection("QnaMaker-KnowledgeBaseId")?.Value;
+                var endpointKey = Configuration.GetSection("QnaMaker-EndpointKey")?.Value;
+                var host = Configuration.GetSection("QnaMaker-Host")?.Value;
+
+                if (string.IsNullOrWhiteSpace(knowledgeBaseId))
+                {
+                    throw new InvalidOperationException("The Qna KnowledgeBaseId ('QnaMaker-KnowledgeBaseId') is required to run this sample.  Please update your appsettings.json for more details.");
+                }
+
+                if (string.IsNullOrWhiteSpace(endpointKey))
+                {
+                    throw new InvalidOperationException("The Qna EndpointKey ('QnaMaker-EndpointKey') is required to run this sample.  Please update your appsettings.json for more details.");
+                }
+
+                if (string.IsNullOrWhiteSpace(host))
+                {
+                    throw new InvalidOperationException("The Qna Host ('QnaMaker-Host') is required to run this sample.  Please update your appsettings.json for more details.");
+                }
+
+                var qnaEndpoint = new QnAMakerEndpoint()
+                {
+                    KnowledgeBaseId = knowledgeBaseId,
+                    EndpointKey = endpointKey,
+                    Host = host
+                };
+
+                var qnaOptions = new QnAMakerOptions()
+                {
+                    ScoreThreshold = 0.3f
+                };
+
+                var myQnaRecognizer = new MyAppInsightsQnaMaker(qnaEndpoint, qnaOptions, logUserName: true, logOriginalMessage: true);
+                return myQnaRecognizer;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}
