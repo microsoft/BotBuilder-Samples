225684ce032073b49daf7f051e0a5011c136e246
Dave Taniguchi <daveta@microsoft.com>
2018-08-31 08:09:35 -0700

    Add BotConfig support - note: Emulator doesn't like AppInsights, hence redundant .bot files!

---
+++ csharp_dotnetcore/11.QnAMaker/QnAMaker/BotServices.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+
+using Microsoft.ApplicationInsights;
+using Microsoft.Bot.Builder.AI.QnA;
+using System;
+using System.Collections.Generic;
+
+
+namespace AspNetCore_QnA_Bot
+{
+    /// <summary>
+    /// Class represents bot configuration.
+    /// </summary>
+    [Serializable]
+    public class BotServices
+    {
+        public BotServices()
+        {
+            QnAServices = new Dictionary<string, QnAMaker>();
+        }
+        public string AuthConnectionName { get; set; }
+
+        public TelemetryClient TelemetryClient { get; set; }
+
+        public Dictionary<string, QnAMaker> QnAServices { get; set; }
+    }
+}
--- csharp_dotnetcore/11.QnAMaker/Startup.cs
+++ csharp_dotnetcore/11.QnAMaker/Startup.cs
@@ -1,16 +1,18 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
+using AspNetCore_QnA_Bot.AppInsights;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Bot.Builder.BotFramework;
-using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using AspNetCore_QnA_Bot.AppInsights;
+using System;
 
 namespace AspNetCore_QnA_Bot
 {
@@ -34,60 +36,95 @@ namespace AspNetCore_QnA_Bot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Load Connected Services from .bot file
+            // BUGBUG: This needs to change to common bot file, once emulator understand appInsights type.
+            var botConfig = BotConfiguration.LoadAsync(@".\QnaBot.bot").Result;
+
+            // Initialize Bot Connected Services Clients
+            var connectedServices = InitBotServices(botConfig);
+            services.AddSingleton(sp => connectedServices);
+
+            services.AddSingleton(sp => botConfig);
+
             services.AddBot<QnABot>(options =>
             {
                 options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
 
                 // Add MyAppInsightsLoggerMiddleware (logs activity messages into Application Insights)
-                var instrumentationKey = Configuration.GetSection("ApplicationInsights")?.GetSection("InstrumentationKey").Value;
-                if (null == instrumentationKey)
-                {
-                    throw new InvalidOperationException("Application Insights instrumentation key must be configured in the appsettings.json file.");
-                }
-                var appInsightsLogger = new MyAppInsightsLoggerMiddleware(instrumentationKey, logUserName: true, logOriginalMessage: true);
+                var appInsightsLogger = new MyAppInsightsLoggerMiddleware(connectedServices.TelemetryClient.InstrumentationKey, logUserName: true, logOriginalMessage: true);
                 options.Middleware.Add(appInsightsLogger);
             });
+            
 
-            // Create a QNA Maker that is initialized and suitable for passing
-            // into the IBot-derived class (QnABot). 
-            // This custom class also logs results to Application Insights.
-            services.AddSingleton<MyAppInsightsQnaMaker>(sp =>
-            {
-                var knowledgeBaseId = Configuration.GetSection("QnaMaker-KnowledgeBaseId")?.Value;
-                var endpointKey = Configuration.GetSection("QnaMaker-EndpointKey")?.Value;
-                var host = Configuration.GetSection("QnaMaker-Host")?.Value;
-
-                if (string.IsNullOrWhiteSpace(knowledgeBaseId))
-                {
-                    throw new InvalidOperationException("The Qna KnowledgeBaseId ('QnaMaker-KnowledgeBaseId') is required to run this sample.  Please update your appsettings.json for more details.");
-                }
-
-                if (string.IsNullOrWhiteSpace(endpointKey))
-                {
-                    throw new InvalidOperationException("The Qna EndpointKey ('QnaMaker-EndpointKey') is required to run this sample.  Please update your appsettings.json for more details.");
-                }
-
-                if (string.IsNullOrWhiteSpace(host))
-                {
-                    throw new InvalidOperationException("The Qna Host ('QnaMaker-Host') is required to run this sample.  Please update your appsettings.json for more details.");
-                }
-
-                var qnaEndpoint = new QnAMakerEndpoint()
-                {
-                    KnowledgeBaseId = knowledgeBaseId,
-                    EndpointKey = endpointKey,
-                    Host = host
-                };
-
-                var qnaOptions = new QnAMakerOptions()
-                {
-                    ScoreThreshold = 0.3f
-                };
-
-                var myQnaRecognizer = new MyAppInsightsQnaMaker(qnaEndpoint, qnaOptions, logUserName: true, logOriginalMessage: true);
-                return myQnaRecognizer;
-            });
         }
+
+        // Configure Bot Services
+        private static BotServices InitBotServices(BotConfiguration config)
+        {
+            var connectedServices = new BotServices();
+
+            foreach (var service in config.Services)
+            {
+                switch (service.Type)
+                {
+                    case ServiceTypes.QnA:
+                        {
+                            // Create a QNA Maker that is initialized and suitable for passing
+                            // into the IBot-derived class (QnABot). 
+                            // This custom class also logs results to Application Insights.
+                            var qna = service as QnAMakerService;
+                            if (string.IsNullOrWhiteSpace(qna.KbId))
+                            {
+                                throw new InvalidOperationException("The Qna KnowledgeBaseId ('kbId') is required to run this sample.  Please update your appsettings.json for more details.");
+                            }
+
+                            if (string.IsNullOrWhiteSpace(qna.EndpointKey))
+                            {
+                                throw new InvalidOperationException("The Qna EndpointKey ('endpointKey') is required to run this sample.  Please update your QnaBot.bot file.");
+                            }
+
+                            if (string.IsNullOrWhiteSpace(qna.Hostname))
+                            {
+                                throw new InvalidOperationException("The Qna Host ('hostname') is required to run this sample.  Please update your QnaBot.bot file.");
+                            }
+
+                            
+                            var qnaEndpoint = new QnAMakerEndpoint()
+                            {
+                                KnowledgeBaseId = qna.KbId,
+                                EndpointKey = qna.EndpointKey,
+                                Host = qna.Hostname,
+                            };
+                            var qnaOptions = new QnAMakerOptions()
+                            {
+                                ScoreThreshold = 0.3f
+                            };
+
+                            var qnaMaker = new MyAppInsightsQnaMaker(qnaEndpoint, qnaOptions, logUserName: false, logOriginalMessage: false);
+                            connectedServices.QnAServices.Add(qna.Name, qnaMaker);
+
+                            break;
+                        }
+
+                    case ServiceTypes.AppInsights:
+                        {
+                            var appInsights = service as AppInsightsService;
+                            if (string.IsNullOrWhiteSpace(appInsights.InstrumentationKey))
+                            {
+                                throw new InvalidOperationException("The Application Insights Instrumentation Key ('instrumentationKey') is required to run this sample.  Please update your QnaBot.bot file.");
+                            }
+
+                            var telemetryConfig = new TelemetryConfiguration(appInsights.InstrumentationKey);
+                            connectedServices.TelemetryClient = new TelemetryClient(telemetryConfig);
+                            connectedServices.TelemetryClient.InstrumentationKey = appInsights.InstrumentationKey;
+                            break;
+                        }
+                }
+            }
+            return connectedServices;
+        }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
