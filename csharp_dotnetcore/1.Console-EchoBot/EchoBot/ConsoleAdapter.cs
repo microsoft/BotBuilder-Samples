e0a052951b88bf3415f24399ce259592cf32588b
Dave Taniguchi <daveta@microsoft.com>
2018-08-31 12:47:42 -0700

    Initial Consolebot with Stylecop

---
+++ csharp_dotnetcore/1.Console-EchoBot/EchoBot/ConsoleAdapter.cs
@@ -0,0 +1,190 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Console_EchoBot
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Schema;
+
+    /// <summary>
+    /// Represents a bot adapter that would typically connect a bot to an
+    /// and external service.
+    /// This implementation interacts with the console.
+    /// </summary>
+    /// <seealso cref="ITurnContext"/>
+    /// <seealso cref="IActivity"/>
+    /// <seealso cref="IBot"/>
+    /// <seealso cref="IMiddleware"/>
+    public class ConsoleAdapter : BotAdapter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleAdapter"/> class.
+        /// </summary>
+        public ConsoleAdapter()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Adds middleware to the adapter's pipeline.
+        /// </summary>
+        /// <param name="middleware">The middleware component to add.</param>
+        /// <returns>The updated adapter object.</returns>
+        public new ConsoleAdapter Use(IMiddleware middleware)
+        {
+            base.Use(middleware);
+            return this;
+        }
+
+        /// <summary>
+        /// Performs the actual translation of input coming from the console
+        /// into the Activity format that the Bot consumes.
+        /// </summary>
+        /// <param name="callback">A callback method to run at the end of the pipeline.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public async Task ProcessActivityAsync(BotCallbackHandler callback = null)
+        {
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                if (msg == null)
+                {
+                    break;
+                }
+
+                var activity = new Activity()
+                {
+                    Text = msg,
+                    ChannelId = "console",
+                    From = new ChannelAccount(id: "user", name: "User1"),
+                    Recipient = new ChannelAccount(id: "bot", name: "Bot"),
+                    Conversation = new ConversationAccount(id: "Convo1"),
+                    Timestamp = DateTime.UtcNow,
+                    Id = Guid.NewGuid().ToString(),
+                    Type = ActivityTypes.Message,
+                };
+
+                using (var context = new TurnContext(this, activity))
+                {
+                    await this.RunPipelineAsync(context, callback, default(CancellationToken));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sends activities to the conversation.
+        /// </summary>
+        /// <param name="context">The context object for the turn.</param>
+        /// <param name="activities">The activities to send.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        /// <remarks>If the activities are successfully sent, the task result contains
+        /// an array of <see cref="ResourceResponse"/> objects containing the IDs that
+        /// the receiving channel assigned to the activities.</remarks>
+        /// <seealso cref="ITurnContext.OnSendActivities(SendActivitiesHandler)"/>
+        public override async Task<ResourceResponse[]> SendActivitiesAsync(ITurnContext context, Activity[] activities, CancellationToken cancellationToken)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (activities == null)
+            {
+                throw new ArgumentNullException(nameof(activities));
+            }
+
+            if (activities.Length == 0)
+            {
+                throw new ArgumentException("Expecting one or more activities, but the array was empty.", nameof(activities));
+            }
+
+            var responses = new ResourceResponse[activities.Length];
+
+            for (var index = 0; index < activities.Length; index++)
+            {
+                var activity = activities[index];
+
+                switch (activity.Type)
+                {
+                    case ActivityTypes.Message:
+                        {
+                            IMessageActivity message = activity.AsMessageActivity();
+                            if (message.Attachments != null && message.Attachments.Any())
+                            {
+                                var attachment = message.Attachments.Count == 1 ? "1 attachment" : $"{message.Attachments.Count()} attachments";
+                                Console.WriteLine($"{message.Text} with {attachment} ");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{message.Text}");
+                            }
+                        }
+
+                        break;
+
+                    case ActivityTypesEx.Delay:
+                        {
+                            // The Activity Schema doesn't have a delay type build in, so it's simulated
+                            // here in the Bot. This matches the behavior in the Node connector.
+                            int delayMs = (int)((Activity)activity).Value;
+                            await Task.Delay(delayMs).ConfigureAwait(false);
+                        }
+
+                        break;
+
+                    case ActivityTypes.Trace:
+                        // don't send trace activities unless you know that the client needs them.  For example: BF protocol only sends Trace Activity when talking to emulator channel
+                        break;
+
+                    default:
+                        Console.WriteLine("Bot: activity type: {0}", activity.Type);
+                        break;
+                }
+
+                responses[index] = new ResourceResponse(activity.Id);
+            }
+
+            return responses;
+        }
+
+        /// <summary>
+        /// Normally, replaces an existing activity in the conversation.
+        /// Not implemented for this sample.
+        /// </summary>
+        /// <param name="turnContext">The context object for the turn.</param>
+        /// <param name="activity">New replacement activity.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        /// <remarks>If the activity is successfully sent, the task result contains
+        /// a <see cref="ResourceResponse"/> object containing the ID that the receiving
+        /// channel assigned to the activity.
+        /// <para>Before calling this, set the ID of the replacement activity to the ID
+        /// of the activity to replace.</para></remarks>
+        /// <seealso cref="ITurnContext.OnUpdateActivityAsync(UpdateActivityHandler)"/>
+        public override Task<ResourceResponse> UpdateActivityAsync(ITurnContext turnContext, Activity activity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes an existing activity in the conversation.
+        /// Not implemented for this sample.
+        /// </summary>
+        /// <param name="turnContext">The context object for the turn.</param>
+        /// <param name="reference">Conversation reference for the activity to delete.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        public override Task DeleteActivityAsync(ITurnContext turnContext, ConversationReference reference, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp_dotnetcore/1.Console-EchoBot/EchoBot/EchoBot.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Console_EchoBot
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Schema;
+
+    /// <summary>
+    /// Represents a bot that can operate on incoming activities.
+    /// </summary>
+    /// <remarks>A <see cref="BotAdapter"/> passes incoming activities from the user's
+    /// channel to the bot's <see cref="OnTurnAsync(ITurnContext, CancellationToken)"/> method.</remarks>
+    /// <seealso cref="IMiddleware"/>
+    public class EchoBot : IBot
+    {
+        /// <summary>
+        /// Every Conversation turn for our EchoBot will call this method. In here
+        /// the bot checks the Activty type to verify it's a message, and then echoes the users typing
+        /// back to them.
+        /// </summary>
+        /// <param name="context">Turn scoped context containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <returns>A <see cref="Task"/> representing the operation result of the Turn operation.</returns>
+        public async Task OnTurnAsync(ITurnContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // This bot is only handling Messages
+            if (context.Activity.Type == ActivityTypes.Message)
+            {
+                // Echo back to the user whatever they typed.
+                await context.SendActivityAsync($"You sent '{context.Activity.Text}'");
+            }
+        }
+    }
+}
