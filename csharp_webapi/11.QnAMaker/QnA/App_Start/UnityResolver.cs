d5c27a00f854fe0be4b6243ed8230bd377c31a35
Dave Taniguchi <daveta@microsoft.com>
2018-08-31 16:14:51 -0700

    QnaMaker on ASP.Net WebAPI w/StyleCop

---
+++ csharp_webapi/11.QnAMaker/QnA/App_Start/UnityResolver.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Unity;
+using Unity.Exceptions;
+
+namespace AspNetWebApi_QnA_Bot
+{
+    public class UnityResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return _container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return _container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = _container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+    }
+}
