8bdb9059dd9d24bd2f9c8cd40d16045ba1c030d5
Ben Brown <ben@xoxco.com>
2018-08-31 10:08:13 -0500

    minor grammar tweaks

--- javascript_ts/2.echobot-with-counter/src/index.ts
+++ javascript_ts/2.echobot-with-counter/src/index.ts
@@ -12,11 +12,11 @@ import { MainDialog } from './dialogs/mainDialog';
 // See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration .
 const BOT_CONFIGURATION = 'echobot-with-counter';
 
-// read botFilePath and botFileSecret from .env file
+// Read botFilePath and botFileSecret from .env file.
 const ENV_FILE = path.join(__dirname, '..', '.env');
 const loadFromEnv = config({path: ENV_FILE});
 
-// Create HTTP server
+// Create HTTP server.
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(`\n${server.name} listening to ${server.url}`);
@@ -27,9 +27,9 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // .bot file path
 const BOT_FILE = path.join(__dirname, '..', process.env.botFilePath);
 
-// read bot configuration from .bot file.
+// Read bot configuration from .bot file.
 const botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
-// Get bot endpoint configuration by service name
+// Get bot endpoint configuration by service name.
 const endpointConfig = <IEndpointService>botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
 
 // Create adapter. See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration .
@@ -38,30 +38,30 @@ const adapter = new BotFrameworkAdapter({
     appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
 });
 
-// Define state store for your bot. See https://aka.ms/about-bot-state to learn more about bots memory service
-// A bot requires a sate store to priciest it dialog and user state between messages
+// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state store to persist the dialog and user state between messages.
 const memoryStorage = new MemoryStorage();
 // CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
 // is restarted, anything stored in memory will be gone. 
-// For production bots use the Azure CosmosDB storage, Azure Blob, or Azure Table storage provides. 
+// For production bots use the Azure CosmosDB storage, Azure Blob storage providers. 
 // const { CosmosDbStorage } = require('botbuilder-azure');
-// const STORAGE_CONFIGURATION = 'cosmosDB'; // this is the name of the cosmos DB configuration in your .bot file
+// const STORAGE_CONFIGURATION = 'cosmosDB'; // this is the name of the CosmosDB configuration in your .bot file
 // const cosmosConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION);
 // const cosmosStorage = new CosmosDbStorage({serviceEndpoint: cosmosConfig.connectionString, 
 //                                            authKey: ?, 
 //                                            databaseId: cosmosConfig.database, 
 //                                            collectionId: cosmosConfig.collection});
 
-// create conversation state with in-memory storage provider. 
+// cCeate conversation state with in-memory storage provider. 
 const conversationState = new ConversationState(memoryStorage);
 
-// register conversation state as a middleware. The ConversationState middleware automatically reads and writes conversation sate 
+// Register conversation state as a middleware. The ConversationState middleware automatically reads and writes conversation sate 
 adapter.use(conversationState);
 
 // Create the main dialog.
 const mainDlg = new MainDialog(conversationState);
 
-// Listen for incoming requests 
+// Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         // route to main dialog.
