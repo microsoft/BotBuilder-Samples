25f5e2cb9b373243dc0ec8b1ce0972b421b96802
Jason Sowers <v-jassow@microsoft.com>
2018-08-31 14:46:26 -0700

    adding cards sample

---
+++ csharp_dotnetcore/Using-Cards/Using_Cards/CardsBot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using Using_Cards.Dialogs;
+
+namespace Using_Cards
+{
+    public class CardsBot : IBot
+    {
+        private readonly CardsBotAccessors _accessors;
+        public DialogSet Dialogs;
+
+        public CardsBot(CardsBotAccessors accessors)
+        {
+            _accessors = accessors;
+
+            Dialogs = new DialogSet(accessors.ConversationDialogState);
+            Dialogs.Add(new MainDialog(MainDialog.DialogId));
+            Dialogs.Add(new ChoicePrompt("cardPrompt"));
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+        {
+            switch (turnContext.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    var dc = await Dialogs.CreateContextAsync(turnContext, cancellationToken);
+                    await dc.ContinueAsync(cancellationToken);
+
+                    if (!dc.Context.Responded)
+                    {
+                        await dc.BeginAsync(MainDialog.DialogId, cancellationToken: cancellationToken);
+                    }
+
+                    break;
+                case ActivityTypes.ConversationUpdate:
+
+                    // Send a welcome message to the user and tell them what actions they need to perform to use this bot
+                    if (turnContext.Activity.MembersAdded.Any())
+                    {
+                        {
+                            foreach (var member in turnContext.Activity.MembersAdded)
+                            {
+                                var newUserName = member.Name;
+                                if (member.Id != turnContext.Activity.Recipient.Id)
+                                {
+                                    await turnContext.SendActivityAsync($"Welcome to CardBot {newUserName}. This bot will show you different types of Rich Cards.  Please type anything to get started.", cancellationToken: cancellationToken);
+                                }
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    // There is no code in this bot to deal with ActivityTypes other than conversationUpdate or message
+                    await turnContext.SendActivityAsync("This type of activity is not handled in this bot", cancellationToken: cancellationToken);
+                    break;
+            }
+        }
+    }
+}
---
+++ csharp_dotnetcore/Using-Cards/Using_Cards/Startup.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Integration;
+using Microsoft.Extensions.Options;
+
+namespace Using_Cards
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            try
+            {
+                services.AddBot<CardsBot>(options =>
+                {
+                    options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+
+                    // The CatchExceptionMiddleware provides a top-level exception handler for your bot. 
+                    // Any exceptions thrown by other Middleware, or by your OnTurn method, will be 
+                    // caught here. To facillitate debugging, the exception is sent out, via Trace, 
+                    // to the emulator. Trace activities are NOT displayed to users, so in addition
+                    // an "Ooops" message is sent. 
+
+
+                    // The Memory Storage used here is for local bot debugging only. When the bot
+                    // is restarted, anything stored in memory will be gone. 
+
+                    IStorage dataStore = new MemoryStorage();
+                    var convoState = new ConversationState(dataStore);
+                    var userState = new UserState(dataStore);
+                    // The File data store, shown here, is suitable for bots that run on 
+                    // a single machine and need durable state across application restarts.                 
+                    // IStorage dataStore = new FileStorage(System.IO.Path.GetTempPath());
+
+                    // For production bots use the Azure Table Store, Azure Blob, or 
+                    // Azure CosmosDB storage provides, as seen below. To include any of 
+                    // the Azure based storage providers, add the Microsoft.Bot.Builder.Azure 
+                    // Nuget package to your solution. That package is found at:
+                    //      https://www.nuget.org/packages/Microsoft.Bot.Builder.Azure/
+
+                    // IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureTableStorage("AzureTablesConnectionString", "TableName");
+                    // IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureBlobStorage("AzureBlobConnectionString", "containerName");
+
+                    options.State.Add(convoState);
+                    options.State.Add(userState);
+
+                    // Add State to BotStateSet Middleware (that require auto-save)
+                    // The BotStateSet Middleware forces state storage to auto-save when the Bot is complete processing the message.
+                    // Note: Developers may choose not to add all the State providers to this Middleware if save is not required.
+                    var stateSet = new BotStateSet(options.State.ToArray());
+                    options.Middleware.Add(stateSet);
+                });
+                services.AddSingleton<CardsBotAccessors>(sp =>
+                {
+                    var options = sp.GetRequiredService<IOptions<BotFrameworkOptions>>().Value;
+                    if (options == null)
+                    {
+                        throw new InvalidOperationException("BotFrameworkOptions must be configured prior to setting up the State Accessors");
+                    }
+
+                    var conversationState = options.State.OfType<ConversationState>().FirstOrDefault();
+                    var userState = options.State.OfType<UserState>().FirstOrDefault();
+                    if (conversationState == null)
+                    {
+                        throw new InvalidOperationException("ConversationState must be defined and added before adding conversation-scoped state accessors.");
+                    }
+
+                    // Create Custom State Property Accessors
+                    // State Property Accessors enable components to read and write individual properties, without having to 
+                    // pass the entire State object.
+                    var accessors = new CardsBotAccessors
+                    {
+                        CommandState = userState.CreateProperty<string>(CardsBotAccessors.CommandStateName),
+                        ConversationDialogState = conversationState.CreateProperty<DialogState>(CardsBotAccessors.DialogStateName),
+                    };
+
+                    return accessors;
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}
