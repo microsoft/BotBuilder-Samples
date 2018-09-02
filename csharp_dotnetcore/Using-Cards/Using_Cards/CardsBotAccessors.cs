d02c157a0813dd6c41bb31a31216461a247dbe1d
Jason Sowers <v-jassow@microsoft.com>
2018-09-02 15:14:06 -0700

    please review

--- csharp_dotnetcore/Using-Cards/Using_Cards/CardsBotAccessors.cs
+++ csharp_dotnetcore/Using-Cards/Using_Cards/CardsBotAccessors.cs
@@ -1,14 +1,24 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 
 namespace Using_Cards
 {
+    /// <summary>
+    /// This is a helper class to support the state accessors for the bot.
+    /// </summary>
     public class CardsBotAccessors
     {
-        internal static string DialogStateName = $"{nameof(CardsBotAccessors)}.DialogState";
+        /// <summary>
+        /// The name of the dialog state.
+        /// </summary>
+        public static readonly string DialogStateName = $"{nameof(CardsBotAccessors)}.DialogState";
 
+        /// <summary>
+        /// Gets or Sets the DialogState accessor value.
+        /// </summary>
         public IStatePropertyAccessor<DialogState> ConversationDialogState { get; set; }
     }
 }
--- csharp_dotnetcore/Using-Cards/Using_Cards/Startup.cs
+++ csharp_dotnetcore/Using-Cards/Using_Cards/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.BotFramework;
