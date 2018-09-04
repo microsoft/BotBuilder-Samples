0a51f4f57a833c29f5242993e52e0c3b0a7bb357
Lauren Mills <lamil@microsoft.com>
2018-09-04 11:22:25 -0700

    added enterprise bot project

---
+++ CSharp/Enterprise-Bot-Template/BotServices.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace EnterpriseBot
+{
+    /// <summary>
+    /// Class represents bot configuration.
+    /// </summary>
+    [Serializable]
+    public class BotServices
+    {
+        public BotServices()
+        {
+            LuisServices = new Dictionary<string, LuisRecognizer>();
+            QnAServices = new Dictionary<string, QnAMaker>();
+        }
+
+        public string AuthConnectionName { get; set; }
+
+        public TelemetryClient TelemetryClient { get; set; }
+
+        public LuisRecognizer DispatchRecognizer { get; set; }
+
+        public Dictionary<string, LuisRecognizer> LuisServices { get; set; }
+
+        public Dictionary<string, QnAMaker> QnAServices { get; set; }
+    }
+}
---
+++ CSharp/Enterprise-Bot-Template/Dialogs/Cancel/CancelDialog.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace EnterpriseBot
+{
+    public class CancelDialog : ComponentDialog
+    {
+        // Constants
+        public const string Name = "cancel";
+        public const string CancelPrompt = "cancelPrompt";
+
+        // Fields
+        private static CancelResponses _responder = new CancelResponses();
+
+        public CancelDialog()
+            : base(Name)
+        {
+            var cancel = new WaterfallStep[]
+            {
+                AskToCancel,
+                FinishCancelDialog,
+            };
+
+            AddDialog(new WaterfallDialog(Name, cancel));
+            AddDialog(new ConfirmPrompt(CancelPrompt));
+        }
+
+        public static async Task<DialogTurnResult> AskToCancel(DialogContext dc, WaterfallStepContext args, CancellationToken cancellationToken)
+        {
+            return await dc.PromptAsync(CancelPrompt, new PromptOptions()
+            {
+                Prompt = await _responder.RenderTemplate(dc.Context, "en", CancelResponses._confirmPrompt),
+            });
+        }
+
+        public static async Task<DialogTurnResult> FinishCancelDialog(DialogContext dc, WaterfallStepContext args, CancellationToken cancellationToken)
+        {
+            return await dc.EndAsync((bool)args.Result);
+        }
+
+        protected override async Task<DialogTurnResult> EndComponentAsync(DialogContext outerDc, object result, CancellationToken cancellationToken)
+        {
+            bool doCancel = (bool)result;
+
+            if (doCancel)
+            {
+                // If user chose to cancel
+                await _responder.ReplyWith(outerDc.Context, CancelResponses._cancelConfirmed);
+
+                // Cancel all in outer stack of component i.e. the stack the component belongs to
+                return await outerDc.CancelAllAsync();
+            }
+            else
+            {
+                // else if user chose not to cancel
+                await _responder.ReplyWith(outerDc.Context, CancelResponses._cancelDenied);
+
+                // End this component. Will trigger reprompt/resume on outer stack
+                return await outerDc.EndAsync();
+            }
+        }
+    }
+}
