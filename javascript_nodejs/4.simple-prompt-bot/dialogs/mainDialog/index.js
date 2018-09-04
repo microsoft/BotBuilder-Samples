b60ee4f360581cf4c0a4ff6894b5c826da1fcfaf
Ben Brown <ben@xoxco.com>
2018-09-04 16:48:51 -0500

    update to latest syntax with waterfalldialogs

--- javascript_nodejs/4.simple-prompt-bot/dialogs/mainDialog/index.js
+++ javascript_nodejs/4.simple-prompt-bot/dialogs/mainDialog/index.js
@@ -3,11 +3,15 @@
 
 const { ActionTypes, MessageFactory } = require('botbuilder');
 
-const { TextPrompt, DialogSet } = require('botbuilder-dialogs');
+const { TextPrompt, DialogSet, WaterfallDialog } = require('botbuilder-dialogs');
 
+const DIALOG_STATE_PROPERTY = 'dialogState';
 const USER_NAME_PROP = 'user_name';
 const WHO_ARE_YOU = 'who_are_you';
 const HELLO_USER = 'hello_user';
+
+const NAME_PROMPT = 'name_prompt';
+
 
 class MainDialog {
     /**
@@ -16,40 +20,41 @@ class MainDialog {
      * @param {Object} userState 
      */
     constructor (conversationState, userState) {
-        var that = this;
 
         // creates a new state accessor property. see https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors 
         this.conversationState = conversationState;
         this.userState = userState;
 
+        this.dialogState = this.conversationState.createProperty(DIALOG_STATE_PROPERTY);
+
         this.userName = this.userState.createProperty(USER_NAME_PROP)
 
-        this.dialogs = new DialogSet();
+        this.dialogs = new DialogSet(this.dialogState);
         
         // Add prompts
-        this.dialogs.add('textPrompt', new TextPrompt());
+        this.dialogs.add(new TextPrompt(NAME_PROMPT));
             
         // Create a dialog that asks the user for their name.
-        this.dialogs.add(WHO_ARE_YOU, [
-            async function(dc) {
-                return await dc.prompt('textPrompt', `What is your name, human?`);
+        this.dialogs.add(new WaterfallDialog(WHO_ARE_YOU, [
+            async (dc, step) => {
+                return await dc.prompt(NAME_PROMPT, `What is your name, human?`);
             },
-            async function(dc, value) {
-                await that.userName.set(dc.context, value);
-                await dc.context.sendActivity(`Got it. You are ${value}`);
+            async (dc, step) => {
+                await this.userName.set(dc.context, step.result);
+                await dc.context.sendActivity(`Got it. You are ${ step.result }`);
                 return await dc.end();
             }
-        ]);
+        ]));
 
 
         // Create a dialog that displays a user name after it has been collceted.
-        this.dialogs.add(HELLO_USER, [
-            async function(dc) {
-                const user_name = await that.userName.get(dc.context, null);
+        this.dialogs.add(new WaterfallDialog(HELLO_USER, [
+            async (dc, step) => {
+                const user_name = await this.userName.get(dc.context, null);
                 await dc.context.sendActivity(`Your name is ${user_name}.`);
                 return await dc.end();
             }
-        ]);
+        ]));
     }
 
 
@@ -61,8 +66,7 @@ class MainDialog {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (context.activity.type === 'message') {
             // Create dialog context
-            const state = this.conversationState.get(context);
-            const dc = this.dialogs.createContext(context, state);
+            const dc = await this.dialogs.createContext(context);
 
             const utterance = (context.activity.text || '').trim().toLowerCase();
             if (utterance === 'cancel') { 
@@ -70,7 +74,9 @@ class MainDialog {
             }
             
             // Continue the current dialog
-            await dc.continue();
+            if (!context.responded) {
+                await dc.continue();
+            }
 
             // Show menu if no response sent
             if (!context.responded) {
