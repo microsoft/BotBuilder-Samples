dfeaa03e82b50427a7543a07471470e079332270
Ben Brown <ben@xoxco.com>
2018-09-05 10:36:38 -0500

    initial commit

---
+++ javascript_nodejs/17.proactive-messages/dialogs/mainDialog/index.js
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { DialogSet } = require('botbuilder-dialogs');
+
+const CreateReminderDialog = require('../createReminder');
+
+const DIALOG_STATE_PROPERTY = 'dialogState';
+
+const CREATE_REMINDER = 'createreminder';
+const INSTRUCTIONS = 'instructions';
+
+class MainDialog {
+    /**
+     * 
+     * @param {ConversationState} conversationState A ConversationState object used to store dialog state.
+     * @param {UserState} userState A UserState object used to store user profile information.
+     */
+    constructor (conversationState, userState) {
+
+        // Creates a new state accessor property. 
+        // See https://aka.ms/about-bot-state-accessors to learn more about bot state and state accessors.
+        this.conversationState = conversationState;
+        this.userState = userState;
+        
+        // Create a property used to store dialog state.
+        this.dialogState = this.conversationState.createProperty(DIALOG_STATE_PROPERTY);
+
+        // Create a dialog set to include the dialogs used by this bot.
+        this.dialogs = new DialogSet(this.dialogState);
+
+        this.dialogs.add(new CreateReminderDialog(CREATE_REMINDER));
+        
+    }
+
+
+    /**
+     * 
+     * @param {TurnContext} context A TurnContext object representing an incoming message to be handled by the bot.
+     */
+    async onTurn(context) {
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        if (context.activity.type === 'message') {
+
+            // Create dialog context
+            const dc = await this.dialogs.createContext(context);
+
+            const utterance = (context.activity.text || '').trim().toLowerCase();
+            if (utterance === 'cancel') { 
+                if (dc.activeDialog) {
+                    await dc.cancelAll();
+                    await dc.context.sendActivity(`Ok... Cancelled.`);
+                } else {
+                    await dc.context.sendActivity(`Nothing to cancel.`);
+                }
+            }
+            
+            // Continue the current dialog if one is pending.
+            if (!context.responded) {
+                await dc.continue();
+            }
+
+            // If no response has been sent, start the onboarding dialog.
+            if (!context.responded) {
+                if (utterance.toLowerCase().includes('remind')) {
+                    await dc.begin(CREATE_REMINDER);
+                } else {
+                    await dc.begin(INSTRUCTIONS);
+                }
+            }
+        } else if (context.activity.type == 'conversationUpdate' && context.activity.membersAdded[0].id === 'default-user') {
+            // send a "this is what the bot does" message
+            const description = [
+                'I am a bot that demonstrates the TextPrompt class to collect your name,',
+                'store it in UserState, and display it.',
+                'Say anything to continue.'
+            ];
+            await context.sendActivity(description.join(' '));
+        }
+    }
+}
+
+module.exports = MainDialog;
---
+++ javascript_nodejs/17.proactive-messages/index.js
@@ -0,0 +1,85 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const restify = require('restify');
+const path = require('path');
+const ERROR = 1;
+
+// Import required bot services. See https://aka.ms/bot-services to learn more about the different part of a bot.
+const { BotFrameworkAdapter, BotStateSet,  MemoryStorage, ConversationState, UserState } = require('botbuilder');
+const { BotConfiguration } = require('botframework-config');
+
+const MainDialog = require('./dialogs/mainDialog');
+
+// Read botFilePath and botFileSecret from .env file.
+// Note: Ensure you have a .env file and include botFilePath and botFileSecret.
+const ENV_FILE = path.join(__dirname, '.env');
+const env = require('dotenv').config({path: ENV_FILE});
+
+// Create HTTP server.
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open prompt-validations-bot.bot file in the Emulator`);
+});
+
+// .bot file path
+const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
+
+// Read the bot's configuration from a .bot file identified by BOT_FILE.
+// This includes information about the bot's endpoints and Bot Framework configuration.
+let botConfig;
+try {
+    botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
+} catch (err) {
+    console.log(`Error reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment`);
+    console.log(err);
+    process.exit(ERROR);
+}
+
+// Define the name of the bot, as specified in .bot file.
+// See https://aka.ms/about-bot-file to learn more about .bot file.
+const BOT_CONFIGURATION = 'prompt-validations-bot';
+
+// Load the configuration profile specific to this bot identity.
+const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
+
+// Create the adapter. See https://aka.ms/about-bot-adapter to learn more about using information from
+// the .bot file when configuring your adapter.
+const adapter = new BotFrameworkAdapter({
+    appId: endpointConfig.appId || process.env.microsoftAppID,
+    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+});
+
+// Define the state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state storage system to persist the dialog and user state between messages.
+const memoryStorage = new MemoryStorage();
+// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
+// is restarted, anything stored in memory will be gone. 
+// For production bots use the Azure Cosmos DB storage, or Azure Blob storage providers. 
+// const { CosmosDbStorage } = require('botbuilder-azure');
+// const STORAGE_CONFIGURATION = 'cosmosDB'; // Cosmos DB configuration in your .bot file
+// const cosmosConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION);
+// const cosmosStorage = new CosmosDbStorage({serviceEndpoint: cosmosConfig.connectionString, 
+//                                            authKey: ?, 
+//                                            databaseId: cosmosConfig.database, 
+//                                            collectionId: cosmosConfig.collection});
+
+// Create conversation state with in-memory storage provider. 
+const conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage)
+
+// Use the BotStateSet middleware to automatically read and write conversation and user state.
+adapter.use(new BotStateSet(conversationState, userState));
+
+// Create the main dialog, which serves as the bot's main handler.
+const mainDlg = new MainDialog(conversationState, userState);
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route the message to the bot's main handler.
+        await mainDlg.onTurn(context);        
+    });
+});
