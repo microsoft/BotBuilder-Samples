2dd9725aaa5ed2d954417d297aac5ff48e88aef8
John Taylor <johtaylo@microsoft.com>
2018-09-04 17:37:10 -0700

    multi turn prompt basic

---
+++ csharp_dotnetcore/5.MultiTurn-Prompts-Bot/BotAccessors.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace MultiTurn_Prompts_Bot
+{
+    public class BotAccessors
+    {
+        public IStatePropertyAccessor<DialogState> ConversationDialogState { get; set; }
+    }
+}
--- csharp_dotnetcore/5.MultiTurn-Prompts-Bot/MultiTurnPromptsBot.cs
+++ csharp_dotnetcore/5.MultiTurn-Prompts-Bot/MultiTurnPromptsBot.cs
@@ -5,14 +5,26 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
 
 namespace MultiTurn_Prompts_Bot
 {
     /// <summary>
-    /// This bot illustrates a simple prompt.
+    /// This bot illustrates a multi-turn scenario. Following a prompt for name it prompts for age.
     /// </summary>
     public class MultiTurnPromptsBot : IBot
     {
+        private DialogSet _dialogs;
+
+        public MultiTurnPromptsBot(BotAccessors accessors)
+        {
+            _dialogs = new DialogSet(accessors.ConversationDialogState);
+            _dialogs.Add(new WaterfallDialog("details", new WaterfallStep[] { NameStep, AgeStep, FinalStep }));
+            _dialogs.Add(new TextPrompt("name"));
+            _dialogs.Add(new NumberPrompt<int>("age"));
+        }
+
         /// <summary>
         /// This controls what happens when an activity gets sent to the bot.
         /// </summary>
@@ -27,9 +39,38 @@ namespace MultiTurn_Prompts_Bot
                 throw new ArgumentNullException(nameof(turnContext));
             }
 
-            // TODO: implementation
+            // We are only interested in Message Activities.
+            if (turnContext.Activity.Type != ActivityTypes.Message)
+            {
+                return;
+            }
 
-            await Task.CompletedTask;
+            // Run the DialogSet - let the framework identify the current state of the dialog from 
+            // the dialog stack and figure out what (if any) is the active dialog.
+            var dialogContext = await _dialogs.CreateContextAsync(turnContext, cancellationToken);
+            var results = await dialogContext.ContinueAsync(cancellationToken);
+
+            // If the DialogTurnStatus is Empty we should start a new dialog.
+            if (results.Status == DialogTurnStatus.Empty)
+            {
+                await dialogContext.BeginAsync("details");
+            }
+        }
+
+        private static async Task<DialogTurnResult> NameStep(DialogContext dc, WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await dc.PromptAsync("name", new PromptOptions { Prompt = MessageFactory.Text("Please enter your name.") }, cancellationToken);
+        }
+        private static async Task<DialogTurnResult> AgeStep(DialogContext dc, WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await dc.Context.SendActivityAsync(MessageFactory.Text($"Thanks {stepContext.Result}."));
+            return await dc.PromptAsync("age", new PromptOptions { Prompt = MessageFactory.Text("Please enter your age") }, cancellationToken);
+        }
+
+        private static async Task<DialogTurnResult> FinalStep(DialogContext dc, WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await dc.Context.SendActivityAsync(MessageFactory.Text($"I have your age as {stepContext.Result}."));
+            return await dc.EndAsync();
         }
     }
 }
