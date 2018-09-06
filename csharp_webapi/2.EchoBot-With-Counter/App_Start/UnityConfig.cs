8198dbb49665771b2d027347d8a4666efb62efa8
Dave Taniguchi <daveta@microsoft.com>
2018-09-06 16:30:58 -0700

    Initial commit

---
+++ csharp_webapi/2.EchoBot-With-Counter/App_Start/UnityConfig.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Web.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Configuration;
+using Unity;
+using Unity.Lifetime;
+
+namespace EchoBotWithCounter
+{
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/dependency-injection"/>
+    public static class UnityConfig
+    {
+        private static Lazy<IUnityContainer> container =
+          new Lazy<IUnityContainer>(() =>
+          {
+              var container = new UnityContainer();
+              RegisterTypes(container);
+              return container;
+          });
+
+        /// <summary>
+        /// Gets configured Unity Container.
+        /// </summary>
+        /// <value>
+        /// The <see cref="IUnityContainer"/> container.
+        /// </value>
+        public static IUnityContainer Container => container.Value;
+
+        /// <summary>
+        /// Registers the type mappings with the Unity container.
+        /// </summary>
+        /// <param name="container">The <see cref="IUnityContainer"/> container to configure.</param>
+        /// <remarks>
+        /// Registers the <see cref="IBot"/> and services <see cref="BotConfiguration"/> (based on the .bot file).
+        /// </remarks>
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.RegisterType<IBot, EchoWithCounterBot>();
+        }
+    }
+}
