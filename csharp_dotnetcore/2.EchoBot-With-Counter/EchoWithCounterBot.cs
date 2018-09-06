d30f0ec2292eb9438e1d8fc1ee701ee01683c412
Dave Taniguchi <daveta@microsoft.com>
2018-09-05 19:10:05 -0700

    Incorporate feedback from cmullins

--- csharp_dotnetcore/2.EchoBot-With-Counter/EchoWithCounterBot.cs
+++ csharp_dotnetcore/2.EchoBot-With-Counter/EchoWithCounterBot.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 
-namespace EchoBotWithCounter
+namespace Microsoft.BotBuilderSamples
 {
     /// <summary>
-    /// Represents a bot that can process incoming activities.
-    /// For each interaction from the user, an instance of this class is called.
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
     /// This is a Transient lifetime service.  Transient lifetime services are created
     /// each time they're requested. For each Activity received, a new instance of this
     /// class is created. Objects that are expensive to construct, or have a lifetime
-    /// beyond the single Turn, should be carefully managed.
+    /// beyond the single turn, should be carefully managed.
     /// For example, the <see cref="MemoryStorage"/> object and associated
-    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a Singleton lifetime.
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
     /// </summary>
     /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
     public class EchoWithCounterBot : IBot
@@ -26,23 +26,22 @@ namespace EchoBotWithCounter
         /// <summary>
         /// Initializes a new instance of the <see cref="EchoWithCounterBot"/> class.
         /// </summary>
-        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> that the bot uses
-        /// to set state.</param>
+        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
         public EchoWithCounterBot(EchoBotAccessors accessors)
         {
             _accessors = accessors ?? throw new System.ArgumentNullException(nameof(accessors));
         }
 
         /// <summary>
-        /// Every Conversation turn for our Echo Bot will call this method.
+        /// Every conversation turn for our Echo Bot will call this method.
         /// There are no dialogs used, since it's "single turn" processing, meaning a single
-        /// request and response, with no stateful conversation.
+        /// request and response.
         /// </summary>
         /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
         /// for processing this conversation turn. </param>
         /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
         /// or threads to receive notice of cancellation.</param>
-        /// <returns>A task that represents the work queued to execute.</returns>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
         /// <seealso cref="BotStateSet"/>
         /// <seealso cref="ConversationState"/>
         /// <seealso cref="IMiddleware"/>
@@ -50,17 +49,18 @@ namespace EchoBotWithCounter
         {
             if (turnContext.Activity.Type == ActivityTypes.Message)
             {
-                // Get the conversation state from the turn context that holds a simple counter
+                // Get the conversation state from the turn context.
                 var state = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
 
                 // Bump the turn count for this conversation.
                 state.TurnCount++;
 
+                // Save the new turn count into the conversation state.
+                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+
                 // Echo back to the user whatever they typed.
                 var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
                 await turnContext.SendActivityAsync(responseMessage);
-
-                // After this Turn completes, the BotStateSet Middleware will save the underlying ConversationState object.
             }
         }
     }
--- csharp_dotnetcore/2.EchoBot-With-Counter/README.md
+++ csharp_dotnetcore/2.EchoBot-With-Counter/README.md
@@ -1,4 +1,4 @@
-﻿This sample shows a simple echo bot using Bot storage with ASP.Net Core 2.
+﻿This sample demonstrates a simple echo bot using state with ASP.Net Core 2.
 
 # To try this sample
 - Clone the samples repository
@@ -7,25 +7,25 @@ git clone https://github.com/Microsoft/botbuilder-samples.git
 ```
 
 # Prerequisites
-## Visual studio
-- Navigate to the samples folder (`BotBuilder-Samples\csharp_dotnetcore\1.Console-EchoBot`) and open EchoBotWithCounter.csproj in Visual studio 
-- Hit F5
+## Visual Studio
+- Navigate to the samples folder (`BotBuilder-Samples\csharp_dotnetcore\2.EchoBot-With-Counter`) and open EchoBotWithCounter.csproj in Visual Studio.
+- Hit F5.
 
-## Visual studio code
-- Open `BotBuilder-Samples\csharp_dotnetcore\1.Console-EchoBot` sample folder.
-- Bring up a terminal, navigate to `BotBuilder-Samples\csharp_dotnetcore\1.Console-EchoBot` folder
-- type 'dotnet run'
+## Visual Studio Code
+- Open `BotBuilder-Samples\csharp_dotnetcore\2.EchoBot-With-Counter` sample folder.
+- Bring up a terminal, navigate to `BotBuilder-Samples\csharp_dotnetcore\2.EchoBot-With-Counter` folder.
+- Type 'dotnet run'.
 
 ## Testing the bot using Bot Framework Emulator
-[Microsoft Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
-
+[Microsoft Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot 
+developers to test and debug their bots on localhost or running remotely through a tunnel.
 - Install the Bot Framework emulator from [here](https://aka.ms/botframeworkemulator).
 
 ## Connect to bot using Bot Framework Emulator **V4**
-- Launch Bot Framework Emulator
-- File -> Open bot and navigate to `BotBuilder-Samples\csharp_dotnetcore\1.Console-EchoBot` folder
-- Select EchoBotWithCounter.bot file
+- Launch the Bot Framework Emulator.
+- File -> Open bot and navigate to `BotBuilder-Samples\csharp_dotnetcore\2.EchoBot-With-Counter` folder.
+- Select `EchoBotWithCounter.bot` file.
 
 # Further reading
-- [Azure Bot Service Introduction](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
+- [Azure Bot Service](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
 - [Bot Storage](https://docs.microsoft.com/en-us/azure/bot-service/dotnet/bot-builder-dotnet-state?view=azure-bot-service-3.0&viewFallbackFrom=azure-bot-service-4.0)
