a064b9e9d961ca7dbe2b6734a6b40e7f539f547f
Steven Gum <steven.gum@gmail.com>
2018-09-06 10:56:44 -0700

    in-person review with @cleemullins for TS console echo bot sample

--- javascript_ts/01.console-echobot/src/index.ts
+++ javascript_ts/01.console-echobot/src/index.ts
@@ -4,14 +4,14 @@
 import { ConsoleAdapter, TurnContext } from 'botbuilder';
 import { ConsoleEchoBot } from './bot';
 
-// Create the bot adapter, which is responsible for sending and receiving messages.
-// The ConsoleAdapter enables a user to chat with a bot from within their terminal window.
-const adapter = new ConsoleAdapter();
+// Create the adapter which is responsible for sending and receiving messages.
+// The ConsoleAdapter enables a user to chat with a bot from within their console window.
+const adapter: ConsoleAdapter = new ConsoleAdapter();
 
-// Create the ConsoleEchoBot that will process incoming Activities via its onTurn() method.
+// Create the bot that processes incoming Activities via its onTurn() method.
 const echoBot: ConsoleEchoBot = new ConsoleEchoBot();
 
-// `adapter.listen` tells the adapter to start listening for incoming messages
+// `adapter.listen` tells the adapter to listen for incoming messages
 // and events, known as "Activities."
 // Activities are wrapped in TurnContext objects by the handler function.
 const closeFn = adapter.listen(async (turnContext: TurnContext) => {
