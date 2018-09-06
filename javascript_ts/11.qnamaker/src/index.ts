753b6b96bd9d14921cb3a4ffeae0fad18160db8c
Steven Gum <steven.gum@gmail.com>
2018-09-05 17:14:02 -0700

    final review feedback conducted in person for TypeScript QnA Maker sample

--- javascript_ts/11.qnamaker/src/index.ts
+++ javascript_ts/11.qnamaker/src/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { BotFrameworkAdapter, MemoryStorage, ConversationState } from 'botbuilder';
+import { BotFrameworkAdapter, ConversationState, MemoryStorage } from 'botbuilder';
 import { QnAMakerEndpoint } from 'botbuilder-ai';
 import { BotConfiguration, IEndpointService, IQnAService, QnaMakerService } from 'botframework-config';
 import { config } from 'dotenv';
@@ -11,12 +11,11 @@ import { QnAMakerBot } from './bot';
 
 const CONFIG_ERROR = 1;
 
-// bot name as defined in .bot file 
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
+// See https://aka.ms/about-bot-file to learn more about .bot files.
 const BOT_CONFIGURATION = 'qnamaker';
 const QNA_CONFIGURATION = 'qnamakerService';
 
-// Read botFilePath and botFileSecret from .env file
+// Read botFilePath and botFileSecret from .env file.
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '../.env');
 const env = require('dotenv').config({path: ENV_FILE});
@@ -34,7 +33,7 @@ const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
 
 let botConfig: BotConfiguration;
 try {
-    // Read bot configuration from .bot file. 
+    // Read the configuration from .bot file. 
     botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
 } catch (err) {
     console.log(`Error reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
@@ -45,20 +44,20 @@ try {
 const endpointConfig = <IEndpointService>botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
 const qnaConfig = <IQnAService>botConfig.findServiceByNameOrId(QNA_CONFIGURATION);
 
-// Map the contents of qnaConfig to the required format for QnA Maker.
+// Map the contents to the required format for QnAMaker.
 const qnaEndpointSettings: QnAMakerEndpoint = {
     knowledgeBaseId: qnaConfig.kbId,
     endpointKey: qnaConfig.endpointKey,
     host: qnaConfig.hostname
 }
 
-// Create adapter. See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+// Create adapter. See https://aka.ms/about-bot-adapter to learn more about adapters.
 const adapter = new BotFrameworkAdapter({
-    appId: endpointConfig.appId || process.env.microsoftAppID,
-    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+    appId: endpointConfig.appId || process.env.MicrosoftAppId,
+    appPassword: endpointConfig.appPassword || process.env.MicrosoftAppPassword
 });
 
-// Create the main dialog.
+// Create the bot that will handle incoming messages.
 const qnaMakerBot = new QnAMakerBot(qnaEndpointSettings, {});
 
 // Listen for incoming requests.
