d69d37401f6f864dd3ed560e9b43a1e207918350
Dave Taniguchi <daveta@microsoft.com>
2018-09-05 19:43:36 -0700

    Add cmullins feedback

--- csharp_dotnetcore/11.QnAMaker/QnAMaker/QnABot.cs
+++ csharp_dotnetcore/11.QnAMaker/QnAMaker/QnABot.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 
-namespace QnABot
+namespace Microsoft.BotBuilderSamples
 {
     /// <summary>
-    /// Represents a bot that can process incoming activities.
-    /// For each interaction from the user, an instance of this class is called.
+    /// For each interaction from the user, an instance of this class is created and
+    /// the OnTurnAsync method is called.
     /// This is a Transient lifetime service.  Transient lifetime services are created
     /// each time they're requested. For each Activity received, a new instance of this
     /// class is created. Objects that are expensive to construct, or have a lifetime
@@ -19,10 +19,10 @@ namespace QnABot
     public class QnABot : IBot
     {
         /// <summary>
-        /// Key in the Bot config (.bot file) for the QnaMaker instance.
-        /// In the .bot file, multiple instances of QnaMaker can be configured.
+        /// Key in the bot config (.bot file) for the QnA Maker instance.
+        /// In the ".bot" file, multiple instances of QnA Maker can be configured.
         /// </summary>
-        public static readonly string QnAMakerKey = "QnaBot";
+        public static readonly string QnAMakerKey = "QnABot";
 
         /// <summary>
         /// Services configured from the ".bot" file.
@@ -32,20 +32,20 @@ namespace QnABot
         /// <summary>
         /// Initializes a new instance of the <see cref="QnABot"/> class.
         /// </summary>
-        /// <param name="services">Services configured from the ".bot" file.</param>
+        /// <param name="services">A <see cref="BotServices"/> configured from the ".bot" file.</param>
         public QnABot(BotServices services)
         {
             _services = services ?? throw new System.ArgumentNullException(nameof(services));
             if (!_services.QnAServices.ContainsKey(QnAMakerKey))
             {
-                throw new System.ArgumentException($"Invalid configuration.  Please check your '.bot' file for a QnA service named '{QnAMakerKey}'.");
+                throw new System.ArgumentException($"Invalid configuration. Please check your '.bot' file for a QnA service named '{QnAMakerKey}'.");
             }
         }
 
         /// <summary>
-        /// Every Conversation turn for our QnA Bot will call this method.
-        /// There are no dialogs used, since it's "single turn" processing, meaning a single
-        /// request and response, with no stateful conversation.
+        /// Every conversation turn for our QnA bot will call this method.
+        /// There are no dialogs used, the sample only uses "single turn" processing,
+        /// meaning a single request and response, with no stateful conversation.
         /// </summary>
         /// <param name="context">A <see cref="ITurnContext"/> containing all the data needed
         /// for processing this conversation turn. </param>
@@ -56,16 +56,18 @@ namespace QnABot
         {
             if (context.Activity.Type == ActivityTypes.Message)
             {
-                // Check QnAMaker model
+                // Check QnA Maker model
                 var response = await _services.QnAServices[QnAMakerKey].GetAnswersAsync(context);
-
                 if (response != null && response.Length > 0)
                 {
                     await context.SendActivityAsync(response[0].Answer);
                 }
                 else
                 {
-                    await context.SendActivityAsync("No QnA Maker answers were found. This example uses a QnA Maker Knowledge Base that focuses on smart light bulbs. To see QnA Maker in action, ask the bot questions like \"Why won't it turn on?\" or say something like \"I need help.\"");
+                    var msg = @"No QnA Maker answers were found. This example uses a QnA Maker Knowledge Base that focuses on smart light bulbs. 
+                        To see QnA Maker in action, ask the bot questions like 'Why won't it turn on?' or 'I need help'.";
+
+                    await context.SendActivityAsync(msg);
                 }
             }
         }
