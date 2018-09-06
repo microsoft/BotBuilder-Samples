b4977f302ed7e6ce79a89f3c17c2c47adb7971fb
Ben Brown <ben@xoxco.com>
2018-09-06 13:47:25 -0500

    update sample to match docs

--- javascript_nodejs/17.proactive-messages/dialogs/mainDialog/index.js
+++ javascript_nodejs/17.proactive-messages/dialogs/mainDialog/index.js
@@ -23,56 +23,111 @@ class MainDialog {
 
             const utterance = (context.activity.text || '').trim().toLowerCase();
 
-            // If the user says "subscribe", extract the conversation reference
-            // and set up a message to send 5 seconds later.
-            if (utterance === 'subscribe') {
-                const reference = TurnContext.getConversationReference(context.activity);
-                const userId = await this.saveReference(reference);
-                await this.subscribeUser(userId);
-                await context.sendActivity(`Thank you! We will message you in 5 seconds.`);
-            } else {
-                await context.sendActivity(`Say "subscribe" to schedule an example proactive message.`);
-            }
+            // If user types in run, create a new job
+            if (utterance == "run"){
+                await this.createJob(this.storage, context);
+            };
 
-        } else if (context.activity.type == 'conversationUpdate' && context.activity.membersAdded[0].name !== 'Bot') {
-            // Send a "this is what the bot does" message.
-            await context.sendActivity(`Say "subscribe" to schedule an example proactive message.`);
+            const firstWord = utterance.split(' ')[0];
+            const secondWord = utterance.split(' ')[1];
+
+            // If the user types done and a Job Id Number
+            // We check if the second word input is a number
+            if (firstWord == "done" && !isNaN(parseInt(secondWord))) {
+                var jobIDNumber = secondWord;
+                await this.completeJob(this.storage, context, jobIDNumber);
+            } else if (firstWord == "done" && isNaN(parseInt(secondWord))) {
+                await context.sendActivity('Enter the job ID number after "done".');
+            };
+
+        } else if (context.activity.type === 'event' && context.activity.name === 'jobCompleted') {
+            var jobIDNumber = context.activity.value;
+            if (!isNaN(parseInt(jobIDNumber))) {
+                await this.completeJob(this.storage, context, jobIDNumber);
+            }
         }
     }
 
-    // Store the conversation reference in the storage system by userId.
-    async saveReference(reference) {
-        const userId = reference.activityId;
-        const changes = {};
-        changes['reference/' + userId] = reference;
-        await this.storage.write(changes);
+    // Save job ID and conversation reference
+    async createJob(storage, context) {
 
-        return userId;
-    }
+        // Create a unique job ID
+        var date = new Date();
+        var jobIDNumber = date.getTime();
 
-    // Load a stored conversation reference from the storage system.
-    async findReference(userId) {
-        const referenceKey = 'reference/' + userId;
-        const rows = await this.storage.read([referenceKey]);
-        const reference = await rows[referenceKey];
+        //Get the conversation reference
+        const reference = TurnContext.getConversationReference(context.activity);
 
-        return reference;
-    }
-    
-    // Set up a proactive message to be delivered to a user with a 5 second delay.
-    async subscribeUser(userId) {
-        setTimeout(async () => {
-            const reference = await this.findReference(userId);
-            if (reference) {
-                this.adapter.continueConversation(reference, async (context) => {
-                    await context.sendActivity(`This is your proactive notication!`);
-                });
+        // Try to find previous information about the saved job:
+        // This object will never be true since we are passing in a unique jobID
+        let jobInfo = await storage.read([jobIDNumber]);
+
+        try{
+            if(isEmpty(jobInfo)){
+                // Job object is empty so we have to create it
+                await context.sendActivity(`need to create new job ID: ${jobIDNumber}`);
+
+                // Update jobInfo with new info
+                jobInfo[jobIDNumber] = {completed: false, reference: reference};
+
+                try {
+                    // Save to storage
+                    await storage.write(jobInfo)
+                    // Notify the user that the job has been processed 
+                    await context.sendActivity('Successful write to log.');
+                } catch (err) {
+                    await context.sendActivity(`Write failed: ${err}`);
+                }
             }
-        }, 5000);
-
+        } catch (err){
+            await context.sendActivity(`Read rejected. ${err}`);
+        }
     }
 
+    async completeJob(storage, context, jobIDNumber){
+
+        // Read from storage
+        let jobInfo = await storage.read([jobIDNumber]);
+
+        // If no job notify the user
+        if(isEmpty(jobInfo)){
+            await context.sendActivity(`Sorry no job with ID ${jobIDNumber}`);
+        }else {
+
+            // Found a job with the ID passed in
+            const reference = jobInfo[jobIDNumber].reference;
+            const completed = jobInfo[jobIDNumber].completed;
+
+            // If not completed and reference exists 
+            if(reference && !completed){
+                // Since activity was not received it has to be created.
+                await this.adapter.continueConversation(reference, async (context) => {
+                    // Complete the job
+                    jobInfo[jobIDNumber].completed = true;
+                    // Save the updated job
+                    await storage.write(jobInfo);
+                    // Notify the user that the job is complete
+                    await context.sendActivity('Job complete');
+                })
+            }
+            // The job has already been completed
+            else if(reference && completed){
+                await this.adapter.continueConversation(reference, async (context) => {
+                    await context.sendActivity('This job is already completed, please start a new job');
+                })
+            };
+        };
+    };
 
 }
+
+// Helper function to check if object is empty
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+};
 
 module.exports = MainDialog;
