3469ce13477b9a1b1006f96ebd013a948e54ff81
Chris Mullins <cleemullins@users.noreply.github.com>
2018-09-06 14:48:33 -0700

    In-Person Code Review with Jason to cleanup code. @jasonsowers to validate and create the PR.

--- csharp_dotnetcore/7.Using-Adaptive-Cards/Startup.cs
+++ csharp_dotnetcore/7.Using-Adaptive-Cards/Startup.cs
@@ -4,25 +4,13 @@
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.BotFramework;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.BotBuilderSamples
 {
-    /// <summary>
-    /// The Startup class configures services and the app's request pipeline.
-    /// </summary>
     public class Startup
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup"/> class.
-        /// This method gets called by the runtime. Use this method to add services to the container.
-        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940.
-        /// </summary>
-        /// <param name="env">Provides information about the <see cref="IHostingEnvironment"/> an application is running in.</param>
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -34,12 +22,6 @@ namespace Microsoft.BotBuilderSamples
             this.Configuration = builder.Build();
         }
 
-        /// <summary>
-        /// Gets the <see cref="IConfiguration"/> that represents a set of key/value application configuration properties.
-        /// </summary>
-        /// <value>
-        /// The <see cref="IConfiguration"/> that represents a set of key/value application configuration properties.
-        /// </value>
         public IConfiguration Configuration { get; }
 
         /// <summary>
@@ -51,26 +33,6 @@ namespace Microsoft.BotBuilderSamples
             services.AddBot<AdaptiveCardsBot>(options =>
             {
                 options.CredentialProvider = new ConfigurationCredentialProvider(this.Configuration);
-
-                // Memory Storage is for local bot debugging only. When the bot
-                // is restarted, anything stored in memory will be gone.
-                IStorage dataStore = new MemoryStorage();
-
-                // For production bots use the Azure Blob or
-                // Azure CosmosDB storage providers, as seen below. To the Azure
-                // based storage providers, add the Microsoft.Bot.Builder.Azure
-                // Nuget package to your solution. That package is found at:
-                // https://www.nuget.org/packages/Microsoft.Bot.Builder.Azure/
-                // Uncomment this line to use Azure Blob Storage
-                // IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureBlobStorage("AzureBlobConnectionString", "containerName");
-                // Create and add conversation state.
-                var convoState = new ConversationState(dataStore);
-                options.State.Add(convoState);
-
-                // The BotStateSet middleware forces state storage to auto-save when the bot is complete processing the message.
-                // Note: Developers may choose not to add all the state providers to this middleware if save is not required.
-                var stateSet = new BotStateSet(options.State.ToArray());
-                options.Middleware.Add(stateSet);
             });
         }
 
