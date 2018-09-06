166f8114c6bf17dd6801e903ff5d713cf80f300a
Dave Taniguchi <daveta@microsoft.com>
2018-09-06 16:30:58 -0700

    Initial commit

---
+++ csharp_webapi/2.EchoBot-With-Counter/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Web.Http;
+
+namespace EchoBotWithCounter
+{
+#pragma warning disable SA1649  // File name should match first type name
+    public class WebApiApplication : System.Web.HttpApplication
+#pragma warning restore SA1649 // File name should match first type name
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                BotConfig.Register(config);
+            });
+        }
+    }
+}
