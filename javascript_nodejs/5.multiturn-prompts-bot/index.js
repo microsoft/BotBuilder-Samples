0fe3b3909ae1b48e12a68289bafb6d8656517087
Ben Brown <ben@xoxco.com>
2018-09-07 16:52:40 -0500

    updates per pull review feedback

--- javascript_nodejs/5.multiturn-prompts-bot/index.js
+++ javascript_nodejs/5.multiturn-prompts-bot/index.js
@@ -3,31 +3,31 @@
 
 const restify = require('restify');
 const path = require('path');
-const ERROR = 1;
+const CONFIG_ERROR = 1;
 
 // Import required bot services. See https://aka.ms/bot-services to learn more about the different part of a bot.
-const { BotFrameworkAdapter, BotStateSet,  MemoryStorage, ConversationState, UserState } = require('botbuilder');
+const { BotFrameworkAdapter, BotStateSet, ConversationState, MemoryStorage, UserState } = require('botbuilder');
 const { BotConfiguration } = require('botframework-config');
 
 const MainDialog = require('./dialogs/mainDialog');
 
-// Read botFilePath and botFileSecret from .env file
+// Read botFilePath and botFileSecret from .env file.
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '.env');
 const env = require('dotenv').config({path: ENV_FILE});
 
-// Create HTTP server
+// Create HTTP server.
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log(`\n${server.name} listening to ${server.url}`);
-    console.log(`\nGet Bot Framework emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open multiturn-prompts-bot.bot file in the Emulator`);
+    console.log(`\n${server.name} listening to ${server.url}.`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator.`);
+    console.log(`\nTo talk to your bot, open multiturn-prompts-bot.bot file in the emulator.`);
 });
 
 // .bot file path
 const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
 
-// Read the bot's configuration from a .bot file identified by BOT_FILE.
+// Read the configuration from a .bot file.
 // This includes information about the bot's endpoints and Bot Framework configuration.
 let botConfig;
 try {
@@ -35,11 +35,11 @@ try {
 } catch (err) {
     console.log(`Error reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
     console.log(err);
-    process.exit(ERROR);
+    process.exit(CONFIG_ERROR);
 }
 
 // Define the name of the bot, as specified in .bot file.
-// See https://aka.ms/about-bot-file to learn more about .bot file.
+// See https://aka.ms/about-bot-file to learn more about .bot files.
 const BOT_CONFIGURATION = 'multiturn-prompts-bot';
 
 // Load the configuration profile specific to this bot identity.
@@ -55,8 +55,9 @@ const adapter = new BotFrameworkAdapter({
 // Define the state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
 // A bot requires a state storage system to persist the dialog and user state between messages.
 const memoryStorage = new MemoryStorage();
+
 // CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
-// is restarted, anything stored in memory will be gone. 
+// is restarted, everything stored in memory will be gone. 
 // For production bots use the Azure Cosmos DB storage, or Azure Blob storage providers. 
 // const { CosmosDbStorage } = require('botbuilder-azure');
 // const STORAGE_CONFIGURATION = 'cosmosDB'; // Cosmos DB configuration in your .bot file
@@ -78,8 +79,8 @@ const mainDlg = new MainDialog(conversationState, userState);
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
+    adapter.processActivity(req, res, async (turnContext) => {
         // Route the message to the bot's main handler.
-        await mainDlg.onTurn(context);        
+        await mainDlg.onTurn(turnContext);        
     });
 });
