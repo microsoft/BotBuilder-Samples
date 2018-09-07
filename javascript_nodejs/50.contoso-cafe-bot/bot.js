918c1526377261548c4d9084cf5957bc5991dbb2
Vishwac Sena Kannan <vishwacsenakannan@Vishwacs-MacBook-Pro.local>
2018-09-07 00:14:07 -0700

    more progress

--- javascript_nodejs/50.contoso-cafe-bot/bot.js
+++ javascript_nodejs/50.contoso-cafe-bot/bot.js
@@ -1,14 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+const { ActivityTypes, CardFactory } = require('botbuilder');
+const welcomeCard = require('./dialogs/mainDialog/resources/welcomeCard.json');
 class Bot {
     /**
      * 
      * @param {Object} conversationState 
      */
     constructor (conversationState) {
-        // creates a new state accessor property.see https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors 
-        this.countProperty = conversationState.createProperty(TURN_COUNTER);
+        
     }
     /**
      * 
@@ -26,16 +27,14 @@ class Bot {
         // 4. Responsible for handling no-match response from mainDialog when main dialog is unable to continue the conversation
 
         // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
-        if (context.activity.type === 'message') {
-            // read from state.
-            let count = await this.countProperty.get(context);
-            count = count === undefined ? 1 : count;
-            await context.sendActivity(`${count}: You said "${context.activity.text}"`);
-            // increment and set turn counter.
-            this.countProperty.set(context, ++count);
+        if (context.activity.type === ActivityTypes.Message) {
+            
         }
-        else {
-            await context.sendActivity(`[${context.activity.type} event detected]`);
+        else if(context.activity.type === ActivityTypes.ConversationUpdate && context.activity.membersAdded[0].name !== 'Bot') {
+            // welcome user
+            await context.sendActivity(`Hello, I am the Contoso Cafe Bot!`);
+            await context.sendActivity(`I can help book a table, find cafe locations and more..`);
+            await context.sendActivity({ attachments: [CardFactory.adaptiveCard(welcomeCard)]});
         }
     }
 }
