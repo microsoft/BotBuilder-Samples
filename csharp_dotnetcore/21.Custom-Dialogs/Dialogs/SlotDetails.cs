221580f6b26b27e8bbfc74ee1dc4262fbdd05c12
John Taylor <johtaylo@microsoft.com>
2018-09-07 16:16:36 -0700

    custom dialog implementation

---
+++ csharp_dotnetcore/21.Custom-Dialogs/Dialogs/SlotDetails.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class SlotDetails
+    {
+        public string Name { get; set; }
+        public string PromptId { get; set; }
+        public string PromptText { get; set; }
+    }
+}
