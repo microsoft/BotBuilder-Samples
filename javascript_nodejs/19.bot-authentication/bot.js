90b9914ba4a3aea5aa13d51d57dde5456123d6c9
Jason Sowers <v-jassow@microsoft.com>
2018-09-08 09:18:31 -0700

    initial auth commit

---
+++ javascript_nodejs/19.bot-authentication/bot.js
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityTypes, MessageFactory } = require('botbuilder');
+const connectionName = "";
+
+/**
+ * A simple bot that authenticates users using OAuth prompts.
+ */
+class AuthenticationBot {
+    /**
+     * Every conversation turn for our AuthenticationBot will call this method.
+     * There are no dialogs used, since it's "single turn" processing, meaning a single request and
+     * response, with no stateful conversation.
+     * @param {Object} turnContext on turn context object.
+     */
+    async onTurn(turnContext) {
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        if (turnContext.activity.type === ActivityTypes.Message) {
+            const text = turnContext.activity.text;
+
+            // Create an array with the valid color options.
+            const validColors = ['Red', 'Blue', 'Yellow'];
+
+            // If the `text` is in the Array, a valid color was selected and send agreement. 
+            if (validColors.includes(text)) {
+                await turnContext.sendActivity(`I agree, ${text} is the best color.`);
+            } else {
+                await turnContext.sendActivity('Please select a color.');
+            }
+
+            // After the bot has responded send the SuggestedActions.
+            await sendSuggestedActions(turnContext);
+        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            let members = turnContext.activity.membersAdded;
+
+            for (let index = 0; index < members.length; index++) {
+                const member = members[index];
+                if (member.id != turnContext.activity.recipient.id) {
+                    const welcomeMessage = `Welcome to suggestedActionsBot ${member.name}.` +
+                        ' This bot will introduce you to Suggested Actions.' +
+                        ' Please select an option:';
+                    await turnContext.sendActivity(welcomeMessage);
+                    await sendSuggestedActions(turnContext);
+                }
+            };
+        } else {
+            await turnContext.sendActivity(`[${turnContext.activity.type} event detected.]`);
+        }
+    }
+}
+
+/**
+ * Send the suggested actions to the user.
+ * @param {Object} turnContext on turn context object.
+ */
+async function sendSuggestedActions(turnContext) {
+    var reply = MessageFactory.suggestedActions(['Red', 'Yellow', 'Blue'], `What is the best color?`);
+    await turnContext.sendActivity(reply);
+}
+
+module.exports = AuthenticationBot;
