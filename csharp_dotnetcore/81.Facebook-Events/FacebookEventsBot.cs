8a16f42911119d67b61fc0018086d06449982a8d
Carlos Castro <ccastro@microsoft.com>
2018-09-10 14:56:09 -0700

    C# Facebook Events Sample: PR Feedback

--- csharp_dotnetcore/81.Facebook-Events/FacebookEventsBot.cs
+++ csharp_dotnetcore/81.Facebook-Events/FacebookEventsBot.cs
@@ -3,17 +3,13 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Facebook_Events_Bot.FacebookModel;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.AI.Luis;
-using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Bot.Schema;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Facebook_Events_Bot
@@ -41,6 +37,11 @@ namespace Facebook_Events_Bot
         /// <param name="accessors">The state accessors this instance will be needing at runtime.</param>
         public FacebookEventsBot(BotAccessors accessors)
         {
+            if (accessors == null)
+            {
+                throw new ArgumentNullException(nameof(accessors));
+            }
+
             _dialogs = new DialogSet(accessors.ConversationDialogState);
             _dialogs.Add(new AttachmentPrompt(DialogId));
         }
