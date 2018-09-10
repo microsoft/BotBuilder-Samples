9e514e97973df7ec3b3f6059257a21bafe879637
Jason Sowers <v-jassow@microsoft.com>
2018-09-10 11:48:57 -0700

    cleanup

--- javascript_nodejs/19.bot-authentication/bot.js
+++ javascript_nodejs/19.bot-authentication/bot.js
@@ -1,23 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
 const { ActivityTypes, MessageFactory } = require('botbuilder');
 const { ChoicePrompt, DialogSet, WaterfallDialog, OAuthPrompt } = require('botbuilder-dialogs');
-const CONNECTION_NAME = "";
 const DIALOG_STATE_PROPERTY = 'dialogState';
 const OAUTH_PROMPT = 'oAuth_prompt';
 const CONFIRM_PROMPT = 'confirm_prompt';
 const AUTH_DIALOG = 'auth_dialog';
 const HELP_TEXT = ' Type anything to get logged in. Type \'logout\' to signout.' +
-                  ' Type \'help\' to view this message again';
+    ' Type \'help\' to view this message again';
+
+// The connection name here must match the the one from
+// your Bot Channels Registration on the settings blade in Azure.
+const CONNECTION_NAME = "";
+
+// Create the settings for the OAuthPrompt.
 const OAUTH_SETTINGS = {
     connectionName: CONNECTION_NAME,
     title: "Sign In",
     text: "Please Sign In",
     timeout: 300000
 };
-// Creat an OAuthPrompt
+
 
 /**
  * A simple bot that authenticates users using OAuth prompts.
@@ -25,9 +29,9 @@ const OAUTH_SETTINGS = {
 class AuthenticationBot {
 
     /**
-    * The constructor for the bot. 
-    * @param {ConversationState} conversationState A ConversationState object used to store the dialog state.
-    */
+     * The constructor for the bot. 
+     * @param {ConversationState} conversationState A ConversationState object used to store the dialog state.
+     */
     constructor(conversationState) {
 
         // Create a new state accessor property. See https://aka.ms/about-bot-state-accessors to learn more about bot state and state accessors.
@@ -40,12 +44,12 @@ class AuthenticationBot {
         this.dialogs.add(new ChoicePrompt(CONFIRM_PROMPT));
         this.dialogs.add(new OAuthPrompt(OAUTH_PROMPT, OAUTH_SETTINGS))
 
-        // the waterfall dialog that controls the flow of the conversation
+        // The waterfall dialog that controls the flow of the conversation.
         this.dialogs.add(new WaterfallDialog(AUTH_DIALOG, [
-            async (dc) => {
+            async(dc) => {
                 return await dc.prompt(OAUTH_PROMPT);
             },
-            async (dc, step) => {
+            async(dc, step) => {
                 let tokenResponse = step.result;
                 if (tokenResponse != null) {
                     await dc.context.sendActivity("You are now logged in.");
@@ -55,9 +59,9 @@ class AuthenticationBot {
                 await dc.Context.sendActivity("Login was not sucessful please try again");
                 return await dc.end();
             },
-            async (dc, step) => {
+            async(dc, step) => {
                 const result = step.result.value;
-                if (result === 'yes'){
+                if (result === 'yes') {
                     // Call the prompt again because we need the token. The reasons for this are:
                     // 1. If the user is already logged in we do not need to store the token locally in the bot and worry
                     // about refreshing it. We can always just call the prompt again to get the token.
@@ -68,7 +72,7 @@ class AuthenticationBot {
                     // the OAuth prompt to get the token or get a new token if needed.
                     let prompt = await dc.prompt(OAUTH_PROMPT);
                     var tokenResponse = prompt.result;
-                    if(tokenResponse != null){
+                    if (tokenResponse != null) {
                         await dc.context.sendActivity(`Here is your token: ${tokenResponse.token}`);
                         await dc.context.sendActivity(HELP_TEXT);
                         return await dc.end();
@@ -76,7 +80,7 @@ class AuthenticationBot {
                 }
 
                 await dc.context.sendActivity(HELP_TEXT);
-                return await dc.end();                
+                return await dc.end();
             },
         ]));
     }
@@ -88,7 +92,7 @@ class AuthenticationBot {
      * @param {Object} turnContext on turn context object.
      */
     async onTurn(turnContext) {
-        
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
             // Create a dialog context object.
@@ -110,7 +114,7 @@ class AuthenticationBot {
                     await botAdapter.signOutUser(turnContext, CONNECTION_NAME);
                     await turnContext.sendActivity("You have been signed out.");
                     await turnContext.sendActivity(HELP_TEXT)
-                }                
+                }
             } else {
                 if (!turnContext.responded) {
                     await dc.begin(AUTH_DIALOG);
@@ -127,19 +131,18 @@ class AuthenticationBot {
                     await turnContext.sendActivity(welcomeMessage);
                 }
             };
-        } else if (turnContext.activity.type === ActivityTypes.Invoke || turnContext.activity.type === ActivityTypes.Event){
+        } else if (turnContext.activity.type === ActivityTypes.Invoke || turnContext.activity.type === ActivityTypes.Event) {
             // This handles the MS Teams Invoke Activity sent when magic code is not used.
             // See: https://docs.microsoft.com/en-us/microsoftteams/platform/concepts/authentication/auth-oauth-card#getting-started-with-oauthcard-in-teams
             // The Teams manifest schema is found : https://docs.microsoft.com/en-us/microsoftteams/platform/resources/schema/manifest-schema
             // It also handles the Event Activity sent from the emulator when the magic code is not used.
             // See: https://blog.botframework.com/2018/08/28/testing-authentication-to-your-bot-using-the-bot-framework-emulator/
-            const dc = await this.dialogs.createContext(turnContext);                  
+            const dc = await this.dialogs.createContext(turnContext);
             await dc.continue;
-            if (!turnContext.responded)
-            {
+            if (!turnContext.responded) {
                 await dc.begin(AUTH_DIALOG);
-            }            
-        }else {
+            }
+        } else {
             await turnContext.sendActivity(`[${turnContext.activity.type} event detected.]`);
         }
     }
