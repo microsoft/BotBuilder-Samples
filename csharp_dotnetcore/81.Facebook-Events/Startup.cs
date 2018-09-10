de67d1aaa10ee97bfdae00dd796a8b1cf6bc946d
Carlos Castro <ccastro@microsoft.com>
2018-09-09 17:04:49 -0700

    C# Facebook Events sample: PR Comments

--- csharp_dotnetcore/81.Facebook-Events/Startup.cs
+++ csharp_dotnetcore/81.Facebook-Events/Startup.cs
@@ -33,8 +33,6 @@ namespace Facebook_Events_Bot
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
