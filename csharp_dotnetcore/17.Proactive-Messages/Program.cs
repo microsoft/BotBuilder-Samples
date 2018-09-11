ca8fcb1cb3e1fca1617ed39044f8890d9a195d93
Dave Taniguchi <daveta@microsoft.com>
2018-09-10 19:17:13 -0700

    feedback from Jonathan nd cmullins

--- csharp_dotnetcore/17.Proactive-Messages/Program.cs
+++ csharp_dotnetcore/17.Proactive-Messages/Program.cs
@@ -13,13 +13,6 @@ namespace Microsoft.BotBuilderSamples
             BuildWebHost(args).Run();
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="T:Microsoft.AspNetCore.Hosting.WebHostBuilder" />
-        /// class with pre-configured defaults.
-        /// The UseStartup method on WebHost specifies the Startup class for your app.
-        /// </summary>
-        /// <param name="args">Arguments for this method.</param>
-        /// <returns>A web server.</returns>
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
