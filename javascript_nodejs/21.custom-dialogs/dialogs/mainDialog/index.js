f8d7d8ea52547b0ed5c4a37072172384f3386616
Ben Brown <benbrown@Bens-MBP.guest.corp.microsoft.com>
2018-09-11 17:47:17 -0700

    resolve conflicts

--- javascript_nodejs/21.custom-dialogs/dialogs/mainDialog/index.js
+++ javascript_nodejs/21.custom-dialogs/dialogs/mainDialog/index.js
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-<<<<<<< HEAD
-const { ActivityTypes } = require('botbuilder-core');
-=======
 const { ActivityTypes } = require('botbuilder');
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
 const { DialogSet, DialogTurnStatus, NumberPrompt, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
 
 const SlotFillingDialog = require('./SlotFillingDialog');
@@ -15,11 +11,7 @@ const DIALOG_STATE_PROPERTY = 'dialogState';
 
 class MainDialog {
     /**
-<<<<<<< HEAD
      * MainDialog defines the core business logic of this bot.
-=======
-     * 
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
      * @param {ConversationState} conversationState A ConversationState object used to store dialog state.
      */
     constructor (conversationState) {
@@ -33,30 +25,21 @@ class MainDialog {
         // Create a dialog set to include the dialogs used by this bot.
         this.dialogs = new DialogSet(this.dialogState);
 
-<<<<<<< HEAD
         // Set up a series of questions for collecting the user's name.
-=======
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         const fullname_slots = [
             new SlotDetails('first', 'text', 'Please enter your first name.'),
             new SlotDetails('last', 'text', 'Please enter your last name.')
         ];
 
-<<<<<<< HEAD
         // Set up a series of questions to collect a street address.
-=======
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         const address_slots = [
             new SlotDetails('street', 'text', 'Please enter your street address.'),
             new SlotDetails('city', 'text', 'Please enter the city.'),
             new SlotDetails('zip', 'text', 'Please enter your zipcode.')
         ];
 
-<<<<<<< HEAD
         // Link the questions together into a parent group that contains references
         // to both the fullname and address questions defined above.
-=======
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         const slots = [
             new SlotDetails('fullname', 'fullname'),
             new SlotDetails('age', 'number', 'Please enter your age.'),
@@ -64,13 +47,9 @@ class MainDialog {
             new SlotDetails('address', 'address')
         ];
 
-<<<<<<< HEAD
         // Add the individual child dialogs and prompts used.
         // Note that the built-in prompts work hand-in-hand with our custom SlotFillingDialog class
         // because they are both based on the provided Dialog class.
-=======
-
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         this.dialogs.add(new SlotFillingDialog('address', address_slots));
         this.dialogs.add(new SlotFillingDialog('fullname', fullname_slots));
         this.dialogs.add(new TextPrompt('text'));
@@ -78,38 +57,27 @@ class MainDialog {
         this.dialogs.add(new NumberPrompt('shoesize', this.showSizeValidator));
         this.dialogs.add(new SlotFillingDialog('slot-dialog', slots));
 
-<<<<<<< HEAD
         // Finally, add a 2-step WaterfallDialog that will initiate the SlotFillingDialog,
         // and then collect and display the results.
-=======
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         this.dialogs.add(new WaterfallDialog('root', [
             this.startDialog,
             this.processResults
         ]));
     }
 
-<<<<<<< HEAD
     // This is the first step of the WaterfallDialog.
     // It kicks off the dialog with the multi-question SlotFillingDialog,
     // then passes the aggregated results on to the next step.
-=======
-    // Kick off the dialog with the main slot dialog.
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
     async startDialog(dc, step) {
         return await dc.begin('slot-dialog');
     }   
 
-<<<<<<< HEAD
     // This is the second step of the WaterfallDialog.
     // It receives the results of the SlotFillingDialog and displays them.
     async processResults(dc, step) {
 
         // Each "slot" in the SlotFillingDialog is represented by a field in step.result.values.
         // The complex that contain subfields have their own .values field containing the sub-values.
-=======
-    async processResults(dc, step) {
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         const values = step.result.values;
 
         const fullname = values['fullname'].values;
@@ -123,32 +91,19 @@ class MainDialog {
         return await dc.end();
     }
 
-<<<<<<< HEAD
     // Validate that the provided shoe size is between 0 and 16, and allow half steps.
     // This is used to instantiate a specialized NumberPrompt.
-=======
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
     async showSizeValidator(turnContext, step) {
 
         const shoesize = step.recognized.value;
         
-<<<<<<< HEAD
-        // Show sizes can range from 0 to 16.
+        // Shoe sizes can range from 0 to 16.
         if (shoesize >= 0 && shoesize <= 16)
         {
             // We only accept round numbers or half sizes.
             if (Math.floor(shoesize) == shoesize || Math.floor(shoesize * 2) == shoesize * 2)
             {
                 // Indicate success by returning the value.
-=======
-        // show sizes can range from 0 to 16
-        if (shoesize >= 0 && shoesize <= 16)
-        {
-            // we only accept round numbers or half sizes
-            if (Math.floor(shoesize) == shoesize || Math.floor(shoesize * 2) == shoesize * 2)
-            {
-                // indicate success by returning the value
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
                 step.end(shoesize);
             }
         }
@@ -174,7 +129,6 @@ class MainDialog {
                 }
             }
             
-<<<<<<< HEAD
             if (!dc.context.responded) {
                 // Continue the current dialog if one is pending.
                 const results = await dc.continue();
@@ -184,15 +138,6 @@ class MainDialog {
                 // If no response has been sent, start the onboarding dialog.
                 await dc.begin('root');
             }
-=======
-            // Continue the current dialog if one is pending.
-            const results = await dc.continue();
-
-            // If no response has been sent, start the onboarding dialog.
-            if (results.status === DialogTurnStatus.empty) {
-                await dc.begin('root');
-            } 
->>>>>>> 73ef1b18b2ae8785930ca5b993a955d1a99beadf
         } else if (
              turnContext.activity.type === ActivityTypes.ConversationUpdate &&
              turnContext.activity.membersAdded[0].name !== 'Bot'
