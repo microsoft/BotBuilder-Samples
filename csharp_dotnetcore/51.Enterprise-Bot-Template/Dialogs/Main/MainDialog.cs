1812e01ab2c7a6cd323729f24f2a4de59c1fb123
Lauren Mills <lamil@microsoft.com>
2018-09-14 15:36:32 -0700

    added router dialog

--- csharp_dotnetcore/51.Enterprise-Bot-Template/Dialogs/Main/MainDialog.cs
+++ csharp_dotnetcore/51.Enterprise-Bot-Template/Dialogs/Main/MainDialog.cs
@@ -14,7 +14,6 @@ namespace EnterpriseBot
 {
     public class MainDialog : RouterDialog
     {
-        // Fields
         private static MainResponses _responder = new MainResponses();
         private readonly BotServices _services;
 
@@ -27,144 +26,87 @@ namespace EnterpriseBot
             AddDialog(new EscalateDialog(_services));
         }
 
-        protected override async Task<DialogTurnResult> RouteAsync(DialogContext dc, DialogTurnResult result, CancellationToken cancellationToken = default(CancellationToken))
+        protected override async Task RouteAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
         {
-            if (dc.Context.Activity.Type == ActivityTypes.Message)
+            // Check dispatch result
+            var dispatchResult = await _services.DispatchRecognizer.RecognizeAsync<Dispatch>(dc.Context, CancellationToken.None);
+            var intent = dispatchResult.TopIntent().intent;
+
+            if (intent == Dispatch.Intent.l_General)
             {
-                // If an active dialog is waiting, continue dialog
-                switch (result.Status)
+                // If dispatch result is general luis model
+                var luisService = _services.LuisServices["EnterpriseBot-General"];
+                var luisResult = await luisService.RecognizeAsync<General>(dc.Context, CancellationToken.None);
+                var luisIntent = luisResult?.TopIntent().intent;
+
+                // switch on general intents
+                switch (luisIntent)
                 {
-                    case DialogTurnStatus.Empty:
+                    case General.Intent.Greeting:
                         {
-                            // No dialog is currently on the stack and we haven't responded to the user
-                            // Check dispatch result
-                            var dispatchResult = await _services.DispatchRecognizer.RecognizeAsync<Dispatch>(dc.Context, CancellationToken.None);
-                            var intent = dispatchResult.TopIntent().intent;
-
-                            if (intent == Dispatch.Intent.l_General)
-                            {
-                                // If dispatch result is general luis model
-                                var luisService = _services.LuisServices["EnterpriseBot-General"];
-                                var luisResult = await luisService.RecognizeAsync<General>(dc.Context, CancellationToken.None);
-                                var luisIntent = luisResult?.TopIntent().intent;
-
-                                // switch on general intents
-                                switch (luisIntent)
-                                {
-                                    case General.Intent.Greeting:
-                                        {
-                                            // send greeting response
-                                            await _responder.ReplyWith(dc.Context, MainResponses.Greeting);
-                                            break;
-                                        }
-
-                                    case General.Intent.Help:
-                                        {
-                                            // send help response
-                                            await _responder.ReplyWith(dc.Context, MainResponses.Help);
-                                            break;
-                                        }
-
-                                    case General.Intent.Cancel:
-                                        {
-                                            // send cancelled response
-                                            await _responder.ReplyWith(dc.Context, MainResponses.Cancelled);
-
-                                            // Cancel any active dialogs on the stack
-                                            await dc.CancelAllAsync();
-                                            break;
-                                        }
-
-                                    case General.Intent.Escalate:
-                                        {
-                                            // start escalate dialog
-                                            await dc.BeginAsync(nameof(EscalateDialog));
-                                            break;
-                                        }
-
-                                    case General.Intent.None:
-                                    default:
-                                        {
-                                            // No intent was identified, send confused message
-                                            await _responder.ReplyWith(dc.Context, MainResponses.Confused);
-                                            break;
-                                        }
-                                }
-                            }
-                            else if (intent == Dispatch.Intent.q_FAQ)
-                            {
-                                var qnaService = _services.QnAServices["EnterpriseBot-FAQ"];
-                                var answers = await qnaService.GetAnswersAsync(dc.Context);
-
-                                if (answers != null && answers.Count() > 0)
-                                {
-                                    await dc.Context.SendActivityAsync(answers[0].Answer);
-                                }
-                            }
-
+                            // send greeting response
+                            await _responder.ReplyWith(dc.Context, MainResponses.Greeting);
                             break;
                         }
 
-                    case DialogTurnStatus.Waiting:
+                    case General.Intent.Help:
                         {
-                            // The active dialog is waiting for a response from the user, so do nothing
+                            // send help response
+                            await _responder.ReplyWith(dc.Context, MainResponses.Help);
                             break;
                         }
 
-                    case DialogTurnStatus.Complete:
+                    case General.Intent.Cancel:
                         {
-                            // The active dialog's stack ended with a complete status
-                            await _responder.ReplyWith(dc.Context, MainResponses.Completed);
+                            // send cancelled response
+                            await _responder.ReplyWith(dc.Context, MainResponses.Cancelled);
 
-                            // End active dialog
-                            await dc.EndAsync();
+                            // Cancel any active dialogs on the stack
+                            await dc.CancelAllAsync();
                             break;
                         }
 
+                    case General.Intent.Escalate:
+                        {
+                            // start escalate dialog
+                            await dc.BeginAsync(nameof(EscalateDialog));
+                            break;
+                        }
+
+                    case General.Intent.None:
                     default:
                         {
-                            // The active dialog's stack ended with an error status
+                            // No intent was identified, send confused message
                             await _responder.ReplyWith(dc.Context, MainResponses.Confused);
-
-                            // End active dialog
-                            await dc.EndAsync();
                             break;
                         }
                 }
             }
-            else if (dc.Context.Activity.Type == ActivityTypes.ConversationUpdate)
+            else if (intent == Dispatch.Intent.q_FAQ)
             {
-                // greet when added to conversation
-                var activity = dc.Context.Activity.AsConversationUpdateActivity();
-                if (activity.MembersAdded.Where(m => m.Id == activity.Recipient.Id).Any())
-                {
-                    var view = new MainResponses();
-                    await view.ReplyWith(dc.Context, MainResponses.Intro);
+                var qnaService = _services.QnAServices["EnterpriseBot-FAQ"];
+                var answers = await qnaService.GetAnswersAsync(dc.Context);
 
-                    // This is the first time the user is interacting with the bot, so gather onboarding information.
-                    await dc.BeginAsync(nameof(OnboardingDialog));
+                if (answers != null && answers.Count() > 0)
+                {
+                    await dc.Context.SendActivityAsync(answers[0].Answer);
                 }
             }
-            else
-            {
-                HandleSystemMessage(dc.Context);
-            }
-
-            return new DialogTurnResult(DialogTurnStatus.Waiting);
         }
 
-        private void HandleSystemMessage(ITurnContext context)
+        protected override async Task OnStart(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken))
         {
-            switch (context.Activity.Type)
-            {
-                case ActivityTypes.ContactRelationUpdate:
-                case ActivityTypes.DeleteUserData:
-                case ActivityTypes.EndOfConversation:
-                case ActivityTypes.Typing:
-                case ActivityTypes.Event:
-                    // Add any additional logic for process these system messages here.
-                    break;
-            }
+            var view = new MainResponses();
+            await view.ReplyWith(innerDc.Context, MainResponses.Intro);
+
+            // This is the first time the user is interacting with the bot, so gather onboarding information.
+            await innerDc.BeginAsync(nameof(OnboardingDialog));
+        }
+
+        protected override async Task OnComplete(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // The active dialog's stack ended with a complete status
+            await _responder.ReplyWith(innerDc.Context, MainResponses.Completed);
         }
     }
 }
--- csharp_dotnetcore/51.Enterprise-Bot-Template/Dialogs/Shared/RouterDialog.cs
+++ csharp_dotnetcore/51.Enterprise-Bot-Template/Dialogs/Shared/RouterDialog.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
 
 namespace EnterpriseBot
 {
@@ -12,21 +13,78 @@ namespace EnterpriseBot
         {
         }
 
-        protected override Task<DialogTurnResult> OnDialogBeginAsync(DialogContext innerDc, object options, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return OnDialogContinueAsync(innerDc, cancellationToken);
-        }
+        protected override Task<DialogTurnResult> OnDialogBeginAsync(DialogContext innerDc, object options, CancellationToken cancellationToken = default(CancellationToken)) => OnDialogContinueAsync(innerDc, cancellationToken);
 
         protected override async Task<DialogTurnResult> OnDialogContinueAsync(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var result = await innerDc.ContinueAsync();
-            return await RouteAsync(innerDc, result);
+            var activity = innerDc.Context.Activity;
+
+            switch (activity.Type)
+            {
+                case ActivityTypes.Message:
+                    {
+                        var result = await innerDc.ContinueAsync();
+
+                        switch (result.Status)
+                        {
+                            case DialogTurnStatus.Empty:
+                                {
+                                    await RouteAsync(innerDc);
+                                    break;
+                                }
+
+                            case DialogTurnStatus.Complete:
+                                {
+                                    await OnComplete(innerDc);
+
+                                    // End active dialog
+                                    await innerDc.EndAsync();
+                                    break;
+                                }
+
+                            default:
+                                {
+                                    break;
+                                }
+                        }
+
+                        break;
+                    }
+
+                case ActivityTypes.Event:
+                    {
+                        await OnEvent(innerDc);
+                        break;
+                    }
+
+                case ActivityTypes.ConversationUpdate:
+                    {
+                        await OnStart(innerDc);
+                        break;
+                    }
+
+                default:
+                    {
+                        await OnSystemMessage(innerDc);
+                        break;
+                    }
+            }
+
+            return EndOfTurn;
         }
 
         protected override Task OnDialogEndAsync(ITurnContext context, DialogInstance instance, DialogReason reason, CancellationToken cancellationToken = default(CancellationToken)) => base.OnDialogEndAsync(context, instance, reason, cancellationToken);
 
         protected override Task OnDialogRepromptAsync(ITurnContext turnContext, DialogInstance instance, CancellationToken cancellationToken = default(CancellationToken)) => base.OnDialogRepromptAsync(turnContext, instance, cancellationToken);
 
-        protected abstract Task<DialogTurnResult> RouteAsync(DialogContext innerDc, DialogTurnResult result, CancellationToken cancellationToken = default(CancellationToken));
+        protected abstract Task RouteAsync(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken));
+
+        protected virtual Task OnStart(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken)) => Task.CompletedTask;
+
+        protected virtual Task OnComplete(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken)) => Task.CompletedTask;
+
+        protected virtual Task OnEvent(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken)) => Task.CompletedTask;
+
+        protected virtual Task OnSystemMessage(DialogContext innerDc, CancellationToken cancellationToken = default(CancellationToken)) => Task.CompletedTask;
     }
 }
