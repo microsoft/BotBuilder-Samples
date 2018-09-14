84df66680bd760ad40fa833189e16fdbc0200897
Lauren Mills <lamil@microsoft.com>
2018-09-14 13:30:49 -0700

    updated to use nameof() for dialogId

--- csharp_dotnetcore/51.Enterprise-Bot-Template/Dialogs/Onboarding/OnboardingDialog.cs
+++ csharp_dotnetcore/51.Enterprise-Bot-Template/Dialogs/Onboarding/OnboardingDialog.cs
@@ -10,7 +10,6 @@ namespace EnterpriseBot
     public class OnboardingDialog : EnterpriseDialog
     {
         // Constants
-        public const string Name = "onboarding";
         public const string NamePrompt = "namePrompt";
         public const string EmailPrompt = "emailPrompt";
         public const string LocationPrompt = "locationPrompt";
@@ -19,8 +18,10 @@ namespace EnterpriseBot
         private static OnboardingResponses _responder = new OnboardingResponses();
 
         public OnboardingDialog(BotServices botServices)
-            : base(botServices, Name)
+            : base(botServices, nameof(OnboardingDialog))
         {
+            InitialDialogId = nameof(OnboardingDialog);
+
             var onboarding = new WaterfallStep[]
             {
                 AskForName,
@@ -29,52 +30,52 @@ namespace EnterpriseBot
                 FinishOnboardingDialog,
             };
 
-            AddDialog(new WaterfallDialog(Name, onboarding));
+            AddDialog(new WaterfallDialog(InitialDialogId, onboarding));
             AddDialog(new TextPrompt(NamePrompt));
             AddDialog(new TextPrompt(EmailPrompt));
             AddDialog(new TextPrompt(LocationPrompt));
         }
 
-        public async Task<DialogTurnResult> AskForName(DialogContext dc, WaterfallStepContext args, CancellationToken cancellationToken)
+        public async Task<DialogTurnResult> AskForName(WaterfallStepContext sc, CancellationToken cancellationToken)
         {
-            return await dc.PromptAsync(NamePrompt, new PromptOptions()
+            return await sc.PromptAsync(NamePrompt, new PromptOptions()
             {
-                Prompt = await _responder.RenderTemplate(dc.Context, "en", OnboardingResponses._namePrompt),
+                Prompt = await _responder.RenderTemplate(sc.Context, "en", OnboardingResponses._namePrompt),
             });
         }
 
-        public async Task<DialogTurnResult> AskForEmail(DialogContext dc, WaterfallStepContext args, CancellationToken cancellationToken)
+        public async Task<DialogTurnResult> AskForEmail(WaterfallStepContext sc, CancellationToken cancellationToken)
         {
-            var name = dc.ActiveDialog.State["name"] = args.Result;
+            var name = sc.Values["name"] = sc.Result;
 
-            await _responder.ReplyWith(dc.Context, OnboardingResponses._haveName, new { name });
+            await _responder.ReplyWith(sc.Context, OnboardingResponses._haveName, new { name });
 
-            return await dc.PromptAsync(EmailPrompt, new PromptOptions()
+            return await sc.PromptAsync(EmailPrompt, new PromptOptions()
             {
-                Prompt = await _responder.RenderTemplate(dc.Context, "en", OnboardingResponses._emailPrompt),
+                Prompt = await _responder.RenderTemplate(sc.Context, "en", OnboardingResponses._emailPrompt),
             });
         }
 
-        public async Task<DialogTurnResult> AskForLocation(DialogContext dc, WaterfallStepContext args, CancellationToken cancellationToken)
+        public async Task<DialogTurnResult> AskForLocation(WaterfallStepContext sc, CancellationToken cancellationToken)
         {
-            var email = dc.ActiveDialog.State["email"] = args.Result;
+            var email = sc.Values["email"] = sc.Result;
 
-            await _responder.ReplyWith(dc.Context, OnboardingResponses._haveEmail, new { email });
+            await _responder.ReplyWith(sc.Context, OnboardingResponses._haveEmail, new { email });
 
-            return await dc.PromptAsync(LocationPrompt, new PromptOptions()
+            return await sc.PromptAsync(LocationPrompt, new PromptOptions()
             {
-                Prompt = await _responder.RenderTemplate(dc.Context, "en", OnboardingResponses._locationPrompt),
+                Prompt = await _responder.RenderTemplate(sc.Context, "en", OnboardingResponses._locationPrompt),
             });
         }
 
-        public async Task<DialogTurnResult> FinishOnboardingDialog(DialogContext dc, WaterfallStepContext args, CancellationToken cancellationToken)
+        public async Task<DialogTurnResult> FinishOnboardingDialog(WaterfallStepContext sc, CancellationToken cancellationToken)
         {
-            var name = dc.ActiveDialog.State["name"];
-            var email = dc.ActiveDialog.State["email"];
-            var location = dc.ActiveDialog.State["location"] = args.Result;
+            var name = sc.Values["name"];
+            var email = sc.Values["email"];
+            var location = sc.Values["location"] = sc.Result;
 
-            await _responder.ReplyWith(dc.Context, OnboardingResponses._haveLocation, new { name, location });
-            return await dc.EndAsync();
+            await _responder.ReplyWith(sc.Context, OnboardingResponses._haveLocation, new { name, location });
+            return await sc.EndAsync();
         }
     }
 }
