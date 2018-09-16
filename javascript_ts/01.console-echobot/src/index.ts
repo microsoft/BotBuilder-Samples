0bf852c2bd640198a48ccca0a8de23c613096b39
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-16 10:37:37 -0700

    move consoleAdapter into samples (#388)
    reviewed and approved by @carlosscastro

--- javascript_ts/01.console-echobot/src/index.ts
+++ javascript_ts/01.console-echobot/src/index.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ConsoleAdapter, TurnContext } from 'botbuilder';
+import { TurnContext } from 'botbuilder';
+import { ConsoleAdapter} from './consoleAdapter';
 import { ConsoleEchoBot } from './bot';
 
 // Create the adapter which is responsible for sending and receiving messages.
