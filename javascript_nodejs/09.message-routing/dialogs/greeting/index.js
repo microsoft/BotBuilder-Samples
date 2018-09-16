66fd6655c8fdf79edc9e1b0ec1df1e41ab07946f
Carlos Castro <carlosscastro@users.noreply.github.com>
2018-09-16 14:48:29 -0700

    Node.js message routing sample (#392)

---
+++ javascript_nodejs/09.message-routing/dialogs/greeting/index.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { GreetingDialog } = require('./greeting');
+
+module.exports.GreetingDialog = GreetingDialog;
