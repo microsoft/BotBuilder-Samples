d1639d676dbbc2a62ac010c15690fbcd5fb47508
Ben Brown <benbro@microsoft.com>
2018-09-19 15:58:30 -0700

    Put bot logic into a simple bot class

--- samples/javascript_nodejs/01.Console-EchoBot/index.js
+++ samples/javascript_nodejs/01.Console-EchoBot/index.js
@@ -12,22 +12,14 @@ require('dotenv').config({ path: ENV_FILE });
 // We are using the ConsoleAdapter, which enables a bot you can chat with from within your terminal window.
 const adapter = new ConsoleAdapter();
 
+// Import our bot class.
+const EchoBot = require('./bot');
+const bot = new EchoBot();
+
 // A call to adapter.listen tells the adapter to start listening for incoming messages and events, known as "activities."
 // Activities are received as TurnContext objects by the handler function.
 adapter.listen(async (context) => {
-    // Check to see if this activity is an incoming message.
-    // (It could theoretically be another type of activity.)
-    if (context.activity.type === 'message') {
-        // Check to see if the user sent a simple "quit" message.
-        if (context.activity.text.toLowerCase() === 'quit') {
-            // Send a reply.
-            context.sendActivity(`Bye!`);
-            process.exit();
-        } else if (context.activity.text) {
-            // Echo the message text back to the user.
-            return context.sendActivity(`I heard you say "${ context.activity.text }"`);
-        }
-    }
+    bot.onTurn(context);
 });
 
 // Emit a startup message with some instructions.
