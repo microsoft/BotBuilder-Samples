f6378555bb88ea7f5658ed6a4d027cd824671b5d
Dave Taniguchi <daveta@microsoft.com>
2018-09-20 15:30:47 -0700

    Fix warnings

--- samples/csharp_dotnetcore/02.echo-with-counter/Startup.cs
+++ samples/csharp_dotnetcore/02.echo-with-counter/Startup.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System;
@@ -9,7 +9,6 @@ using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Configuration;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -55,20 +54,20 @@ namespace Microsoft.BotBuilderSamples
         {
             services.AddBot<EchoWithCounterBot>(options =>
             {
-				var secretKey = Configuration.GetSection("botFileSecret")?.Value;
-				var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+                var secretKey = Configuration.GetSection("botFileSecret")?.Value;
+                var botFilePath = Configuration.GetSection("botFilePath")?.Value;
 
-				// Loads .bot configuration file and adds a singleton that your Bot can access through dependency injection.
-				var botConfig = BotConfiguration.Load(botFilePath ?? @".\BotConfiguration.bot", secretKey);
-				services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot config file could not be loaded. ({botConfig})"));
+                // Loads .bot configuration file and adds a singleton that your Bot can access through dependency injection.
+                var botConfig = BotConfiguration.Load(botFilePath ?? @".\BotConfiguration.bot", secretKey);
+                services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot config file could not be loaded. ({botConfig})"));
 
-				// Retrieve current endpoint.
-				var environment = _isProduction ? "production" : "development";
-				var service = botConfig.Services.Where(s => s.Type == "endpoint" && s.Name == environment).FirstOrDefault();
-				if (!(service is EndpointService endpointService))
-				{
-					throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
-				}
+                // Retrieve current endpoint.
+                var environment = _isProduction ? "production" : "development";
+                var service = botConfig.Services.Where(s => s.Type == "endpoint" && s.Name == environment).FirstOrDefault();
+                if (!(service is EndpointService endpointService))
+                {
+                    throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
+                }
 
                 // Creates a logger for the application to use.
                 ILogger logger = _loggerFactory.CreateLogger<EchoWithCounterBot>();
