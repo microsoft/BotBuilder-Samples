fa6320fc0b36fd73a72e330d61ee3345bc70054d
Steven Gum <steven.gum@gmail.com>
2018-09-20 15:44:39 -0700

    cleanup for 08.suggested-actions

---
+++ samples/javascript_nodejs/08.suggested-actions/bot.js
@@ -0,0 +1,84 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityTypes, MessageFactory } = require('botbuilder');
+
+/**
+ * A bot that responds to input from suggested actions.
+ */
+class SuggestedActionsBot {
+    /**
+     * Every conversation turn for our SuggestedActionsbot will call this method.
+     * There are no dialogs used, since it's "single turn" processing, meaning a single request and
+     * response, with no stateful conversation.
+     * @param {Object} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
+     */
+    async onTurn(turnContext) {
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        if (turnContext.activity.type === ActivityTypes.Message) {
+            const text = turnContext.activity.text;
+
+            // Create an array with the valid color options.
+            const validColors = ['Red', 'Blue', 'Yellow'];
+
+            // If the `text` is in the Array, a valid color was selected and send agreement.
+            if (validColors.includes(text)) {
+                await turnContext.sendActivity(`I agree, ${ text } is the best color.`);
+            } else {
+                await turnContext.sendActivity('Please select a color.');
+            }
+
+            // After the bot has responded send the suggested actions.
+            await this.sendSuggestedActions(turnContext);
+        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            await this.sendWelcomeMessage(turnContext);
+        } else {
+            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected.]`);
+        }
+    }
+
+    /**
+     * Send a welcome message along with suggested actions for the user to click.
+     * @param {Object} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
+     */
+    async sendWelcomeMessage(turnContext) {
+        const that = this;
+
+        // Define a promise that will welcome the user.
+        async function welcomeUser(conversationMember) {
+            // Create welcome message to send to new conversation members.
+            const welcomeMessage = `Welcome to suggestedActionsBot ${ conversationMember.name }. ` +
+            `This bot will introduce you to Suggested Actions. ` +
+            `Please select an option:`;
+
+            // Greet anyone that was not the target (recipient) of this message.
+            // The bot is the recipient of all events from the channel, including all ConversationUpdate-type activities
+            // turnContext.activity.membersAdded !== turnContext.activity.recipient.id indicates
+            // a user was added to the conversation.
+            if (conversationMember.id !== this.activity.recipient.id) {
+                // Because the TurnContext was bound to this function, the bot can call
+                // `TurnContext.sendActivity` via `this.sendActivity`;
+                await this.sendActivity(welcomeMessage);
+                await that.sendSuggestedActions(this);
+            }
+        }
+
+        // If new members are added to the conversation, welcome them via `welcomeUser()`.
+        if (turnContext.activity && turnContext.activity.membersAdded) {
+            // Prepare Promises to greet the users.
+            const replyPromises = turnContext.activity.membersAdded.map(welcomeUser.bind(turnContext));
+            await Promise.all(replyPromises);
+        }
+    }
+
+    /**
+     * Send suggested actions to the user.
+     * @param {Object} turnContext A TurnContext instance containing all the data needed for processing this conversation turn.
+     */
+    async sendSuggestedActions(turnContext) {
+        var reply = MessageFactory.suggestedActions(['Red', 'Yellow', 'Blue'], 'What is the best color?');
+        await turnContext.sendActivity(reply);
+    }
+}
+
+module.exports.SuggestedActionsBot = SuggestedActionsBot;
--- samples/javascript_nodejs/08.suggested-actions/index.js
+++ samples/javascript_nodejs/08.suggested-actions/index.js
@@ -1,42 +1,43 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { BotFrameworkAdapter } = require('botbuilder');
-const { BotConfiguration } = require('botframework-config');
 const path = require('path');
 const restify = require('restify');
-const SuggestedActionsBot = require('./bot');
-
-const CONFIG_ERROR = 1;
+const { BotFrameworkAdapter } = require('botbuilder');
+const { BotConfiguration } = require('botframework-config');
+const { SuggestedActionsBot } = require('./bot');
 
 // Read botFilePath and botFileSecret from .env file.
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '.env');
 const env = require('dotenv').config({ path: ENV_FILE });
 
+const DEV_ENVIRONMENT = 'development';
+
+// Bot name as defined in .bot file.
+// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
+const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
+
 // Create HTTP server.
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${server.name} listening to ${server.url}.`);
+    console.log(`\n${ server.name } listening to ${ server.url }.`);
     console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator.`);
     console.log(`\nTo talk to your bot, open suggested-actions.bot file in the Emulator.`);
 });
 
 // .bot file path.
 const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
-
-// Read bot configuration from .bot file. 
 let botConfig;
 try {
+    // Read bot configuration from .bot file.
     botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
 } catch (err) {
-    console.log(`Error reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
-    process.exit(CONFIG_ERROR);
+    console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
+    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
+    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.\n\n`);
+    process.exit();
 }
-
-// Bot name as defined in .bot file. 
-// See https://aka.ms/about-bot-file to learn more about .bot file usage and configuration.
-const BOT_CONFIGURATION = 'suggested-actions';
 
 // Get bot endpoint configuration by service name.
 const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
@@ -47,12 +48,12 @@ const adapter = new BotFrameworkAdapter({
     appPassword: endpointConfig.appPassword || process.env.MicrosoftAppPassword
 });
 
-// Create the bot that will handle incoming messages.
+// Create the SuggestedActionsBot.
 const suggestedActionsBot = new SuggestedActionsBot();
 
-// Listen for incoming requests. 
+// Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async(context) => {
+    adapter.processActivity(req, res, async (context) => {
         await suggestedActionsBot.onTurn(context);
     });
 });
