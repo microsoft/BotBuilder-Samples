262e4273d0842a8c8e3c41fc2e8a9f61dbbc6aad
Steven Gum <steven.gum@gmail.com>
2018-09-20 09:43:17 -0700

    git status

--- javascript_nodejs/06.using-cards/index.js
+++ javascript_nodejs/06.using-cards/index.js
@@ -1,27 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
- 
+
+const { BotFrameworkAdapter, MemoryStorage, ConversationState } = require('botbuilder');
+const { BotConfiguration } = require('botframework-config');
 const path = require('path');
 const restify = require('restify');
-const RichCardsBot = require('./bot');
+const { RichCardsBot } = require('./bot');
 
 const CONFIG_ERROR = 1;
-
-// Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter, MemoryStorage, ConversationState } = require('botbuilder');
-
-// Import required bot configuration.
-const { BotConfiguration } = require('botframework-config');
 
 // Read botFilePath and botFileSecret from .env file.
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '.env');
-const env = require('dotenv').config({path: ENV_FILE});
+const env = require('dotenv').config({ path: ENV_FILE });
 
 // Create HTTP server.
 let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log(`\n${server.name} listening to ${server.url}.`);
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }.`);
     console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator.`);
     console.log(`\nTo talk to your bot, open using-cards.bot file in the emulator.`);
 });
@@ -29,7 +25,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // .bot file path.
 const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
 
-// Read configuration from .bot file. 
+// Read configuration from .bot file.
 let botConfig;
 try {
     botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
@@ -61,12 +57,12 @@ const memoryStorage = new MemoryStorage();
 // const { CosmosDbStorage } = require('botbuilder-azure');
 // const STORAGE_CONFIGURATION = 'cosmosDB'; // This is the name of the Cosmos DB configuration in your .bot file.
 // const cosmosConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION);
-// const cosmosStorage = new CosmosDbStorage({serviceEndpoint: cosmosConfig.connectionString, 
-//                                            authKey: ?, 
-//                                            databaseId: cosmosConfig.database, 
+// const cosmosStorage = new CosmosDbStorage({serviceEndpoint: cosmosConfig.connectionString,
+//                                            authKey: ?,
+//                                            databaseId: cosmosConfig.database,
 //                                            collectionId: cosmosConfig.collection});
 
-// Create conversation state with the in-memory storage provider. 
+// Create conversation state with the in-memory storage provider.
 const conversationState = new ConversationState(memoryStorage);
 
 // Create a RichCardsBot which responds to a user with Rich Cards.
