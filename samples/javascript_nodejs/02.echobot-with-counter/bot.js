66731c8dc9db1b5f326a0995081ee37aa781bada
Yochay Kiriaty <yochayk@microsoft.com>
2018-09-20 08:46:37 -0700

    update comments and method order

--- samples/javascript_nodejs/02.echobot-with-counter/bot.js
+++ samples/javascript_nodejs/02.echobot-with-counter/bot.js
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// bot.js is your main bot dialog entry point for handilng activity types
+
+// Import required Bot Builder
 const { ActivityTypes } = require('botbuilder');
 
 // Turn counter property
@@ -12,15 +15,20 @@ class EchoBot {
      * @param {ConversationState} conversation state object
      */
     constructor(conversationState) {
-        // creates a new state accessor property.see https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors
+        // Creates a new state accessor property.
+        // See https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors
         this.countProperty = conversationState.createProperty(TURN_COUNTER_PROPERTY);
         this.conversationState = conversationState;
     }
     /**
      *
+     * Use onTuen to handle an incoming activity, received from a user, process it, and reply as needed
+     *
      * @param {TurnContext} on turn context object.
      */
     async onTurn(turnContext) {
+        // Handle Message activity type, which is the main activity type for shown within a conversational interface
+        // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
         // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
         if (turnContext.activity.type === ActivityTypes.Message) {
             // read from state.
@@ -29,7 +37,8 @@ class EchoBot {
             await turnContext.sendActivity(`${ count }: You said "${ turnContext.activity.text }"`);
             // increment and set turn counter.
             await this.countProperty.set(turnContext, count);
-        } else {
+        } else { 
+            // Generic hendler for all other activity types.
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
         // Save state changes
