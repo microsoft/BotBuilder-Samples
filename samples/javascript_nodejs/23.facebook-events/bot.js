8edb8d697f06436ed2e3c4e7b0ebb8948367a70a
Ben Brown <benbro@microsoft.com>
2018-09-20 11:33:35 -0700

    tuneups for the facebook sample

--- samples/javascript_nodejs/23.facebook-events/bot.js
+++ samples/javascript_nodejs/23.facebook-events/bot.js
@@ -16,8 +16,7 @@ class FacebookEventsBot {
      * @param {ConversationState} conversationState A ConversationState object used to store the dialog state.
      * @param {UserState} userState A UserState object used to store values specific to the user.
      */
-    constructor (conversationState, userState) {
-
+    constructor(conversationState, userState) {
         this.conversationState = conversationState;
         this.userState = userState;
 
@@ -47,42 +46,45 @@ class FacebookEventsBot {
                 processFacebookPayload(turnContext.activity.channelData);
 
                 // Initially the bot offers to showcase 3 Facebook features: Quick replies, PostBack and getting the Facebook Page Name.
-                // Below we also show how to get the messaging_optin payload separately as well.
-                switch(text) {
-                    // Here we showcase how to obtain the Facebook page name.
-                    // This can be useful for the Facebook multi-page support provided by the Bot Framework.
-                    // The Facebook page name from which the message comes from is in turnContext.activity.recipient.name.
-                    case facebookPageNameOption:
-                        await turnContext.sendActivity(`This message comes from the following Facebook Page: ${turnContext.activity.recipient.name}`);
-                        break;
-                    case postBackOption:
-                        // Construct a DialogContext instance which is used to resume any 
-                        // existing Dialogs and prompt users.
-                        const dc = await this.dialogs.createContext(turnContext);
+                // Below we also show how to get the messaging_optin payload as well.
+                switch (text) {
+                // Here we showcase how to obtain the Facebook page name.
+                // This can be useful for the Facebook multi-page support provided by the Bot Framework.
+                // The Facebook page name from which the message comes from is in turnContext.activity.recipient.name.
+                case facebookPageNameOption:
+                    await turnContext.sendActivity(`This message comes from the following Facebook Page: ${ turnContext.activity.recipient.name }`);
+                    break;
+                case postBackOption:
+                    // Construct a DialogContext instance which is used to resume any
+                    // existing Dialogs and prompt users.
+                    const dc = await this.dialogs.createContext(turnContext);
 
-                        const results = await dc.continue();
-                        
-                        // Create the PromptOptions which contain the prompt and reprompt messages.
-                        // PromptOptions also contains the list of choices available to the user.
-                        const promptOptions = {
-                            prompt: 'Is 42 the answer to the ultimate question of Life, the Universe, and Everything?',
-                            reprompt: 'Please answer Yes or No.',
-                            choices: [{ value: 'Yes', },{ value: 'No' }]};
+                    await dc.continueDialog();
 
-                        // Prompt the user with the configured PromptOptions.
-                        await dc.prompt(SAMPLE_PROMPT, promptOptions);
-                        break;
+                    // Create the PromptOptions which contain the prompt and reprompt messages.
+                    // PromptOptions also contains the list of choices available to the user.
+                    const promptOptions = {
+                        prompt: 'Is 42 the answer to the ultimate question of Life, the Universe, and Everything?',
+                        reprompt: 'Please answer Yes or No.',
+                        choices: [{ value: 'Yes' }, { value: 'No' }] };
 
-                    case quickRepliesOption:
-                    default:
-                        var reply = MessageFactory.suggestedActions(
-                            [facebookPageNameOption, postBackOption, quickRepliesOption], 
-                            `What Facebook feature would you like to try? Here are some quick replies to choose from!`);
-                        await turnContext.sendActivity(reply);
+                    // Prompt the user with the configured PromptOptions.
+                    await dc.prompt(SAMPLE_PROMPT, promptOptions);
+                    break;
+
+                case quickRepliesOption:
+                default:
+                    var reply = MessageFactory.suggestedActions(
+                        [facebookPageNameOption, postBackOption, quickRepliesOption],
+                        `What Facebook feature would you like to try? Here are some quick replies to choose from!`);
+                    await turnContext.sendActivity(reply);
                 }
+            } else {
+                // This message didn't come from Facebook. Make sure the user knows this bot works on FB only.
+                await turnContext.sendActivity(`I am a Facebook bot. To test me, send me a message with Facebook Messenger.`);
             }
         } else {
-            await turnContext.sendActivity(`[${turnContext.activity.type} event detected.]`);
+            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected.]`);
 
             // Check if we are on the Facebook channel.
             if (turnContext.activity.channelId === 'facebook') {
@@ -102,16 +104,16 @@ class FacebookEventsBot {
  * @param {Object} channelData Channel data for the current turn.
  */
 function processFacebookPayload(channelData) {
-    if(!channelData) {
+    if (!channelData) {
         return;
     }
-    
-    if(channelData.postback) {
-        this.onFacebookPostback(channelData.postback);
+
+    if (channelData.postback) {
+        onFacebookPostback(channelData.postback);
     } else if (channelData.optin) {
-        this.onFacebookOptin(channeldata.optin);
+        onFacebookOptin(channelData.optin);
     } else if (channelData.message && channelData.message.quick_reply) {
-        this.onFacebookQuickReply(channelData.message.quick_reply);
+        onFacebookQuickReply(channelData.message.quick_reply);
     } else {
         // TODO: Handle other events that you're interested in...
     }
@@ -146,4 +148,5 @@ function onFacebookOptin(optin) {
 function onFacebookQuickReply(quickReply) {
     // TODO: Your QuickReply handling logic here...
 }
+
 module.exports = FacebookEventsBot;
