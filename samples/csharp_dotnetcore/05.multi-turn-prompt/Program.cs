ba99668f93bd9b88d7ff5ed818caf7cd55cb1b8b
Jason Sowers <jason.w.sowers@gmail.com>
2018-09-20 21:26:30 -0700

    mirroring echobot

--- samples/csharp_dotnetcore/05.multi-turn-prompt/Program.cs
+++ samples/csharp_dotnetcore/05.multi-turn-prompt/Program.cs
@@ -3,6 +3,7 @@
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -15,6 +16,22 @@ namespace Microsoft.BotBuilderSamples
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    // Add Azure Logging
+                    logging.AddAzureWebAppDiagnostics();
+
+                    // Logging Options.
+                    // There are other logging options available:
+                    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1
+                    // logging.AddDebug();
+                    // logging.AddConsole();
+                })
+
+                // Logging Options.
+                // Consider using Application Insights for your logging and metrics needs.
+                // https://azure.microsoft.com/en-us/services/application-insights/
+                // .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
