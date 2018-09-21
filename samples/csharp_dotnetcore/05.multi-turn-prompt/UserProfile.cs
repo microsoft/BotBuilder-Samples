bbbb01b030dbe949442e6a26fd0a976838cb58e3
Jason Sowers <Jason.w.sowers@gmail.com>
2018-09-21 09:55:41 -0700

    c#/05.multi-turn-prompts (#449)
    * final pass fixes
    
    * folder rename
    
    * added missing convostate
    
    * mirroring echobot
    
    * mirroring echobot
    
    * align with echobot
    
    * Update appsettings.Development.json
    
    * update appsettings

---
+++ samples/csharp_dotnetcore/05.multi-turn-prompt/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// This is our application state. Just a regular serializable .NET class.
+    /// </summary>
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
