a978c0e6f9730ac5b06a11f61cbf4926d72b14a7
vishwacsena <vishwack@hotmail.com>
2018-09-22 15:41:32 -0700

    Dispatch nodejs  (#487)
    * staging.
    
    * cleaned up dispatch. updated all recipes
    
    * clean up bot file.
    
    * updates based on PR feedback.
    
    * updates based on PR feedback

--- samples/javascript_nodejs/14.nlp-with-dispatch/homeAutomation.js
+++ samples/javascript_nodejs/14.nlp-with-dispatch/homeAutomation.js
@@ -6,7 +6,7 @@ const { LuisRecognizer } = require('botbuilder-ai');
 const HOME_AUTOMATION_INTENT = 'HomeAutomation';
 const NONE_INTENT = 'None';
 
-// LUIS entity names. 
+// LUIS entity names.
 const DEVICE_PROPERTY_ENTITY = 'deviceProperty';
 const NUMBER_ENTITY = 'number';
 const ROOM_ENTITY = 'Room';
@@ -15,30 +15,30 @@ const DEVICE_ENTITY = 'Device';
 const DEVICE_PATTERNANY_ENTITY = 'Device_PatternAny';
 const ROOM_PATTERNANY_ENTITY = 'Room_PatternAny';
 
-// STATE
-const HomeAutomationState = require('./home-automation-state');
+// State
+const { HomeAutomationState } = require('./home-automation-state');
 
 // this is the LUIS service type entry in the .bot file.
-const LUIS_CONFIGURATION = 'homeautomation.luis';
+const LUIS_CONFIGURATION = 'Home Automation';
 
-class homeAutomation {
+class HomeAutomation {
     /**
-     * 
-     * @param {Object} convoState conversation state
-     * @param {Object} userState user state
-     * @param {Object} botConfig bot configuration from .bot file
+     *
+     * @param {ConversationState} convoState conversation state
+     * @param {UserState} userState user state
+     * @param {BotConfiguration} botConfig bot configuration from .bot file
      */
     constructor(convoState, userState, botConfig) {
-        if(!convoState) throw ('Need converstaion state');
-        if(!userState) throw ('Need user state');
-        if(!botConfig) throw ('Need bot config');
+        if (!convoState) throw new Error('Need converstaion state');
+        if (!userState) throw new Error('Need user state');
+        if (!botConfig) throw new Error('Need bot config');
 
         // home automation state
         this.state = new HomeAutomationState(convoState, userState);
-        
+
         // add recogizers
         const luisConfig = botConfig.findServiceByNameOrId(LUIS_CONFIGURATION);
-        if(!luisConfig || !luisConfig.appId) throw (`Home automation LUIS model not found in .bot file. Please ensure you have all required LUIS models created and available in the .bot file. See readme.md for additional information\n`);
+        if (!luisConfig || !luisConfig.appId) throw new Error(`Home automation LUIS model not found in .bot file. Please ensure you have all required LUIS models created and available in the .bot file. See readme.md for additional information\n`);
         this.luisRecognizer = new LuisRecognizer({
             applicationId: luisConfig.appId,
             azureRegion: luisConfig.region,
@@ -47,45 +47,45 @@ class homeAutomation {
         });
     }
     /**
-     * 
-     * @param {Object} context context object
+     *
+     * @param {TurnContext} turn context object
      */
-    async onTurn(context) {
+    async onTurn(turnContext) {
         // make call to LUIS recognizer to get home automation intent + entities
-        const homeAutoResults = await this.luisRecognizer.recognize(context);
+        const homeAutoResults = await this.luisRecognizer.recognize(turnContext);
         const topHomeAutoIntent = LuisRecognizer.topIntent(homeAutoResults);
-        
+
         // depending on intent, call turn on or turn off or return unknown
-        switch(topHomeAutoIntent) {
-            case HOME_AUTOMATION_INTENT: 
-                await this.handleDeviceUpdate(homeAutoResults, context);
-                break;
-            case NONE_INTENT:
-            default:
-                await context.sendActivity(`HomeAutomation dialog cannot fulfill this request.`);
+        switch (topHomeAutoIntent) {
+        case HOME_AUTOMATION_INTENT:
+            await this.handleDeviceUpdate(homeAutoResults, turnContext);
+            break;
+        case NONE_INTENT:
+        default:
+            await turnContext.sendActivity(`HomeAutomation dialog cannot fulfill this request.`);
         }
     }
     /**
-     * 
-     * @param {Object} homeAutoResults results from LUIS recognizer
-     * @param {Object} context context object
+     *
+     * @param {RecognizerResults} homeAutoResults results from LUIS recognizer
+     * @param {TurnContext} context context object
      */
     async handleDeviceUpdate(homeAutoResults, context) {
         // Find entities from the LUIS model
-        // The LUIS language understanding model is set up with both machine learned simple entities as well as pattern.any entities. 
+        // The LUIS language understanding model is set up with both machine learned simple entities as well as pattern.any entities.
         // Check to see if either one has value. In cases where both have value, take the machine learned simple entities value.
         const devices = findEntities(DEVICE_ENTITY, homeAutoResults.entities);
-        const devices_patternAny = findEntities(DEVICE_PATTERNANY_ENTITY, homeAutoResults.entities);
+        const devicesPatternAny = findEntities(DEVICE_PATTERNANY_ENTITY, homeAutoResults.entities);
         // Find any rooms specified in the user utterance. Look for both the machine learned simple entity as well as pattern.any entity
         const rooms = findEntities(ROOM_ENTITY, homeAutoResults.entities);
-        const rooms_patternAny = findEntities(ROOM_PATTERNANY_ENTITY, homeAutoResults.entities);
+        const roomsPatternAny = findEntities(ROOM_PATTERNANY_ENTITY, homeAutoResults.entities);
         // Find any entity that indicates the requested operation. E.g. Turn on/ Turn off.
         const operations = findEntities(OPERATION_ENTITY, homeAutoResults.entities);
-        // Find any additional device properties specified. 
+        // Find any additional device properties specified.
         const deviceProperties = findEntities(DEVICE_PROPERTY_ENTITY, homeAutoResults.entities);
         const numberProperties = findEntities(NUMBER_ENTITY, homeAutoResults.entities);
         // Update device state.
-        await this.state.setDevice((devices || devices_patternAny), (rooms || rooms_patternAny), operations[0], (deviceProperties || numberProperties), context);
+        await this.state.setDevice((devices || devicesPatternAny), (rooms || roomsPatternAny), operations[0], (deviceProperties || numberProperties), context);
         await context.sendActivity(`You reached the "HomeAutomation" dialog.`);
         await context.sendActivity(`Here's the current snapshot of your prior operations`);
         // read out operations list
@@ -93,11 +93,11 @@ class homeAutomation {
     }
 };
 
-module.exports = homeAutomation;
+module.exports.HomeAutomation = HomeAutomation;
 
 // Helper function to retrieve specific entities from LUIS results
 function findEntities(entityName, entityResults) {
-    let entities = []
+    let entities = [];
     if (entityName in entityResults) {
         entityResults[entityName].forEach(entity => {
             entities.push(entity);
--- samples/javascript_nodejs/14.nlp-with-dispatch/weather.js
+++ samples/javascript_nodejs/14.nlp-with-dispatch/weather.js
@@ -7,24 +7,24 @@ const GET_CONDITION_INTENT = 'Get_Weather_Condition';
 const GET_FORECAST_INTENT = 'Get_Weather_Forecast';
 const NONE_INTENT = 'None';
 
-// LUIS entity names. 
+// LUIS entity names.
 const LOCATION_ENTITY = 'Location';
 const LOCATION_PATTERNANY_ENTITY = 'Location_PatternAny';
 
 // this is the LUIS service type entry in the .bot file.
-const WEATHER_LUIS_CONFIGURATION = 'weather.luis';
+const WEATHER_LUIS_CONFIGURATION = 'Weather';
 
 class Weather {
     /**
-     * 
-     * @param {Object} botConfig bot configuration from .bot file
+     *
+     * @param {BotConfiguration} bot configuration from .bot file
      */
     constructor(botConfig) {
-        if(!botConfig) throw ('Need bot config');
-        
+        if (!botConfig) throw new Error('Need bot config');
+
         // add recogizers
         const luisConfig = botConfig.findServiceByNameOrId(WEATHER_LUIS_CONFIGURATION);
-        if(!luisConfig || !luisConfig.appId) throw (`Weather LUIS model not found in .bot file. Please ensure you have all required LUIS models created and available in the .bot file. See readme.md for additional information\n`);
+        if (!luisConfig || !luisConfig.appId) throw new Error(`Weather LUIS model not found in .bot file. Please ensure you have all required LUIS models created and available in the .bot file. See readme.md for additional information\n`);
         this.luisRecognizer = new LuisRecognizer({
             applicationId: luisConfig.appId,
             azureRegion: luisConfig.region,
@@ -33,29 +33,29 @@ class Weather {
         });
     }
     /**
-     * 
-     * @param {Object} context context object
+     *
+     * @param {TurnContext} turn context object
      */
-    async onTurn(context) {
+    async onTurn(turnContext) {
         // Call weather LUIS model.
-        const weatherResults = await this.luisRecognizer.recognize(context);
+        const weatherResults = await this.luisRecognizer.recognize(turnContext);
         const topWeatherIntent = LuisRecognizer.topIntent(weatherResults);
         // Get location entity if available.
         const locationEntity = (LOCATION_ENTITY in weatherResults.entities) ? weatherResults.entities[LOCATION_ENTITY][0] : undefined;
         const locationPatternAnyEntity = (LOCATION_PATTERNANY_ENTITY in weatherResults.entities) ? weatherResults.entities[LOCATION_PATTERNANY_ENTITY][0] : undefined;
         // Depending on intent, call "Turn On" or "Turn Off" or return unknown.
-        switch(topWeatherIntent) {
-            case GET_CONDITION_INTENT: 
-                await context.sendActivity(`You asked for current weather condition in Location = ` + (locationEntity || locationPatternAnyEntity));
-                break;
-            case GET_FORECAST_INTENT: 
-                await context.sendActivity(`You asked for weather forecast in Location = ` + (locationEntity || locationPatternAnyEntity));
-                break;
-            case NONE_INTENT:
-            default:
-                await context.sendActivity(`Weather dialog cannot fulfill this request.`);
+        switch (topWeatherIntent) {
+        case GET_CONDITION_INTENT:
+            await turnContext.sendActivity(`You asked for current weather condition in Location = ` + (locationEntity || locationPatternAnyEntity));
+            break;
+        case GET_FORECAST_INTENT:
+            await turnContext.sendActivity(`You asked for weather forecast in Location = ` + (locationEntity || locationPatternAnyEntity));
+            break;
+        case NONE_INTENT:
+        default:
+            await turnContext.sendActivity(`Weather dialog cannot fulfill this request.`);
         }
     }
 };
 
-module.exports = Weather;
+module.exports.Weather = Weather;
