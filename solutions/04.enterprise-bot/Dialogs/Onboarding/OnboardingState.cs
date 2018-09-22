00c33c8538bf8c0da784654932cb72b25cb3dcb0
Dave Taniguchi <daveta@microsoft.com>
2018-09-22 16:37:38 -0700

    Final Check Enterprise Bot

---
+++ solutions/04.enterprise-bot/Dialogs/Onboarding/OnboardingState.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace EnterpriseBot
+{
+    public class OnboardingState
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Location { get; set; }
+    }
+}
---
+++ solutions/04.enterprise-bot/Middleware/RegexRecognizer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace EnterpriseBot
+{
+    public class RegexRecognizer : IRecognizer
+    {
+        private Dictionary<Regex, string> _map;
+
+        public RegexRecognizer(Dictionary<Regex, string> map)
+        {
+            _map = map;
+        }
+
+        public async Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+            => await RecognizeInternalAsync(turnContext, cancellationToken);
+
+        public async Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken)
+            where T : IRecognizerConvert, new()
+        {
+            var result = new T();
+            result.Convert(await RecognizeInternalAsync(turnContext, cancellationToken));
+            return result;
+        }
+
+        private Task<RecognizerResult> RecognizeInternalAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var text = turnContext.Activity.Text;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+            else
+            {
+                foreach (var regexMap in _map)
+                {
+                    var match = regexMap.Key.Match(text);
+
+                    if (match.Success)
+                    {
+                        var score = new IntentScore() { Score = 1.0 };
+                        var intents = new Dictionary<string, IntentScore> { { regexMap.Value, score } };
+
+                        return Task.FromResult(new RecognizerResult()
+                        {
+                            Text = text,
+                            Intents = intents,
+                        });
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
