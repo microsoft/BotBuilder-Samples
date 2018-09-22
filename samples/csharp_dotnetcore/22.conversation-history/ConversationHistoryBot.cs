005b13bbf2090dc27e4c2abb2e67d222326cec1f
Jason Sowers <jason.w.sowers@gmail.com>
2018-09-22 16:46:11 -0700

    final pass updates

--- samples/csharp_dotnetcore/22.conversation-history/ConversationHistoryBot.cs
+++ samples/csharp_dotnetcore/22.conversation-history/ConversationHistoryBot.cs
@@ -1,15 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
-using Microsoft.Bot.Builder.Azure;
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -84,8 +83,9 @@ namespace Microsoft.BotBuilderSamples
                             throw new InvalidOperationException("Attempt to upload too many activities");
                         }
 
-                        var transcript = new Bot.Schema.Transcript(activities);
-                        await connectorClient.Conversations.SendConversationHistoryAsync(activity.Conversation.Id, transcript);
+                        var transcript = new Transcript(activities);
+
+                        await connectorClient.Conversations.SendConversationHistoryAsync(activity.Conversation.Id, transcript, cancellationToken: cancellationToken);
 
                         continuationToken = pagedTranscript.ContinuationToken;
                     }
@@ -94,7 +94,7 @@ namespace Microsoft.BotBuilderSamples
                 else
                 {
                     // Echo back to the user whatever they typed.
-                    await turnContext.SendActivityAsync($"You sent '{activity.Text}'\n");
+                    await turnContext.SendActivityAsync($"You sent '{activity.Text}'\n", cancellationToken: cancellationToken);
                 }
             }
         }
