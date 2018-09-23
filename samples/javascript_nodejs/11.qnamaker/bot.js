e3f02061eb99b372e482fe2141aab602e1f06b7d
Steven Gum <steven.gum@gmail.com>
2018-09-23 11:35:49 -0700

    move Node.js 11.qnamaker sample, clean up eslint errors

--- samples/javascript_nodejs/11.qnamaker/bot.js
+++ samples/javascript_nodejs/11.qnamaker/bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityTypes } = require('botbuilder');
+const { ActivityTypes, TurnContext } = require('botbuilder');
 const { QnAMaker, QnAMakerEndpoint, QnAMakerOptions } = require('botbuilder-ai');
 
 /**
@@ -9,7 +9,6 @@ const { QnAMaker, QnAMakerEndpoint, QnAMakerOptions } = require('botbuilder-ai')
  * If an answer is not found for an utterance, the bot responds with help.
  */
 class QnAMakerBot {
-    
     /**
      * The QnAMakerBot constructor requires one argument (`endpoint`) which is used to create an instance of `QnAMaker`.
      * @param {QnAMakerEndpoint} endpoint The basic configuration needed to call QnA Maker. In this sample the configuration is retrieved from the .bot file.
@@ -23,7 +22,7 @@ class QnAMakerBot {
      * Every conversation turn for our QnAMakerBot will call this method.
      * There are no dialogs used, since it's "single turn" processing, meaning a single request and
      * response, with no stateful conversation.
-     * @param turnContext A TurnContext instance, containing all the data needed for processing the conversation turn.
+     * @param {TurnContext} turnContext A TurnContext instance, containing all the data needed for processing the conversation turn.
      */
     async onTurn(turnContext) {
         // By checking the incoming Activity type, the bot only calls QnA Maker in appropriate cases.
@@ -34,7 +33,7 @@ class QnAMakerBot {
             // If an answer was received from QnA Maker, send the answer back to the user.
             if (qnaResults[0]) {
                 await turnContext.sendActivity(qnaResults[0].answer);
-            
+
             // If no answers were returned from QnA Maker, reply with help.
             } else {
                 await turnContext.sendActivity('No QnA Maker answers were found. This example uses a QnA Maker Knowledge Base that focuses on smart light bulbs. To see QnA Maker in action, ask the bot questions like "Why won\'t it turn on?" or "I need help."');
@@ -44,12 +43,12 @@ class QnAMakerBot {
         } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate &&
                    turnContext.activity.recipient.id !== turnContext.activity.membersAdded[0].id) {
             await turnContext.sendActivity('Welcome to the QnA Maker sample! Ask me a question and I will try to answer it.');
-        
+
         // Respond to all other Activity types.
         } else if (turnContext.activity.type !== ActivityTypes.ConversationUpdate) {
-            await turnContext.sendActivity(`[${turnContext.activity.type}]-type activity detected.`);
+            await turnContext.sendActivity(`[${ turnContext.activity.type }]-type activity detected.`);
         }
     }
 }
 
-module.exports = QnAMakerBot;
+module.exports.QnAMakerBot = QnAMakerBot;
