fc42bb9624aad1cf083c396dbc4dd88454faf95c
Enzo Cano <enzo.cano@southworks.com>
2018-09-24 15:20:43 -0300

    Add Enterprise Bot Template
    Squashed commit:
    
    [37dacb50] Fix typo
    
    [f3ac88d9] Review documentation
    
    [3dc4545e] Add (c) header
    
    [5a377e62] Delete unused classes
    
    [74de8edf] Formatting code
    
    [85835639] Build before start bot
    
    [6d61c025] Change string keys to env.vars
    
    [01a77427] Add bot section to bot.recipe for successful msbot clone services command
    
    [f3ba106c] Add env.development vars
    
    [a35f20c7] Fix LUIS appName
    
    [48464a53] Fix issue with LUIS top intent
    
    [e18ddd86] Migrate documentation and apply minor fixes
    
    [c6677bb0] Various fixes
    
    [1b42ae5a] Padding tsconfig
    
    [dcd46bb7]  Fix resources parsing
    
    [9d8c6f52] Fix propAccessor initialization
    
    [35752d78] Add .map files for easy debug
    
    [11970588] Use single comma
    
    [67fb82a4] Fix resources name
    
    [8c832528] Delete unused resources
    
    [7a8c2807] Add clean and copy-resources scripts
    
    [af5b051a] Implement onboardingDialog.ts and onboardingResponses.ts
    
    [2fdf37e9] Enable use of 'CancelResponses' in 'enterpriseDialog'
    
    [59c40c5f] Fix wrong parameter in 'signInResponses'
    
    [6ca779a8] implemented escalateDialog.ts
    
    [0b06789e] implemented escalateState.ts
    
    [b27c8608] Fix MainDialog properties
    
    [e19c4001] implemented escalateResponses.ts
    
    [65c8994a] implemented cancelState.ts + minor typo fix
    
    [45d7df45] Implement EnterpriseBotAccessors
    
    [04ff4245] Implement cancelResponses.ts
    
    [ed48b4c0] Implement GraphClient
    
    [1bbf84fd] Add SignInDialog
    
    [711d5715] Reorganize imports and remove unused imports
    
    [090d842d] Fix mainDialog
    
    [2acdb8ef] Add files for 'dialogs/shared/'
    Add the following files to 'dialogs/shared/':
    - interruptableStatus.ts
    - interruptableDialog.ts
    - enterpriseDialog.ts
    
    [34b2d2ed] Add auto-generated code from LuisGen
    
    [67d6cfbd] Implement mainResponses
    
    [cb07ec2d] Add ActivityEx.createReply
    
    [6308cb4c] Add resourceParser
    
    [9fdc1e7f] Add enterpriseBot
    
    [c3bce667] Fix telemetry send messages
    
    [0cac042d] Add contentModerator middleware
    
    [74a27984] Add luis and qna telemetry
    
    [0cc5ec80] Add TelemetryLoggerMiddleware implementation
    
    [cbe8ccac] Rebase with master

---
+++ samples/javascript_typescript/52.enterprise-bot/src/dialogs/main/mainResponses.ts
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+
+import { TemplateManager } from '../templateManager/templateManager';
+import { LanguageTemplateDictionary, DictionaryRenderer, TemplateFunction } from '../templateManager/dictionaryRenderer';
+import { TurnContext, Activity, CardFactory, ActionTypes } from 'botbuilder';
+import { ResourceParser } from '../shared/resourceParser';
+import { ActivityEx } from '../../utils/activityEx';
+
+const introCard = require('./resources/Intro.json');
+const resourcesPath = require.resolve('./resources/MainStrings.resx');
+
+export class MainResponses extends TemplateManager {
+    // Constants
+    public static readonly Cancelled: string = 'cancelled';
+    public static readonly Completed: string = 'completed';
+    public static readonly Confused: string = 'confused';
+    public static readonly Greeting: string = 'greeting';
+    public static readonly Help: string = 'help';
+    public static readonly Intro: string = 'intro';
+
+    private static readonly resources: ResourceParser = new ResourceParser(resourcesPath);
+
+    private static fromResources(name: string): TemplateFunction {
+        return (context: TurnContext, data: any) => MainResponses.resources.get(name);
+    }
+
+    private static readonly _responseTemplates: LanguageTemplateDictionary = new Map([
+        ['default', new Map([
+            [MainResponses.Cancelled, MainResponses.fromResources('CANCELLED')],
+            [MainResponses.Completed, MainResponses.fromResources('COMPLETED')],
+            [MainResponses.Confused, MainResponses.fromResources('CONFUSED')],
+            [MainResponses.Greeting, MainResponses.fromResources('GREETING')],
+            [MainResponses.Help, (context: TurnContext, data: any) => MainResponses.sendHelpCard(context, data)],
+            [MainResponses.Intro, (context: TurnContext, data: any) => MainResponses.sendIntroCard(context, data)]
+        ])],
+        ['en', undefined],
+        ['fr', undefined]
+    ]);
+
+    constructor() {
+        super();
+        this.register(new DictionaryRenderer(MainResponses._responseTemplates));
+    }
+
+    public static sendIntroCard(turnContext: TurnContext, data: any): Promise<Activity> {
+        const response = ActivityEx.createReply(turnContext.activity);
+
+        response.attachments = [{
+            contentType: 'application/vnd.microsoft.card.adaptive',
+            content: introCard
+        }];
+
+        return Promise.resolve(response);
+    }
+
+    public static async sendHelpCard(turnContext: TurnContext, data: any): Promise<Activity> {
+        const response = ActivityEx.createReply(turnContext.activity);
+        const title = await this.resources.get('HELP_TITLE');
+        const text = await this.resources.get('HELP_TEXT');
+
+        response.attachments = [CardFactory.heroCard(
+            title,
+            text,
+            undefined,
+            [
+                {
+                    type: ActionTypes.ImBack,
+                    title: 'Test LUIS',
+                    value: 'hello'
+                },
+                {
+                    type: ActionTypes.ImBack,
+                    title: 'Test QnAMaker',
+                    value: 'What is the sdk v4?'
+                },
+                {
+                    type: ActionTypes.OpenUrl,
+                    title: 'Learn More',
+                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/?view=azure-bot-service-4.0'
+                }
+            ]
+        )];
+
+        return Promise.resolve(response);
+    }
+}
---
+++ samples/javascript_typescript/52.enterprise-bot/src/dialogs/onboarding/onboardingState.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License
+
+export interface OnboardingState {
+    name: string;
+    email: string;
+    location: string;
+}
