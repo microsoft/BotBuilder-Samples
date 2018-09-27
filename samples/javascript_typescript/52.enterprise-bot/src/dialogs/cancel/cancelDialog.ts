65c8994a3a46f6093e05c8119f12f2a1eeeb37f4
JuanAr <juan.arguello@southworks.com>
2018-09-27 17:20:30 -0300

    implemented cancelState.ts + minor typo fix

--- samples/javascript_typescript/52.enterprise-bot/src/dialogs/cancel/cancelDialog.ts
+++ samples/javascript_typescript/52.enterprise-bot/src/dialogs/cancel/cancelDialog.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.import { CancelResponses } from './cancelResponeses';
+// Licensed under the MIT License
 
 import { CancelResponses } from './cancelResponses'
 import { ComponentDialog, DialogContext, WaterfallStepContext, DialogTurnResult, PromptOptions, WaterfallDialog, ConfirmPrompt } from 'botbuilder-dialogs';
