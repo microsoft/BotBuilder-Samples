ab4d018c3614ed0dbcc94806c15d2b242c982cd5
John Taylor <johtaylo@microsoft.com>
2018-10-04 16:19:42 -0700

    new ASP MVC sample

---
+++ samples/csharp_dotnetcore/30.asp-mvc-bot/Asp-Mvc-Bot/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using Asp_Mvc_Bot.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_Mvc_Bot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ samples/csharp_dotnetcore/30.asp-mvc-bot/Asp-Mvc-Bot/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Asp_Mvc_Bot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
