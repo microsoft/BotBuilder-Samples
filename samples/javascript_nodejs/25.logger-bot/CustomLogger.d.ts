6bf974a023d14d4fdabdccad2886109370828d44
nyxsys <isaprettycoolguy@gmail.com>
2018-10-12 16:06:56 -0700

    Address comments from PR

--- samples/javascript_nodejs/25.logger-bot/CustomLogger.d.ts
+++ samples/javascript_nodejs/25.logger-bot/CustomLogger.d.ts
@@ -1,6 +1,6 @@
-/**
- * Licensed under the MIT License.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { TranscriptLogger } from 'botframework-core';
 
 /*
