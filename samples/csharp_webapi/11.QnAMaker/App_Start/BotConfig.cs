ab56fa8d6809fea62a2f2d85675004f3d70480fd
Joe <quesjose1@gmail.com>
2018-10-16 12:15:20 -0300

    Minor typos fixes

--- samples/csharp_webapi/11.QnAMaker/App_Start/BotConfig.cs
+++ samples/csharp_webapi/11.QnAMaker/App_Start/BotConfig.cs
@@ -15,7 +15,7 @@ namespace QnABot
     public class BotConfig
     {
         /// <summary>
-        /// Register the bot framwork with Asp.net.
+        /// Register the bot framework with Asp.net.
         /// </summary>
         /// <param name="config">Represents the configuration of the HttpServer.</param>
         public static void Register(HttpConfiguration config)
