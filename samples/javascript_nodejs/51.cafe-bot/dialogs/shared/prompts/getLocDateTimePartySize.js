bf3a933aec3b14e8dcb44bf13f70a18b660670fe
Joe <jose.ques@southworks.com>
2018-10-19 09:47:28 -0300

    Revert change for mistaken typo correction

--- samples/javascript_nodejs/51.cafe-bot/dialogs/shared/prompts/getLocDateTimePartySize.js
+++ samples/javascript_nodejs/51.cafe-bot/dialogs/shared/prompts/getLocDateTimePartySize.js
@@ -205,7 +205,7 @@ module.exports = {
             }
         }
         /**
-         * Over ride re-prompt Dialog.
+         * Over ride reprompt Dialog.
          * This is useful to ensure the right re-prompt text is set before re-prompting.
          * This method is called anytime a re-prompt is initiated.
          * @param {TurnContext} turn context
