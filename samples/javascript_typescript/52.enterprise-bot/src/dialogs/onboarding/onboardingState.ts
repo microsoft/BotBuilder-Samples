5858bd9beff4f22f810673d38c66ae150e97aab9
Lauren Mills <lamil@microsoft.com>
2018-10-25 15:16:15 -0700

    removed enterprise bot projects from samples

--- samples/javascript_typescript/52.enterprise-bot/src/dialogs/onboarding/onboardingState.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License
-
-export interface OnboardingState {
-    name: string;
-    email: string;
-    location: string;
-}
