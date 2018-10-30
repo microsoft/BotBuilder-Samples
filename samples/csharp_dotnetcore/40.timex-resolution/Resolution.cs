929dffc9312bea07143b552b2c5dcb881912ee95
John Taylor <johtaylo@microsoft.com>
2018-10-30 15:55:04 -0700

    TimexExpression sample

---
+++ samples/csharp_dotnetcore/40.timex-resolution/Resolution.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// Given the TIMEX expressions it is easy to create the computed example values that the recognizer gives.
+    /// </summary>
+    public static class Resolution
+    {
+        public static void Examples()
+        {
+            // When you give the recognzier the text "Wednesday 4 o'clock" you get these distinct TIMEX values back.
+
+            var today = DateTime.Now;
+            var resolution = TimexResolver.Resolve(new[] { "XXXX-WXX-3T04", "XXXX-WXX-3T16" }, today);
+
+            Console.WriteLine(resolution.Values.Count);
+
+            Console.WriteLine(resolution.Values[0].Timex);
+            Console.WriteLine(resolution.Values[0].Type);
+            Console.WriteLine(resolution.Values[0].Value);
+
+            Console.WriteLine(resolution.Values[1].Timex);
+            Console.WriteLine(resolution.Values[1].Type);
+            Console.WriteLine(resolution.Values[1].Value);
+
+            Console.WriteLine(resolution.Values[2].Timex);
+            Console.WriteLine(resolution.Values[2].Type);
+            Console.WriteLine(resolution.Values[2].Value);
+
+            Console.WriteLine(resolution.Values[3].Timex);
+            Console.WriteLine(resolution.Values[3].Type);
+            Console.WriteLine(resolution.Values[3].Value);
+        }
+    }
+}
