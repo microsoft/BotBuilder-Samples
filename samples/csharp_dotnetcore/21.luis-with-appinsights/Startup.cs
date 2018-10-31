351c4747a32b2e40c1fe2db6c73203720dea3e2b
mbarb <isaprettycoolguy@gmail.com>
2018-10-30 17:36:48 -0700

    Add fileExists check to all dotnet samples (#805)
    * Add fileExists check to all dotnet samples
    
    * Fixed missing System.IO references to File Class
    
    File checks were causing build errors due to a missing reference - this is now fixed.

--- samples/csharp_dotnetcore/21.luis-with-appinsights/Startup.cs
+++ samples/csharp_dotnetcore/21.luis-with-appinsights/Startup.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
@@ -51,6 +52,10 @@ namespace Microsoft.BotBuilderSamples
         {
             var secretKey = Configuration.GetSection("botFileSecret")?.Value;
             var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+            if (!File.Exists(botFilePath))
+            {
+                throw new FileNotFoundException($"The .bot configuration file was not found. botFilePath: {botFilePath}");
+            }
 
             // Loads .bot configuration file and adds a singleton that your Bot can access through dependency injection.
             var botConfig = BotConfiguration.Load(botFilePath ?? @".\<YOUR BOT CONFIGURATION>.bot", secretKey);
