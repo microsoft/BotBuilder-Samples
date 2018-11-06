f2e7a020f2d356e0561bca8976d0fe55a6527897
AhmedMagdyG <33265289+AhmedMagdyG@users.noreply.github.com>
2018-11-06 03:19:17 +0200

    Add Multilingual LUIS Bot Sample (#544)

---
+++ samples/csharp_dotnetcore/60.multilinugal-luis-bot/Libraries/Microsoft.Bot.Builder.AI.Translation/Model/TranslatorRequestModel.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Builder.AI.Translation.Model
+{
+    /// <summary>
+    /// Model for translator request to Translator API v3.
+    /// </summary>
+    internal class TranslatorRequestModel
+    {
+        public string Text { get; set; }
+    }
+}
