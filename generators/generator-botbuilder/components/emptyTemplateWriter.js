5873a5c0e4f360a564c769a4eb26d113decb02fe
Scott Gellock <sgellock@gmail.com>
2018-11-28 10:41:57 -0800

    Fixes:  987 Yeoman Empty Bot has numerous Echobot references
    - remove unused code
    - update references to Empty from Echo

--- generators/generator-botbuilder/components/emptyTemplateWriter.js
+++ generators/generator-botbuilder/components/emptyTemplateWriter.js
@@ -18,37 +18,14 @@ const GENERATOR_TEMPLATE_NAME = 'empty';
  * @param {String} templatePath file path to write the generated code
  */
 const writeEmptyTemplateFiles = (gen, templatePath) => {
-  // const DEPLOYMENT_SCRIPTS = 0;
-  // const DEPLOYMENT_MSBOT = 1;
-  // const RESOURCES = 2;
   const TS_SRC_FOLDER = 'src'
-  // const folders = [
-  //   'deploymentScripts',
-  //   path.join('deploymentScripts', 'msbotClone'),
-  //   'resources'
-  // ];
   const extension = _.toLower(gen.props.language) === 'javascript' ? 'js' : 'ts';
   const srcFolder = _.toLower(gen.props.language) === 'javascript' ? '' : TS_SRC_FOLDER;
 
-  // create the echo bot folder structure common to both languages
-  // for (let cnt = 0; cnt < folders.length; ++cnt) {
-  //   mkdirp.sync(folders[cnt]);
-  // }
   // create a src directory if we are generating TypeScript
   if (_.toLower(gen.props.language) === 'typescript') {
     mkdirp.sync(TS_SRC_FOLDER);
   }
-
-  // write out the deployment scripts
-  // let sourcePath = path.join(templatePath, folders[DEPLOYMENT_MSBOT]);
-  // let destinationPath = path.join(gen.destinationPath(), folders[DEPLOYMENT_MSBOT]);
-  // // gen.fs.copyTpl(
-  //   path.join(sourcePath, 'bot.recipe'),
-  //   path.join(destinationPath, 'bot.recipe'),
-  //   {
-  //     botName: gen.props.botName
-  //   }
-  // );
 
   // write out the index.js and bot.js
   let destinationPath = path.join(gen.destinationPath(), srcFolder);
@@ -66,18 +43,10 @@ const writeEmptyTemplateFiles = (gen, templatePath) => {
     gen.templatePath(path.join(templatePath, `bot.${extension}`)),
     path.join(destinationPath, `bot.${extension}`)
   );
-
-  // // write out the  AI resource(s)
-  // sourcePath = path.join(templatePath, folders[RESOURCES]);
-  // destinationPath = path.join(gen.destinationPath(), folders[RESOURCES]);
-  // gen.fs.copy(
-  //   path.join(sourcePath, `echo.chat`),
-  //   path.join(destinationPath, `echo.chat`)
-  // );
 }
 
 /**
- * Write project files for Echo template
+ * Write project files for Empty template
  *
  * @param {Generator} gen Yeoman's generator object
  */
@@ -89,13 +58,13 @@ module.exports.emptyTemplateWriter = gen => {
     throw new Error(`basicTemplateWriter called for wrong template: ${ gen.props.template }`);
   }
 
-  // build the path to the echo template source folder
+  // build the path to the empty template source folder
   const templatePath = path.join(gen.templatePath(), GENERATOR_TEMPLATE_NAME);
 
   // write files common to all our template options
   commonFilesWriter(gen, templatePath);
 
-  // write files specific to the echo bot template
+  // write files specific to the empty bot template
   writeEmptyTemplateFiles(gen, templatePath);
 }
 
