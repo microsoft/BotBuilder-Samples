57011691fc4a62ecccc1b64235125fefce4aeb93
Ben Brown <benbro@microsoft.com>
2018-11-30 11:05:39 -0600

    remove unnecessary imports

--- samples/javascript_nodejs/20.qna-with-appinsights/middleware/myAppInsightsMiddleware.js
+++ samples/javascript_nodejs/20.qna-with-appinsights/middleware/myAppInsightsMiddleware.js
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { Activity, TurnContext } = require('botbuilder');
-
 /**
  * Middleware for logging incoming activities into Application Insights.
  * In addition, registers a service so other components can log telemetry.
--- samples/javascript_nodejs/20.qna-with-appinsights/myAppInsightsQnAMaker.js
+++ samples/javascript_nodejs/20.qna-with-appinsights/myAppInsightsQnAMaker.js
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { TurnContext } = require('botbuilder');
 const { QnAMaker } = require('botbuilder-ai');
 
 /**
