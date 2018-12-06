56dd747f65136e5409a5553b0efc2accb5d20926
Dave Taniguchi <daveta@microsoft.com>
2018-12-06 07:16:09 -0800

    Keep casing inline with Solutions

--- samples/csharp_dotnetcore/21.luis-with-appinsights/Middleware/Telemetry/LuisTelemetryConstants.cs
+++ samples/csharp_dotnetcore/21.luis-with-appinsights/Middleware/Telemetry/LuisTelemetryConstants.cs
@@ -10,11 +10,11 @@ namespace Microsoft.BotBuilderSamples
     {
         public const string IntentPrefix = "LuisIntent";  // Application Insights Custom Event name (with Intent)
 
-        public const string IntentProperty = "intent";
-        public const string IntentScoreProperty = "intent_Score";
-        public const string QuestionProperty = "question";
-        public const string SentimentLabelProperty = "sentiment_Label";
-        public const string SentimentScoreProperty = "sentiment_Score";
-        public const string DialogId = "dialog_Id";
+        public const string IntentProperty = "Intent";
+        public const string IntentScoreProperty = "IntentScore";
+        public const string QuestionProperty = "Question";
+        public const string SentimentLabelProperty = "SentimentLabel";
+        public const string SentimentScoreProperty = "SentimentScore";
+        public const string DialogId = "DialogId";
     }
 }
