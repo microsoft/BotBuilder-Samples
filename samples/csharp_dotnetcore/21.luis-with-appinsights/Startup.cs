c09212bae99ba4eacbe790f6ba6e51a47271988b
Dave Taniguchi <daveta@microsoft.com>
2018-12-05 16:47:57 -0800

    Update against myget feed

--- samples/csharp_dotnetcore/21.luis-with-appinsights/Startup.cs
+++ samples/csharp_dotnetcore/21.luis-with-appinsights/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.AI.Luis;
-using Microsoft.Bot.Builder.ApplicationInsights.Core;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Configuration;
 using Microsoft.Bot.Connector.Authentication;
