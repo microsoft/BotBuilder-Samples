1eab0653b332e67beb48b9e97051859c9891d3ef
Ben Brown <benbro@microsoft.com>
2019-02-04 17:17:41 -0600

    initial import of activity handler class

---
+++ samples/javascript_typescript/30.activity-handler/src/ActivityHandler.ts
@@ -0,0 +1,148 @@
+import { ActivityTypes, TurnContext } from 'botbuilder';
+
+export type BotHandler = (context: TurnContext, next: () => Promise<void>) => Promise<void>;
+
+export class ActivityHandler {
+    private readonly handlers: {[type: string]: BotHandler[]} = {};
+
+    // Fires first for every incoming activity
+    public onTurn(handler: BotHandler): this {
+        return this.on('Turn', handler);
+    }
+
+    // Fires for incoming message activities
+    public onMessage(handler: BotHandler): this {
+        return this.on('Message', handler);
+    }
+
+    public onContactRelationUpdate(handler: BotHandler): this {
+        return this.on('ContactRelationUpdate', handler);
+    }
+
+    public onDeleteUserData(handler: BotHandler): this {
+        return this.on('DeleteUserData', handler);
+    }
+
+    public onConversationUpdate(handler: BotHandler): this {
+        return this.on('ConversationUpdate', handler);
+    }
+
+    public onConversationMembersAdded(handler: BotHandler): this {
+        return this.on('ConversationMembersAdded', handler);
+    }
+
+    public onConversationMembersRemoved(handler: BotHandler): this {
+        return this.on('ConversationMembersRemoved', handler);
+    }
+
+    public onEvent(handler: BotHandler): this {
+        return this.on('Event', handler);
+    }
+
+    public onCreateConversation(handler: BotHandler): this {
+        return this.on('CreateConversation', handler);
+    }
+
+   public onContinueConversation(handler: BotHandler): this {
+        return this.on('ContinueConversation', handler);
+   }
+
+    public onInvoke(handler: BotHandler): this {
+        return this.on('Invoke', handler);
+    }
+
+    public onEndOfConversation(handler: BotHandler): this {
+        return this.on('EndOfConversation', handler);
+    }
+
+    public onUnrecognizedActivityType(handler: BotHandler): this {
+        return this.on('UnrecognizedActivityType', handler);
+    }
+
+    // Fires last, and can be used to manage dialogs
+    public onDialog(handler: BotHandler): this {
+        return this.on('Dialog', handler);
+    }
+
+    public async run(context: TurnContext): Promise<void> {
+
+        // Allow the dialog system to be triggered at the end of the chain
+        const runDialogs = async (): Promise<void> => {
+            await this.handle(context, 'Dialog', async () => {
+                // noop
+            });
+        };
+
+        // List of all Activity Types:
+        // https://github.com/Microsoft/botbuilder-js/blob/master/libraries/botframework-schema/src/index.ts#L1627
+        // TODO: Implement handlers for all valid activity types
+        await this.handle(context, 'Turn', async () => {
+            switch (context.activity.type) {
+                case ActivityTypes.Message:
+                    await this.handle(context, 'Message', runDialogs);
+                    break;
+                case ActivityTypes.ContactRelationUpdate:
+                    await this.handle(context, 'ContactRelationUpdate', runDialogs);
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    await this.handle(context, 'ConversationUpdate', async () => {
+                        if (context.activity.membersAdded && context.activity.membersAdded.length > 0) {
+                            await this.handle(context, 'ConversationMembersAdded', runDialogs);
+                        } else if (context.activity.membersRemoved && context.activity.membersRemoved.length > 0) {
+                            await this.handle(context, 'ConversationMembersRemoved', runDialogs);
+                        } else {
+                            await runDialogs();
+                        }
+                    });
+                    break;
+                case ActivityTypes.Event:
+                    await this.handle(context, 'Event', async () => {
+                        if (context.activity.name === 'createConversation') {
+                            await this.handle(context, 'CreateConversation', runDialogs);
+                        } else if (context.activity.name === 'continueConversation') {
+                            await this.handle(context, 'ContinueConversation', runDialogs);
+                        } else {
+                            await runDialogs();
+                        }
+                    });
+                    break;
+                case ActivityTypes.DeleteUserData:
+                    await this.handle(context, 'DeleteUserData', runDialogs);
+                    break;
+                case ActivityTypes.Invoke:
+                    await this.handle(context, 'Invoke', runDialogs);
+                    break;
+                case ActivityTypes.EndOfConversation:
+                    await this.handle(context, 'EndOfConversation', runDialogs);
+                    break;
+                default:
+                    // handler for unknown or unhandled types
+                    await this.handle(context, 'UnrecognizedActivityType', runDialogs);
+                    break;
+            }
+        });
+    }
+
+    private on(type: string, handler: BotHandler) {
+        if (!this.handlers[type]) {
+            this.handlers[type] = [handler];
+        } else {
+            this.handlers[type].push(handler);
+        }
+        return this;
+    }
+
+    private async handle(context: TurnContext, type: string,  onNext: () => Promise<void>): Promise<void> {
+        async function runHandler(index: number): Promise<void> {
+            if (index < handlers.length) {
+                await handlers[index](context, () => runHandler(index + 1));
+            } else {
+                await onNext();
+            }
+        }
+
+        const handlers = this.handlers[type] || [];
+        await runHandler(0);
+    }
+
+}
---
+++ samples/javascript_typescript/30.activity-handler/src/index.ts
@@ -0,0 +1,161 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import {  ActivityTypes, BotFrameworkAdapter, ConversationState, MemoryStorage } from 'botbuilder';
+import { DialogSet, DialogTurnStatus, TextPrompt,  WaterfallDialog } from 'botbuilder-dialogs';
+
+// Import the ActivityHandler class onto which handlers will be bound.
+import { ActivityHandler } from './ActivityHandler';
+
+// Import required bot configuration.
+import { BotConfiguration, IEndpointService } from 'botframework-config';
+
+// Read botFilePath and botFileSecret from .env file.
+// Note: Ensure you have a .env file and include botFilePath and botFileSecret.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+// bot endpoint name as defined in .bot file
+// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
+const DEV_ENVIRONMENT = 'development';
+
+// bot name as defined in .bot file
+// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
+const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
+
+// Create HTTP server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open sample.bot file in the Emulator.`);
+});
+
+// .bot file path
+const BOT_FILE = path.join(__dirname, '..', (process.env.botFilePath || ''));
+
+// Read bot configuration from .bot file.
+let botConfig = {};
+try {
+    botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
+} catch (err) {
+    console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
+    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
+    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.\n\n`);
+    process.exit();
+}
+
+// Get bot endpoint configuration by service name
+const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION) as IEndpointService;
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
+const adapter = new BotFrameworkAdapter({
+    appId: endpointConfig.appId || process.env.microsoftAppID,
+    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword,
+});
+
+const storage = new MemoryStorage({});
+
+const conversationState = new ConversationState(storage);
+const dialogState = conversationState.createProperty('dialogState');
+const dialogSet = new DialogSet(dialogState);
+
+dialogSet.add(new TextPrompt('textPrompt'));
+
+dialogSet.add(new WaterfallDialog('hello', [
+    async (step) => {
+        await step.context.sendActivity('Hi!');
+        return step.next();
+    },
+    async (step) => {
+        return step.prompt('textPrompt', 'How are you?');
+    },
+    async (step) => {
+        await step.context.sendActivity('ACKNOWLEDGED.');
+        return step.next();
+    },
+]));
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    console.error(`\n [onTurnError]: ${ error }`);
+    // Send a message to the user
+    await context.sendActivity(`Oops. Something went wrong!`);
+};
+
+// Create the main dialog.
+const myBot = new ActivityHandler();
+
+myBot.onTurn(async (context, next) => {
+    // await context.sendActivity(`Received activity of type ${ context.activity.type }`);
+    await next();
+});
+
+myBot.onMessage(async (context, next) => {
+    // await context.sendActivity(`Echo: ${ context.activity.text }`);
+
+    if (context.activity.text.match(/help/i)) {
+        await context.sendActivity(`YOU NEED HELP! I saw this in my onMessage handler and will stop processing this message any further.`);
+    } else {
+        await next();
+    }
+});
+
+myBot.onDeleteUserData(async (context, next) => {
+    await context.sendActivity(`Delete data for user: ${ context.activity.from.id }`);
+    await next();
+});
+
+myBot.onConversationUpdate(async (context, next) => {
+    await context.sendActivity('Something about this conversation has been updated.');
+    await next();
+});
+
+myBot.onConversationMembersAdded(async (context, next) => {
+    await context.sendActivity('Members have been added.');
+    await next();
+});
+
+myBot.onConversationMembersRemoved(async (context, next) => {
+    await context.sendActivity('Members have been removed.');
+    await next();
+});
+
+myBot.onUnrecognizedActivityType(async (context, next) => {
+    await context.sendActivity(`I am not sure to do with an event of type ${ context.activity.type }.`);
+    await next();
+});
+
+myBot.onDialog(async (context, next) => {
+
+    if (context.activity.type === ActivityTypes.Message) {
+        const dialogContext = await dialogSet.createContext(context);
+        const results = await dialogContext.continueDialog();
+
+        // fallback behavior is to run welcome dialog
+        if (results.status === DialogTurnStatus.empty) {
+        await dialogContext.beginDialog('hello');
+        }
+
+        await conversationState.saveChanges(context);
+    }
+
+    await next();
+
+});
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Route to main dialog.
+        await myBot.run(context);
+    });
+});
