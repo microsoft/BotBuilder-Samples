5acf5b9da9d90f4c3c1d4a4a998ce4f052f394fe
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-02-08 11:05:17 -0300

    Update UI to bind with BotConfigurationViewModel

--- BotFileCreator/BotFileCreationWizard.xaml.cs
+++ BotFileCreator/BotFileCreationWizard.xaml.cs
@@ -11,36 +11,20 @@ namespace BotFileCreator
     /// </summary>
     public partial class BotFileCreationWizard : BaseDialogWindow
     {
-        private string botFileName;
-        private string endpoint;
-        private string botFileFullPath;
-        private BotConfigurationViewModel botConfigurationViewModel;
+        private string botFileFullPath; // This should be in the model class or where the logic run the command to store the file
+        private BotConfigurationViewModel botConfiguration;
 
         public BotFileCreationWizard()
         {
-            botConfigurationViewModel = new BotConfigurationViewModel();
-
-            DataContext = botConfigurationViewModel;
+            botConfiguration = new BotConfigurationViewModel();
+            DataContext = botConfiguration;
 
             InitializeComponent();
-            this.botFileName = string.Empty;
-            this.botFileFullPath = GeneralSettings.Default.ProjectName;
-            this.endpoint = string.Empty;
+            this.botFileFullPath = GeneralSettings.Default.ProjectName;  // This should be in the model class or where the logic run the command to store the file
             ValidatePrerequisites();
         }
 
         public bool PrerequisitesInstalled { get; set; }
-
-        /// <summary>
-        /// Manages the changes of the BotFileName's textbox changes
-        /// </summary>
-        /// <param name="sender">sender</param>
-        /// <param name="e">event</param>
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var textBox = sender as TextBox;
-            this.botFileName = textBox.Text ?? string.Empty;
-        }
 
         /// <summary>
         /// When the Create button is clicked, it will execute a process to create a .bot file in the project where the VS Extension is being executed.
@@ -51,7 +35,7 @@ namespace BotFileCreator
         {
             try
             {
-                BotFileNameManager botFileNameManager = new BotFileNameManager(this.botFileName, this.botFileFullPath);
+                BotFileNameManager botFileNameManager = new BotFileNameManager(this.botConfiguration.BotFileName, this.botFileFullPath);
                 MSBotCommandManager commandManager = CreateMSBotCommandManager(botFileNameManager);
 
                 // Checks if the bot configuration is valid
@@ -65,7 +49,7 @@ namespace BotFileCreator
                 }
 
                 // Class that will create the .bot file
-                BotFileCreatorManager fileCreator = new BotFileCreatorManager(botFileNameManager, commandManager, botConfigurationViewModel);
+                BotFileCreatorManager fileCreator = new BotFileCreatorManager(botFileNameManager, commandManager, botConfiguration);
 
                 // Creates the .bot file
                 Tuple<bool, string> fileCreatorResult = fileCreator.CreateBotFile();
@@ -170,10 +154,10 @@ namespace BotFileCreator
 
             commandManager = init;
 
-            if (!string.IsNullOrWhiteSpace(this.endpoint))
+            if (!string.IsNullOrWhiteSpace(botConfiguration.Endpoint))
             {
                 // Adds Endpoint to the bot file
-                MSBotCommandEndpoint endpoint = new MSBotCommandEndpoint(init, this.endpoint);
+                MSBotCommandEndpoint endpoint = new MSBotCommandEndpoint(init, botConfiguration.Endpoint);
                 commandManager = endpoint;
             }
 
@@ -199,17 +183,6 @@ namespace BotFileCreator
         {
             MessageBox.Show(msgError, title, MessageBoxButton.OK, MessageBoxImage.Error);
             this.Close();
-        }
-
-        /// <summary>
-        /// Manages the changes of the Endpoint's textbox
-        /// </summary>
-        /// <param name="sender">sender</param>
-        /// <param name="e">event</param>
-        private void Endpoint_Changed(object sender, TextChangedEventArgs e)
-        {
-            var textBox = sender as TextBox;
-            this.endpoint = textBox.Text ?? string.Empty;
         }
 
         private void BotName_Click(object sender, RoutedEventArgs e)
@@ -278,7 +251,7 @@ namespace BotFileCreator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Clipboard.SetText(botConfigurationViewModel.SecretKey);
+            Clipboard.SetText(botConfiguration.SecretKey);
         }
 
         private void DisableAfterEncryption(Grid gridPanel)
--- BotFileCreator/ViewModels/BotConfigurationViewModel.cs
+++ BotFileCreator/ViewModels/BotConfigurationViewModel.cs
@@ -5,5 +5,9 @@ namespace BotFileCreator
     public class BotConfigurationViewModel : BaseViewModel
     {
         public string SecretKey { get; set; }
+
+        public string Endpoint { get; set; }
+
+        public string BotFileName { get; set; } = string.Empty;
     }
 }
