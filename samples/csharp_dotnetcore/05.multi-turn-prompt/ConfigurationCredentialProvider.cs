8b6090c27a5b00bf99d26283f76b301a837994cb
cleemullins <cleemullins@users.noreply.github.com>
2019-02-28 15:27:19 -0800

    Provide interface based Implementation for fwd compat

--- samples/csharp_dotnetcore/05.multi-turn-prompt/ConfigurationCredentialProvider.cs
+++ samples/csharp_dotnetcore/05.multi-turn-prompt/ConfigurationCredentialProvider.cs
@@ -1,16 +1,29 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Threading.Tasks;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.BotBuilderSamples
 {
-    public class ConfigurationCredentialProvider : SimpleCredentialProvider
+    public class ConfigurationCredentialProvider : ICredentialProvider
     {
+        // Encapsulate the provider, so we can more easily change implementation in the future
+        // without breaking compatability. 
+        private readonly SimpleCredentialProvider _provider;
+
         public ConfigurationCredentialProvider(IConfiguration configuration)
-            : base(configuration["BotFramework:AppId"], configuration["BotFramework:Password"])
         {
+            _provider = new SimpleCredentialProvider(
+                configuration["BotFramework:AppId"],
+                configuration["BotFramework:Password"]);
         }
+
+        public Task<string> GetAppPasswordAsync(string appId) => _provider.GetAppPasswordAsync(appId);
+
+        public Task<bool> IsAuthenticationDisabledAsync() => _provider.IsAuthenticationDisabledAsync();
+
+        public Task<bool> IsValidAppIdAsync(string appId) => _provider.IsValidAppIdAsync(appId);
     }
 }
