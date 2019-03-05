a9e21b965f9df002a6c0fdf32b4d9c7f5e75dfe0
John Taylor <johtaylo@microsoft.com>
2019-03-05 11:27:11 -0800

    move LUIS call into helper and add child dialog for date

--- samples/csharp_dotnetcore/13.basic-bot/Dialogs/MainDialog.cs
+++ samples/csharp_dotnetcore/13.basic-bot/Dialogs/MainDialog.cs
@@ -1,14 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Linq;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.AI.Luis;
 using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -43,7 +42,7 @@ namespace Microsoft.BotBuilderSamples
         private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             // Call LUIS and gather any potential booking details.
-            var bookingDetails = await ExecuteLuisQuery(stepContext.Context, cancellationToken);
+            var bookingDetails = await LuisHelper.ExecuteLuisQuery(_configuration, stepContext.Context, cancellationToken);
 
             // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
             return await stepContext.BeginDialogAsync(nameof(BookingDialog), bookingDetails, cancellationToken);
@@ -59,46 +58,17 @@ namespace Microsoft.BotBuilderSamples
                 // Now we have all the booking details call the booking service.
 
                 // If the call to the booking service was successful tell the user.
-                var msg = $"I have you booked to {result.Destination} from {result.Origin} on {result.TravelDate}";
+
+                var timeProperty = new TimexProperty(result.TravelDate);
+                var travelDateMsg = timeProperty.ToNaturalLanguage(DateTime.Now);
+                var msg = $"I have you booked to {result.Destination} from {result.Origin} on {travelDateMsg}";
                 await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);
             }
-            return await stepContext.EndDialogAsync();
-        }
-
-        private async Task<BookingDetails> ExecuteLuisQuery(ITurnContext turnContext, CancellationToken cancellationToken)
-        {
-            // Create the LUIS client from configuration.
-            var luisService = new LuisService
+            else
             {
-                AppId = _configuration["BotServices:Luis-Booking-AppId"],
-                AuthoringKey = _configuration["BotServices:Luis-Booking-AuthoringKey"],
-                Region = _configuration["BotServices:Luis-Booking-Region"],
-            };
-
-            var recognizer = new LuisRecognizer(new LuisApplication(
-                luisService.AppId,
-                luisService.AuthoringKey,
-                luisService.GetEndpoint()));
-
-            // The actual call to LUIS
-            var recognizerResult = await recognizer.RecognizeAsync(turnContext, cancellationToken);
-
-            // Now process the result from LUIS.
-            var bookingDetails = new BookingDetails();
-
-            var (intent, score) = recognizerResult.GetTopScoringIntent();
-            if (intent == "Book_flight")
-            {
-                // We need to get the result from the LUIS JSON which at every level returns an array.
-                bookingDetails.Destination = recognizerResult.Entities["To"]?.FirstOrDefault()?["Airport"]?.FirstOrDefault()?.FirstOrDefault()?.ToString();
-                bookingDetails.Origin = recognizerResult.Entities["From"]?.FirstOrDefault()?["Airport"]?.FirstOrDefault()?.FirstOrDefault()?.ToString();
-
-                // This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop the Time part.
-                // TIMEX is a format that represents DateTime expressions that include some ambiguity. e.g. missing a Year.
-                bookingDetails.TravelDate = recognizerResult.Entities["datetime"]?.FirstOrDefault()?["timex"]?.FirstOrDefault()?.ToString().Split('T')[0];
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Thank you."), cancellationToken);
             }
-
-            return bookingDetails;
+            return await stepContext.EndDialogAsync();
         }
     }
 }
---
+++ samples/csharp_dotnetcore/13.basic-bot/LuisHelper.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public static class LuisHelper
+    {
+        public static async Task<BookingDetails> ExecuteLuisQuery(IConfiguration configuration, ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            // Create the LUIS client from configuration.
+            var luisService = new LuisService
+            {
+                AppId = configuration["BotServices:Luis-Booking-AppId"],
+                AuthoringKey = configuration["BotServices:Luis-Booking-AuthoringKey"],
+                Region = configuration["BotServices:Luis-Booking-Region"],
+            };
+
+            var recognizer = new LuisRecognizer(new LuisApplication(
+                luisService.AppId,
+                luisService.AuthoringKey,
+                luisService.GetEndpoint()));
+
+            // The actual call to LUIS
+            var recognizerResult = await recognizer.RecognizeAsync(turnContext, cancellationToken);
+
+            // Now process the result from LUIS.
+            var bookingDetails = new BookingDetails();
+
+            var (intent, score) = recognizerResult.GetTopScoringIntent();
+            if (intent == "Book_flight")
+            {
+                // We need to get the result from the LUIS JSON which at every level returns an array.
+                bookingDetails.Destination = recognizerResult.Entities["To"]?.FirstOrDefault()?["Airport"]?.FirstOrDefault()?.FirstOrDefault()?.ToString();
+                bookingDetails.Origin = recognizerResult.Entities["From"]?.FirstOrDefault()?["Airport"]?.FirstOrDefault()?.FirstOrDefault()?.ToString();
+
+                // This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop the Time part.
+                // TIMEX is a format that represents DateTime expressions that include some ambiguity. e.g. missing a Year.
+                bookingDetails.TravelDate = recognizerResult.Entities["datetime"]?.FirstOrDefault()?["timex"]?.FirstOrDefault()?.ToString().Split('T')[0];
+            }
+
+            return bookingDetails;
+        }
+    }
+}
