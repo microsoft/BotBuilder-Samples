0adf51df80d1ec0496dc68c0350774eabdeb7d11
John Taylor <johtaylo@microsoft.com>
2019-03-07 10:24:44 -0800

    minor clean up in Startup and better error handling

--- samples/csharp_dotnetcore/19.custom-dialogs/Startup.cs
+++ samples/csharp_dotnetcore/19.custom-dialogs/Startup.cs
@@ -14,13 +14,6 @@ namespace Microsoft.BotBuilderSamples
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
