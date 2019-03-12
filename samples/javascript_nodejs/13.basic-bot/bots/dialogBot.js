8d6f40bb341ee53f5d181a3ee4f68587bd672e4f
stevengum <14935595+stevengum@users.noreply.github.com>
2019-03-12 15:40:05 -0700

    rewrite Node.js corebot to get parity with .netcore sample

---
+++ samples/javascript_nodejs/13.basic-bot/bots/dialogBot.js
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler, ConversationState, UserState } = require('botbuilder');
+const { Dialog } = require('botbuilder-dialogs');
+
+class DialogBot extends ActivityHandler {
+    /**
+     *
+     * @param {ConversationState} conversationState
+     * @param {UserState} userState
+     * @param {Dialog} dialog
+     * @param {any} logger object for logging events, defaults to console if none is provided
+     */
+    constructor(conversationState, userState, dialog, logger) {
+        super();
+        if (!conversationState) throw new Error('[DialogBot]: Missing parameter. conversationState is required');
+        if (!userState) throw new Error('[DialogBot]: Missing parameter. userState is required');
+        if (!dialog) throw new Error('[DialogBot]: Missing parameter. dialog is required');
+        if (!logger) {
+            logger = console;
+            logger.log('[DialogBot]: logger not passed in, defaulting to console');
+        }
+        
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+        this.logger = logger;
+        this.dialogState = this.conversationState.createProperty('DialogState');
+
+        this.onMessage(async context => {
+            this.logger.log('Running dialog with Message Activity.');
+
+            // Run the Dialog with the new message Activity.
+            await this.dialog.run(context, this.dialogState);
+    
+            // Save any state changes. The load happened during the execution of the Dialog. 
+            await this.conversationState.saveChanges(context, false);
+            await this.userState.saveChanges(context, false);
+        });
+    }
+
+    
+}
+
+module.exports.DialogBot = DialogBot;
---
+++ samples/javascript_nodejs/13.basic-bot/dialogs/mainDialog.js
@@ -0,0 +1,77 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { TimexProperty } = require('@microsoft/recognizers-text-data-types-timex-expression');
+const { ComponentDialog, DialogSet, DialogTurnStatus, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
+const { BookingDialog } = require('./bookingDialog');
+const { LuisHelper } = require('./luisHelper');
+
+const MAIN_WATERFALL_DIALOG = 'mainWaterfallDialog';
+const BOOKING_DIALOG = 'bookingDialog';
+
+class MainDialog extends ComponentDialog {
+    constructor(logger) {
+        super('MainDialog');
+
+        if (!logger) {
+            logger = console;
+            logger.log('[MainDialog]: logger not passed in, defaulting to console');
+        }
+
+        this.logger = logger;
+        this.addDialog(new TextPrompt('TextPrompt'))
+            .addDialog(new BookingDialog(BOOKING_DIALOG))
+            .addDialog(new WaterfallDialog(MAIN_WATERFALL_DIALOG, [
+                this.introStep.bind(this),
+                this.actStep.bind(this),
+                this.finalStep.bind(this)
+            ]));
+        
+        this.initialDialogId = MAIN_WATERFALL_DIALOG;
+    }
+
+    async run(context, accessor) {
+        const dialogSet = new DialogSet(accessor);
+        dialogSet.add(this);
+
+        const dialogContext = await dialogSet.createContext(context);
+        const results = await dialogContext.continueDialog();
+        if (results.status === DialogTurnStatus.empty) {
+            await dialogContext.beginDialog(this.id);
+        }
+    }
+
+    async introStep(stepContext) {
+        return await stepContext.prompt('TextPrompt', { prompt: 'What can I help you with today?'});
+    }
+
+    async actStep(stepContext) {
+        // Call LUIS and gather any potential booking details.
+        const bookingDetails = await LuisHelper.executeLuisQuery(this.logger, stepContext.context);
+
+        // In this sample we only have a single Intent we are concerned with. However, typically a scneario
+        // will have multiple different Intents each corresponding to starting a different child Dialog.
+
+        // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
+        return await stepContext.beginDialog('bookingDialog', bookingDetails);
+    }
+
+    async finalStep(stepContext) {
+        // If the child dialog ("bookingDialog") was cancelled or the user failed to confirm, the Result here will be null.
+        if (stepContext.result) {
+            const result = stepContext.result;
+            // Now we have all the booking details call the booking service.
+
+            // If the call to the booking service was successful tell the user.
+            const timeProperty = new TimexProperty(result.travelDate);
+            const travelDateMsg = timeProperty.toNaturalLanguage(new Date(Date.now()));
+            const msg = `I have you booked to ${result.destination} from ${result.origin} on ${travelDateMsg}`;
+            await stepContext.context.sendActivity(msg);
+        } else {
+            await stepContext.context.sendActivity('Thank you.');
+        }
+        return await stepContext.endDialog();
+    }
+}
+
+module.exports.MainDialog = MainDialog;
