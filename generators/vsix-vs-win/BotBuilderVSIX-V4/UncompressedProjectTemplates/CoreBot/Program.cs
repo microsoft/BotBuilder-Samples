f2f0b44f2d9e4295742aa69e227797e2132cb8d5
Gabo Gilabert <ggilaber@microsoft.com>
2019-03-18 23:39:04 -0400

    Changed references to Microsoft.AspNetCore.All to Microsoft.AspNetCore.App in CoreBot.csproj, EchoBotWithCounter.csproj and EmptyBot.csproj Removed call to logging.AddAzureWebAppDiagnostics(); from Program.cs. Updated Program.cs code to match OOTB AspNetCore web api template. Removed references for Microsoft.Azure.CognitiveServices.ContentModerator, Microsoft.Azure.CognitiveServices.Language preview and Microsoft.Graph from CoreBot.csproj

--- generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/CoreBot/Program.cs
+++ generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/CoreBot/Program.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// Generated with `dotnet new corebot` vX.X.X
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace $safeprojectname$
 {
@@ -12,28 +10,24 @@ namespace $safeprojectname$
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureLogging((hostingContext, logging) =>
                 {
-                    // Add Azure Logging
-                    logging.AddAzureWebAppDiagnostics();
-
-                    // Other Loggers.
+                    // Logging Options.
                     // There are other logging options available:
-                    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.2
+                    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1
                     // logging.AddDebug();
                     // logging.AddConsole();
                 })
 
-                // Application Insights.
-                // An alternative logging and metrics service for your application.
+                // Logging Options.
+                // Consider using Application Insights for your logging and metrics needs.
                 // https://azure.microsoft.com/en-us/services/application-insights/
                 // .UseApplicationInsights()
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
     }
 }
