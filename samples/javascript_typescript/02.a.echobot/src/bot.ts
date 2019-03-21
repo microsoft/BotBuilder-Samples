55b3c965bf02a8c994ee2937e89b37f200b71fbe
stevengum <14935595+stevengum@users.noreply.github.com>
2019-03-21 13:43:33 -0700

    update ts echobot, remove PREREQUISITES.md

--- samples/javascript_typescript/02.a.echobot/src/bot.ts
+++ samples/javascript_typescript/02.a.echobot/src/bot.ts
@@ -1,21 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ActivityTypes, TurnContext } from 'botbuilder';
+import { ActivityHandler } from 'botbuilder';
 
-export class MyBot {
-    /**
-     * Use onTurn to handle an incoming activity, received from a user, process it, and reply as needed
-     *
-     * @param {TurnContext} context on turn context object.
-     */
-    public onTurn = async (turnContext: TurnContext) => {
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
-        if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
-        } else {
-            // Generic handler for all other activity types.
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
-        }
+export class MyBot extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async turnContext => { await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`) });
+        this.onConversationUpdate(async turnContext => { await turnContext.sendActivity('[conversationUpdate event detected]') });
     }
 }
