38b905172892745e48670001f63779eea3b87559
stevengum <14935595+stevengum@users.noreply.github.com>
2019-03-21 13:14:46 -0700

    revised TypeScript Basic Bot

---
+++ samples/javascript_typescript/13.basic-bot/src/dialogs/bookingDetails.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export class BookingDetails {
+    intent: string;
+    origin: string;
+    destination: string;
+    travelDate: string;
+}
--- samples/javascript_typescript/13.basic-bot/src/dialogs/welcome/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-const WelcomeCard = require('../../../resources/WelcomeCard.json');
-
-export { WelcomeCard };
