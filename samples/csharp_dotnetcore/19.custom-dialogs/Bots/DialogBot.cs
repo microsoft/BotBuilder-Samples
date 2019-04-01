8425245a8b604c308506ca6456b484e5d3afe812
Gabo Gilabert <ggilaber@microsoft.com>
2019-04-01 11:25:51 -0400

    Reverted parametrization to parameterization Changed DialogExtensions.Run cancelationToken parameter to take a default. Reverted adaptive cards reference to 1.0.3 until Will confirms there are no know issues with webchat and 1.1.2

--- samples/csharp_dotnetcore/19.custom-dialogs/Bots/DialogBot.cs
+++ samples/csharp_dotnetcore/19.custom-dialogs/Bots/DialogBot.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Microsoft.BotBuilderSamples
 {
-    // This IBot implementation can run any type of Dialog. The use of type parametrization is to allows multiple different bots
+    // This IBot implementation can run any type of Dialog. The use of type parameterization is to allows multiple different bots
     // to be run at different endpoints within the same project. This can be achieved by defining distinct Controller types
     // each with dependency on distinct IBot types, this way ASP Dependency Injection can glue everything together without ambiguity.
     // The ConversationState is used by the Dialog system. The UserState isn't, however, it might have been used in a Dialog implementation,
