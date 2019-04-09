2ccf162c7a91026f4ef9c963c80934c7a8bec374
Eric Dahlvang <v-eridah@microsoft.com>
2019-04-09 16:29:40 -0700

    Fix issues with proactive bot sample

--- samples/csharp_dotnetcore/16.proactive-messages/Controllers/NotifyController.cs
+++ samples/csharp_dotnetcore/16.proactive-messages/Controllers/NotifyController.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Concurrent;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -25,15 +27,34 @@ namespace ProactiveBot.Controllers
             _adapter = adapter;
             _conversationReferences = conversationReferences;
             _appId = ((SimpleCredentialProvider)credentials).AppId;
+            if (string.IsNullOrEmpty(_appId))
+            {
+                _appId = Guid.NewGuid().ToString(); //if no AppId, use a random Guid
+            }
         }
 
         public async Task<IActionResult> Get()
         {
+            string names = string.Empty;
+
             foreach (var conversationReference in _conversationReferences.Values)
             {
                 await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, BotCallback, default(CancellationToken));
+                
+                names += $"<li>{conversationReference.User.Name ?? conversationReference.User.Id} on {conversationReference.ChannelId}</li><br>";
             }
-            return Ok();
+
+            names = string.IsNullOrEmpty(names) ? "nobody has messaged the bot" : names;
+
+            // Let the caller know who was proactively notified
+            var content = $"<html><body><h1>Proactively Messaged:</h1><p>{names}</p></body></html>";
+
+            return new ContentResult()
+            {
+                Content = content,
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+            };
         }
 
         private async Task BotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
