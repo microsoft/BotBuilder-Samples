f0a30f3cd9a403326d1ef7df7be6c40b4ea20b46
Scott Gellock <scott.gellock@microsoft.com>
2019-04-10 20:46:23 -0700

    VSIX:  Write version information in generated code
    - add template name and version to each source file during template generation
    - added a HOW_TO_VERSION.md

--- generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/EchoBot/Bots/EchoBot.cs
+++ generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/EchoBot/Bots/EchoBot.cs
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v$templateversion$
 
 using System.Collections.Generic;
 using System.Threading;
