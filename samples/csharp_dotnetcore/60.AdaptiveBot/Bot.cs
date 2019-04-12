954c5eb7fb297e23c12b34a60ed3572fdfc098be
Tom Laird-McConnell <thermous@iciclecreek.com>
2019-04-12 00:08:05 -0700

    add sample 60

---
+++ samples/csharp_dotnetcore/60.AdaptiveBot/Bot.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Schema;
+
+namespace AdaptiveBotSample
+{
+    public class Bot : ActivityHandler
+    {
+        private DialogSet _dialogs;
+
+        private AdaptiveDialog rootDialog;
+
+        private readonly ResourceExplorer resourceExplorer;
+
+        private UserState userState;
+        private ConversationState conversationState;
+        private Source.IRegistry registry;
+
+        public Bot(UserState userState, ConversationState conversationState, ResourceExplorer resourceExplorer, Source.IRegistry registry)
+        {
+            this.registry = registry;
+            this.conversationState = conversationState;
+            this.userState = userState;
+            this.resourceExplorer = resourceExplorer;
+
+            // auto reload dialogs when file changes
+            this.resourceExplorer.Changed += (paths) =>
+            {
+                if (paths.Any(p => Path.GetExtension(p) == ".dialog"))
+                    LoadRootDialog();
+            };
+
+            LoadRootDialog();
+        }
+
+        private void LoadRootDialog()
+        {
+            System.Diagnostics.Trace.TraceInformation("Loading resources...");
+            var rootFile = resourceExplorer.GetResource(@"ToDoBot.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("ToDoLuisBot.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("NoMatchRule.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("EndTurn.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("IfCondition.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("TextInput.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("WelcomeRule.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("DoSteps.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("BeginDialog.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("ExternalLanguage.main.dialog");
+            //var rootFile = resourceExplorer.GetResource("CustomStep.dialog");
+
+            rootDialog = DeclarativeTypeLoader.Load<AdaptiveDialog>(rootFile.FullName, resourceExplorer, registry);
+            _dialogs = new DialogSet(conversationState.CreateProperty<DialogState>("DialogState"));
+            _dialogs.Add(rootDialog);
+
+            System.Diagnostics.Trace.TraceInformation("Done loading resources.");
+        }
+
+        protected async override Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await rootDialog.OnTurnAsync(turnContext, null, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/csharp_dotnetcore/60.AdaptiveBot/Startup.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Types;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.LanguageGeneration.Renderer;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace AdaptiveBotSample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            HostingEnvironment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+        }
+
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            //if (System.Diagnostics.Debugger.IsAttached)
+            //{
+            //    TelemetryConfiguration.Active.DisableTelemetry = true;
+            //}
+
+            // hook up debugging support
+            var sourceMap = new SourceMap();
+            DebugAdapter debugAdapter = null;
+            bool enableDebugger = true;
+            if (enableDebugger)
+            {
+                // by setting the source registry all dialogs will register themselves to be debugged as execution flows
+                DebugSupport.SourceRegistry = sourceMap;
+                debugAdapter = new DebugAdapter(sourceMap, sourceMap, new DebugLogger(nameof(DebugAdapter)));
+            }
+
+            services.AddSingleton(DebugSupport.SourceRegistry);
+
+            // set the configuration for types
+            TypeFactory.Configuration = this.Configuration;
+
+            // register adaptive library components
+            TypeFactory.RegisterAdaptiveTypes();
+
+            // register custom components
+            TypeFactory.Register("Testbot.CalculateDogYears", typeof(CalculateDogYears));
+            TypeFactory.Register("Testbot.JavascriptStep", typeof(JavascriptStep));
+            TypeFactory.Register("Testbot.CSharpStep", typeof(CSharpStep));
+
+            IStorage dataStore = new MemoryStorage();
+            services.AddSingleton(dataStore);
+
+            var userState = new UserState(dataStore);
+            services.AddSingleton(userState);
+
+            var conversationState = new ConversationState(dataStore);
+            services.AddSingleton(conversationState);
+
+            var resourceExplorer = ResourceExplorer.LoadProject(HostingEnvironment.ContentRootPath);
+            services.AddSingleton(resourceExplorer);
+
+            services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkHttpAdapter>((s) =>
+            {
+                // manage all bot resources
+                var lg = new LGLanguageGenerator(resourceExplorer);
+
+                var adapter = new BotFrameworkHttpAdapter();
+
+                adapter
+                    .Use(new RegisterClassMiddleware<IStorage>(dataStore))
+                    .Use(new RegisterClassMiddleware<ResourceExplorer>(resourceExplorer))
+                    .Use(new RegisterClassMiddleware<ILanguageGenerator>(lg))
+                    .Use(new RegisterClassMiddleware<IMessageActivityGenerator>(new TextMessageActivityGenerator(lg)))
+                    .Use(new AutoSaveStateMiddleware(userState, conversationState));
+
+                if (debugAdapter != null)
+                {
+                    adapter.Use(debugAdapter);
+                }
+
+                adapter.OnTurnError = async (turnContext, exception) =>
+                {
+                    await conversationState.ClearStateAsync(turnContext);
+                    await conversationState.SaveChangesAsync(turnContext);
+                };
+                return adapter;
+            });
+
+            services.AddSingleton<IBot, Bot>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
