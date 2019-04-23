06880e49aee6457dd5e884ed2fa9813aea60d531
Eric Dahlvang <v-eridah@microsoft.com>
2019-04-22 22:40:03 -0700

    Remove ConversationState from AdapterWithErrorHandler

--- samples/csharp_dotnetcore/23.facebook-events/AdapterWithErrorHandler.cs
+++ samples/csharp_dotnetcore/23.facebook-events/AdapterWithErrorHandler.cs
@@ -11,7 +11,7 @@ namespace Microsoft.BotBuilderSamples
 {
     public class AdapterWithErrorHandler : BotFrameworkHttpAdapter
     {
-        public AdapterWithErrorHandler(ICredentialProvider credentialProvider, ILogger<BotFrameworkHttpAdapter> logger, ConversationState conversationState = null)
+        public AdapterWithErrorHandler(ICredentialProvider credentialProvider, ILogger<BotFrameworkHttpAdapter> logger)
             : base(credentialProvider)
         {
             OnTurnError = async (turnContext, exception) =>
@@ -21,21 +21,6 @@ namespace Microsoft.BotBuilderSamples
 
                 // Send a catch-all apology to the user.
                 await turnContext.SendActivityAsync("Sorry, it looks like something went wrong.");
-
-                if (conversationState != null)
-                {
-                    try
-                    {
-                        // Delete the conversationState for the current conversation to prevent the
-                        // bot from getting stuck in a error-loop caused by being in a bad state.
-                        // ConversationState should be thought of as similar to "cookie-state" in a Web pages.
-                        await conversationState.DeleteAsync(turnContext);
-                    }
-                    catch (Exception e)
-                    {
-                        logger.LogError($"Exception caught on attempting to Delete ConversationState : {e.Message}");
-                    }
-                }
             };
         }
     }
