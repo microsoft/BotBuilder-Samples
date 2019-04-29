dd4a85b9b99db0f1011a2550c2f9ef548de2be7a
Anusha Rao Ravindra (TEKsystems) <v-anravi@microsoft.com>
2019-04-29 11:24:16 -0700

    updated the README and facebookBot files

--- samples/javascript_nodejs/23.facebook-events/README.md
+++ samples/javascript_nodejs/23.facebook-events/README.md
@@ -1,24 +1,23 @@
-# Concepts introduced in this sample
+# Facebook events
 
-This sample shows how to integrate and consume Facebook specific payloads, such as postbacks, quick replies and optin events.
+Bot Framework v4 facebook events bot sample
 
-Note: Messenger Webhook Events can be subscribed to when configuring the Messenger application associated with the bot:
-https://developers.facebook.com/apps/YourFacebookApplicationId/messenger/settings/
-
-![Webhooks](Webhooks.png)
-
-Since Bot Framework supports multiple Facebook pages for a single bot, we also show how to know the page to which the message was sent, so developers can have custom behavior per page.
+This bot has been created using [Bot Framework](https://dev.botframework.com), is shows how to integrate and consume Facebook specific payloads, such as postbacks, quick replies and optin events.  Since Bot Framework supports multiple Facebook pages for a single bot, we also show how to know the page to which the message was sent, so developers can have custom behavior per page.
 
 More information about configuring a bot for Facebook Messenger can be found here: [Connect a bot to Facebook](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-channel-connect-facebook)
 
-## Install Node and CLI Tooling
+## Prerequisites
+
 - [Node.js][4] version 10.14 or higher
     ```bash
     # determine node version
     node --version
     ```
-- If you don't have an Azure subscription, create a [free account](https://azure.microsoft.com/free/).
-- Install the latest version of the [Azure CLI](https://docs.microsoft.com/cli/azure/install-azure-cli?view=azure-cli-latest) tool. Version 2.0.54 or higher.
+
+- Enable Facebook Channel
+    The final step to test Facebook-specific features is to publish your bot for the Facebook channel. The Bot Framework makes this very easy,
+    and the detailed steps are explained in the [Bot Framework Channel Documentation](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-channel-connect-facebook?view=azure-bot-service-3.0).
+
 
 # To try this sample
 - Clone the repository
@@ -38,26 +37,35 @@ More information about configuring a bot for Facebook Messenger can be found her
     npm start
     ```
 
-# Testing the bot using Bot Framework Emulator
-[Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
+## Testing the bot using Bot Framework Emulator
+
+[Microsoft Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
 
 - Install the Bot Framework emulator from [here](https://github.com/microsoft/botframework-emulator/releases)
 
 ## Connect to the bot using Bot Framework Emulator 
+
 - Launch Bot Framework Emulator
 - File -> Open Bot
 - Enter a Bot URL of `http://localhost:3978/api/messages`
 
-# Enable Facebook Channel
-
-The final step to test Facebook-specific features is to publish your bot for the Facebook channel. The Bot Framework makes this very easy, and the detailed steps are explained in the [Bot Framework Channel Documentation](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-channel-connect-facebook?view=azure-bot-service-3.0).
-
 # Deploy the bot to Azure
+
 To learn more about deploying a bot to Azure, see [Deploy your bot to Azure][https://aka.ms/azuredeployment] for a complete list of deployment instructions.
 
 
 # Further reading
-- [Azure Bot Service](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
-- [Facebook Quick Replies](https://developers.facebook.com/docs/messenger-platform/send-messages/quick-replies)
+
+- [Bot Framework Documentation](https://docs.botframework.com)
+- [Bot Basics](https://docs.microsoft.com/azure/bot-service/bot-builder-basics?view=azure-bot-service-4.0)
+- [Facebook Quick Replies](https://developers.facebook.com/docs/messenger-platform/send-messages/quick-replies/0)
 - [Facebook PostBack](https://developers.facebook.com/docs/messenger-platform/reference/webhook-events/messaging_postbacks/)
 - [Facebook Opt-in](https://developers.facebook.com/docs/messenger-platform/reference/webhook-events/messaging_optins/)
+- [Activity processing](https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-concept-activity-processing?view=azure-bot-service-4.0)
+- [Azure Bot Service Introduction](https://docs.microsoft.com/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
+- [Azure Bot Service Documentation](https://docs.microsoft.com/azure/bot-service/?view=azure-bot-service-4.0)
+- [.NET Core CLI tools](https://docs.microsoft.com/en-us/dotnet/core/tools/?tabs=netcore2x)
+- [Azure CLI](https://docs.microsoft.com/cli/azure/?view=azure-cli-latest)
+- [Azure Portal](https://portal.azure.com)
+- [Language Understanding using LUIS](https://docs.microsoft.com/en-us/azure/cognitive-services/luis/)
+- [Channels and Bot Connector Service](https://docs.microsoft.com/en-us/azure/bot-service/bot-concepts?view=azure-bot-service-4.0)
--- samples/javascript_nodejs/23.facebook-events/bots/facebookBot.js
+++ samples/javascript_nodejs/23.facebook-events/bots/facebookBot.js
@@ -1,17 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, TurnContext } = require('botbuilder');
+const { ActivityHandler, TurnContext, CardFactory , ActionTypes} = require('botbuilder');
 const { ChoiceFactory } = require('botbuilder-dialogs');
+// These are the options provided to the user when they message the bot
 const facebookPageIdOption = "Facebook Page Id";
 const quickRepliesOption = "Quick Replies";
 const postBackOption = "PostBack";
-
-// This IBot implementation can run any type of Dialog. The use of type parameterization is to allows multiple different bots
-// to be run at different endpoints within the same project. This can be achieved by defining distinct Controller types
-// each with dependency on distinct IBot types, this way ASP Dependency Injection can glue everything together without ambiguity.
-// The ConversationState is used by the Dialog system. The UserState isn't, however, it might have been used in a Dialog implementation,
-// and the requirement is that all BotState objects are saved at the end of a turn.
 
 class FacebookBot extends ActivityHandler {
 
@@ -30,8 +25,12 @@ class FacebookBot extends ActivityHandler {
 
         this.onMessage(async (context) => {
             this.logger.log('Processing a Message Activity.');
-
+            
+            // Show choices if the Facebook Payload from ChannelData is not handled
             if (!await this.processFacebookPayload(context, context.activity.channelData)) {
+                if (context.activity.channelId !== "facebook") {
+                    await context.sendActivity("This sample is intended to be used with a Facebook bot.");
+                }
                 await this.showChoices(context);
             }
         });
@@ -41,73 +40,69 @@ class FacebookBot extends ActivityHandler {
         this.onEvent(async (context) => {
             this.logger.log('Processing an Event Activity.');
 
-            // Analyze Facebook payload from channel data.
+            // Analyze Facebook payload from EventActivity.Value
             await this.processFacebookPayload(context, context.activity.value);
 
         });
 
-        
+
     }
 
     /**
      * 
      * @param {TurnContext} context 
      */
-    async showChoices(context) 
-    {
-       
-          const choices = [
-                {
-                    value: quickRepliesOption
-                },
-                {
-                    value: facebookPageIdOption
-                },
-                {
-                    value: postBackOption
-                }
-            ]
+    async showChoices(context) {
+        // Create choices for the prompt
+        const choices = [
+            {
+                value: quickRepliesOption
+            },
+            {
+                value: facebookPageIdOption
+            },
+            {
+                value: postBackOption
+            }
+        ]
         
+        // Create the prompt message
         var message = ChoiceFactory.forChannel(context.activity.channelId, choices, "What Facebook feature would you like to try? Here are some quick replies to choose from!");
         await context.sendActivity(message);
     };
+
     /**
- * Process a Facebook payload from channel data, to handle optin events, postbacks and quick replies.
- * NOTE: This is a simplification of the Facebook object model. There are many more events and payloads
- * that could be captured here. We only show some key features that are commonly used. The sample
- * can be extended to account for more Facebook-specific events according to developers' needs.
-     * 
      * @param {TurnContext} context 
      * @param {Object} data
      */
- 
 
     async processFacebookPayload(context, data) {
 
         // At this point we know we are on Facebook channel, and can consume the Facebook custom payload present in channelData.
         const facebookPayload = data;
-        if (facebookPayload != null) {
+        if (facebookPayload) {
+
             //Postback
-            if (facebookPayload.postback != null) {
+            if (facebookPayload.postback) {
                 await this.onFacebookPostback(context, facebookPayload.postback);
                 return true;
             }
 
             //Optin
-            else if (facebookPayload.optin != null) {
+            else if (facebookPayload.optin) {
                 await this.onFacebookOptin(context, facebookPayload.optin);
                 return true;
             }
 
             //Quick Reply
-            else if (facebookPayload.message != null && facebookPayload.message.quickReply != null) {
-                await this.onFacebookQuickReply(context, facebookPayload.message.quickReply);
+            else if (facebookPayload.message && facebookPayload.message.quick_reply) {
+                await this.onFacebookQuickReply(context, facebookPayload.message.quick_reply);
                 return true;
             }
 
 
             //Echo
-            else if (facebookPayload.message != null && facebookPayload.message.isEcho != null && facebookPayload.message.isEcho) {
+            else if (facebookPayload.message && facebookPayload.message.is_echo) {
                 await this.onFacebookEcho(context, facebookPayload.message);
                 return true;
             }
@@ -116,12 +111,10 @@ class FacebookBot extends ActivityHandler {
     }
 
     /**
-     * Called when receiving a Facebook messaging_postback event.
-     * Facebook Developer Reference: Postback
-     * https://developers.facebook.com/docs/messenger-platform/reference/webhook-events/messaging_postbacks/
      * @param {Object} postback JSON object for postback payload.
      * @param {TurnContext} context
      */
+
     async onFacebookPostback(context, postback) {
         this.logger.log('Postback message received.');
         // TODO: Your postBack handling logic here...
@@ -132,30 +125,28 @@ class FacebookBot extends ActivityHandler {
     }
 
     /**
-     * Called when receiving a Facebook messaging_optin event.
-     * Facebook Developer Reference: Optin
-     * https://developers.facebook.com/docs/messenger-platform/reference/webhook-events/messaging_optins/
-     * @param {Object} optin JSON object for postback payload.
+     * @param {Object} optin 
      * @param {TurnContext} context
      */
+
     async onFacebookOptin(context, optin) {
         this.logger.log('Optin message received.')
         // TODO: Your optin handling logic here...
     }
 
     /**
-     * Called when receiving a Facebook quick reply.
-     * Facebook Developer Reference: Quick reply
-     * https://developers.facebook.com/docs/messenger-platform/send-messages/quick-replies/
      * @param {Object} quickReply JSON object for postback payload.
      * @param {TurnContext} context
      */
+
     async onFacebookQuickReply(context, quickReply) {
         this.logger.log('QuickReply message received.');
-        // TODO: Your QuickReply handling logic here...
+         // TODO: Your QuickReply handling logic here...
 
+         // Process the message by checking the Activity.Text.  The FacebookQuickReply could also contain a json payload.
 
-        switch (context.activity.text) {
+         // Initially the bot offers to showcase 3 Facebook features: Quick replies, PostBack and getting the Facebook Page Name.
+         switch (context.activity.text) {
 
             // Here we showcase how to obtain the Facebook page id.
             // This can be useful for the Facebook multi-page support provided by the Bot Framework.
@@ -165,7 +156,8 @@ class FacebookBot extends ActivityHandler {
                 await context.sendActivity(`This message comes from the following Facebook Page: ${context.activity.recipient.id}`);
                 await this.showChoices(context);
                 break;
-
+         
+            // Here we send a HeroCard with 2 options that will trigger a Facebook PostBack.
             case postBackOption:
                 var card = CardFactory.heroCard(
                     'Is 42 the answer to the ultimate question of Life, the Universe, and Everything?',
@@ -193,6 +185,7 @@ class FacebookBot extends ActivityHandler {
                 await context.sendActivity(reply);
                 break;
 
+            // By default we offer the users different actions that the bot supports, through quick replies.
             case quickRepliesOption:
             default:
                 await this.showChoices(context);
@@ -202,6 +195,5 @@ class FacebookBot extends ActivityHandler {
     }
 }
 
-
-
 module.exports.FacebookBot = FacebookBot;
+
