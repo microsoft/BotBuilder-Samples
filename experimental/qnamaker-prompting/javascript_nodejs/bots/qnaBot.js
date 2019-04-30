5ed07e1a8258de1fcb972b00037847452609d473
Rohan Kulkarni <rokulka@microsoft.com>
2019-04-30 19:21:40 +0530

    Add NodeJS prompting sample Update Readme file

---
+++ experimental/qnamaker-prompting/javascript_nodejs/bots/qnaBot.js
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { MessageFactory } = require('botbuilder');
+const { DialogBot } = require('./dialogBot');
+
+class QnABot extends DialogBot {
+    constructor(conversationState, userState, dialog, logger) {
+        super(conversationState, userState, dialog, logger);
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity(MessageFactory.text('Welcome to QnA Bot'));
+                }
+            }
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+}
+
+module.exports.QnABot = QnABot;
