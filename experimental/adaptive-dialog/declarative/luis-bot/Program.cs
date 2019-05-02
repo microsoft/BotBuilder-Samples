7c22ef9cda20b2bf5e096755408494dd5ffa9a0c
Vishwac Sena Kannan <vkannan@microsoft.com>
2019-05-01 20:33:18 -0700

    Finalizing todo bot - decalarative

--- experimental/adaptive-dialog/declarative/luis-bot/Program.cs
+++ experimental/adaptive-dialog/declarative/luis-bot/Program.cs
@@ -26,8 +26,8 @@ namespace Microsoft.BotBuilderSamples
                     // add luis LU model environment settings
                     var env = hostingContext.HostingEnvironment;
                     var luisAuthoringRegion = Environment.GetEnvironmentVariable("LUIS_AUTHORING_REGION") ?? "westus";
-                    config.AddJsonFile($"luis.settings.{env.EnvironmentName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true);
-                    config.AddJsonFile($"luis.settings.{Environment.UserName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile($"generated\\luis.settings.{env.EnvironmentName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile($"generated\\luis.settings.{Environment.UserName}.{luisAuthoringRegion}.json", optional: true, reloadOnChange: true);
                 })
             .UseStartup<Startup>()
             .Build();
