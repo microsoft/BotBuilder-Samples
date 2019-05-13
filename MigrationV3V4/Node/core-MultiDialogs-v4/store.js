29f539067b6a4e0b7cdb35a47478b2b14c6b3bd6
JSpru <v-jonspr@microsoft.com>
2019-05-13 15:47:26 -0700

    Changes to align code w generator (#1505)

--- MigrationV3V4/Node/core-MultiDialogs-v4/store.js
+++ MigrationV3V4/Node/core-MultiDialogs-v4/store.js
@@ -2,23 +2,23 @@
 // Licensed under the MIT License.
 
 module.exports = {
-    searchHotels: (destination, checkInDate, checkOutDate) => {
-        return new Promise((resolve) => {
+    searchHotels: destination => {
+        return new Promise(resolve => {
 
             // Filling the hotels results manually just for demo purposes
             const hotels = [];
-            for (var i = 1; i <= 5; i++) {
+            for (let i = 1; i <= 5; i++) {
                 hotels.push({
-                    name: destination + ' Hotel ' + i,
+                    name: `${destination} Hotel ${i}`,
                     location: destination,
                     rating: Math.ceil(Math.random() * 5),
                     numberOfReviews: Math.floor(Math.random() * 5000) + 1,
                     priceStarting: Math.floor(Math.random() * 450) + 80,
-                    image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+' + i + '&w=500&h=260'
+                    image: `https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel${i}&w=500&h=260`
                 });
             }
 
-            hotels.sort(function (a, b) { return a.priceStarting - b.priceStarting; });
+            hotels.sort((a, b) => a.priceStarting - b.priceStarting);
 
             // complete promise with a timer to simulate async response
             setTimeout(() => { resolve(hotels); }, 1000);
