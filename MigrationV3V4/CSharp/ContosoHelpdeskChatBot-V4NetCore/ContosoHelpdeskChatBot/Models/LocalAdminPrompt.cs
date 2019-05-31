d65f9a86910ce141fd239a5cc1a6e85054d96f58
Eric Dahlvang <v-eridah@microsoft.com>
2019-05-31 11:53:52 -0700

    Add NetCore ContosoHelpdeskChatBot (#1545)

---
+++ MigrationV3V4/CSharp/ContosoHelpdeskChatBot-V4NetCore/ContosoHelpdeskChatBot/Models/LocalAdminPrompt.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Bot.Builder.Community.Dialogs.FormFlow;
+
+namespace ContosoHelpdeskChatBot.Models
+{
+    public class LocalAdminPrompt
+    {
+        [Prompt("What is the machine name to add you to local admin group?")]
+        public string MachineName { get; set; }
+
+        [Prompt("How many days do you need the admin access?")]
+        public int? AdminDuration { get; set; }
+    }
+}
