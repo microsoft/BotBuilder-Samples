aeca49120879cc6c946312d9062b51b7f877cc31
Thulani Chivandikwa <thulani.chivandikwa@coolblue.nl>
2019-06-10 18:58:00 +0200

    Making it easy to read through samples by created named groups with spacing

--- samples/csharp_dotnetcore/40.timex-resolution/Program.cs
+++ samples/csharp_dotnetcore/40.timex-resolution/Program.cs
@@ -1,24 +1,37 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+
 namespace Microsoft.BotBuilderSamples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Creating TIMEX expressions from natural language using the Recognizer package.
-            Ambiguity.DateAmbiguity();
-            Ambiguity.TimeAmbiguity();
-            Ambiguity.DateTimeAmbiguity();
-            Ranges.DateRange();
-            Ranges.TimeRange();
+            var samples = new List<Action>
+            {
+                // Creating TIMEX expressions from natural language using the Recognizer package.
+                Ambiguity.DateAmbiguity,
+                Ambiguity.TimeAmbiguity,
+                Ambiguity.DateTimeAmbiguity,
+                Ranges.DateRange,
+                Ranges.TimeRange,
 
-            // Manipulating TIMEX expressions in code using the TIMEX Datatype package.
-            Parsing.Examples();
-            LanguageGeneration.Examples();
-            Resolution.Examples();
-            Constraints.Examples();
+                // Manipulating TIMEX expressions in code using the TIMEX Datatype package.
+                Parsing.Examples,
+                LanguageGeneration.Examples,
+                Resolution.Examples,
+                Constraints.Examples
+            };
+
+            foreach (var sample in samples)
+            {
+                Console.WriteLine(sample.Method.Name);
+                sample();
+                Console.WriteLine();
+            }
         }
     }
 }
