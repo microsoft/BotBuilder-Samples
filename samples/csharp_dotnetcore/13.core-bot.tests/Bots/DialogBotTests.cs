98e4cdff65b69b8175a715a05ae50ab833048138
Gabo Gilabert <ggilaber@microsoft.com>
2019-06-20 22:34:19 -0400

    Added UnitTests for CoreBot

---
+++ samples/csharp_dotnetcore/13.core-bot.tests/Bots/DialogBotTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.BotBuilderSamples.Bots;
+using Microsoft.BotBuilderSamples.Tests.Framework;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace CoreBot.Tests.Bots
+{
+    public class DialogBotTests
+    {
+        [Fact]
+        public async Task LogsInformationToILogger()
+        {
+            // Arrange
+            var memoryStorage = new MemoryStorage();
+            var conversationState = new ConversationState(memoryStorage);
+            var userState = new UserState(memoryStorage);
+
+            var mockRootDialog = SimpleMockFactory.CreateMockDialog<Dialog>(null, "mockRootDialog");
+            var mockLogger = new Mock<ILogger<DialogBot<Dialog>>>();
+            mockLogger.Setup(x =>
+                x.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<object>(), null, It.IsAny<Func<object, Exception, string>>()));
+
+            // Run the bot
+            var sut = new DialogBot<Dialog>(conversationState, userState, mockRootDialog.Object, mockLogger.Object);
+            var testAdapter = new TestAdapter();
+            var testFlow = new TestFlow(testAdapter, sut);
+            await testFlow.Send("Hi").StartTestAsync();
+
+            // Assert that log was changed with the expected parameters
+            mockLogger.Verify(
+                x => x.Log(
+                    LogLevel.Information,
+                    It.IsAny<EventId>(),
+                    It.Is<object>(o => o.ToString() == "Running dialog with Message Activity."),
+                    null,
+                    It.IsAny<Func<object, Exception, string>>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task SavesTurnStateUsingMockWithVirtualSaveChangesAsync()
+        {
+            // Note: this test requires that SaveChangesAsync is made virtual in order to be able to create a mock.
+            var memoryStorage = new MemoryStorage();
+            var mockConversationState = new Mock<ConversationState>(memoryStorage)
+            {
+                CallBase = true,
+            };
+
+            var mockUserState = new Mock<UserState>(memoryStorage)
+            {
+                CallBase = true,
+            };
+
+            var mockRootDialog = SimpleMockFactory.CreateMockDialog<Dialog>(null, "mockRootDialog");
+            var mockLogger = new Mock<ILogger<DialogBot<Dialog>>>();
+
+            // Act
+            var sut = new DialogBot<Dialog>(mockConversationState.Object, mockUserState.Object, mockRootDialog.Object, mockLogger.Object);
+            var testAdapter = new TestAdapter();
+            var testFlow = new TestFlow(testAdapter, sut);
+            await testFlow.Send("Hi").StartTestAsync();
+
+            // Assert that SaveChangesAsync was called
+            mockConversationState.Verify(x => x.SaveChangesAsync(It.IsAny<TurnContext>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()), Times.Once);
+            mockUserState.Verify(x => x.SaveChangesAsync(It.IsAny<TurnContext>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
---
+++ samples/csharp_dotnetcore/13.core-bot.tests/Dialogs/BookingDialogTests.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using CoreBot.Tests.Dialogs.TestData;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Testing;
+using Microsoft.Bot.Builder.Testing.XUnit;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using Microsoft.BotBuilderSamples;
+using Microsoft.BotBuilderSamples.Dialogs;
+using Microsoft.BotBuilderSamples.Tests.Framework;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreBot.Tests.Dialogs
+{
+    public class BookingDialogTests : BotTestBase
+    {
+        private readonly XUnitOutputMiddleware[] _middlewares;
+
+        public BookingDialogTests(ITestOutputHelper output)
+            : base(output)
+        {
+            _middlewares = new[] { new XUnitOutputMiddleware(output) };
+        }
+
+        [Fact]
+        public void DialogConstructor()
+        {
+            var sut = new BookingDialog();
+
+            Assert.Equal("BookingDialog", sut.Id);
+            Assert.IsType<TextPrompt>(sut.FindDialog("TextPrompt"));
+            Assert.IsType<ConfirmPrompt>(sut.FindDialog("ConfirmPrompt"));
+            Assert.IsType<DateResolverDialog>(sut.FindDialog("DateResolverDialog"));
+            Assert.IsType<WaterfallDialog>(sut.FindDialog("WaterfallDialog"));
+        }
+
+        [Theory]
+        [MemberData(nameof(BookingDialogTestsDataGenerator.BookingFlows), MemberType = typeof(BookingDialogTestsDataGenerator))]
+        public async Task DialogFlowUseCases(TestDataObject testData)
+        {
+            // Arrange
+            var bookingTestData = testData.GetObject<BookingDialogTestCase>();
+            var sut = new BookingDialog();
+            var testClient = new DialogTestClient(Channels.Test, sut, bookingTestData.InitialBookingDetails, _middlewares);
+
+            // Act/Assert
+            Output.WriteLine($"Test Case: {bookingTestData.Name}");
+            for (var i = 0; i < bookingTestData.UtterancesAndReplies.GetLength(0); i++)
+            {
+                var reply = await testClient.SendActivityAsync<IMessageActivity>(bookingTestData.UtterancesAndReplies[i, 0]);
+                Assert.Equal(bookingTestData.UtterancesAndReplies[i, 1], reply?.Text);
+            }
+
+            var bookingResults = (BookingDetails)testClient.DialogTurnResult.Result;
+            Assert.Equal(bookingTestData.ExpectedBookingDetails?.Origin, bookingResults?.Origin);
+            Assert.Equal(bookingTestData.ExpectedBookingDetails?.Destination, bookingResults?.Destination);
+            Assert.Equal(bookingTestData.ExpectedBookingDetails?.TravelDate, bookingResults?.TravelDate);
+        }
+
+        [Theory]
+        [MemberData(nameof(BookingDialogTestsDataGenerator.CancelFlows), MemberType = typeof(BookingDialogTestsDataGenerator))]
+        public async Task ShouldBeAbleToCancelAtAnyTime(TestDataObject testData)
+        {
+            // Arrange
+            var bookingTestData = testData.GetObject<BookingDialogTestCase>();
+            var sut = new BookingDialog();
+            var testClient = new DialogTestClient(Channels.Test, sut, bookingTestData.InitialBookingDetails, _middlewares);
+
+            // Act/Assert
+            Output.WriteLine($"Test Case: {bookingTestData.Name}");
+            for (var i = 0; i < bookingTestData.UtterancesAndReplies.GetLength(0); i++)
+            {
+                var reply = await testClient.SendActivityAsync<IMessageActivity>(bookingTestData.UtterancesAndReplies[i, 0]);
+                Assert.Equal(bookingTestData.UtterancesAndReplies[i, 1], reply.Text);
+            }
+
+            Assert.Equal(DialogTurnStatus.Cancelled, testClient.DialogTurnResult.Status);
+            Assert.Null(testClient.DialogTurnResult.Result);
+        }
+    }
+}
