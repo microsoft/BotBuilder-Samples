edfa9405abaca8cbf1aacfb68824322f05f410fe
John Taylor <johtaylo@microsoft.com>
2019-06-27 16:15:39 -0700

    code review feedback

--- samples/javascript_nodejs/46.teams/bots/teamsBot.js
+++ samples/javascript_nodejs/46.teams/bots/teamsBot.js
@@ -9,16 +9,15 @@ class TeamsBot extends DialogBot {
      * @param {ConversationState} conversationState
      * @param {UserState} userState
      * @param {Dialog} dialog
-     * @param {any} logger object for logging events, defaults to console if none is provided
      */
-    constructor(conversationState, userState, dialog, logger) {
-        super(conversationState, userState, dialog, logger);
+    constructor(conversationState, userState, dialog) {
+        super(conversationState, userState, dialog);
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Welcome to AuthenticationBot. Type anything to get logged in. Type \'logout\' to sign-out.');
+                    await context.sendActivity('Welcome to TeamsBot. Type anything to get logged in. Type \'logout\' to sign-out.');
                 }
             }
 
