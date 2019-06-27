925d25eea284b83af13f7dc0c3f1f93942f2d5a4
Scott Gellock <sgellock@gmail.com>
2019-06-27 11:18:01 -0700

    Fixes 1558 - corebot should work in gov cloud
    Update AdapterWithErrorHandler to use IConfiguration
    Update csproj to use 4.4.5 bot fx
    Remove ICredentialProvider from startup.cs

--- samples/csharp_dotnetcore/13.core-bot/AdapterWithErrorHandler.cs
+++ samples/csharp_dotnetcore/13.core-bot/AdapterWithErrorHandler.cs
@@ -4,16 +4,16 @@
 using System;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.BotBuilderSamples
 {
     public class AdapterWithErrorHandler : BotFrameworkHttpAdapter
     {
-        public AdapterWithErrorHandler(ICredentialProvider credentialProvider, ILogger<BotFrameworkHttpAdapter> logger, ConversationState conversationState = null)
-            : base(credentialProvider)
+        public AdapterWithErrorHandler(IConfiguration configuration, ILogger<BotFrameworkHttpAdapter> logger, ConversationState conversationState = null)
+            : base(configuration, logger)
         {
             OnTurnError = async (turnContext, exception) =>
             {
