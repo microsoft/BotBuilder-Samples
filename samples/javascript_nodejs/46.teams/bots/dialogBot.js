f0fcb392d1ea0fb4bdae77a390417136d9747996
johnataylor <johtaylo@microsoft.com>
2019-06-28 10:13:18 -0700

    code review feedback (#1608)

--- samples/javascript_nodejs/46.teams/bots/dialogBot.js
+++ samples/javascript_nodejs/46.teams/bots/dialogBot.js
@@ -9,26 +9,20 @@ class DialogBot extends TeamsActivityHandler {
      * @param {ConversationState} conversationState
      * @param {UserState} userState
      * @param {Dialog} dialog
-     * @param {any} logger object for logging events, defaults to console if none is provided
      */
-    constructor(conversationState, userState, dialog, logger) {
+    constructor(conversationState, userState, dialog) {
         super();
         if (!conversationState) throw new Error('[DialogBot]: Missing parameter. conversationState is required');
         if (!userState) throw new Error('[DialogBot]: Missing parameter. userState is required');
         if (!dialog) throw new Error('[DialogBot]: Missing parameter. dialog is required');
-        if (!logger) {
-            logger = console;
-            logger.log('[DialogBot]: logger not passed in, defaulting to console');
-        }
 
         this.conversationState = conversationState;
         this.userState = userState;
         this.dialog = dialog;
-        this.logger = logger;
         this.dialogState = this.conversationState.createProperty('DialogState');
 
         this.onMessage(async (context, next) => {
-            this.logger.log('Running dialog with Message Activity.');
+            console.log('Running dialog with Message Activity.');
 
             // Run the Dialog with the new message Activity.
             await this.dialog.run(context, this.dialogState);
