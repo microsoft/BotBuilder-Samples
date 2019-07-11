659b6277536e56b384e3944e47501669678c2c8a
Vishwac Sena Kannan <vkannan@microsoft.com>
2019-07-10 17:47:10 -0700

    Updates to 4.6 preview bits for multi-turn prompt and cards bot.

--- experimental/adaptive-dialog/csharp_dotnetcore/02.multi-turn-prompt/Dialogs/RootDialog.cs
+++ experimental/adaptive-dialog/csharp_dotnetcore/02.multi-turn-prompt/Dialogs/RootDialog.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Input;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Rules;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Steps;
-using Microsoft.Bot.Builder.Expressions.Parser;
+using Microsoft.Bot.Builder.LanguageGeneration;
+using Microsoft.Bot.Schema;
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -12,12 +16,32 @@ namespace Microsoft.BotBuilderSamples
         public RootDialog()
             : base(nameof(RootDialog))
         {
-
+            string[] paths = { ".", "Dialogs", "RootDialog.LG" };
+            string fullPath = Path.Combine(paths);
             // Create instance of adaptive dialog. 
             var rootDialog = new AdaptiveDialog(nameof(AdaptiveDialog))
             {
                 // These steps are executed when this Adaptive Dialog begins
-                Steps = OnBeginDialogSteps(),
+                Rules = new List<IRule>()
+                {
+                    // Add a rule to welcome user
+                    new ConversationUpdateActivityRule()
+                    {
+                        Steps = WelcomeUserSteps()
+                    },
+
+                    // Respond to user on message activity
+                    new EventRule()
+                    {
+                        Events = new List<String> ()
+                        {
+                            AdaptiveEvents.BeginDialog
+                        },
+                        Constraint = $"turn.activity.type == '{ActivityTypes.Message}'",
+                        Steps = OnBeginDialogSteps()
+                    }
+                },
+                Generator = new TemplateEngineLanguageGenerator(new TemplateEngine().AddFile(fullPath))
             };
 
             // Add named dialogs to the DialogSet. These names are saved in the dialog state.
@@ -26,6 +50,34 @@ namespace Microsoft.BotBuilderSamples
             // The initial child Dialog to run.
             InitialDialogId = nameof(AdaptiveDialog);
         }
+
+        private static List<IDialog> WelcomeUserSteps()
+        {
+            return new List<IDialog>()
+            {
+                // Iterate through membersAdded list and greet user added to the conversation.
+                new Foreach()
+                {
+                    ListProperty = "turn.activity.membersAdded",
+                    ValueProperty = "turn.memberAdded",
+                    Steps = new List<IDialog>()
+                    {
+                        // Note: Some channels send two conversation update events - one for the Bot added to the conversation and another for user.
+                        // Filter cases where the bot itself is the recipient of the message. 
+                        new IfCondition()
+                        {
+                            Condition = "turn.memberAdded.name != turn.activity.recipient.name",
+                            Steps = new List<IDialog>()
+                            {
+                                new SendActivity("Hello, I'm the cards bot. Please send a message to get started!")
+                            }
+                        }
+                    }
+                }
+            };
+
+        }
+
         private static List<IDialog> OnBeginDialogSteps()
         {
             return new List<IDialog>()
@@ -55,7 +107,7 @@ namespace Microsoft.BotBuilderSamples
                 {
                     // All conditions are expressed using the common expression language.
                     // See https://github.com/Microsoft/BotBuilder-Samples/tree/master/experimental/common-expression-language to learn more
-                    Condition = new ExpressionEngine().Parse("turn.ageConfirmation == true"),
+                    Condition = "turn.ageConfirmation == true",
                     Steps = new List<IDialog>()
                     {
                          new NumberInput()
@@ -63,10 +115,10 @@ namespace Microsoft.BotBuilderSamples
                              Prompt = new ActivityTemplate("[AskForAge]"),
                              Property = "user.userProfile.Age",
                              // Set min and max constraints for age.
-                             MinValue = 1,
-                             MaxValue = 150,
+                             //MinValue = 1,
+                             //MaxValue = 150,
                              // Specify a retry prompt if user input does not meet min or max constraint.
-                             RetryPrompt = new ActivityTemplate("[AskForAge.reprompt]")
+                             //RetryPrompt = new ActivityTemplate("[AskForAge.reprompt]")
 
                          },
                          new SendActivity("[UserAgeReadBack]")
