2d6896b8cd736d66314167366b801f0eaf8afe2e
WashingtonKayaker <stevem@terawe.com>
2019-08-06 17:51:47 -0700

    New Sample App
    Sample app to show QnA Maker with Telemetry integrated

---
+++ samples/csharp_dotnetcore/22.qnamaker-App_insights/MyQnAMaker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class MyQnAMaker : QnAMaker
+    {
+        public MyQnAMaker(
+            QnAMakerEndpoint endpoint,
+            QnAMakerOptions options = null,
+            HttpClient httpClient = null,
+            IBotTelemetryClient telemetryClient = null,
+            bool logPersonalInformation = false)
+            : base(endpoint, options, httpClient, telemetryClient, logPersonalInformation)
+        {
+
+        }
+
+        protected override async Task OnQnaResultsAsync(
+                            QueryResult[] queryResults,
+                            Microsoft.Bot.Builder.ITurnContext turnContext,
+                            Dictionary<string, string> telemetryProperties = null,
+                            Dictionary<string, double> telemetryMetrics = null,
+                            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var eventData = await FillQnAEventAsync(
+                                    queryResults,
+                                    turnContext,
+                                    telemetryProperties,
+                                    telemetryMetrics,
+                                    cancellationToken)
+                                .ConfigureAwait(false);
+
+            // Add my property
+            eventData.Properties.Add("MyImportantProperty", "myImportantValue");
+
+            // Log QnaMessage event
+            TelemetryClient.TrackEvent(
+                            QnATelemetryConstants.QnaMsgEvent,
+                            eventData.Properties,
+                            eventData.Metrics
+                            );
+
+            // Create second event.
+            var secondEventProperties = new Dictionary<string, string>();
+            secondEventProperties.Add("MyImportantProperty2",
+                                        "myImportantValue2");
+            TelemetryClient.TrackEvent(
+                            "MySecondEvent",
+                            secondEventProperties);
+        }
+    }
+}
---
+++ samples/csharp_dotnetcore/22.qnamaker-App_insights/Startup.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Bot.Builder.ApplicationInsights;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Add the HttpClientFactory to be used for the QnAMaker calls.
+            services.AddHttpClient();
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Create the Bot Framework Adapter with error handling enabled. 
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+
+
+            #region Telemetry code
+            // Add Application Insights services into service collection
+            services.AddApplicationInsightsTelemetry();
+
+            // Create the telemetry client.
+            services.AddSingleton<IBotTelemetryClient, BotTelemetryClient>();
+
+            // Add ASP middleware to store the http body mapped with bot activity key in the httpcontext.items. This will be picked by the TelemetryBotIdInitializer
+            services.AddTransient<TelemetrySaveBodyASPMiddleware>();
+
+            // Add telemetry initializer that will set the correlation context for all telemetry items.
+            services.AddSingleton<ITelemetryInitializer, OperationCorrelationTelemetryInitializer>();
+
+            // Add telemetry initializer that sets the user ID and session ID (in addition to other bot-specific properties such as activity ID)
+            services.AddSingleton<ITelemetryInitializer, TelemetryBotIdInitializer>();
+
+            // Create the telemetry middleware to track conversation events
+            services.AddSingleton<IMiddleware, TelemetryLoggerMiddleware>();
+            #endregion
+
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, QnABot>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseHttpsRedirection();
+            app.UseBotApplicationInsights();
+            app.UseMvc();
+        }
+    }
+}
