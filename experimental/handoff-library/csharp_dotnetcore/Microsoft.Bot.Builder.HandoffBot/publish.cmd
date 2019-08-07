081eb95b6cfc276284c7678ca2afdbca9f63f838
Artur Laksberg <arturl@microsoft.com>
2019-08-07 10:46:25 -0700

    update

--- experimental/handoff-library/csharp_dotnetcore/Microsoft.Bot.Builder.HandoffBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=arturl-handoff-bot-bd6d-Web-Deploy.pubxml -p:Password=0xscoKlgAzQZ38o6369ctvpkcPFBtNi49e6013ZduaRYgon6vwDMy9nwe5LY
-
