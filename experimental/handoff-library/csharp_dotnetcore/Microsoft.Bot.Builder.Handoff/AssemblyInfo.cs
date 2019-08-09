8af4deb499453784766d1f6d884b6fb2080eed73
Artur Laksberg <arturl@microsoft.com>
2019-08-09 14:38:06 -0700

    Handoff Library (#1691)
    * Handoff Library
    
    moved from https://github.com/microsoft/botbuilder-dotnet
    
    * Readme fix
    
    * update
    
    * Addressed PR feedback
    
    * Use ArgumentNullException
    
    * Renamed class TurnContextExtender to HandoffExtensions
    
    * Moved bot to samples folder
    
    * New forwarding constructors for BotFrameworkHttpAdapterWithHandoff

---
+++ experimental/handoff-library/csharp_dotnetcore/Microsoft.Bot.Builder.Handoff/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+
+#if DEBUG
+[assembly: InternalsVisibleTo("Microsoft.Bot.Builder.Handoff.Test")]
+#endif
---
+++ experimental/handoff-library/csharp_dotnetcore/samples/Microsoft.Bot.Builder.HandoffBot/Bots/EchoBot.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Builder.Handoff;
+
+namespace Microsoft.Bot.Builder.EchoBot
+{
+    public class EchoBot : ActivityHandlerWithHandoff
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Text.Contains("human"))
+            {
+                await turnContext.SendActivityAsync("You will be transferred to a human agent. Sit tight.");
+
+                var a1 = MessageFactory.Text($"first message");
+                var a2 = MessageFactory.Text($"second message");
+                var transcript = new Activity[] { a1, a2 };
+                var context = new { Skill = "credit cards", MSCallerId = "CCI" };
+                var request = await turnContext.InitiateHandoffAsync(transcript, context, cancellationToken);
+
+                if (await request.IsCompletedAsync())
+                {
+                    await turnContext.SendActivityAsync("Handoff request has been completed");
+                }
+                else
+                {
+                    await turnContext.SendActivityAsync("Handoff request has NOT been completed");
+                }
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+            }
+        }
+
+        protected override async Task OnHandoffActivityAsync(ITurnContext<IHandoffActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var conversationId = turnContext.Activity.Conversation.Id;
+            await turnContext.SendActivityAsync($"Received Handoff ack for conversation {conversationId}");
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome!"), cancellationToken);
+                }
+            }
+        }
+    }
+}
