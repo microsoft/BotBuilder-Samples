febe2b33edac07936cbe2ffc099bb02b31692f37
Scott Gellock <scott.gellock@microsoft.com>
2019-08-27 09:10:03 -0700

    Fixes 1710 - 1.console-echo bot does not exit after type 'quit' (#1716)
    * Fixes 1710 - charp-dotnetcore 1.console-echo bot does not exit after typing 'quit'
    
    - added code to condition for `quit`
    
    * Fixes 1710 - charp-dotnetcore 1.console-echo bot does not exit after typing 'quit'
    
    - added code to condition for `quit`
    
    * Update EchoBot.cs
    
    Moved { to next line

--- samples/csharp_dotnetcore/01.console-echo/Program.cs
+++ samples/csharp_dotnetcore/01.console-echo/Program.cs
@@ -9,7 +9,8 @@ namespace Console_EchoBot
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the EchoBot. Type something to get started.");
+            Console.WriteLine("Console EchoBot is online. I will repeat any message you send me!");
+            Console.WriteLine("Say \"quit\" to end.");
 
             // Create the Console Adapter, and add Conversation State
             // to the Bot. The Conversation State will be stored in memory.
