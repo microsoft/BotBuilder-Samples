99e0b3a3e7ad07a882e3babab10e00df5e85a253
John Taylor <johtaylo@microsoft.com>
2019-09-04 09:38:33 -0700

    finish ConversationUpdate

---
+++ experimental/teams/csharp-dotnetcore/Microsoft.Bot.Builder.Teams/ActivityExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder
+{
+    public static class TeamsActivityExtensions
+    {
+        public static Activity CreateReply(this IActivity activity, string text = null, string locale = null)
+        {
+            return ((Activity)activity).CreateReply(text, locale);
+        }
+    }
+}
--- experimental/teams/csharp-dotnetcore/Scenarios/ConversationUpdate/Bots/ConversationUpdateBot.cs
+++ experimental/teams/csharp-dotnetcore/Scenarios/ConversationUpdate/Bots/ConversationUpdateBot.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
@@ -11,16 +13,50 @@ namespace Microsoft.BotBuilderSamples.Bots
 {
     public class ConversationUpdateBot : TeamsActivityHandler
     {
-        protected async override Task OnChannelRenamedEventAsync(TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        protected override async Task OnChannelRenamedEventAsync(TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var reply = ((Activity)turnContext.Activity).CreateReply();
+            var reply = turnContext.Activity.CreateReply();
+            var heroCard = new HeroCard(text: $"{channelData.Channel.Name} is the new Channel name");
+            reply.Attachments.Add(heroCard.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
 
-            var heroCard = new HeroCard
-            {
-                Text = $"{channelData.Channel.Name} is the new Channel name"
+        protected override async Task OnChannelCreatedEventAsync(TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = turnContext.Activity.CreateReply();
+            var heroCard = new HeroCard(text: $"{channelData.Channel.Name} is the Channel created");
+            reply.Attachments.Add(heroCard.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
 
-            };
+        protected override async Task OnChannelDeletedEventAsync(TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = turnContext.Activity.CreateReply();
+            var heroCard = new HeroCard(text: $"{channelData.Channel.Name} is the Channel deleted");
+            reply.Attachments.Add(heroCard.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
 
+        protected override async Task OnTeamRenamedEventAsync(TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = turnContext.Activity.CreateReply();
+            var heroCard = new HeroCard(text: $"{channelData.Channel.Name} is the new Team name");
+            reply.Attachments.Add(heroCard.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
+        protected override async Task OnTeamMembersAddedEventAsync(IList<ChannelAccount> membersAdded, TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = turnContext.Activity.CreateReply();
+            var heroCard = new HeroCard(text: $"{string.Join(' ', membersAdded.Select(member => member.Id))} joined {channelData.Team.Name}");
+            reply.Attachments.Add(heroCard.ToAttachment());
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
+        protected override async Task OnTeamMembersRemovedEventAsync(IList<ChannelAccount> membersRemoved, TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var reply = turnContext.Activity.CreateReply();
+            var heroCard = new HeroCard(text: $"{string.Join(' ', membersRemoved.Select(member => member.Id))} joined {channelData.Team.Name}");
             reply.Attachments.Add(heroCard.ToAttachment());
             await turnContext.SendActivityAsync(reply, cancellationToken);
         }
