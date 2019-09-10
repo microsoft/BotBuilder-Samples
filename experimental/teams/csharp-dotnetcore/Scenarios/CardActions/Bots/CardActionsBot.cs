c03ce55ac4bcf000b7cdf61418b58f70e42d90c5
John Taylor <johtaylo@microsoft.com>
2019-09-09 18:33:17 -0700

    adaptive cards

--- experimental/teams/csharp-dotnetcore/Scenarios/CardActions/Bots/CardActionsBot.cs
+++ experimental/teams/csharp-dotnetcore/Scenarios/CardActions/Bots/CardActionsBot.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using AdaptiveCards;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams.Internal;
 using Microsoft.Bot.Connector;
@@ -19,56 +19,78 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            //await turnContext.SendActivityAsync(MessageFactory.Text($"MessagingExtensions echo: {turnContext.Activity.Text}"), cancellationToken);
 
-            var connectorClient = turnContext.TurnState.Get<IConnectorClient>();
-            var teamsConnectorClient = new TeamsConnectorClient(connectorClient.Credentials);
-            teamsConnectorClient.BaseUri = new Uri(turnContext.Activity.ServiceUrl);
-            var teamsContext = new TeamsContext(turnContext, teamsConnectorClient);
-
-            string actualText = teamsContext.GetActivityTextWithoutMentions();
-            if (actualText.Equals("Cards", StringComparison.OrdinalIgnoreCase))
+            if (turnContext.Activity.Text != null)
             {
-                // Demo card 1 - adaptive card with bot bulder actions
-                AdaptiveCards.AdaptiveCard adaptiveCard = new AdaptiveCards.AdaptiveCard();
-                adaptiveCard.Body.Add(new AdaptiveCards.AdaptiveTextBlock("Bot Builder actions"));
+                var teamsContext = new TeamsContext(turnContext, null);
+                string actualText = teamsContext.GetActivityTextWithoutMentions();
 
-                var action1 = new CardAction("imback", "imBack", null, null, null, "text");
-                var action2 = new CardAction("messageBack", "message back", null, "text received by bots", "text display to users", JObject.Parse(@"{ ""key"" : ""value"" }"));
-                var action3 = new CardAction("invoke", "invoke", null, null, null, JObject.Parse(@"{ ""key"" : ""value"" }"));
-                var action4 = new CardAction("openUrl", "Buckethead", null, null, null, "https://en.wikipedia.org/wiki/Buckethead");
-                var action5 = new CardAction("invoke", "a special invoke", null, "text on an invoke", "display text", JObject.Parse(@"{ ""key"" : ""value"" }"));
-                adaptiveCard.Actions.Add(action1.ToAdaptiveCardAction());
-                adaptiveCard.Actions.Add(action2.ToAdaptiveCardAction());
-                adaptiveCard.Actions.Add(action3.ToAdaptiveCardAction());
-                adaptiveCard.Actions.Add(action4.ToAdaptiveCardAction());
-                adaptiveCard.Actions.Add(action5.ToAdaptiveCardAction());
+                if (actualText.Equals("1"))
+                {
+                    var adaptiveCard = new AdaptiveCard();
+                    adaptiveCard.Body.Add(new AdaptiveTextBlock("Bot Builder actions"));
 
-                // Task module action
-                var taskModuleAction = new TaskModuleAction("Launch Task Module", @"{ ""hiddenKey"": ""hidden value from task module launcher"" }");
+                    var action1 = new CardAction(ActionTypes.ImBack, "imBack", null, null, null, "text");
+                    var action2 = new CardAction(ActionTypes.MessageBack, "message back", null, null, null, JObject.Parse(@"{ ""key"" : ""value"" }"));
+                    var action3 = new CardAction(ActionTypes.MessageBack, "message back local echo", null, "text received by bots", "display text message back", JObject.Parse(@"{ ""key"" : ""value"" }"));
+                    var action4 = new CardAction("invoke", "invoke", null, null, null, JObject.Parse(@"{ ""key"" : ""value"" }"));
+                    adaptiveCard.Actions.Add(action1.ToAdaptiveCardAction());
+                    adaptiveCard.Actions.Add(action2.ToAdaptiveCardAction());
+                    adaptiveCard.Actions.Add(action3.ToAdaptiveCardAction());
+                    adaptiveCard.Actions.Add(action4.ToAdaptiveCardAction());
 
-                // Demo card 2 - launch task module from adaptive card
-                AdaptiveCards.AdaptiveCard taskModuleCard1 = new AdaptiveCards.AdaptiveCard();
-                taskModuleCard1.Body.Add(new AdaptiveCards.AdaptiveTextBlock("Task Module Adaptive Card"));
-                taskModuleCard1.Actions.Add(taskModuleAction.ToAdaptiveCardAction());
+                    var replyActivity = MessageFactory.Attachment(adaptiveCard.ToAttachment());
+                    await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+                }
+                else if (actualText.Equals("2"))
+                {
+                    var taskModuleAction = new TaskModuleAction("Launch Task Module", @"{ ""hiddenKey"": ""hidden value from task module launcher"" }");
 
-                // Demo card 3 - launch task module from hero card (or any bot-builder framework card)
-                HeroCard taskModuleCard2 = new HeroCard("Launch Task Module", null, null, null, new List<CardAction> { taskModuleAction });
+                    var adaptiveCard = new AdaptiveCard();
+                    adaptiveCard.Body.Add(new AdaptiveTextBlock("Task Module Adaptive Card"));
+                    adaptiveCard.Actions.Add(taskModuleAction.ToAdaptiveCardAction());
 
+                    var replyActivity = MessageFactory.Attachment(adaptiveCard.ToAttachment());
+                    await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+                }
+                else if (actualText.Equals("3"))
+                {
+                    var adaptiveCard = new AdaptiveCard();
+                    adaptiveCard.Body.Add(new AdaptiveTextBlock("Bot Builder actions"));
+                    adaptiveCard.Body.Add(new AdaptiveTextInput { Id = "x" });
+                    adaptiveCard.Actions.Add(new AdaptiveSubmitAction { Type = "Action.Submit", Title = "Action.Submit", Data = new JObject { { "key", "value" } } });
 
-                var replyActivity = MessageFactory.Attachment(new Attachment[] { adaptiveCard.ToAttachment(), taskModuleCard1.ToAttachment(), taskModuleCard2.ToAttachment() });
-
-                await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+                    var replyActivity = MessageFactory.Attachment(adaptiveCard.ToAttachment());
+                    await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+                }
+                else
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"You said: {turnContext.Activity.Text}"), cancellationToken);
+                }
             }
             else
             {
-                await turnContext.SendActivityAsync(MessageFactory.Text($"You said: {turnContext.Activity.Text}"), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text($"App sent a message with null text"), cancellationToken);
+
+                // TODO: process the response from the card
+
+                if (turnContext.Activity.Value != null)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"but with value {turnContext.Activity.Value}"), cancellationToken);
+                }
             }
         }
 
-        protected override Task<InvokeResponse> OnInvokeActivityAsync(ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
+        protected override async Task<InvokeResponse> OnInvokeActivityAsync(ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
         {
-            return base.OnInvokeActivityAsync(turnContext, cancellationToken);
+            if (turnContext.Activity.Name != null)
+            {
+                return await base.OnInvokeActivityAsync(turnContext, cancellationToken);
+            }
+
+            // TODO: process the response from the card
+
+            return new InvokeResponse { Status = 200 };
         }
     }
 }
