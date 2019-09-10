969ba1d76f15097b8d15557bbe497edb6a9ff73b
Eric Dahlvang <v-eridah@microsoft.com>
2019-09-10 16:37:52 -0700

    Add OnMembersAddedAsync and OnMembersRemovedAsync to ConversationUpdateBot

--- experimental/teams/csharp-dotnetcore/Scenarios/ConversationUpdate/Bots/ConversationUpdateBot.cs
+++ experimental/teams/csharp-dotnetcore/Scenarios/ConversationUpdate/Bots/ConversationUpdateBot.cs
@@ -45,7 +45,19 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnTeamMembersRemovedEventAsync(IList<ChannelAccount> membersRemoved, TeamsChannelData channelData, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var heroCard = new HeroCard(text: $"{string.Join(' ', membersRemoved.Select(member => member.Id))} joined {channelData.Team.Name}");
+            var heroCard = new HeroCard(text: $"{string.Join(' ', membersRemoved.Select(member => member.Id))} removed from {channelData.Team.Name}");
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(heroCard.ToAttachment()), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var heroCard = new HeroCard(text: $"{string.Join(' ', membersAdded.Select(member => member.Id))} joined {turnContext.Activity.Conversation.ConversationType}");
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(heroCard.ToAttachment()), cancellationToken);
+        }
+
+        protected override async Task OnMembersRemovedAsync(IList<ChannelAccount> membersRemoved, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var heroCard = new HeroCard(text: $"{string.Join(' ', membersRemoved.Select(member => member.Id))} removed from {turnContext.Activity.Conversation.ConversationType}");
             await turnContext.SendActivityAsync(MessageFactory.Attachment(heroCard.ToAttachment()), cancellationToken);
         }
     }
