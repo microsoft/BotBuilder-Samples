1ea573dbf4be88712d5615d66bc88bf49146f5a3
Wesley Wigham <wewigham@microsoft.com>
2019-09-10 15:38:59 -0700

    Add provisioning script to echo bot

--- generators/generator-botbuilder/generators/app/templates/echo/package.json.js
+++ generators/generator-botbuilder/generators/app/templates/echo/package.json.js
@@ -9,7 +9,8 @@
         "start": "node ./index.js",
         "watch": "nodemon ./index.js",
         "lint": "eslint .",
-        "test": "echo \"Error: no test specified\" && exit 1"
+        "test": "echo \"Error: no test specified\" && exit 1",
+        "provision": "node ./scripts/provision.js"
     },
     "repository": {
         "type": "git",
@@ -21,12 +22,18 @@
         "restify": "~8.3.3"
     },
     "devDependencies": {
+        "@azure/arm-resources": "~1.1.0",
+        "@azure/arm-subscriptions": "^2.0.0",
+        "@azure/graph": "~5.0.0",
+        "@azure/ms-rest-js": "^2.0.4",
+        "@azure/ms-rest-nodeauth": "~3.0.1",
         "eslint": "^6.0.1",
         "eslint-config-standard": "^13.0.1",
         "eslint-plugin-import": "^2.18.2",
         "eslint-plugin-node": "^9.1.0",
         "eslint-plugin-promise": "^4.2.1",
         "eslint-plugin-standard": "^4.0.0",
+        "prompts": "~2.2.1",
         "nodemon": "~1.19.1"
     }
 }
--- generators/generator-botbuilder/generators/app/templates/echo/package.json.ts
+++ generators/generator-botbuilder/generators/app/templates/echo/package.json.ts
@@ -11,7 +11,8 @@
         "postinstall": "npm run build && node ./deploymentScripts/webConfigPrep.js",
         "start": "tsc --build && node ./lib/index.js",
         "test": "echo \"Error: no test specified\" && exit 1",
-        "watch": "nodemon --watch ./src -e ts --exec \"npm run start\""
+        "watch": "nodemon --watch ./src -e ts --exec \"npm run start\"",
+        "provision": "node ./scripts/provision.js"
     },
     "repository": {
         "type": "git",
@@ -24,10 +25,17 @@
         "restify": "~8.3.3"
     },
     "devDependencies": {
+        "@azure/arm-resources": "~1.1.0",
+        "@azure/arm-subscriptions": "^2.0.0",
+        "@azure/graph": "~5.0.0",
+        "@azure/ms-rest-js": "^2.0.4",
+        "@azure/ms-rest-nodeauth": "~3.0.1",
         "@types/dotenv": "6.1.1",
+        "@types/prompts": "~2.0.1",
         "@types/restify": "7.2.12",
         "nodemon": "~1.19.1",
         "tslint": "~5.18.0",
+        "prompts": "~2.2.1",
         "typescript": "~3.5.3"
     }
 }
