93db43660dcda219fe2e296b0e06570fa4c2cbf9
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-09-11 13:31:38 -0300

    [AppInsights sample] Add dialogBot

---
+++ samples/javascript_nodejs/21.corebot-app-insights/bots/dialogBot.js
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler } = require('botbuilder');
+
+class DialogBot extends ActivityHandler {
+    /**
+     * 
+     * @param {ConversationState} conversationState 
+     * @param {UserState} userState 
+     * @param {Dialog} dialog 
+     */
+    constructor(conversationState, userState, dialog) {
+        super();
+
+        if (!conversationState) { throw new Error("[DialogBot]: Missing conversationState parameter."); }
+        if (!userState) { throw new Error("[DialogBot]: Missing userState parameter."); }
+        if (!dialog) { throw new Error("[DialogBot]: Missing dialog parameter."); }
+
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+
+        this.onTurn(async (turnContext, next) => {
+            await this.conversationState.saveChanges(turnContext, false);
+            await this.userState.saveChanges(turnContext, false);
+
+            await next();
+        });
+
+        this.onMessage(async (turnContext, next) => {
+            console.log('Running dialog with Message Activity.');
+            let dialogState = this.conversationState.createProperty('');
+            
+            await this.dialog.run(turnContext, dialogState);
+
+            await next();
+        });
+    }
+}
