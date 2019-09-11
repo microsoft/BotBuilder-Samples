0c1e07a7956f3501451c41006dc5911f96905faa
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-09-11 13:55:30 -0300

    [AppInsight sample] dialogAndWelcomeBot + resource

---
+++ samples/javascript_nodejs/21.corebot-app-insights/bots/dialogAndWelcomeBot.js
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { CardFactory } = require('botbuilder');
+const { DialogBot } = require('./dialogBot');
+const WelcomeCard = require('./resources/welcomeCard.json');
+
+class DialogAndWelcomeBot extends DialogBot {
+    /**
+     * 
+     * @param {ConversationState} conversationState 
+     * @param {UserState} userState 
+     * @param {Dialog} dialog 
+     */
+    constructor(conversationState, userState, dialog) {
+        super(conversationState, userState, dialog);
+
+        this.onMembersAdded(async (turnContext, next) => {
+            const membersAdded = turnContext.activity.membersAdded;
+
+            for (let pos = 0; pos < membersAdded.length; pos++) {                
+
+                if (membersAdded[pos].id != turnContext.activity.recipient.id) {
+                    const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
+                    await turnContext.sendActivity({ attachments: [welcomeCard] });
+                    await dialog.run(turnContext, conversationState.createProperty('DialogState'));
+                }
+            }
+
+            // Ensure next BotHandler is executed.
+            await next();
+        })
+    }
+}
+
+module.exports.DialogAndWelcomeBot = DialogAndWelcomeBot;
