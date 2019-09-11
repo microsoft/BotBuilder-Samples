f5643db223070472923e789bef0933cb4fb658d2
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-09-11 11:41:51 -0300

    [AppInsights Sample] Init project [WIP]

---
+++ samples/javascript_nodejs/21.corebot-app-insights/index.js
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
