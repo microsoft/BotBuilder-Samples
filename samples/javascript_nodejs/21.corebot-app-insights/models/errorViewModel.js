1fec03a48549350fd60d3ca87587609929e92e2f
Ali <mica.andini@gmail.com>
2019-09-11 15:31:08 -0300

    add header

--- samples/javascript_nodejs/21.corebot-app-insights/models/errorViewModel.js
+++ samples/javascript_nodejs/21.corebot-app-insights/models/errorViewModel.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 class ErrorViewModel {
     constructor(requestId = null) {
         this.requestId = requestId;
