f0a84d1c8ee6f3ea018b1e9b528a509505150367
Scott Gellock <sgellock@gmail.com>
2019-10-13 10:28:26 -0700

    update c# dotnet core and web api to use new OnTurnError pattern

--- samples/csharp_dotnetcore/02.echo-bot/AdapterWithErrorHandler.cs
+++ samples/csharp_dotnetcore/02.echo-bot/AdapterWithErrorHandler.cs
@@ -2,10 +2,14 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -17,11 +21,33 @@ namespace Microsoft.BotBuilderSamples
             OnTurnError = async (turnContext, exception) =>
             {
                 // Log any leaked exception from the application.
-                logger.LogError($"Exception caught : {exception.Message}");
+                logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
 
-                // Send a catch-all apology to the user.
-                await turnContext.SendActivityAsync("Sorry, it looks like something went wrong.");
+                // Send a message to the user
+                await turnContext.SendActivityAsync("The bot encounted an error or bug.");
+                await turnContext.SendActivityAsync("To continue to run this bot, please fix the bot source code.");
+
+                // Send a trace activity, which will be displayed in the Bot Framework Emulator
+                await SendTraceActivityAsync(turnContext, exception);
             };
+        }
+
+        private static async Task SendTraceActivityAsync(ITurnContext turnContext, Exception exception)
+        {
+            // Only send a trace activity if we're talking to the Bot Framework Emulator
+            if (turnContext.Activity.ChannelId == Channels.Emulator)
+            {
+                Activity traceActivity = new Activity(ActivityTypes.Trace)
+                {
+                    Label = "TurnError",
+                    Name = "OnTurnError Trace",
+                    Value = exception.Message,
+                    ValueType = "https://www.botframework.com/schemas/error",
+                };
+
+                // Send a trace activity
+                await turnContext.SendActivityAsync(traceActivity);
+            }
         }
     }
 }
