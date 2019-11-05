fe672e34f03854d72b07c6fdcaea5af2a47573f5
stevengum <14935595+stevengum@users.noreply.github.com>
2019-11-05 12:28:03 -0800

    update experimental dls node.js echo-bot

--- experimental/directline-speech/javascript_nodejs/02.echo-bot/bot.js
+++ experimental/directline-speech/javascript_nodejs/02.echo-bot/bot.js
@@ -8,7 +8,7 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+            await context.sendActivity(this.speak(`You said '${ context.activity.text }'`));
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
@@ -18,12 +18,18 @@ class EchoBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome!');
+                    await context.sendActivity(this.speak('Hello and welcome!'));
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    speak(text) {
+        const speak = `<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='Microsoft Server Speech Text to Speech Voice (en-US, JessaRUS)'>${text}</voice></speak>`;
+        const activity = { text, speak };
+        return activity;
     }
 }
 
