fe2f88ca885cb388afbf9c46fd42083332cd4434
Scott Gellock <sgellock@gmail.com>
2019-11-06 13:58:42 -0800

    Update VSIX to 4.6.2
    - update to use 4.6.2 bot fx
    - bump version number to 4.6.2
    - revert cancel all dialogs changes to test cases

--- generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/CoreBotWithTests/CoreBot.tests/Dialogs/BookingDialogTests.cs
+++ generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/CoreBotWithTests/CoreBot.tests/Dialogs/BookingDialogTests.cs
@@ -68,7 +68,7 @@ namespace $ext_safeprojectname$.Tests.Dialogs
                 Assert.Equal(bookingTestData.UtterancesAndReplies[i, 1], reply.Text);
             }
 
-            Assert.Equal(DialogTurnStatus.Cancelled, testClient.DialogTurnResult.Status);
+            Assert.Equal(DialogTurnStatus.Complete, testClient.DialogTurnResult.Status);
             Assert.Null(testClient.DialogTurnResult.Result);
         }
     }
