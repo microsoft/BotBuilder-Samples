b2b4cbc5e82113da84486e9b3add8698db1e4ed9
Tracy Boehrer <tracy.boehrer@gmail.com>
2019-11-08 14:04:30 -0600

    Fixes #2945 in 17.multilingual-bot

--- samples/csharp_dotnetcore/17.multilingual-bot/Translation/MicrosoftTranslator.cs
+++ samples/csharp_dotnetcore/17.multilingual-bot/Translation/MicrosoftTranslator.cs
@@ -22,12 +22,16 @@ namespace Microsoft.BotBuilderSamples.Translation
         private static HttpClient _client = new HttpClient();
 
         private readonly string _key;
+        private readonly string _region;
 
 
         public MicrosoftTranslator(IConfiguration configuration)
         {
             var key = configuration["TranslatorKey"];
             _key = key ?? throw new ArgumentNullException(nameof(key));
+
+            var region = configuration["TranslateRegion"];
+            _region = region == null ? "westus" : region;
         }
 
         public async Task<string> TranslateAsync(string text, string targetLocale, CancellationToken cancellationToken = default(CancellationToken))
@@ -43,6 +47,7 @@ namespace Microsoft.BotBuilderSamples.Translation
                 request.Method = HttpMethod.Post;
                 request.RequestUri = new Uri(uri);
                 request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Region", _region);
                 request.Headers.Add("Ocp-Apim-Subscription-Key", _key);
 
                 var response = await _client.SendAsync(request, cancellationToken);
