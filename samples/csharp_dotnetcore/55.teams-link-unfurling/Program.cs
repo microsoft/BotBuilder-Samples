aef4d7a43d1064e233283c721759df47afc65b4f
IgorHrabrov <18480638+IgorHrabrov@users.noreply.github.com>
2019-11-19 18:31:31 +0200

    Update Program.cs

--- samples/csharp_dotnetcore/55.teams-link-unfurling/Program.cs
+++ samples/csharp_dotnetcore/55.teams-link-unfurling/Program.cs
@@ -8,10 +8,7 @@ namespace Microsoft.BotBuilderSamples
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
