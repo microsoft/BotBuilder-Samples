1eb26366198b4a31897a9d894092286235cbba4f
Tracy Boehrer <tracy.boehrer@gmail.com>
2019-11-20 14:25:11 -0800

    Corrected 18.bot-authentication logout handling

--- samples/python/18.bot-authentication/dialogs/logout_dialog.py
+++ samples/python/18.bot-authentication/dialogs/logout_dialog.py
@@ -12,13 +12,17 @@ class LogoutDialog(ComponentDialog):
 
         self.connection_name = connection_name
 
-    async def on_begin_dialog(
-        self, inner_dc: DialogContext, options: object
-    ) -> DialogTurnResult:
-        return await inner_dc.begin_dialog(self.initial_dialog_id, options)
+    async def on_begin_dialog(self, inner_dc: DialogContext, options: object) -> DialogTurnResult:
+        result = await self._interrupt(inner_dc)
+        if result:
+            return result
+        return await super().on_begin_dialog(inner_dc, options)
 
     async def on_continue_dialog(self, inner_dc: DialogContext) -> DialogTurnResult:
-        return await inner_dc.continue_dialog()
+        result = await self._interrupt(inner_dc)
+        if result:
+            return result
+        return await super().on_continue_dialog(inner_dc)
 
     async def _interrupt(self, inner_dc: DialogContext):
         if inner_dc.context.activity.type == ActivityTypes.message:
@@ -26,4 +30,5 @@ class LogoutDialog(ComponentDialog):
             if text == "logout":
                 bot_adapter: BotFrameworkAdapter = inner_dc.context.adapter
                 await bot_adapter.sign_out_user(inner_dc.context, self.connection_name)
+                await inner_dc.context.send_activity("You have been signed out.")
                 return await inner_dc.cancel_all_dialogs()
