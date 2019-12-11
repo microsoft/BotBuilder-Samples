a4eea7867e7db593c7258ce46cf840787e95f14a
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-11 08:02:23 -0600

    Fixes #471: 05.multi-turn-prompt parity

--- samples/python/05.multi-turn-prompt/data_models/user_profile.py
+++ samples/python/05.multi-turn-prompt/data_models/user_profile.py
@@ -1,13 +1,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-"""
-  This is our application state. Just a regular serializable Python class.
-"""
+from botbuilder.schema import Attachment
 
 
 class UserProfile:
-    def __init__(self, name: str = None, transport: str = None, age: int = 0):
+    """
+      This is our application state. Just a regular serializable Python class.
+    """
+
+    def __init__(self, name: str = None, transport: str = None, age: int = 0, picture: Attachment = None):
         self.name = name
         self.transport = transport
         self.age = age
+        self.picture = picture
