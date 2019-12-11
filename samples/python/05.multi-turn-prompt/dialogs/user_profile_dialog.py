5ba2546cb863a22912a41b628939fcbdb582ec9a
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-11 08:02:23 -0600

    Fixes #471: 05.multi-turn-prompt parity

--- samples/python/05.multi-turn-prompt/dialogs/user_profile_dialog.py
+++ samples/python/05.multi-turn-prompt/dialogs/user_profile_dialog.py
@@ -12,6 +12,7 @@ from botbuilder.dialogs.prompts import (
     NumberPrompt,
     ChoicePrompt,
     ConfirmPrompt,
+    AttachmentPrompt,
     PromptOptions,
     PromptValidatorContext,
 )
@@ -35,6 +36,7 @@ class UserProfileDialog(ComponentDialog):
                     self.name_step,
                     self.name_confirm_step,
                     self.age_step,
+                    self.picture_step,
                     self.confirm_step,
                     self.summary_step,
                 ],
@@ -46,6 +48,11 @@ class UserProfileDialog(ComponentDialog):
         )
         self.add_dialog(ChoicePrompt(ChoicePrompt.__name__))
         self.add_dialog(ConfirmPrompt(ConfirmPrompt.__name__))
+        self.add_dialog(
+            AttachmentPrompt(
+                AttachmentPrompt.__name__, UserProfileDialog.picture_prompt_validator
+            )
+        )
 
         self.initial_dialog_id = WaterfallDialog.__name__
 
@@ -108,11 +115,11 @@ class UserProfileDialog(ComponentDialog):
         # User said "no" so we will skip the next step. Give -1 as the age.
         return await step_context.next(-1)
 
-    async def confirm_step(
+    async def picture_step(
         self, step_context: WaterfallStepContext
     ) -> DialogTurnResult:
         age = step_context.result
-        step_context.values["age"] = step_context.result
+        step_context.values["age"] = age
 
         msg = (
             "No age given."
@@ -122,6 +129,32 @@ class UserProfileDialog(ComponentDialog):
 
         # We can send messages to the user at any point in the WaterfallStep.
         await step_context.context.send_activity(MessageFactory.text(msg))
+
+        if step_context.context.activity.channel_id == "msteams":
+            # This attachment prompt example is not designed to work for Teams attachments, so skip it in this case
+            await step_context.context.send_activity(
+                "Skipping attachment prompt in Teams channel..."
+            )
+            return await step_context.next(None)
+
+        # WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt
+        # Dialog.
+        prompt_options = PromptOptions(
+            prompt=MessageFactory.text(
+                "Please attach a profile picture (or type any message to skip)."
+            ),
+            retry_prompt=MessageFactory.text(
+                "The attachment must be a jpeg/png image file."
+            ),
+        )
+        return await step_context.prompt(AttachmentPrompt.__name__, prompt_options)
+
+    async def confirm_step(
+        self, step_context: WaterfallStepContext
+    ) -> DialogTurnResult:
+        step_context.values["picture"] = (
+            None if not step_context.result else step_context.result[0]
+        )
 
         # WaterfallStep always finishes with the end of the Waterfall or
         # with another dialog; here it is a Prompt Dialog.
@@ -134,7 +167,7 @@ class UserProfileDialog(ComponentDialog):
         self, step_context: WaterfallStepContext
     ) -> DialogTurnResult:
         if step_context.result:
-            #  Get the current profile object from user state.  Changes to it
+            # Get the current profile object from user state.  Changes to it
             # will saved during Bot.on_turn.
             user_profile = await self.user_profile_accessor.get(
                 step_context.context, UserProfile
@@ -143,12 +176,24 @@ class UserProfileDialog(ComponentDialog):
             user_profile.transport = step_context.values["transport"]
             user_profile.name = step_context.values["name"]
             user_profile.age = step_context.values["age"]
+            user_profile.picture = step_context.values["picture"]
 
             msg = f"I have your mode of transport as {user_profile.transport} and your name as {user_profile.name}."
             if user_profile.age != -1:
                 msg += f" And age as {user_profile.age}."
 
             await step_context.context.send_activity(MessageFactory.text(msg))
+
+            if user_profile.picture:
+                await step_context.context.send_activity(
+                    MessageFactory.attachment(
+                        user_profile.picture, "This is your profile picture."
+                    )
+                )
+            else:
+                await step_context.context.send_activity(
+                    "A profile picture was saved but could not be displayed here."
+                )
         else:
             await step_context.context.send_activity(
                 MessageFactory.text("Thanks. Your profile will not be kept.")
@@ -165,3 +210,26 @@ class UserProfileDialog(ComponentDialog):
             prompt_context.recognized.succeeded
             and 0 < prompt_context.recognized.value < 150
         )
+
+    @staticmethod
+    async def picture_prompt_validator(prompt_context: PromptValidatorContext) -> bool:
+        if not prompt_context.recognized.succeeded:
+            await prompt_context.context.send_activity(
+                "No attachments received. Proceeding without a profile picture..."
+            )
+
+            # We can return true from a validator function even if recognized.succeeded is false.
+            return True
+
+        attachments = prompt_context.recognized.value
+
+        valid_images = [
+            attachment
+            for attachment in attachments
+            if attachment.content_type in ["image/jpeg", "image/png"]
+        ]
+
+        prompt_context.recognized.value = valid_images
+
+        # If none of the attachments are valid images, the retry prompt should be sent.
+        return len(valid_images) > 0
