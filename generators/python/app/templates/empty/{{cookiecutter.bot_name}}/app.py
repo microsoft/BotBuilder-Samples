c763358dba12a6b79aa8e3a64584055279959654
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-12 12:41:02 -0600

    Fixes Python #474.  Typos in templates.

--- generators/python/app/templates/empty/{{cookiecutter.bot_name}}/app.py
+++ generators/python/app/templates/empty/{{cookiecutter.bot_name}}/app.py
@@ -33,7 +33,7 @@ async def on_error(self, context: TurnContext, error: Exception):
     print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
 
     # Send a message to the user
-    await context.send_activity("The bot encounted an error or bug.")
+    await context.send_activity("The bot encountered an error or bug.")
     await context.send_activity("To continue to run this bot, please fix the bot source code.")
 
 ADAPTER.on_turn_error = MethodType(on_error, ADAPTER)
