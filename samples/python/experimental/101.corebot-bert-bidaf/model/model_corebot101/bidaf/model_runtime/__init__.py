7d2fc4044a6b43789137a2fc9b4105f55b61f2af
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-13 12:36:44 -0600

    Removed experimental from Python Samples

--- samples/python/experimental/101.corebot-bert-bidaf/model/model_corebot101/bidaf/model_runtime/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from .bidaf_model_runtime import BidafModelRuntime
-
-__all__ = ["BidafModelRuntime"]
