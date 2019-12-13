ce39ad3c93cacbda1375878ae4c5221a45e9baa4
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-13 12:36:44 -0600

    Removed experimental from Python Samples

--- samples/python/experimental/101.corebot-bert-bidaf/bot/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-"""Helpers module."""
-
-from . import activity_helper, dialog_helper
-
-__all__ = ["activity_helper", "dialog_helper"]
--- samples/python/experimental/101.corebot-bert-bidaf/model/model_corebot101/bert/model_runtime/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from .bert_model_runtime import BertModelRuntime
-
-__all__ = ["BertModelRuntime"]
