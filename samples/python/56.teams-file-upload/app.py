82ed3d9ec244410cb7216b2786cd636657e69a9a
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-16 09:15:05 -0600

    Updated to use aiohttp

--- samples/python/56.teams-file-upload/app.py
+++ samples/python/56.teams-file-upload/app.py
@@ -1,29 +1,28 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import asyncio
 import sys
 import traceback
 from datetime import datetime
 
-from flask import Flask, request, Response
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
     BotFrameworkAdapterSettings,
     TurnContext,
     BotFrameworkAdapter,
 )
+from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
 
 from bots import TeamsFileUploadBot
+from config import DefaultConfig
 
-# Create the loop and Flask app
-LOOP = asyncio.get_event_loop()
-APP = Flask(__name__, instance_relative_config=True)
-APP.config.from_object("config.DefaultConfig")
+CONFIG = DefaultConfig()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(APP.config["APP_ID"], APP.config["APP_PASSWORD"])
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
 
 
@@ -33,7 +32,7 @@ async def on_error(context: TurnContext, error: Exception):
     # NOTE: In production environment, you should consider logging this to Azure
     #       application insights.
     print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
-    print(traceback.format_exc())
+    traceback.print_exc()
 
     # Send a message to the user
     await context.send_activity("The bot encountered an error or bug.")
@@ -61,31 +60,34 @@ ADAPTER.on_turn_error = on_error
 BOT = TeamsFileUploadBot()
 
 # Listen for incoming requests on /api/messages.s
-@APP.route("/api/messages", methods=["POST"])
-def messages():
+async def messages(req: Request) -> Response:
     # Main bot message handler.
-    if "application/json" in request.headers["Content-Type"]:
-        body = request.json
+    if "application/json" in req.headers["Content-Type"]:
+        body = await req.json()
     else:
         return Response(status=415)
 
     activity = Activity().deserialize(body)
-    auth_header = (
-        request.headers["Authorization"] if "Authorization" in request.headers else ""
-    )
+    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
 
     try:
-        task = LOOP.create_task(
-            ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+        invoke_response = await ADAPTER.process_activity(
+            activity, auth_header, BOT.on_turn
         )
-        LOOP.run_until_complete(task)
+        if invoke_response:
+            return json_response(
+                data=invoke_response.body, status=invoke_response.status
+            )
         return Response(status=201)
     except Exception as exception:
         raise exception
 
 
+APP = web.Application(middlewares=[aiohttp_error_middleware])
+APP.router.add_post("/api/messages", messages)
+
 if __name__ == "__main__":
     try:
-        APP.run(debug=False, port=APP.config["PORT"])  # nosec debug
-    except Exception as exception:
-        raise exception
+        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
