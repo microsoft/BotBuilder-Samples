0ce9cc3802868c1dc01f98a517e9e6384dc7042a
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-16 15:58:44 -0600

    Updated Python generator templates to use aiohttp, and include ARM templates.

--- generators/python/app/templates/core/{{cookiecutter.bot_name}}/app.py
+++ generators/python/app/templates/core/{{cookiecutter.bot_name}}/app.py
@@ -1,6 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-# pylint: disable=import-error
 
 """
 This sample shows how to create a bot that demonstrates the following:
@@ -10,101 +9,70 @@ This sample shows how to create a bot that demonstrates the following:
 - Prompt for and validate requests for information from the user.
 """
 
-import asyncio
-import sys
-from datetime import datetime
-from types import MethodType
-
-from flask import Flask, request, Response
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
-    BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
     ConversationState,
     MemoryStorage,
     UserState,
-    TurnContext
 )
-from botbuilder.schema import Activity, ActivityTypes
+from botbuilder.schema import Activity
 
-from bots import DialogAndWelcomeBot
+from config import DefaultConfig
 from dialogs import MainDialog, BookingDialog
+from bots import DialogAndWelcomeBot
+
+from adapter_with_error_handler import AdapterWithErrorHandler
 from flight_booking_recognizer import FlightBookingRecognizer
 
-LOOP = asyncio.get_event_loop()
-APP = Flask(__name__, instance_relative_config=True)
-APP.config.from_object("config.DefaultConfig")
+CONFIG = DefaultConfig()
 
-SETTINGS = BotFrameworkAdapterSettings(APP.config["APP_ID"], APP.config["APP_PASSWORD"])
-ADAPTER = BotFrameworkAdapter(SETTINGS)
+# Create adapter.
+# See https://aka.ms/about-bot-adapter to learn more about how bots work.
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 
 # Create MemoryStorage, UserState and ConversationState
 MEMORY = MemoryStorage()
 USER_STATE = UserState(MEMORY)
 CONVERSATION_STATE = ConversationState(MEMORY)
-RECOGNIZER = FlightBookingRecognizer(APP.config)
-BOOKING_DIALOG = BookingDialog()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(APP.config["APP_ID"], APP.config["APP_PASSWORD"])
-ADAPTER = BotFrameworkAdapter(SETTINGS)
+ADAPTER = AdapterWithErrorHandler(SETTINGS, CONVERSATION_STATE)
 
-# Catch-all for errors.
-# pylint: disable=unused-argument
-async def on_error(self, context: TurnContext, error: Exception):
-    # This check writes out errors to console log .vs. app insights.
-    # NOTE: In production environment, you should consider logging this to Azure
-    #       application insights.
-    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
-
-    # Send a message to the user
-    await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity("To continue to run this bot, please fix the bot source code.")
-    # Send a trace activity if we're talking to the Bot Framework Emulator
-    if context.activity.channel_id == 'emulator':
-        # Create a trace activity that contains the error object
-        trace_activity = Activity(
-            label="TurnError",
-            name="on_turn_error Trace",
-            timestamp=datetime.utcnow(),
-            type=ActivityTypes.trace,
-            value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error"
-        )
-        # Send a trace activity, which will be displayed in Bot Framework Emulator
-        await context.send_activity(trace_activity)
-
-ADAPTER.on_turn_error = MethodType(on_error, ADAPTER)
-
+# Create dialogs and Bot
+RECOGNIZER = FlightBookingRecognizer(CONFIG)
+BOOKING_DIALOG = BookingDialog()
 DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG)
 BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG)
 
 
-@APP.route("/api/messages", methods=["POST"])
-def messages():
-    """Main bot message handler."""
-    if "application/json" in request.headers["Content-Type"]:
-        body = request.json
+# Listen for incoming requests on /api/messages.
+async def messages(req: Request) -> Response:
+    # Main bot message handler.
+    if "application/json" in req.headers["Content-Type"]:
+        body = await req.json()
     else:
         return Response(status=415)
 
     activity = Activity().deserialize(body)
-    auth_header = (
-        request.headers["Authorization"] if "Authorization" in request.headers else ""
-    )
+    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
 
     try:
-        task = LOOP.create_task(
-            ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
-        )
-        LOOP.run_until_complete(task)
+        response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+        if response:
+            return json_response(data=response.body, status=response.status)
         return Response(status=201)
     except Exception as exception:
         raise exception
 
 
+APP = web.Application()
+APP.router.add_post("/api/messages", messages)
+
 if __name__ == "__main__":
     try:
-        APP.run(debug=False, port=APP.config["PORT"])  # nosec debug
-    except Exception as exception:
-        raise exception
+        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
--- generators/python/app/templates/echo/{{cookiecutter.bot_name}}/app.py
+++ generators/python/app/templates/echo/{{cookiecutter.bot_name}}/app.py
@@ -1,43 +1,46 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import asyncio
 import sys
+import traceback
 from datetime import datetime
-from types import MethodType
 
-from flask import Flask, request, Response
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
-    BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
     TurnContext,
+    BotFrameworkAdapter,
 )
+from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
-from bot import MyBot
 
-# Create the loop and Flask app
-LOOP = asyncio.get_event_loop()
-APP = Flask(__name__, instance_relative_config=True)
-APP.config.from_object("config.DefaultConfig")
+from bots import EchoBot
+from config import DefaultConfig
+
+CONFIG = DefaultConfig()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(APP.config["APP_ID"], APP.config["APP_PASSWORD"])
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
 
+
 # Catch-all for errors.
-# pylint: disable=unused-argument
-async def on_error(self, context: TurnContext, error: Exception):
+async def on_error(context: TurnContext, error: Exception):
     # This check writes out errors to console log .vs. app insights.
     # NOTE: In production environment, you should consider logging this to Azure
     #       application insights.
     print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
+    traceback.print_exc()
 
     # Send a message to the user
     await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity("To continue to run this bot, please fix the bot source code.")
+    await context.send_activity(
+        "To continue to run this bot, please fix the bot source code."
+    )
     # Send a trace activity if we're talking to the Bot Framework Emulator
-    if context.activity.channel_id == 'emulator':
+    if context.activity.channel_id == "emulator":
         # Create a trace activity that contains the error object
         trace_activity = Activity(
             label="TurnError",
@@ -45,42 +48,43 @@ async def on_error(self, context: TurnContext, error: Exception):
             timestamp=datetime.utcnow(),
             type=ActivityTypes.trace,
             value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error"
+            value_type="https://www.botframework.com/schemas/error",
         )
         # Send a trace activity, which will be displayed in Bot Framework Emulator
         await context.send_activity(trace_activity)
 
-ADAPTER.on_turn_error = MethodType(on_error, ADAPTER)
 
-# Create the main dialog
-BOT = MyBot()
+ADAPTER.on_turn_error = on_error
 
-# Listen for incoming requests on /api/messages.
-@APP.route("/api/messages", methods=["POST"])
-def messages():
+# Create the Bot
+BOT = EchoBot()
+
+
+# Listen for incoming requests on /api/messages
+async def messages(req: Request) -> Response:
     # Main bot message handler.
-    if "application/json" in request.headers["Content-Type"]:
-        body = request.json
+    if "application/json" in req.headers["Content-Type"]:
+        body = await req.json()
     else:
         return Response(status=415)
 
     activity = Activity().deserialize(body)
-    auth_header = (
-        request.headers["Authorization"] if "Authorization" in request.headers else ""
-    )
+    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
 
     try:
-        task = LOOP.create_task(
-            ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
-        )
-        LOOP.run_until_complete(task)
+        response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+        if response:
+            return json_response(data=response.body, status=response.status)
         return Response(status=201)
     except Exception as exception:
         raise exception
 
 
+APP = web.Application(middlewares=[aiohttp_error_middleware])
+APP.router.add_post("/api/messages", messages)
+
 if __name__ == "__main__":
     try:
-        APP.run(debug=False, port=APP.config["PORT"])  # nosec debug
-    except Exception as exception:
-        raise exception
+        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
