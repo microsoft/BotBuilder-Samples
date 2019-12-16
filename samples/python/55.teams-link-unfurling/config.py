d8c7c462a46be739ad3914477f57b926f9c78662
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-16 09:26:35 -0600

    Added 55.teams-link-unfurling

---
+++ samples/python/55.teams-link-unfurling/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = "abb2a9c8-edb0-4442-af63-45da3e292ac3"
+    APP_PASSWORD = "=FFrYcm-APKK8?S2]cCf5xUobpX26Gqa"
