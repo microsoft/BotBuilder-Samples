4fb342da5f72bb9376145d3f684720d02c9296ef
CK Kashyap <ckk@microsoft.com>
2019-12-17 15:18:43 -0800

    A complete MVC DirectLine token controller sample

---
+++ experimental/DirectLineTokenSite/Bot_Auth_DL_Secure_Site_MVC/Models/ChatConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Bot_Auth_DL_Secure_Site_MVC.Models
+{
+    public class ChatConfig
+    {
+        public string Token { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ experimental/DirectLineTokenSite/Bot_Auth_DL_Secure_Site_MVC/Models/DirectLineToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Bot_Auth_DL_Secure_Site_MVC.Models
+{
+    public class DirectLineToken
+    {
+        public string conversationId { get; set; }
+        public string token { get; set; }
+        public int expires_in { get; set; }
+    }
+}
