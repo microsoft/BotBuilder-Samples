a5622e43dd6a68b70c567b7f4b705d00c7963ed5
tracyboehrer <tracyboehrer@users.noreply.github.com>
2019-12-18 11:08:30 -0600

    Fixed _message_all_members in Python 57.teams-conversation-bot

--- samples/python/57.teams-conversation-bot/bots/teams_conversation_bot.py
+++ samples/python/57.teams-conversation-bot/bots/teams_conversation_bot.py
@@ -1,6 +1,6 @@
 from botbuilder.core import CardFactory, TurnContext, MessageFactory
 from botbuilder.core.teams import TeamsActivityHandler, TeamsInfo
-from botbuilder.schema import CardAction, HeroCard, Mention
+from botbuilder.schema import CardAction, HeroCard, Mention, ConversationParameters
 from botbuilder.schema._connector_client_enums import ActionTypes
 
 
@@ -34,26 +34,28 @@ class TeamsConversationBot(TeamsActivityHandler):
             text="Click the buttons to update this card",
             buttons=[
                 CardAction(
-                        type=ActionTypes.message_back,
-                        title="Update Card",
-                        text="UpdateCardAction",
-                        value={"count": 0}
+                    type=ActionTypes.message_back,
+                    title="Update Card",
+                    text="UpdateCardAction",
+                    value={"count": 0},
                 ),
                 CardAction(
-                        type=ActionTypes.message_back,
-                        title="Message all memebers",
-                        text="MessageAllMembers"
-                )
-            ]
+                    type=ActionTypes.message_back,
+                    title="Message all memebers",
+                    text="MessageAllMembers",
+                ),
+            ],
         )
-        await turn_context.send_activity(MessageFactory.attachment(CardFactory.hero_card(card)))
+        await turn_context.send_activity(
+            MessageFactory.attachment(CardFactory.hero_card(card))
+        )
         return
 
     async def _mention_activity(self, turn_context: TurnContext):
         mention = Mention(
             mentioned=turn_context.activity.from_property,
             text=f"<at>{turn_context.activity.from_property.name}</at>",
-            type="mention"
+            type="mention",
         )
 
         reply_activity = MessageFactory.text(f"Hello {mention.text}")
@@ -64,55 +66,68 @@ class TeamsConversationBot(TeamsActivityHandler):
         data = turn_context.activity.value
         data["count"] += 1
 
-        card = CardFactory.hero_card(HeroCard(
-            title="Welcome Card",
-            text=f"Updated count - {data['count']}",
-            buttons=[
-                CardAction(
-                    type=ActionTypes.message_back,
-                    title='Update Card',
-                    value=data,
-                    text='UpdateCardAction'
-                ),
-                CardAction(
-                    type=ActionTypes.message_back,
-                    title='Message all members',
-                    text='MessageAllMembers'
-
-                ),
-                CardAction(
-                    type= ActionTypes.message_back,
-                    title='Delete card',
-                    text='Delete'
-                )
-            ]
+        card = CardFactory.hero_card(
+            HeroCard(
+                title="Welcome Card",
+                text=f"Updated count - {data['count']}",
+                buttons=[
+                    CardAction(
+                        type=ActionTypes.message_back,
+                        title="Update Card",
+                        value=data,
+                        text="UpdateCardAction",
+                    ),
+                    CardAction(
+                        type=ActionTypes.message_back,
+                        title="Message all members",
+                        text="MessageAllMembers",
+                    ),
+                    CardAction(
+                        type=ActionTypes.message_back,
+                        title="Delete card",
+                        text="Delete",
+                    ),
+                ],
             )
         )
-        
+
         updated_activity = MessageFactory.attachment(card)
         updated_activity.id = turn_context.activity.reply_to_id
         await turn_context.update_activity(updated_activity)
-    
+
     async def _message_all_members(self, turn_context: TurnContext):
         team_members = await TeamsInfo.get_members(turn_context)
 
         for member in team_members:
-            proactive_message = MessageFactory.text(f"Hello {member.name}. I'm a Teams conversation bot.")
+            conversation_reference = TurnContext.get_conversation_reference(
+                turn_context.activity
+            )
+
+            conversation_parameters = ConversationParameters(
+                is_group=False,
+                bot=turn_context.activity.recipient,
+                members=[member],
+                tenant_id=turn_context.activity.conversation.tenant_id,
+            )
 
             async def get_ref(tc1):
-                ref2 = TurnContext.get_conversation_reference(tc1.activity)
-                return await tc1.adapter.continue_conversation(self._app_id, ref2, send_message)
+                conversation_reference_inner = TurnContext.get_conversation_reference(
+                    tc1.activity
+                )
+                return await tc1.adapter.continue_conversation(
+                    conversation_reference_inner, send_message, self._app_id
+                )
 
             async def send_message(tc2: TurnContext):
-                return tc2
+                return await tc2.send_activity(f"Hello {member.name}. I'm a Teams conversation bot.")  # pylint: disable=cell-var-from-loop
 
-            ref = TurnContext.get_conversation_reference(turn_context.activity)
-            result = await turn_context.adapter.create_conversation(ref, get_ref)
-            await result.send_activity(proactive_message)
-        
-        await turn_context.send_activity(MessageFactory.text("All messages have been sent"))
-            
-           
-    
+            await turn_context.adapter.create_conversation(
+                conversation_reference, get_ref, conversation_parameters
+            )
+
+        await turn_context.send_activity(
+            MessageFactory.text("All messages have been sent")
+        )
+
     async def _delete_card_activity(self, turn_context: TurnContext):
         await turn_context.delete_activity(turn_context.activity.reply_to_id)
