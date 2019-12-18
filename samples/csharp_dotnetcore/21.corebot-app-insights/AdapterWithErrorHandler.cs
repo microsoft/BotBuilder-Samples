6ca6255b3cbb291e783927e618827aac5b02fe80
Gary Pretty <gary@garypretty.co.uk>
2019-12-18 14:45:21 +0000

    update .21 core app insights sample with core bot 4.5.1 changes  (#2031)
    * - Update bot with 4.5.1 changes to core bot sample
    - Update to pass TelemetryClient via property on ComponentDialog (in startup.cs) instead of by constructor
    - Update FlightBookingRecgnizer to pass TelemetryClient to LuisRecognizer
    
    * Small fixes
    
    * Set LangVersion to latest to ensure build does not fail with C# 7.1 features
    
    * - Remove unnecessary setting of TelemetryClient when adding child dialogs
    - Made TelemetryInitializerMiddleware DI scope narrower as per comment from Gabo
    - Added missing copyright header
    - Reverted pattern to pass TelemetryClient into Main dialog via DI
    
    * Removed remaining redundant TelemetryClient setters when adding new dialogs
    
    * Update comment for setting TelemetryClient in MainDialog

--- samples/csharp_dotnetcore/21.corebot-app-insights/AdapterWithErrorHandler.cs
+++ samples/csharp_dotnetcore/21.corebot-app-insights/AdapterWithErrorHandler.cs
@@ -3,19 +3,21 @@
 
 using System;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.TraceExtensions;
-using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.BotBuilderSamples
 {
     public class AdapterWithErrorHandler : BotFrameworkHttpAdapter
     {
-        public AdapterWithErrorHandler(ICredentialProvider credentialProvider, ILogger<BotFrameworkHttpAdapter> logger, IMiddleware middleware, ConversationState conversationState = null)
-            : base(credentialProvider)
+        public AdapterWithErrorHandler(IConfiguration configuration, ILogger<BotFrameworkHttpAdapter> logger, TelemetryInitializerMiddleware telemetryInitializerMiddleware, ConversationState conversationState = null)
+            : base(configuration, logger)
         {
-            Use(middleware);
+            Use(telemetryInitializerMiddleware);
 
             OnTurnError = async (turnContext, exception) =>
             {
@@ -23,8 +25,13 @@ namespace Microsoft.BotBuilderSamples
                 logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
 
                 // Send a message to the user
-                await turnContext.SendActivityAsync("The bot encountered an error or bug.");
-                await turnContext.SendActivityAsync("To continue to run this bot, please fix the bot source code.");
+                var errorMessageText = "The bot encountered an error or bug.";
+                var errorMessage = MessageFactory.Text(errorMessageText, errorMessageText, InputHints.IgnoringInput);
+                await turnContext.SendActivityAsync(errorMessage);
+
+                errorMessageText = "To continue to run this bot, please fix the bot source code.";
+                errorMessage = MessageFactory.Text(errorMessageText, errorMessageText, InputHints.ExpectingInput);
+                await turnContext.SendActivityAsync(errorMessage);
 
                 if (conversationState != null)
                 {
