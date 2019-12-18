f1d9e1d5de4c0b62448aee25dfa0cbb530d153c9
Gabo Gilabert <gabog@users.noreply.github.com>
2019-12-18 17:10:29 -0500

    Skill Samples (dotnet) (#2032)
    * First pass at DialogToDialog sample
    
    * Updated nuget packages to latest nightly.
    
    * Updated to 4.7 RC0.
    Updated readmes
    Reorganized MainDialog, SkillDialog and SkillDialogArgs.
    
    * Updated readmes
    
    * Adds SimpleBotToBot sample.
    Updated readmes for DialotToDialog.
    
    * Added a readme for the root folder.
    
    * Addressed some of Jonathan's comments.
    Addressed LUIS warning.
    
    * Changed the conversation ID to avoid using hashcodes (the unique value will be given by the parent conversation ID).
    
    * Changed how the skill conversation ID key gets generated.
    
    * Fixed stylecop issues.
    
    * Updated nuget references to RC1
    
    * Updated code for partiy with JS
    
    * Moved SimpleBotToBot in main samples solution.
    Updated SimpleBotToBot to send an eoc on error with a status and text message.
    
    * Rename skill adapter.
    Updated OnError
    Upated nuget packages to released versions.
    
    * Readded skill sample that was lost in mege from master
    
    * Updated readmes
    Added sample claim validators for skill and parent.
    Added status checks for skill call invocations.
    
    * Updated links.
    
    * Fixed some strings and comments.
    
    * Update samples/csharp_dotnetcore/70.skills-simple-bot-to-bot/README.md
    
    Co-Authored-By: Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
    
    * Updated readme

---
+++ experimental/skills/DialogToDialog/DialogSkillBot/Dialogs/OAuthTestDialog.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.BotBuilderSamples.DialogSkillBot.Dialogs
+{
+    public class OAuthTestDialog : CancelAndHelpDialog
+    {
+        private readonly string _connectionName;
+
+        public OAuthTestDialog(IConfiguration configuration)
+            : base(nameof(OAuthTestDialog))
+        {
+            _connectionName = configuration["ConnectionName"];
+
+            AddDialog(new OAuthPrompt(
+                nameof(OAuthPrompt),
+                new OAuthPromptSettings
+                {
+                    ConnectionName = _connectionName,
+                    Text = $"Please Sign In to connection: '{_connectionName}'",
+                    Title = "Sign In",
+                    Timeout = 300000 // User has 5 minutes to login (1000 * 60 * 5)
+                }));
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[] { PromptStepAsync, LoginStepAsync }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> PromptStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.BeginDialogAsync(nameof(OAuthPrompt), null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> LoginStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Get the token from the previous step.
+            var tokenResponse = (TokenResponse)stepContext.Result;
+            if (tokenResponse != null)
+            {
+                // Show the token
+                var loggedInMessage = "You are now logged in.";
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text(loggedInMessage, loggedInMessage, InputHints.IgnoringInput), cancellationToken);
+                var showTokenMessage = "Here is your token:";
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{showTokenMessage} {tokenResponse.Token}", showTokenMessage, InputHints.IgnoringInput), cancellationToken);
+
+                // Sign out
+                var botAdapter = (BotFrameworkAdapter)stepContext.Context.Adapter;
+                await botAdapter.SignOutUserAsync(stepContext.Context, _connectionName, null, cancellationToken);
+                var signOutMessage = "I have signed you out.";
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text(signOutMessage, signOutMessage, inputHint: InputHints.IgnoringInput), cancellationToken);
+
+                return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+            }
+
+            var tryAgainMessage = "Login was not successful please try again.";
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(tryAgainMessage, tryAgainMessage, InputHints.IgnoringInput), cancellationToken);
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
