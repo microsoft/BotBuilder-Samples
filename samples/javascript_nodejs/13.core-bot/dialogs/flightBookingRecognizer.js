2ab358996e558a9ee1b56c7b7f510cf3226e471d
Emilio Munoz <emmunozp@microsoft.com>
2019-12-19 15:47:25 -0800

    Fixing linting errors

--- samples/javascript_nodejs/13.core-bot/dialogs/flightBookingRecognizer.js
+++ samples/javascript_nodejs/13.core-bot/dialogs/flightBookingRecognizer.js
@@ -7,12 +7,11 @@ class FlightBookingRecognizer {
     constructor(config) {
         const luisIsConfigured = config && config.applicationId && config.endpointKey && config.endpoint;
         if (luisIsConfigured) {
-
             // Set the recognizer options depending on which endpoint version you want to use e.g v2 or v3.
             // More details can be found in https://docs.microsoft.com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
             const recognizerOptions = {
                 apiVersion: 'v3'
-            }
+            };
 
             this.recognizer = new LuisRecognizer(config, recognizerOptions);
         }
--- samples/typescript_nodejs/13.core-bot/src/dialogs/flightBookingRecognizer.ts
+++ samples/typescript_nodejs/13.core-bot/src/dialogs/flightBookingRecognizer.ts
@@ -10,12 +10,12 @@ export class FlightBookingRecognizer {
     constructor(config: LuisApplication) {
         const luisIsConfigured = config && config.applicationId && config.endpoint && config.endpointKey;
         if (luisIsConfigured) {
-
             // Set the recognizer options depending on which endpoint version you want to use e.g LuisRecognizerOptionsV2 or LuisRecognizerOptionsV3.
             // More details can be found in https://docs.microsoft.com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
             const recognizerOptions: LuisRecognizerOptionsV3 = {
                 apiVersion : 'v3'
-            }
+            };
+
             this.recognizer = new LuisRecognizer(config, recognizerOptions);
         }
     }
