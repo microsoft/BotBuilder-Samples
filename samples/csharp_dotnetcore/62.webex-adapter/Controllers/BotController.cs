7d3d9f889c65121ae11a1d62bb1f44a0239c550f
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2020-01-06 21:45:32 -0300

    [botkit] Add Webex Adapter Bot sample (#1857)
    * Add WebexAdapterBot
    
    * Handle adaptive cards
    
    * Update README file with detailed instructions
    
    * Update NuGet package version
    
    * Update NuGet package version to 46.0-rc0-preview
    
    * Update NuGet package version to 46.0-preview-rc1
    
    * Added / fixed registration of adapters in Startup. Updated to latest adapter preview package.
    
    * - Rename sample number to 62.
    - Update NuGet packages to 4.7
    
    * Update readme to reference new MS doc with instructions for adapter config / usage
    
    * Update adapter classes with error handling
    
    * Fix header / copyright comments
    
    Co-authored-by: Gary Pretty <gary@garypretty.co.uk>

---
+++ samples/csharp_dotnetcore/62.webex-adapter/Controllers/BotController.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace WebexAdapterBot.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly IBot _bot;
+
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            _adapter = adapter;
+            _bot = bot;
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            // Delegate the processing of the HTTP POST to the adapter.
+            // The adapter will invoke the bot.
+            await _adapter.ProcessAsync(Request, Response, _bot, new CancellationToken());
+        }
+    }
+}
