f09c537f28e3e99516e2b05db11e722b77e2c9e8
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-02-05 11:12:51 -0600

    Python: Added 60.slack-adapter

---
+++ samples/python/60.slack-adapter/config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+
+    SLACK_VERIFICATION_TOKEN = os.environ.get("SlackVerificationToken", "lvIbjaGf3r3mBACD6KkiiEYN")
+    SLACK_BOT_TOKEN = os.environ.get("SlackBotToken", "xoxb-679025723075-911490631362-bnMuHZRekEfqc018cSemaDxi")
+    SLACK_CLIENT_SIGNING_SECRET = os.environ.get("SlackClientSigningSecret", "a6807dbde5f2485e0397ab9117f9895d")
