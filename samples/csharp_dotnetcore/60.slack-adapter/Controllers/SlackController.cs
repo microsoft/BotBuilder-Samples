0921ef37ec0c93f97fa192a90e477659cf7a0772
Anshul21 <anshul2117@gmail.com>
2020-03-11 17:57:22 -0700

    Adding missing paramenter to ProcessAsync

--- samples/csharp_dotnetcore/60.slack-adapter/Controllers/SlackController.cs
+++ samples/csharp_dotnetcore/60.slack-adapter/Controllers/SlackController.cs
@@ -28,7 +28,7 @@ namespace SlackAdapterBot.Controllers
         {
             // Delegate the processing of the HTTP POST to the adapter.
             // The adapter will invoke the bot.
-            await _adapter.ProcessAsync(Request, Response, _bot);
+            await _adapter.ProcessAsync(Request, Response, _bot, default(CancellationToken));
         }
     }
 }
