68ed6fcd77d638aa541b9caf6a26f3694055dac2
swagat mishra <swagatmishra2007@gmail.com>
2020-03-20 00:12:44 -0700

    initial commit for sso with skills

---
+++ experimental/sso-with-skills/SimpleRootBot/SkillConversationIdFactory.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Skills;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Microsoft.BotBuilderSamples.SimpleRootBot
+{
+    /// <summary>
+    /// A <see cref="SkillConversationIdFactory"/> that uses an in memory <see cref="ConcurrentDictionary{TKey,TValue}"/>
+    /// to store and retrieve <see cref="ConversationReference"/> instances.
+    /// </summary>
+    public class SkillConversationIdFactory : SkillConversationIdFactoryBase
+    {
+        private readonly ConcurrentDictionary<string, string> _conversationRefs = new ConcurrentDictionary<string, string>();
+
+        public override Task<string> CreateSkillConversationIdAsync(ConversationReference conversationReference, CancellationToken cancellationToken)
+        {
+            var crJson = JsonConvert.SerializeObject(conversationReference);
+            var key = $"{conversationReference.ChannelId}:{conversationReference.Conversation.Id}";
+            _conversationRefs.GetOrAdd(key, crJson);
+            return Task.FromResult(key);
+        }
+
+        public override Task<ConversationReference> GetConversationReferenceAsync(string skillConversationId, CancellationToken cancellationToken)
+        {
+            var conversationReference = JsonConvert.DeserializeObject<ConversationReference>(_conversationRefs[skillConversationId]);
+            return Task.FromResult(conversationReference);
+        }
+
+        public override Task DeleteConversationReferenceAsync(string skillConversationId, CancellationToken cancellationToken)
+        {
+            _conversationRefs.TryRemove(skillConversationId, out _);
+            return Task.CompletedTask;
+        }
+    }
+}
