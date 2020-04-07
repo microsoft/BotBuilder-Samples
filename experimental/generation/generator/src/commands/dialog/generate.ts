011f4fa859ef3377b063a394d38d9316fc6cc82c
coldplaying42 <mzyone@gmail.com>
2020-04-07 11:29:33 +0800

    update integrating

--- experimental/generation/generator/src/commands/dialog/generate.ts
+++ experimental/generation/generator/src/commands/dialog/generate.ts
@@ -5,8 +5,11 @@
 
 import { Command, flags } from '@microsoft/bf-cli-command';
 import * as gen from '../../library/dialogGenerator'
+import * as integ from '../../library/integration'
+import * as fs from 'fs-extra';
+import * as os from 'os';
 
-export default class DialogGenerate extends Command {
+export default class GenerateDialog extends Command {
     static description = '[PREVIEW] Generate localized .lu, .lg, .qna and .dialog assets to define a bot based on a schema using templates.'
 
     static examples = [`
@@ -26,25 +29,70 @@ export default class DialogGenerate extends Command {
         schema: flags.string({ char: 's', description: 'Path to your app.schema file.', required: false }),
         templates: flags.string({ char: 't', description: 'Directory with templates to use for generating assets.  With multiple directories, the first definition found wins.  To include the standard templates, just use "standard" as a template directory name.', multiple: true }),
         verbose: flags.boolean({ description: 'Output verbose logging of files as they are processed', default: false }),
+        integrate: flags.boolean({ description: 'if merge or not', default: false })
     }
 
     async run() {
-        const { args, flags } = this.parse(DialogGenerate)
-        try {
-            await gen.generate(args.schema, flags.prefix, flags.output, flags.schema, flags.locale, flags.templates, flags.force,
-                (type, msg) => {
-                    if (type === gen.FeedbackType.message
-                        || (type === gen.FeedbackType.info && flags.verbose)) {
+        const { args, flags } = this.parse(GenerateDialog)
+        if (flags.integrate && fs.pathExists(flags.output)) {
+            let start = args.schema.lastIndexOf('/')
+            let end = args.schema.lastIndexOf('.')
+            let schemaName = args.schema.slice(start + 1, end)
+            let tempNewPath = `${os.tmpdir}/tempNew/`
+            let tempOldPath = `${os.tmpdir}/tempOld/`
+            try {
+                fs.emptyDirSync(tempNewPath)
+                fs.emptyDirSync(tempOldPath)
+                fs.copySync(flags.output, tempOldPath)
+                fs.emptyDirSync(flags.output)
+                await gen.generate(args.schema, flags.prefix, tempNewPath, flags.schema, flags.locale, flags.templates, flags.force,
+                    (type, msg) => {
+                        if (type === gen.FeedbackType.message
+                            || (type === gen.FeedbackType.info && flags.verbose)) {
+                            this.info(msg)
+                        } else if (type === gen.FeedbackType.warning) {
+                            this.warning(msg)
+                        } else if (type === gen.FeedbackType.error) {
+                            this.errorMsg(msg)
+                        }
+                    })
+            } catch (e) {
+                this.thrownError(e)
+            }
+
+            try {
+                await integ.integrateAssets(schemaName, tempOldPath, tempNewPath, flags.output, 'en-us', (type, msg) => {
+                    if (type === integ.FeedbackType.message
+                        || (type === integ.FeedbackType.info && flags.verbose)) {
                         this.info(msg)
-                    } else if (type === gen.FeedbackType.warning) {
+                    } else if (type === integ.FeedbackType.warning) {
                         this.warning(msg)
-                    } else if (type === gen.FeedbackType.error) {
+                    } else if (type === integ.FeedbackType.error) {
                         this.errorMsg(msg)
                     }
                 })
-            return true;
-        } catch (e) {
-            this.thrownError(e)
+                return true;
+            } catch (e) {
+                this.thrownError(e)
+            }
+
+        } else {
+            try {
+                await gen.generate(args.schema, flags.prefix, flags.output, flags.schema, flags.locale, flags.templates, flags.force,
+                    (type, msg) => {
+                        if (type === gen.FeedbackType.message
+                            || (type === gen.FeedbackType.info && flags.verbose)) {
+                            this.info(msg)
+                        } else if (type === gen.FeedbackType.warning) {
+                            this.warning(msg)
+                        } else if (type === gen.FeedbackType.error) {
+                            this.errorMsg(msg)
+                        }
+                    })
+                return true;
+            } catch (e) {
+                this.thrownError(e)
+            }
         }
     }
 
