89f5e00aeaeb852ceba121f18ef891fcbc0fb69d
vipeketi <58211229+vipeketi@users.noreply.github.com>
2020-04-15 11:50:46 +0530

    Modified to includes V5.0

--- samples/javascript_nodejs/70.qnamaker-multiturn-sample/index.js
+++ samples/javascript_nodejs/70.qnamaker-multiturn-sample/index.js
@@ -66,7 +66,7 @@ if (!endpointHostName.startsWith('https://')) {
     endpointHostName = 'https://' + endpointHostName;
 }
 
-if (!endpointHostName.endsWith('/v5.0-preview.1') && !endpointHostName.endsWith('/qnamaker') ) {
+if (!endpointHostName.includes('/v5.0') && !endpointHostName.endsWith('/qnamaker') ) {
     endpointHostName = endpointHostName + '/qnamaker';
 }
 
