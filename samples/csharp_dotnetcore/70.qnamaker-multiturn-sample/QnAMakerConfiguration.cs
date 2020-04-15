13d1ce8707f6867db6a498f183dcb915b5d76dea
vipeketi <58211229+vipeketi@users.noreply.github.com>
2020-04-15 14:04:19 +0530

    Modified Endswith to Contains

--- samples/csharp_dotnetcore/70.qnamaker-multiturn-sample/QnAMakerConfiguration.cs
+++ samples/csharp_dotnetcore/70.qnamaker-multiturn-sample/QnAMakerConfiguration.cs
@@ -27,7 +27,7 @@ namespace Microsoft.BotBuilderSamples
                 hostname = string.Concat("https://", hostname);
             }
 
-            if ((!hostname.EndsWith("/qnamaker")) && (!hostname.EndsWith("/v5.0-preview.1")))
+            if ((!hostname.EndsWith("/qnamaker")) && (!hostname.Contains("/v5.0")))
             {
                 hostname = string.Concat(hostname, "/qnamaker");
             }
