e9ab7d65b19b97feee2647df43974d147182b5b6
hond <hond@microsoft.com>
2020-04-16 10:09:44 +0800

    regine

--- experimental/adaptive-tool/src/lg/providers/keyBinding.ts
+++ experimental/adaptive-tool/src/lg/providers/keyBinding.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { commands, ExtensionContext, Position, window, workspace} from 'vscode';
+import { commands, ExtensionContext, Position, window} from 'vscode';
 import { isInFencedCodeBlock } from '../util';
 import * as vscode from 'vscode';
 
@@ -18,7 +18,6 @@ function onEnterKey(modifiers?: string) {
     let cursorPos: Position = editor.selection.active;
     let line = editor.document.lineAt(cursorPos.line);
     let textBeforeCursor = line.text.substr(0, cursorPos.character);
-    let textAfterCursor = line.text.substr(cursorPos.character);
 
     let lineBreakPos = cursorPos;
 
@@ -43,27 +42,6 @@ function onEnterKey(modifiers?: string) {
             editBuilder.delete(range);
         }).then(() => { editor.revealRange(editor.selection); });
     } else {
-        return asNormal('enter', modifiers);
-    }
-}
-
-function asNormal(key: string, modifiers?: string) {
-    switch (key) {
-        case 'enter':
-            if (modifiers === 'ctrl') {
-                return commands.executeCommand('editor.action.insertLineAfter');
-            } else {
-                return commands.executeCommand('type', { source: 'keyboard', text: '\n' });
-            }
-        case 'tab':
-            if (workspace.getConfiguration('emmet').get<boolean>('triggerExpansionOnTab')) {
-                return commands.executeCommand('editor.emmet.action.expandAbbreviation');
-            } else if (modifiers === 'shift') {
-                return commands.executeCommand('editor.action.outdentLines');
-            } else {
-                return commands.executeCommand('tab');
-            }
-        case 'backspace':
-            return commands.executeCommand('deleteLeft');
+        return commands.executeCommand('type', { source: 'keyboard', text: '\n' });
     }
 }
