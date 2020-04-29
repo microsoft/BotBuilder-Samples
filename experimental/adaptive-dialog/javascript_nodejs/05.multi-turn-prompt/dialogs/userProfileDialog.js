62512ef73b60a8029af99df8adbad87840afac39
Vishwac Sena Kannan <vishwacsenakannan@ALLISONT6-PROBK.northamerica.corp.microsoft.com>
2020-04-29 15:06:44 -0700

    updates

--- experimental/adaptive-dialog/javascript_nodejs/05.multi-turn-prompt/dialogs/userProfileDialog.js
+++ experimental/adaptive-dialog/javascript_nodejs/05.multi-turn-prompt/dialogs/userProfileDialog.js
@@ -1,15 +1,25 @@
 const { ComponentDialog } = require('botbuilder-dialogs');
-const { AdaptiveDialog, OnUnknownIntent, SendActivity, TemplateEngineLanguageGenerator } = require('botbuilder-dialogs-adaptive');
+const { ActivityTemplate, AdaptiveDialog, TextInput, ChoiceInput, OnUnknownIntent, SendActivity, TemplateEngineLanguageGenerator, OnBeginDialog } = require('botbuilder-dialogs-adaptive');
+const { Templates } = require('botbuilder-lg');
+const path = require('path');
+
 const ROOT_DIALOG = "USER_PROFILE_DIALOG";
 
 class UserProfileDialog extends ComponentDialog {
     constructor() {
         super('userProfileDialog');
         let userProfileAdaptiveDialog = new AdaptiveDialog(ROOT_DIALOG);
-        userProfileAdaptiveDialog.generator = new TemplateEngineLanguageGenerator()
-        userProfileAdaptiveDialog.triggers.push(new OnUnknownIntent([
-            new SendActivity("You said ${turn.activity.text}")
-        ]));
+        console.log(path.join(__dirname, "userProfileDialog.lg"));
+        let lgFile = Templates.parseFile(path.join(__dirname, "userProfileDialog.lg"));
+        userProfileAdaptiveDialog.generator = new TemplateEngineLanguageGenerator(lgFile);
+        userProfileAdaptiveDialog.triggers.push(
+            new OnBeginDialog(
+                [
+                    new TextInput("dialog.modeOfTransport", new ActivityTemplate("${ModeOfTransportPrompt.Text()}")),
+                    new SendActivity("I have ${dialog.modeOfTransport}")
+                ]
+            )
+        );
         this.addDialog(userProfileAdaptiveDialog);
         this.initialDialogId = ROOT_DIALOG;
     }
