dc96cadff656d5058bce4226553ae1970d909308
Vishwac Sena Kannan <vkannan@microsoft.com>
2020-05-01 07:41:12 -0700

    updates

--- experimental/adaptive-dialog/javascript_nodejs/05.multi-turn-prompt/dialogs/userProfileDialog.js
+++ experimental/adaptive-dialog/javascript_nodejs/05.multi-turn-prompt/dialogs/userProfileDialog.js
@@ -1,5 +1,5 @@
 const { ComponentDialog } = require('botbuilder-dialogs');
-const { ActivityTemplate, AdaptiveDialog, TextInput, ChoiceInput, OnUnknownIntent, SendActivity, TemplateEngineLanguageGenerator, OnBeginDialog } = require('botbuilder-dialogs-adaptive');
+const { NumberInput, AttachmentInput, ConfirmInput, IfCondition, ActivityTemplate, AdaptiveDialog, TextInput, ChoiceInput, OnUnknownIntent, SendActivity, TemplateEngineLanguageGenerator, OnBeginDialog } = require('botbuilder-dialogs-adaptive');
 const { Templates } = require('botbuilder-lg');
 const { StringExpression } = require('adaptive-expressions');
 
@@ -15,11 +15,80 @@ class UserProfileDialog extends ComponentDialog {
             generator: new TemplateEngineLanguageGenerator(lgFile),
             triggers: [
                 new OnBeginDialog([
-                    new TextInput().configure({
-                        property: new StringExpression("dialog.modeOfTransport"),
-                        prompt: new ActivityTemplate("${ModeOfTransportPrompt.Text()}")
+                    // Ask for user's age and set it in user.userProfile scope.
+                    new TextInput().configure(
+                    {
+                        // Set the output of the text input to this property in memory.
+                        property: new StringExpression("user.userProfile.Transport"),
+                        prompt: new ActivityTemplate("${ModeOfTransportPrompt()}")
                     }),
-                    new SendActivity("I have ${dialog.modeOfTransport}")
+                    new TextInput().configure(
+                    {
+                        property: new StringExpression("user.userProfile.Name"),
+                        prompt: new ActivityTemplate("${AskForName()}")
+                    }),
+                    // SendActivity supports full language generation resolution.
+                    // See here to learn more about language generation
+                    // https://aka.ms/language-generation
+                    new SendActivity("${AckName()}"),
+                    new ConfirmInput().configure(
+                    {
+                        Property: new StringExpression("turn.ageConfirmation"),
+                        Prompt: new ActivityTemplate("${AgeConfirmPrompt()}")
+                    }),
+                    new IfCondition().configure(
+                    {
+                        // All conditions are expressed using the common expression language.
+                        // See https://aka.ms/adaptive-expressions to learn more
+                        condition: "turn.ageConfirmation == true",
+                        actions: [
+                            new NumberInput().configure(
+                            {
+                                Prompt: new ActivityTemplate("${AskForAge()}"),
+                                Property: new StringExpression("user.userProfile.Age"),
+                                // Add validations
+                                Validations: [
+                                    // Age must be greater than or equal 1
+                                    "int(this.value) >= 1",
+                                    // Age must be less than 150
+                                    "int(this.value) < 150"
+                                ],
+                                InvalidPrompt: new ActivityTemplate("${AskForAge.invalid()}"),
+                                UnrecognizedPrompt: new ActivityTemplate("${AskForAge.unRecognized()}")
+                            }),
+                            new SendActivity("${UserAgeReadBack()}")
+                        ],
+                        elseActions: [
+                            new SendActivity("${NoAge()}") 
+                        ]
+                    }),
+                    new IfCondition().configure(
+                    {
+                        condition: "turn.activity.channelId == 'msteams'",
+                        actions: [
+                            // This attachment prompt example is not designed to work for Teams attachments, so skip it in this case
+                            new SendActivity('Skipping attachment prompt in Teams channel...')
+                        ],
+                        elseActions: [
+                            new AttachmentInput().configure(
+                            {
+                                prompt: new ActivityTemplate("${AskForImage()}"),
+                                property: new StringExpression("user.userProfile.picture"),
+                                validations: [
+                                    "this.value.contentType == 'image/jpeg' || this.value.contentType == 'image/png'"
+                                ],
+                                InvalidPrompt: new ActivityTemplate("${AskForImage.Invalid()}")
+                            })
+                        ]
+                    }),
+                    new ConfirmInput().configure(
+                    {
+                        Prompt: new ActivityTemplate("${ConfirmPrompt()}"),
+                        Property: new StringExpression("turn.finalConfirmation")
+                    }),
+                    // Use LG template to come back with the final read out.
+                    // This LG template is a great example of what logic can be wrapped up in LG sub-system.
+                    new SendActivity("${FinalUserProfileReadOut()}"), // examines turn.finalConfirmation
                 ])
             ]
         });
