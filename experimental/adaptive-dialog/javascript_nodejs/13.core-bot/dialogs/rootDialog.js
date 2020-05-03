d184b213178e68b5a14d4ac1d7ad6eecfcdda232
Ashley Finafrock <a.finafrock@gmail.com>
2020-05-03 00:45:52 -0700

    Added Book Flight intent actions

--- experimental/adaptive-dialog/javascript_nodejs/13.core-bot/dialogs/rootDialog.js
+++ experimental/adaptive-dialog/javascript_nodejs/13.core-bot/dialogs/rootDialog.js
@@ -7,8 +7,8 @@ const { LuisRecognizer } = require('botbuilder-ai');
 
 const path = require('path');
 const { ComponentDialog, ListStyle } = require('botbuilder-dialogs');
-const { ActivityTemplate, AdaptiveDialog, CancelAllDialogs, Case, ChoiceInput, EndDialog, ForEach, IfCondition, LuisAdaptiveRecognizer, OnConversationUpdateActivity, OnIntent, OnUnknownIntent, RepeatDialog, SendActivity, SwitchCondition, TemplateEngineLanguageGenerator } = require('botbuilder-dialogs-adaptive');
-const { ArrayExpression, BoolExpression, EnumExpression, StringExpression } = require('adaptive-expressions');
+const { ActivityTemplate, AdaptiveDialog, CancelAllDialogs, Case, ChoiceInput, ConfirmInput, DateTimeInput, DeleteProperty, EndDialog, ForEach, IfCondition, LuisAdaptiveRecognizer, OnConversationUpdateActivity, OnIntent, OnUnknownIntent, PropertyAssignment, RepeatDialog, SendActivity, SetProperties, SwitchCondition, TemplateEngineLanguageGenerator, TextInput } = require('botbuilder-dialogs-adaptive');
+const { ArrayExpression, BoolExpression, EnumExpression, StringExpression, ValueExpression, ValueExpressionConverter } = require('adaptive-expressions');
 const { Templates } = require('botbuilder-lg');
 
 const ROOT_DIALOG = 'mainWaterfallDialog';
@@ -17,7 +17,7 @@ class RootDialog extends ComponentDialog {
     constructor() {
         super(ROOT_DIALOG);
         
-        const lgFile = Templates.parseFile(path.join(__dirname, "rootDialog.lg"));
+        const lgFile = Templates.parseFile(path.join(__dirname, 'rootDialog.lg'));
         // There are no steps associated with this dialog.
         // This dialog will react to user input using its own Recognizer's output and Rules.
         const rootDialog = new AdaptiveDialog(ROOT_DIALOG).configure({
@@ -28,13 +28,84 @@ class RootDialog extends ComponentDialog {
             recognizer: this.createLuisRecognizer(),
             triggers: [
                 new OnConversationUpdateActivity(this.welcomeUserSteps()),
-                new OnIntent('Greeting', [], [ new SendActivity("${BotOverview()}")]),
-                new OnIntent('Help', [], [ new SendActivity("${BotOverview()}") ], "#Help.Score >= 0.8"),
+                new OnIntent('Greeting', [], [ new SendActivity('${BotOverview()}')]),
+                new OnIntent('Help', [], [ new SendActivity('${BotOverview()}') ], '#Help.Score >= 0.8'),
                 new OnIntent('Cancel', [], [
-                    // This is the global cancel in case a child dialog did not explicitly handle cancelling.
                     new SendActivity('Sure, cancelling that...'),
                     new CancelAllDialogs(),
                     new EndDialog()
+                ]),
+                new OnUnknownIntent([ new SendActivity("${UnknownIntent()}") ]),
+                new OnIntent('Book_flight', [], [
+                    // Save any entities returned by LUIS.
+                    new SetProperties([
+                        {
+                            property: new StringExpression('conversation.flightBooking'),
+                            value: new ValueExpression('={}')
+                        },
+                        // We will only save any geography city entities that explicitly have been classified as either fromCity or toCity.
+                        {
+                            property: new StringExpression('conversation.flightBooking.departureCity'),
+                            // `value` is an expression. @entityName is shorthand to refer to the value of an entity recognized.
+                            // @xxx is same as turn.recognized.entities.xxx
+                            value: new ValueExpression('=@fromCity.location')
+                        },
+                        {
+                            property: new StringExpression('conversation.flightBooking.destinationCity'),
+                            value: new ValueExpression('=@toCity.location')
+                        },
+                        {
+                            property: new StringExpression ('conversation.flightBooking.departureDate'),
+                            value: new ValueExpression ('=@datetime.timex[0]')
+                        },
+                    ]),
+                    // Steps to book A flight.
+                    // Help and Cancel intents are always available since TextInput will always initiate
+                        // consultation up the parent dialog chain to see if anyone else wants to take the user input.
+                    new TextInput().configure({
+                        property: new StringExpression('conversation.flightBooking.departureCity'),
+                        // Prompt property supports full language generation resolution.
+                        // See here to learn more about language generation:
+                        // https://github.com/Microsoft/BotBuilder-Samples/tree/master/experimental/language-generation
+                        prompt: new ActivityTemplate('${PromptForMissingInformation()}'),
+                        // We will allow interruptions as long as the user did not explicitly answer the question
+                        // This property supports an expression so you can examine presence of an intent via #intentName, 
+                        //    detect presence of an entity via @entityName etc. Interruption is allowed if the expression
+                        //    evaluates to `true`. This property defaults to `true`.
+                        allowInterruptions: new BoolExpression('!@fromCity || !@geographyV2'),
+                        // value is an expression. Take first non-null value.
+                        value: new ValueExpression('=coalesce(@fromCity.location, @geographyV2.location)')
+                    }),
+                    // Delete entity so it is not overconsumed as destination as well.
+                    new DeleteProperty('turn.recognized.entities.geographyV2'),
+                    new TextInput().configure({
+                        property: new StringExpression('conversation.flightBooking.destinationCity'),
+                        prompt: new ActivityTemplate('${PromptForMissingInformation()}'),
+                        allowInterruptions: new BoolExpression('!@toCity || !@geographyV2'),
+                        // value is an expression. Take any recognized city name as fromCity.
+                        value: new ValueExpression('=coalesce(@toCity.location, @geographyV2.location)')
+                    }),
+                    new DateTimeInput().configure({
+                        property: new StringExpression('conversation.flightBooking.departureDate'),
+                        prompt: new ActivityTemplate('${PromptForMissingInformation()}'),
+                        allowInterruptions: new BoolExpression('!@datetime'),
+                        // value is an expression. Take any date time entity recognition as departure date.
+                        value: new ValueExpression('=@datetime.timex[0]')
+                    }),
+                    new ConfirmInput().configure({
+                        property: new StringExpression('turn.bookingConfirmation'),
+                        prompt: new ActivityTemplate('${ConfirmBooking()}'),
+                        // You can use this flag to control when a specific input participates in consultation bubbling and can be interrupted.
+                        // 'false' means interruption is not allowed when this input is active.
+                        allowInterruptions: new BoolExpression('false')
+                    }),
+                    new IfCondition().configure({
+                        // All conditions are expressed using the common expression language.
+                        // See https://github.com/Microsoft/BotBuilder-Samples/tree/master/experimental/common-expression-language to learn more
+                        condition: new BoolExpression('turn.bookingConfirmation == true'),
+                        actions: [ new SendActivity('${BookingConfirmation()}')],
+                        elseActions: [ new SendActivity('Thank you.') ]
+                    })
                 ]),
             ]
         });
@@ -74,117 +145,6 @@ class RootDialog extends ComponentDialog {
             }
         );
     }
-
-    // /**
-    //  * First step in the waterfall dialog. Prompts the user for a command.
-    //  * Currently, this expects a booking request, like "book me a flight from Paris to Berlin on march 22"
-    //  * Note that the sample LUIS model will only recognize Paris, Berlin, New York and London as airport cities.
-    //  */
-    // async introStep(stepContext) {
-    //     if (!this.luisRecognizer.isConfigured) {
-    //         const messageText = 'NOTE: LUIS is not configured. To enable all capabilities, add `LuisAppId`, `LuisAPIKey` and `LuisAPIHostName` to the .env file.';
-    //         await stepContext.context.sendActivity(messageText, null, InputHints.IgnoringInput);
-    //         return await stepContext.next();
-    //     }
-
-    //     const messageText = stepContext.options.restartMsg ? stepContext.options.restartMsg : 'What can I help you with today?\nSay something like "Book a flight from Paris to Berlin on March 22, 2020"';
-    //     const promptMessage = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
-    //     return await stepContext.prompt('TextPrompt', { prompt: promptMessage });
-    // }
-
-    // /**
-    //  * Second step in the waterfall.  This will use LUIS to attempt to extract the origin, destination and travel dates.
-    //  * Then, it hands off to the bookingDialog child dialog to collect any remaining details.
-    //  */
-    // async actStep(stepContext) {
-    //     const bookingDetails = {};
-
-    //     if (!this.luisRecognizer.isConfigured) {
-    //         // LUIS is not configured, we just run the BookingDialog path.
-    //         return await stepContext.beginDialog('bookingDialog', bookingDetails);
-    //     }
-
-    //     // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt)
-    //     const luisResult = await this.luisRecognizer.executeLuisQuery(stepContext.context);
-    //     switch (LuisRecognizer.topIntent(luisResult)) {
-    //     case 'BookFlight': {
-    //         // Extract the values for the composite entities from the LUIS result.
-    //         const fromEntities = this.luisRecognizer.getFromEntities(luisResult);
-    //         const toEntities = this.luisRecognizer.getToEntities(luisResult);
-
-    //         // Show a warning for Origin and Destination if we can't resolve them.
-    //         await this.showWarningForUnsupportedCities(stepContext.context, fromEntities, toEntities);
-
-    //         // Initialize BookingDetails with any entities we may have found in the response.
-    //         bookingDetails.destination = toEntities.airport;
-    //         bookingDetails.origin = fromEntities.airport;
-    //         bookingDetails.travelDate = this.luisRecognizer.getTravelDate(luisResult);
-    //         console.log('LUIS extracted these booking details:', JSON.stringify(bookingDetails));
-
-    //         // Run the BookingDialog passing in whatever details we have from the LUIS call, it will fill out the remainder.
-    //         return await stepContext.beginDialog('bookingDialog', bookingDetails);
-    //     }
-
-    //     case 'GetWeather': {
-    //         // We haven't implemented the GetWeatherDialog so we just display a TODO message.
-    //         const getWeatherMessageText = 'TODO: get weather flow here';
-    //         await stepContext.context.sendActivity(getWeatherMessageText, getWeatherMessageText, InputHints.IgnoringInput);
-    //         break;
-    //     }
-
-    //     default: {
-    //         // Catch all for unhandled intents
-    //         const didntUnderstandMessageText = `Sorry, I didn't get that. Please try asking in a different way (intent was ${ LuisRecognizer.topIntent(luisResult) })`;
-    //         await stepContext.context.sendActivity(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
-    //     }
-    //     }
-
-    //     return await stepContext.next();
-    // }
-
-    // /**
-    //  * Shows a warning if the requested From or To cities are recognized as entities but they are not in the Airport entity list.
-    //  * In some cases LUIS will recognize the From and To composite entities as a valid cities but the From and To Airport values
-    //  * will be empty if those entity values can't be mapped to a canonical item in the Airport.
-    //  */
-    // async showWarningForUnsupportedCities(context, fromEntities, toEntities) {
-    //     const unsupportedCities = [];
-    //     if (fromEntities.from && !fromEntities.airport) {
-    //         unsupportedCities.push(fromEntities.from);
-    //     }
-
-    //     if (toEntities.to && !toEntities.airport) {
-    //         unsupportedCities.push(toEntities.to);
-    //     }
-
-    //     if (unsupportedCities.length) {
-    //         const messageText = `Sorry but the following airports are not supported: ${ unsupportedCities.join(', ') }`;
-    //         await context.sendActivity(messageText, messageText, InputHints.IgnoringInput);
-    //     }
-    // }
-
-    // /**
-    //  * This is the final step in the main waterfall dialog.
-    //  * It wraps up the sample "book a flight" interaction with a simple confirmation.
-    //  */
-    // async finalStep(stepContext) {
-    //     // If the child dialog ("bookingDialog") was cancelled or the user failed to confirm, the Result here will be null.
-    //     if (stepContext.result) {
-    //         const result = stepContext.result;
-    //         // Now we have all the booking details.
-
-    //         // This is where calls to the booking AOU service or database would go.
-
-    //         // If the call to the booking service was successful tell the user.
-    //         const timeProperty = new TimexProperty(result.travelDate);
-    //         const travelDateMsg = timeProperty.toNaturalLanguage(new Date(Date.now()));
-    //         const msg = `I have you booked to ${ result.destination } from ${ result.origin } on ${ travelDateMsg }.`;
-    //         await stepContext.context.sendActivity(msg, msg, InputHints.IgnoringInput);
-    //     }
-
-    //     // Restart the main dialog with a different message the second time around
-    //     return await stepContext.replaceDialog(this.initialDialogId, { restartMsg: 'What else can I do for you?' });
-    // }
 }
 
 module.exports.RootDialog = RootDialog;
