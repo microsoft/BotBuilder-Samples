e9cc5783ec055d20d1850ae8e28fbe880f79f259
Ashley Finafrock <a.finafrock@gmail.com>
2020-05-04 11:11:48 -0700

    Updated C# dates to take week-later date rather than hardcoded date

--- samples/csharp_webapi/13.core-bot/Dialogs/MainDialog.cs
+++ samples/csharp_webapi/13.core-bot/Dialogs/MainDialog.cs
@@ -50,7 +50,8 @@ namespace Microsoft.BotBuilderSamples.Dialogs
             }
 
             // Use the text provided in FinalStepAsync or the default if it is the first time.
-            var messageText = stepContext.Options?.ToString() ?? "What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on March 22, 2020\"";
+            var weekLaterDate = DateTime.Now.AddDays(7).ToString("MMMM d, yyyy");
+            var messageText = stepContext.Options?.ToString() ?? $"What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on {weekLaterDate}\"";
             var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
             return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
         }
