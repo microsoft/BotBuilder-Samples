09ec4bc28a0e8d3262b5b920941529a89a193de7
Hannah Olliges <haollige@microsoft.com>
2020-05-06 14:47:49 -0700

    add dynamics bot handoff bot to omnichannel in bot framework samples

---
+++ experimental/handoff-library/csharp_dotnetcore/samples/DynamicsOmnichannelBot/AdaptiveCards/AdaptiveCards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace DynamicsOmnichannelBot.AdaptiveCards
+{
+    public class AdaptiveCards
+    {
+        public static Attachment CreateAdaptiveCardAttachment(string card)
+        {
+            string[] paths = { ".", "AdaptiveCards", card };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var attachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return attachment;
+        }
+
+        public static SuggestedActions CreateSuggestedAction(string[] actions)
+        {
+            var actionsList = new List<CardAction>();
+            foreach (string action in actions)
+            {
+                actionsList.Add(new CardAction() { Title = action, Type = ActionTypes.ImBack, Value = action });
+            }
+
+            return new SuggestedActions() { Actions = actionsList };
+        }
+    }
+}
