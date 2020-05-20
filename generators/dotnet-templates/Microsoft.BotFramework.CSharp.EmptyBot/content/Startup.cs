446e181d8e46f1f9419dc0870c9f3a10a36a30bc
Scott Gellock <sgellock@gmail.com>
2020-05-20 13:33:07 -0700

    Update EmptyBot Template for 4.9
    - update to use .NET Core 3.1
    - update .nuspec to use updated license and icon tags
    - add LICENSE.txt and nuget_icon.png to support updated .nuspec tags
    - bump package version number to 4.9
    - remove —framework command line option

--- generators/dotnet-templates/Microsoft.BotFramework.CSharp.EmptyBot/content/Startup.cs
+++ generators/dotnet-templates/Microsoft.BotFramework.CSharp.EmptyBot/content/Startup.cs
@@ -5,13 +5,11 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace __PROJECT_NAME__
 {
@@ -27,9 +25,9 @@ namespace __PROJECT_NAME__
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers().AddNewtonsoftJson();
 
-            // Create the Bot Framework Adapter.
+            // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkHttpAdapter>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
@@ -37,22 +35,24 @@ namespace __PROJECT_NAME__
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseWebSockets();
-            //app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            // app.UseHttpsRedirection();
         }
     }
 }
