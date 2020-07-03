c31b2d3d04347721efd112c58dffef8083003a3e
Zongyang Ma <zoma@microsoft.com>
2020-07-03 14:59:19 +0800

    update processSchema

--- experimental/generation/generator/src/library/processSchemas.ts
+++ experimental/generation/generator/src/library/processSchemas.ts
@@ -78,6 +78,7 @@ function mergeSchemas(allSchema: any, schemas: any[]) {
         if (schema.required) allSchema.required = allSchema.required.concat(schema.required)
         if (schema.$defaultOperation) allSchema.$defaultOperation = allSchema.$defaultOperation.concat(schema.$defaultOperation)
         if (schema.$examples) allSchema.$examples = {...allSchema.$examples, ...schema.$examples}
+        if (schema.$parameters) allSchema.$examples = {...allSchema.$parameters, ...schema.$parameters}
         if (schema.$expectedOnly) allSchema.$expectedOnly = allSchema.$expectedOnly.concat(schema.$expectedOnly)
         if (schema.$operations) allSchema.$operations = allSchema.$operations.concat(schema.$operations)
         if (schema.$public) allSchema.$public = allSchema.$public.concat(schema.$public)
@@ -171,6 +172,7 @@ export async function processSchemas(schemaPath: string, templateDirs: string[],
     if (!allSchema.$operations) allSchema.$operations = []
     if (!allSchema.$defaultOperation) allSchema.$defaultOperation = []
     if (!allSchema.$examples) allSchema.$examples = []
+    if (!allSchema.$parameters) allSchema.$parameters = []
     if (formSchema.$public) {
         allSchema.$public = formSchema.$public
     } else {
