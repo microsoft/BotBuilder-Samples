b07140e29cf496c5bea68dab140dd6ba4d334d7d
Yuepeng Shen <shenyuepeng@bupt.edu.cn>
2020-07-06 23:33:11 +0800

    add gitignore

--- experimental/adaptive-tool/server/lg/src/util.ts
+++ experimental/adaptive-tool/server/lg/src/util.ts
@@ -1,3 +1,7 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
 
 import { TextDocument, Range, Position } from "vscode-languageserver-textdocument";
 import { DocumentUri, Files, TextDocuments, WorkspaceFolder } from 'vscode-languageserver';
