725a5e6c9cb008fda35ed11698eefec58de3f7d2
Artur Laksberg <arturl@microsoft.com>
2020-07-10 11:08:49 -0700

    Fix handling of messages with empty text (#2580)
    Messages such as postBack actions have no text and cause NREs.

--- experimental/handoff-library/csharp_dotnetcore/samples/LivePersonConnector/Middleware/LoggingMiddleware.cs
+++ experimental/handoff-library/csharp_dotnetcore/samples/LivePersonConnector/Middleware/LoggingMiddleware.cs
@@ -24,21 +24,24 @@ namespace LivePersonProxyBot
 
         public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
         {
-            var conversationStateAccessors = _conversationState.CreateProperty<LoggingConversationData>(nameof(LoggingConversationData));
-            var conversationData = await conversationStateAccessors.GetAsync(turnContext, () => new LoggingConversationData()).ConfigureAwait(false);
-
-            // Log the transcript of the conversation so it can be passed to the agent upon escalation
-            conversationData.ConversationLog.Add(turnContext.Activity);
-            await _conversationState.SaveChangesAsync(turnContext).ConfigureAwait(false);
-
-            turnContext.OnSendActivities(async (sendTurnContext, activities, nextSend) =>
+            if (turnContext.Activity.Text != null)
             {
-                conversationData.ConversationLog.AddRange(activities);
+                var conversationStateAccessors = _conversationState.CreateProperty<LoggingConversationData>(nameof(LoggingConversationData));
+                var conversationData = await conversationStateAccessors.GetAsync(turnContext, () => new LoggingConversationData()).ConfigureAwait(false);
+
+                // Log the transcript of the conversation so it can be passed to the agent upon escalation
+                conversationData.ConversationLog.Add(turnContext.Activity);
                 await _conversationState.SaveChangesAsync(turnContext).ConfigureAwait(false);
-                // run full pipeline
-                var responses = await nextSend().ConfigureAwait(false);
-                return responses;
-            });
+
+                turnContext.OnSendActivities(async (sendTurnContext, activities, nextSend) =>
+                {
+                    conversationData.ConversationLog.AddRange(activities);
+                    await _conversationState.SaveChangesAsync(turnContext).ConfigureAwait(false);
+                    // run full pipeline
+                    var responses = await nextSend().ConfigureAwait(false);
+                    return responses;
+                });
+            }
 
             await next(cancellationToken).ConfigureAwait(false);
         }
