912987d03d31cdb7eb57e3502d4a929aeccd17e7
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-07-21 10:57:17 -0500

    Python: 54.teams-task-module

--- samples/python/54.teams-task-module/bots/teams_task_module_bot.py
+++ samples/python/54.teams-task-module/bots/teams_task_module_bot.py
@@ -2,18 +2,15 @@
 # Licensed under the MIT License.
 
 import json
+import os
+
 from botbuilder.core import (
     CardFactory,
     MessageFactory,
     TurnContext,
 )
-from botbuilder.schema import (
-    HeroCard,
-    CardAction,
-    Attachment,
-)
+from botbuilder.schema import HeroCard, Attachment, CardAction
 from botbuilder.schema.teams import (
-    TaskModuleContinueResponse,
     TaskModuleMessageResponse,
     TaskModuleRequest,
     TaskModuleResponse,
@@ -21,42 +18,53 @@ from botbuilder.schema.teams import (
 )
 from botbuilder.core.teams import TeamsActivityHandler
 
+from config import DefaultConfig
+from models import (
+    UISettings,
+    TaskModuleUIConstants,
+    TaskModuleIds,
+    TaskModuleResponseFactory,
+)
+
 
 class TeamsTaskModuleBot(TeamsActivityHandler):
+    def __init__(self, config: DefaultConfig):
+        self.__base_url = config.BASE_URL
+
     async def on_message_activity(self, turn_context: TurnContext):
-        reply = MessageFactory.attachment(self._get_task_module_hero_card())
+        reply = MessageFactory.list(
+            [
+                TeamsTaskModuleBot.__get_task_module_hero_card_options(),
+                TeamsTaskModuleBot.__get_task_module_adaptive_card_options(),
+            ]
+        )
         await turn_context.send_activity(reply)
 
     async def on_teams_task_module_fetch(
         self, turn_context: TurnContext, task_module_request: TaskModuleRequest
     ) -> TaskModuleResponse:
-        await turn_context.send_activity(
-            MessageFactory.text(
-                f"on_teams_task_module_fetch:{json.dumps(task_module_request.data)}"
-            )
-        )
-        card = CardFactory.adaptive_card(
-            {
-                "version": "1.0.0",
-                "type": "AdaptiveCard",
-                "body": [
-                    {"type": "TextBlock", "text": "Enter Text Here",},
-                    {
-                        "type": "Input.Text",
-                        "id": "usertext",
-                        "placeholder": "add some text and submit",
-                        "IsMultiline": "true",
-                    },
-                ],
-                "actions": [{"type": "Action.Submit", "title": "Submit",}],
-            }
-        )
+        card_task_fetch_value = task_module_request.data["data"]
 
-        task_info = TaskModuleTaskInfo(
-            card=card, title="Adaptive Card: Inputs", height=200, width=400
-        )
-        continue_response = TaskModuleContinueResponse(type="continue", value=task_info)
-        return TaskModuleResponse(task=continue_response)
+        task_info = TaskModuleTaskInfo()
+        if card_task_fetch_value == TaskModuleIds.YOUTUBE:
+            task_info.url = task_info.fallback_url = (
+                self.__base_url + "/" + TaskModuleIds.YOUTUBE + ".html"
+            )
+            TeamsTaskModuleBot.__set_task_info(task_info, TaskModuleUIConstants.YOUTUBE)
+        elif card_task_fetch_value == TaskModuleIds.CUSTOM_FORM:
+            task_info.url = task_info.fallback_url = (
+                self.__base_url + "/" + TaskModuleIds.CUSTOM_FORM + ".html"
+            )
+            TeamsTaskModuleBot.__set_task_info(
+                task_info, TaskModuleUIConstants.CUSTOM_FORM
+            )
+        elif card_task_fetch_value == TaskModuleIds.ADAPTIVE_CARD:
+            task_info.card = TeamsTaskModuleBot.__create_adaptive_card_attachment()
+            TeamsTaskModuleBot.__set_task_info(
+                task_info, TaskModuleUIConstants.ADAPTIVE_CARD
+            )
+
+        return TaskModuleResponseFactory.to_task_module_response(task_info)
 
     async def on_teams_task_module_submit(
         self, turn_context: TurnContext, task_module_request: TaskModuleRequest
@@ -67,19 +75,67 @@ class TeamsTaskModuleBot(TeamsActivityHandler):
             )
         )
 
-        message_response = TaskModuleMessageResponse(type="message", value="Thanks!")
+        message_response = TaskModuleMessageResponse(value="Thanks!")
         return TaskModuleResponse(task=message_response)
 
-    def _get_task_module_hero_card(self) -> Attachment:
-        task_module_action = CardAction(
-            type="invoke",
-            title="Adaptive Card",
-            value={"type": "task/fetch", "data": "adaptivecard"},
-        )
-        card = HeroCard(
-            title="Task Module Invocation from Hero Card",
-            subtitle="This is a hero card with a Task Module Action button.  Click the button to show an Adaptive "
-                     "Card within a Task Module.",
-            buttons=[task_module_action],
-        )
+    @staticmethod
+    def __set_task_info(task_info: TaskModuleTaskInfo, ui_constants: UISettings):
+        task_info.height = ui_constants.height
+        task_info.width = ui_constants.width
+        task_info.title = ui_constants.title
+
+    @staticmethod
+    def __get_task_module_hero_card_options() -> Attachment:
+        buttons = [
+            CardAction(
+                type="invoke",
+                title=card_type.button_title,
+                value={"type": "task/fetch", "data": card_type.id},
+            )
+            for card_type in [
+                TaskModuleUIConstants.ADAPTIVE_CARD,
+                TaskModuleUIConstants.CUSTOM_FORM,
+                TaskModuleUIConstants.YOUTUBE,
+            ]
+        ]
+
+        card = HeroCard(title="Task Module Invocation from Hero Card", buttons=buttons,)
         return CardFactory.hero_card(card)
+
+    @staticmethod
+    def __get_task_module_adaptive_card_options() -> Attachment:
+        adaptive_card = {
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "version": "1.0",
+            "type": "AdaptiveCard",
+            "body": [
+                {
+                    "type": "TextBlock",
+                    "text": "Task Module Invocation from Adaptive Card",
+                    "weight": "bolder",
+                    "size": 3,
+                },
+            ],
+            "actions": [
+                {
+                    "type": "Action.Submit",
+                    "title": card_type.button_title,
+                    "data": {"msteams": {"type": "task/fetch"}, "data": card_type.id},
+                }
+                for card_type in [
+                    TaskModuleUIConstants.ADAPTIVE_CARD,
+                    TaskModuleUIConstants.CUSTOM_FORM,
+                    TaskModuleUIConstants.YOUTUBE,
+                ]
+            ],
+        }
+
+        return CardFactory.adaptive_card(adaptive_card)
+
+    @staticmethod
+    def __create_adaptive_card_attachment() -> Attachment:
+        card_path = os.path.join(os.getcwd(), "resources/adaptiveCard.json")
+        with open(card_path, "rb") as in_file:
+            card_data = json.load(in_file)
+
+        return CardFactory.adaptive_card(card_data)
