2a65d09b40fbcb95eec3e3b537dba2a882e820c9
Hongyang <hond@microsoft.com>
2020-07-22 21:31:46 +0800

    init

---
+++ experimental/adaptive-tool/server/webpack.config.js
@@ -0,0 +1,23 @@
+  
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//@ts-check
+
+'use strict';
+
+const withDefaults = require('../shared.webpack.config');
+const path = require('path');
+
+module.exports = withDefaults({
+	context: path.join(__dirname),
+	entry: {
+		extension: './lg/src/server.ts',
+	},
+	output: {
+		filename: 'server.js',
+		path: path.join(__dirname, 'out')
+	}
+});
