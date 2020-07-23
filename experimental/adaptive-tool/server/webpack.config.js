8754a820360b55ac2c2319c89dc3637f3ce744f4
Hongyang <hond@microsoft.com>
2020-07-23 20:12:33 +0800

    init

--- experimental/adaptive-tool/server/webpack.config.js
+++ experimental/adaptive-tool/server/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
   
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
