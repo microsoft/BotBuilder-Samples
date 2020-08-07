128aebd79687084166135e4d5023a5321dd3794c
Dong Lei <donglei@microsoft.com>
2020-08-07 09:13:03 +0000

    fix issue

--- experimental/adaptive-tool/server/lg/webpack.config.js
+++ experimental/adaptive-tool/server/lg/webpack.config.js
@@ -9,16 +9,16 @@
 
 'use strict';
 
-const withDefaults = require('../shared.webpack.config');
+const withDefaults = require('../../shared.webpack.config');
 const path = require('path');
 
 module.exports = withDefaults({
 	context: path.join(__dirname),
 	entry: {
-		extension: './lg/src/server.ts',
+		extension: './src/server.ts',
 	},
 	output: {
-		filename: 'server.js',
-		path: path.join(__dirname, 'out')
+		filename: 'lgserver.js',
+		path: path.join(__dirname, '..', 'out')
 	}
 });
