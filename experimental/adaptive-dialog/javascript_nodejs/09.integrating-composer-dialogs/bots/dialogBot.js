0f498706f7479edc6faf679fef546cefb74362fd
Zichuan Ma <zim@microsoft.com>
2020-08-20 16:41:41 +0800

    bump sdk version to 4.10.0 for adaptive sample 09

--- experimental/adaptive-dialog/javascript_nodejs/09.integrating-composer-dialogs/bots/dialogBot.js
+++ experimental/adaptive-dialog/javascript_nodejs/09.integrating-composer-dialogs/bots/dialogBot.js
@@ -3,6 +3,8 @@
 
 const { ActivityHandler } = require('botbuilder');
 const { DialogManager } = require('botbuilder-dialogs');
+const { LanguageGeneratorExtensions, ResourceExtensions } = require('botbuilder-dialogs-adaptive');
+const { ResourceExplorer } = require('botbuilder-dialogs-declarative');
 
 class DialogBot extends ActivityHandler {
     /**
@@ -10,16 +12,20 @@ class DialogBot extends ActivityHandler {
      * @param {ConversationState} conversationState
      * @param {UserState} userState
      * @param {Dialog} dialog
+     * @param {ResourceExplorer} resourceExplorer
      */
-    constructor(conversationState, userState, dialog) {
+    constructor(conversationState, userState, dialog, resourceExplorer) {
         super();
         if (!conversationState) throw new Error('[DialogBot]: Missing parameter. conversationState is required');
         if (!userState) throw new Error('[DialogBot]: Missing parameter. userState is required');
         if (!dialog) throw new Error('[DialogBot]: Missing parameter. dialog is required');
+        if (!resourceExplorer) throw new Error('[DialogBot]: Missing parameter. resourceExplorer is required');
 
         this.dialogManager = new DialogManager(dialog);
         this.dialogManager.conversationState = conversationState;
         this.dialogManager.userState = userState;
+        ResourceExtensions.useResourceExplorer(this.dialogManager, resourceExplorer);
+        LanguageGeneratorExtensions.useLanguageGeneration(this.dialogManager);
 
         this.onTurn(async (context, next) => {
             console.log('Running dialog with activity.');
--- experimental/adaptive-dialog/javascript_nodejs/09.integrating-composer-dialogs/index.js
+++ experimental/adaptive-dialog/javascript_nodejs/09.integrating-composer-dialogs/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const restify = require('restify');
 
 const { ResourceExplorer } = require('botbuilder-dialogs-declarative');
-const { AdaptiveDialogComponentRegistration, LanguageGeneratorMiddleWare } = require('botbuilder-dialogs-adaptive');
+const { AdaptiveDialogComponentRegistration } = require('botbuilder-dialogs-adaptive');
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
@@ -21,7 +21,7 @@ const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 
 // Set up resource explorer
-const resourceExplorer = new ResourceExplorer().addFolder(__dirname, true, true);
+const resourceExplorer = new ResourceExplorer().addFolders(__dirname, ['node_modules'], false);
 resourceExplorer.addComponent(new AdaptiveDialogComponentRegistration(resourceExplorer));
 
 // Create adapter.
@@ -54,7 +54,6 @@ const onTurnErrorHandler = async (context, error) => {
 
 // Catch-all for errors.
 adapter.onTurnError = onTurnErrorHandler;
-adapter.use(new LanguageGeneratorMiddleWare(resourceExplorer));
 
 // Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
 // A bot requires a state store to persist the dialog and user state between messages.
@@ -64,7 +63,7 @@ const userState = new UserState(memoryStorage);
 
 // Create the main dialog.
 const dialog = new RootDialog(userState, resourceExplorer);
-const bot = new DialogBot(conversationState, userState, dialog);
+const bot = new DialogBot(conversationState, userState, dialog, resourceExplorer);
 
 // Create HTTP server.
 const server = restify.createServer();
