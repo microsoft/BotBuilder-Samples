f0b9e919585d50c4929e309d5a05e11622578821
Monica Rivera <moniriv@microsoft.com>
2020-08-20 17:06:08 -0700

    Update Yeoman to 4.10.1

--- generators/generator-botbuilder/generators/app/templates/empty/package.json.ts
+++ generators/generator-botbuilder/generators/app/templates/empty/package.json.ts
@@ -18,7 +18,7 @@
         "url": "https://github.com"
     },
     "dependencies": {
-        "botbuilder": "~4.9.1",
+        "botbuilder": "~4.10.1",
         "replace": "~1.2.0",
         "restify": "~8.5.1"
     },
