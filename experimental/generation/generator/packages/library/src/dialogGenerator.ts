9f3ba1ba5c2760d02bef7c50ea95639bf52673f8
Chris McConnell <chrimc@microsoft.com>
2020-08-25 16:05:44 -0700

    Add support for skipping a clarification

--- experimental/generation/generator/packages/library/src/dialogGenerator.ts
+++ experimental/generation/generator/packages/library/src/dialogGenerator.ts
@@ -692,7 +692,7 @@ export async function generate(
     }
 
     if (!metaSchema) {
-        metaSchema = 'https://raw.githubusercontent.com/microsoft/botbuilder-samples/master/experimental/generation/runbot/runbot.schema'
+        metaSchema = 'https://raw.githubusercontent.com/microsoft/botbuilder-samples/main/experimental/generation/runbot/runbot.schema'
     } else if (!metaSchema.startsWith('http')) {
         // Adjust relative to outDir
         metaSchema = ppath.relative(outDir, metaSchema)
