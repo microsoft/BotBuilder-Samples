4a3023bc72bfdae8b23e44ee6a80d9d67ea46a17
Jos Verlinde <josverl@microsoft.com>
2020-08-26 22:17:53 +0200

    remove extra files & changes

--- samples/python/54.teams-task-module/config.py
+++ samples/python/54.teams-task-module/config.py
@@ -9,6 +9,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "fb33674c-a6b0-431e-8538-8dc27dc0bea6")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "4iXtf6EFo9ofmjAHg-YeL._8Ut4rN-LHR4") #Dev-1y
-
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
