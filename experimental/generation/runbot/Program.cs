e9a074f8c8cabd4fdf1c754c1defee61f4c3b3c1
Chris McConnell <chrimc@microsoft.com>
2020-08-28 16:43:11 -0700

    Update to SDK support for new generation model. Add --dialog to specify dialog to run.

--- experimental/generation/runbot/Program.cs
+++ experimental/generation/runbot/Program.cs
@@ -23,6 +23,7 @@ namespace RunBotServer
             Console.WriteLine("--root <PATH>: Absolute path to the root directory for declarative resources all *.main.dialog be options.  Default current directory");
             Console.WriteLine("--region <REGION>: LUIS endpoint region.  Default westus");
             Console.WriteLine("--environment <ENVIRONMENT>: LUIS environment settings to use.  Default is user alias.");
+            Console.WriteLine("--dialog <DIALOG>: Name of root dialog to run.  By default all root *.dialog will be choices.");
             Console.WriteLine("To use LUIS you should do 'dotnet user-secrets set --id RunBot luis:endpointKey <yourKey>'");
         }
 
