899e1af1a1af1d40f0559390f6c44ab6ebb48c85
Monica Rivera <moniriv@microsoft.com>
2020-09-01 16:42:47 -0700

    Update namespace of EchoBot and EmptyBot.

--- generators/dotnet-templates/Microsoft.BotFramework.CSharp.EchoBot/content/AdapterWithErrorHandler.cs
+++ generators/dotnet-templates/Microsoft.BotFramework.CSharp.EchoBot/content/AdapterWithErrorHandler.cs
@@ -8,7 +8,7 @@ using Microsoft.Bot.Builder.TraceExtensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace __PROJECT_NAME__
+namespace EchoBot
 {
     public class AdapterWithErrorHandler : BotFrameworkHttpAdapter
     {
