5a5673b65ff2cb0ab6ead6e0b05a16865aa66377
Zongyang Ma <zoma@microsoft.com>
2020-10-22 15:08:22 +0800

    update script for unit tests

--- experimental/generation/generator/packages/cli/scripts/genUnitTests.cmd
+++ experimental/generation/generator/packages/cli/scripts/genUnitTests.cmd
@@ -1,5 +1,6 @@
 @echo off
-rd /s /q %temp%\unittests.out
-call ..\bin\run dialog:generate ../../library/test/forms/unittests.schema -o %temp%/unittests.out --force --verbose
-call gencopy %temp%\unittests.out %REPOS%\botbuilder-dotnet\tests\Microsoft.Bot.Builder.Dialogs.Adaptive.Tests\Tests\GeneratorTests\unittests\ %1
-
+for /l %%i in (1, 1, 23) do (
+    call rd /s /q %temp%\unittest%%i.out
+    call ..\bin\run dialog:generate ../../library/test/forms/unittest%%i.schema -o %temp%/unittest%%i.out --force --verbose
+    call gencopy %temp%\unittest%%i.out %REPOS%\botbuilder-dotnet\tests\Microsoft.Bot.Builder.Dialogs.Adaptive.Tests\Tests\GeneratorTests\unittest%%i\ %1
+)
