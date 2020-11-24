29e727ba148d94687cb2532b3b1101fb9865755b
Wajeed-msft <31851992+Wajeed-msft@users.noreply.github.com>
2020-11-24 10:16:01 +0530

    Adding new sample to demo authentication in Messaging Extension action.
    Adding new sample to demo authentication in Messaging Extension action.

---
+++ experimental/teams-messaging-extension-action-auth/csharp_dotnetcore/Bots/TeamsMessagingExtensionsActionAuthBot.cs
@@ -0,0 +1,180 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveCards;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class TeamsMessagingExtensionsActionAuthBot : TeamsActivityHandler
+    {
+        readonly string _connectionName;
+        readonly UserState _userState;
+        readonly IStatePropertyAccessor<string> _userConfigProperty;
+
+        public TeamsMessagingExtensionsActionAuthBot(IConfiguration configuration, UserState userState)
+        {
+            _connectionName = configuration["ConnectionName"] ?? throw new NullReferenceException("ConnectionName");
+            _userState = userState ?? throw new NullReferenceException(nameof(userState));
+            _userConfigProperty = userState.CreateProperty<string>("UserConfiguration");
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            // After the turn is complete, persist any UserState changes.
+            await _userState.SaveChangesAsync(turnContext);
+        }
+
+        protected override Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionSubmitActionAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
+        {
+            // This method is to handle the 'Close' button on the confirmation Task Module after the user signs out.
+            return Task.FromResult(new MessagingExtensionActionResponse());
+        }
+
+        protected override async Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionFetchTaskAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
+        {
+            if (action.CommandId.ToUpper() == "SHOWPROFILE")
+            {
+                var attachments = new List<MessagingExtensionAttachment>();
+                var userConfigSettings = await _userConfigProperty.GetAsync(turnContext, () => string.Empty);
+
+                // When the Bot Service Auth flow completes, the action.State will contain a magic code used for verification.
+                var magicCode = string.Empty;
+
+                var state = action.State; // Check the state value 
+
+                if (!string.IsNullOrEmpty(state))
+                {
+                    var parsed = 0;
+                    if (int.TryParse(state, out parsed))
+                    {
+                        magicCode = parsed.ToString();
+                    }
+                }
+
+                var tokenResponse = await (turnContext.Adapter as IUserTokenProvider).GetUserTokenAsync(turnContext, _connectionName, magicCode, cancellationToken: cancellationToken);
+                if (tokenResponse == null || string.IsNullOrEmpty(tokenResponse.Token))
+                {
+                    // There is no token, so the user has not signed in yet.
+
+                    // Retrieve the OAuth Sign in Link to use in the MessagingExtensionResult Suggested Actions
+                    var signInLink = await (turnContext.Adapter as IUserTokenProvider).GetOauthSignInLinkAsync(turnContext, _connectionName, cancellationToken);
+
+                    return new MessagingExtensionActionResponse
+                    {
+                        ComposeExtension = new MessagingExtensionResult
+                        {
+                            Type = "auth",
+                            SuggestedActions = new MessagingExtensionSuggestedAction
+                            {
+                                Actions = new List<CardAction>
+                                {
+                                    new CardAction
+                                    {
+                                        Type = ActionTypes.OpenUrl,
+                                        Value = signInLink,
+                                        Title = "Bot Service OAuth",
+                                    },
+                                },
+                            },
+                        },
+                    };
+                }
+
+                var client = new SimpleGraphClient(tokenResponse.Token);
+
+                var profile = await client.GetMyProfile();
+
+                return new MessagingExtensionActionResponse
+                {
+                    Task = new TaskModuleContinueResponse
+                    {
+                        Value = new TaskModuleTaskInfo
+                        {
+                            Card = GetProfileCard(profile),
+                            Height = 250,
+                            Width = 400,
+                            Title = "Adaptive Card: Inputs",
+                        },
+                    },
+                };
+            }
+
+            if (action.CommandId.ToUpper() == "SIGNOUTCOMMAND")
+            {
+                await (turnContext.Adapter as IUserTokenProvider).SignOutUserAsync(turnContext, _connectionName, turnContext.Activity.From.Id, cancellationToken);
+
+                return new MessagingExtensionActionResponse
+                {
+                    Task = new TaskModuleContinueResponse
+                    {
+                        Value = new TaskModuleTaskInfo
+                        {
+                            Card = CreateAdaptiveCardAttachment(),
+                            Height = 200,
+                            Width = 400,
+                            Title = "Adaptive Card: Inputs",
+                        },
+                    },
+                };
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// This is simple profile card with display name and staic image.
+        /// </summary>
+        /// <param name="profile">Profile of current logged in user.</param>
+        /// <returns>Adaptive card attachment with profile details.</returns>
+        private static Attachment GetProfileCard(Graph.User profile)
+        {
+            var card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+
+            card.Body.Add(new AdaptiveTextBlock()
+            {
+                Text = $"Hello, {profile.DisplayName}",
+                Size = AdaptiveTextSize.ExtraLarge
+            });
+
+            card.Body.Add(new AdaptiveImage()
+            {
+                Url = new Uri("http://adaptivecards.io/content/cats/1.png")
+            });
+            return new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = card,
+            };
+        }
+
+        private static Attachment CreateAdaptiveCardAttachment()
+        {
+            // combine path for cross platform support
+            string[] paths = { ".", "Resources", "adaptiveCard.json" };
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(paths));
+
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
---
+++ experimental/teams-messaging-extension-action-auth/csharp_dotnetcore/README.md
@@ -0,0 +1,68 @@
+﻿# Teams Search Auth Config Bot
+
+Bot Framework v4 sample for Teams expands the [51.teams-messaging-extensions-action](https://github.com/microsoft/BotBuilder-Samples/tree/main/samples/csharp_dotnetcore/51.teams-messaging-extensions-action) sample to include a configuration page and Bot Service authentication.
+
+This bot has been created using [Bot Framework](https://dev.botframework.com), it shows how to sign in from a Messaging Extension action command. In this sample we are assuming the OAuth 2 provider is Azure Active Directory v2 (AADv2) and are utilizing the Microsoft Graph API to retrieve data about the user. Check [here](https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-authentication) for information about getting an AADv2 application setup for use in Azure Bot Service. The scopes used in this sample are the following:
+
+- `openid` 
+- `profile` 
+- `User.Read` 
+
+## Prerequisites
+
+- Microsoft Teams is installed and you have an account
+- [.NET Core SDK](https://dotnet.microsoft.com/download) version 3.1
+- [ngrok](https://ngrok.com/) or equivalent tunnelling solution
+
+## To try this sample
+
+> Note these instructions are for running the sample on your local machine, the tunnelling solution is required because
+the Teams service needs to call into the bot.
+
+1) Clone the repository
+
+    ```bash
+    git clone https://github.com/Microsoft/botbuilder-samples.git
+    ```
+
+1) If you are using Visual Studio
+   - Launch Visual Studio
+   - File -> Open -> Project/Solution
+   - Navigate to `samples/csharp_dotnetcore/52.teams-messaging-extensions-search-auth-config` folder
+   - Select `TeamsMessagingExtensionsSearchAuthConfig.csproj` file
+
+1) Run ngrok - point to port 3978
+
+    ```bash
+    ngrok http -host-header=rewrite 3978
+    ```
+
+1) Create [Bot Framework registration resource](https://docs.microsoft.com/en-us/azure/bot-service/bot-service-quickstart-registration) in Azure
+    - Use the current `https` URL you were given by running ngrok. Append with the path `/api/messages` used by this sample
+    - Ensure that you've [enabled the Teams Channel](https://docs.microsoft.com/en-us/azure/bot-service/channel-connect-teams?view=azure-bot-service-4.0)
+    - __*If you don't have an Azure account*__ you can use this [Bot Framework registration](https://docs.microsoft.com/en-us/microsoftteams/platform/bots/how-to/create-a-bot-for-teams#register-your-web-service-with-the-bot-framework)
+
+1) Update the `appsettings.json` configuration for the bot to use the following:
+- Microsoft App Id
+- App Password from the Bot Framework registration. (Note the App Password is referred to as the "client secret" in the azure portal and you can always create a new client secret anytime.)
+- The AAD ConnectionName from the OAuth Connection Settings on Azure
+- The ngrok forwarding url (ie `https://d5b9f739.ngrok.io`) from starting ngrok
+
+1) __*This step is specific to Teams.*__
+    - **Edit** the `manifest.json` contained in the  `teamsAppManifest` folder to replace your Microsoft App Id (that was created when you registered your bot earlier) *everywhere* you see the place holder string `<<YOUR-MICROSOFT-APP-ID>>` (depending on the scenario the Microsoft App Id may occur multiple times in the `manifest.json`)
+    - **Zip** up the contents of the `teamsAppManifest` folder to create a `manifest.zip`
+    - **Upload** the `manifest.zip` to Teams (in the Apps view click "Upload a custom app")
+
+1) Run your bot, either from Visual Studio with `F5` or using `dotnet run` in the appropriate folder.
+
+## Interacting with the bot in Teams
+
+Once the Messaging Extension is installed, click the icon for **ME Auth Action** in the Compose Box's Messaging Extension menu to display the action window.  You will be prompted to Sign In. Once Sign In is completed, you will see demo card with logged in user's name displayed on card.'
+
+## Deploy the bot to Azure
+
+To learn more about deploying a bot to Azure, see [Deploy your bot to Azure](https://aka.ms/azuredeployment) for a complete list of deployment instructions.
+
+## Further reading
+
+- [How Microsoft Teams bots work](https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-basics-teams?view=azure-bot-service-4.0&tabs=javascript)
