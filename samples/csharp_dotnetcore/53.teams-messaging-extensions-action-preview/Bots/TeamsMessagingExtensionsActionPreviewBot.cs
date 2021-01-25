10f3bee9d7cf93607de72ec13a2de278fc09de31
Trinetra Kumar <v-trkum@microsoft.com>
2021-01-25 15:37:06 +0530

    Commiting User Attribution in ME Preview sample code

--- samples/csharp_dotnetcore/53.teams-messaging-extensions-action-preview/Bots/TeamsMessagingExtensionsActionPreviewBot.cs
+++ samples/csharp_dotnetcore/53.teams-messaging-extensions-action-preview/Bots/TeamsMessagingExtensionsActionPreviewBot.cs
@@ -143,7 +143,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             var activityPreview = action.BotActivityPreview[0];
             var attachmentContent = activityPreview.Attachments[0].Content;
             var previewedCard = JsonConvert.DeserializeObject<AdaptiveCard>(attachmentContent.ToString(), new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-
+            previewedCard.Version = "1.0";
             var exampleData = AdaptiveCardHelper.CreateExampleData(previewedCard);
 
             // This is a send so we are done and we will create the adaptive card editor.
@@ -151,9 +151,24 @@ namespace Microsoft.BotBuilderSamples.Bots
 
             var message = MessageFactory.Attachment(new Attachment { ContentType = AdaptiveCard.ContentType, Content = adaptiveCard });
 
+            //User Attribution for Bot messages
+            message.ChannelData = new
+            {
+                OnBehalfOf = new[]
+               {
+                    new
+                       {
+                         ItemId = 0,
+                         MentionType = "person",
+                         Mri = turnContext.Activity.From.Id,
+                         DisplayName = turnContext.Activity.From.Name
+                    }
+                }
+            };
+
             // THIS WILL WORK IF THE BOT IS INSTALLED. (SendActivityAsync will throw if the bot is not installed.)
             await turnContext.SendActivityAsync(message, cancellationToken);
-            
+
             return null;
         }
 
