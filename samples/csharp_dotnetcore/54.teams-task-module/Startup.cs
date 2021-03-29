2d1ae52d8c4d078858276c4c1f159747063489d0
Eric Dahlvang <erdahlva@microsoft.com>
2021-03-29 13:58:43 -0700

    Update dotnet samples to use CoreBotAdapter

--- samples/csharp_dotnetcore/54.teams-task-module/Startup.cs
+++ samples/csharp_dotnetcore/54.teams-task-module/Startup.cs
@@ -26,7 +26,7 @@ namespace Microsoft.BotBuilderSamples
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddHttpClient().AddControllers().AddNewtonsoftJson();
             services.AddRazorPages();
 
             // Create the Bot Framework Adapter with error handling enabled.
