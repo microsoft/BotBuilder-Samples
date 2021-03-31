ff016c196e4884b8f532d6a2bcdf9b48be08ba14
Trinetra Kumar <v-trkum@microsoft.com>
2021-03-31 18:01:08 +0530

    Messaging Extension Search for all card and grid node js sample
    1. Added Adaptive card flow in existing sample
    2. Added connector card
    3. Added result grid show

--- samples/javascript_nodejs/50.teams-messaging-extensions-search/bots/teamsMessagingExtensionsSearchBot.js
+++ samples/javascript_nodejs/50.teams-messaging-extensions-search/bots/teamsMessagingExtensionsSearchBot.js
@@ -3,22 +3,70 @@
 
 const axios = require('axios');
 const querystring = require('querystring');
-const { TeamsActivityHandler, CardFactory } = require('botbuilder');
+const { TeamsActivityHandler, CardFactory,MessageFactory, ConsoleTranscriptLogger } = require('botbuilder');
+var fs = require('fs');
+const AdaptiveCard = require('../Resources/RestaurantCard.json');
+const ConnectorCard = require('../Resources/ConnectorCard.json');
+var configuration = require('dotenv').config();
+var env = configuration.parsed;
+var baseurl = env.BaseUrl;
+
+debugger;
+var publicDir = require('path').join(__dirname,'../public/Images'); 
 
 class TeamsMessagingExtensionsSearchBot extends TeamsActivityHandler {
+    
     async handleTeamsMessagingExtensionQuery(context, query) {
         const searchQuery = query.parameters[0].value;
-        const response = await axios.get(`http://registry.npmjs.com/-/v1/search?${ querystring.stringify({ text: searchQuery, size: 8 }) }`);
+      
+        var attachments = [];
+        switch(searchQuery){
+            case 'adaptive card':           
+               var adadptivecardresponse =   this.GetAdaptiveCard();
+               return adadptivecardresponse;
+               
+            case 'connector card':              
+                var connectorcardresponse = this.GetConnectorCard();
+                return connectorcardresponse;
 
+            case `result grid`:              
+                var resultgridresponse = this.GetResultGrid();
+                return resultgridresponse;
+            default: 
+            const response = await axios.get(`http://registry.npmjs.com/-/v1/search?${ querystring.stringify({ text: searchQuery, size: 8 }) }`);
+
+           
+            response.data.objects.forEach(obj => {
+                const heroCard = CardFactory.heroCard(obj.package.name);
+                const preview = CardFactory.heroCard(obj.package.name);
+                preview.content.tap = { type: 'invoke', value: { description: obj.package.description } };
+                const attachment = { ...heroCard, preview };
+                attachments.push(attachment);
+            });
+    
+            return {
+                composeExtension: {
+                    type: 'result',
+                    attachmentLayout: 'list',
+                    attachments: attachments
+                }
+            };
+        }       
+    }
+
+     GetAdaptiveCard() {
+       
+        var filepath = "./Resources/RestaurantCard.json";
         const attachments = [];
-        response.data.objects.forEach(obj => {
-            const heroCard = CardFactory.heroCard(obj.package.name);
-            const preview = CardFactory.heroCard(obj.package.name);
-            preview.content.tap = { type: 'invoke', value: { description: obj.package.description } };
-            const attachment = { ...heroCard, preview };
-            attachments.push(attachment);
-        });
+        const preview = CardFactory.thumbnailCard(
+            'Adaptive Card',
+            'Please select to get the card'
+        );
 
+        const adaptive = CardFactory.adaptiveCard(AdaptiveCard);
+        
+        const attachment = {...adaptive,preview}
+        attachments.push(attachment);
         return {
             composeExtension: {
                 type: 'result',
@@ -27,7 +75,49 @@ class TeamsMessagingExtensionsSearchBot extends TeamsActivityHandler {
             }
         };
     }
+    
+    GetConnectorCard() {    
+        const attachments = [];
+        const preview = CardFactory.thumbnailCard(
+        'Connector Card',
+        'Please select to get the card');
 
+    const connector = CardFactory.o365ConnectorCard(ConnectorCard);
+    const attachment = {...connector,preview}
+    attachments.push(attachment);
+    return {
+        composeExtension: {
+            type: 'result',
+            attachmentLayout: 'list',
+            attachments: attachments
+        }
+     };
+    }
+
+    GetResultGrid() {
+       const attachments = [];   
+       var files = fs.readdirSync(publicDir,function(err, result) {
+           if(err) 
+           console.log('error', err);
+             });
+       var grid="";
+       files.forEach(file=>{
+       grid = CardFactory.thumbnailCard(
+            '',
+            [{ url: baseurl+"/Images/"+ file }]
+        );
+        attachments.push(grid);
+       });
+       
+    return {
+        composeExtension: {
+            type: 'result',
+            attachmentLayout: 'grid',
+            attachments: attachments
+        }
+    };
+     
+    }
     async handleTeamsMessagingExtensionSelectItem(context, obj) {
         return {
             composeExtension: {
@@ -36,7 +126,7 @@ class TeamsMessagingExtensionsSearchBot extends TeamsActivityHandler {
                 attachments: [CardFactory.thumbnailCard(obj.description)]
             }
         };
-    }
+    } 
 }
 
 module.exports.TeamsMessagingExtensionsSearchBot = TeamsMessagingExtensionsSearchBot;
