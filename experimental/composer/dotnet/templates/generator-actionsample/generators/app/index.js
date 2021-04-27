7ff5d5e205e3d3af2c62cf498f6b1f525d789db8
tracyboehrer <tracyboehrer@users.noreply.github.com>
2021-04-27 12:52:49 -0500

    Composer samples: Added some template examples

---
+++ experimental/composer/dotnet/templates/generator-actionsample/generators/app/index.js
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+'use strict';
+
+const { BaseGenerator } = require('@microsoft/generator-bot-adaptive');
+
+module.exports = class extends BaseGenerator {
+  initializing() {
+    this.composeWith(
+      require.resolve('@microsoft/generator-bot-adaptive/generators/app'),
+      Object.assign(this.options, {
+        arguments: this.args,
+        applicationSettingsDirectory: 'settings',
+      })
+    );
+  }
+
+  writing() {
+    this._copyBotTemplateFiles();
+  }
+};
