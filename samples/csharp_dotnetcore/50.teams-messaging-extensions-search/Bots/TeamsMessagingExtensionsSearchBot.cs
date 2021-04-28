6c97c9f4d76899522402d2ef65e778eafe756561
Trinetra Kumar <v-trkum@microsoft.com>
2021-04-28 16:03:56 +0530

    Revert "ME Search Review comment fixes"
    This reverts commit 1996329f65d8ac8b4e2a6114666a4bab46eddb94.

--- samples/csharp_dotnetcore/50.teams-messaging-extensions-search/Bots/TeamsMessagingExtensionsSearchBot.cs
+++ samples/csharp_dotnetcore/50.teams-messaging-extensions-search/Bots/TeamsMessagingExtensionsSearchBot.cs
@@ -131,17 +131,18 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         public MessagingExtensionResponse GetAdaptiveCard()
         {
-            var paths = new[] { ".", "Resources", "RestaurantCard.json" };
-            string filepath = Path.Combine(paths); 
+
+            string filepath = "./RestaurantCard.json";
             var previewcard = new ThumbnailCard
             {
                 Title = "Adaptive Card",
                 Text = "Please select to get Adaptive card"
             };
             var adaptiveList = FetchAdaptive(filepath);
+
             var attachment = new MessagingExtensionAttachment
             {
-                ContentType = "application/vnd.microsoft.card.adaptive",
+                ContentType = AdaptiveCards.AdaptiveCard.ContentType,
                 Content = adaptiveList.Content,
                 Preview = previewcard.ToAttachment()
             };
@@ -156,18 +157,23 @@ namespace Microsoft.BotBuilderSamples.Bots
                 }
             };
         }
-        
+
         public MessagingExtensionResponse GetConnectorCard()
         {
-            var path = new[] { ".", "Resources", "connectorCard.json" };
-            var filepath = Path.Combine(path);   
+          
+            string filepath = "./connectorCard.json";
+            var json = File.ReadAllText(filepath);
+            var cardJson = JsonConvert.DeserializeObject<ConnectorJsonSerializer>(json);
+            cardJson.sections[0].activityImage = _baseURL+"/imgConnector.jpg";
+            var ConnectorCardJson = JsonConvert.SerializeObject(cardJson);
+
             var previewcard = new ThumbnailCard
             {
                 Title = "O365 Connector Card",
                 Text = "Please select to get Connector card"
             };
 
-            var connector = FetchConnector(filepath);
+            var connector = FetchConnector(ConnectorCardJson);
             var attachment = new MessagingExtensionAttachment
             {
                 ContentType = O365ConnectorCard.ContentType,
@@ -186,48 +192,61 @@ namespace Microsoft.BotBuilderSamples.Bots
             };
         }
 
-        public  Attachment FetchAdaptive(string filepath)
+        public static Attachment FetchAdaptive(string filepath)
         {
             var adaptiveCardJson = File.ReadAllText(filepath);
             var adaptiveCardAttachment = new Attachment
             {
-                ContentType = "application/vnd.microsoft.card.adaptive",
-                Content = JsonConvert.DeserializeObject(adaptiveCardJson)
-            };      
+                ContentType = /*"application/vnd.microsoft.card.adaptive"*/ AdaptiveCard.ContentType,
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+
             return adaptiveCardAttachment;
         }
 
-        public Attachment FetchConnector(string filepath)
+        public Attachment FetchConnector(string cardJson)
         {
-            var connectorCardJson = File.ReadAllText(filepath);
             var connectorCardAttachment = new MessagingExtensionAttachment
             {
-                ContentType = O365ConnectorCard.ContentType,
-                Content = JsonConvert.DeserializeObject(connectorCardJson),
+                ContentType = O365ConnectorCard.ContentType/*"application/vnd.microsoft.teams.card.o365connector"*/,
+                Content = JsonConvert.DeserializeObject(cardJson),
 
             };
+            
             return connectorCardAttachment;
         }
 
         public MessagingExtensionResponse GetResultGrid()
         {
-            var imageFiles = Directory.EnumerateFiles("wwwroot", "*.*", SearchOption.AllDirectories)
-            .Where(s => s.EndsWith(".jpg"));
+            var files = Directory.GetFiles("wwwroot");
+
+            List<string> imageFiles = new List<string>();
+
+            foreach (string filename in files)
+            {
+                if (Regex.IsMatch(filename, @".jpg"))
+                    imageFiles.Add(filename);
+
+            }
 
             List<MessagingExtensionAttachment> attachments = new List<MessagingExtensionAttachment>();
 
             foreach (string img in imageFiles)
             {
-                var image = img.Split("\\");                
+                var image = img.Split("\\");
+                
                 var thumbnailCard = new ThumbnailCard();
-                thumbnailCard.Images = new List<CardImage>() { new CardImage(_baseUrl + "/" + image[1]) };
+                thumbnailCard.Images = new List<CardImage>() { new CardImage(_baseURL +"/" + image[1]) };
                 var attachment = new MessagingExtensionAttachment
                 {
                     ContentType = ThumbnailCard.ContentType,
                     Content = thumbnailCard,
                 };
+
                 attachments.Add(attachment);
+
             }
+
             return new MessagingExtensionResponse
             {
                 ComposeExtension = new MessagingExtensionResult
@@ -237,6 +256,7 @@ namespace Microsoft.BotBuilderSamples.Bots
                     Attachments = attachments
                 }
             };
+
         }
 
     }
