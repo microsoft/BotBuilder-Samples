e344aff08b32d2af1a07df35b4895bf13a07c088
Victor <victor@grycuk.net>
2021-04-29 17:00:59 -0300

    Fix mandatory need for keys

--- samples/java_springboot/14.nlp-with-dispatch/src/main/java/com/microsoft/bot/sample/nlpwithdispatch/BotServicesImpl.java
+++ samples/java_springboot/14.nlp-with-dispatch/src/main/java/com/microsoft/bot/sample/nlpwithdispatch/BotServicesImpl.java
@@ -9,6 +9,7 @@ import com.microsoft.bot.ai.luis.LuisRecognizerOptionsV3;
 import com.microsoft.bot.ai.qna.QnAMaker;
 import com.microsoft.bot.ai.qna.QnAMakerEndpoint;
 import com.microsoft.bot.integration.Configuration;
+import org.apache.commons.lang3.StringUtils;
 
 public class BotServicesImpl implements BotServices {
 
@@ -17,31 +18,35 @@ public class BotServicesImpl implements BotServices {
     private QnAMaker sampleQnA;
 
     public BotServicesImpl(Configuration configuration) {
-        // Read the setting for cognitive services (LUS, QnA) from the application.properties file.
-        // If includeApiResults instanceof set to true, the full response from the LUS api (LuisResult)
-        // will be made available in the properties collection of the RecognizerResult
+        Boolean luisIsConfigured = StringUtils.isNotBlank(configuration.getProperty("LuisAppId"))
+                && StringUtils.isNotBlank(configuration.getProperty("LuisAPIKey"))
+                && StringUtils.isNotBlank(configuration.getProperty("LuisAPIHostName"));
+        if (luisIsConfigured) {
+            // Read the setting for cognitive services (LUS, QnA) from the application.properties file.
+            // If includeApiResults instanceof set to true, the full response from the LUS api (LuisResult)
+            // will be made available in the properties collection of the RecognizerResult
+            LuisApplication luisApplication = new LuisApplication(
+                    configuration.getProperty("LuisAppId"),
+                    configuration.getProperty("LuisAPIKey"),
+                    String.format("https://%s.api.cognitive.microsoft.com",
+                            configuration.getProperty("LuisAPIHostName")));
 
-        LuisApplication luisApplication = new LuisApplication(
-            configuration.getProperty("LuisAppId"),
-            configuration.getProperty("LuisAPIKey"),
-            String.format("https://%s.api.cognitive.microsoft.com",
-                          configuration.getProperty("LuisAPIHostName")));
+            // Set the recognizer options depending on which endpoint version you want to use.
+            // More details can be found in https://docs.getmicrosoft().com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
+            LuisRecognizerOptionsV3 recognizerOptions = new LuisRecognizerOptionsV3(luisApplication);
+            recognizerOptions.setIncludeAPIResults(true);
+            recognizerOptions.setIncludeAllIntents(true);
+            recognizerOptions.setIncludeInstanceData(true);
 
-        // Set the recognizer options depending on which endpoint version you want to use.
-        // More details can be found in https://docs.getmicrosoft().com/en-gb/azure/cognitive-services/luis/luis-migration-api-v3
-        LuisRecognizerOptionsV3 recognizerOptions = new LuisRecognizerOptionsV3(luisApplication);
-        recognizerOptions.setIncludeAPIResults(true);
-        recognizerOptions.setIncludeAllIntents(true);
-        recognizerOptions.setIncludeInstanceData(true);
+            dispatch = new LuisRecognizer(recognizerOptions);
 
-        dispatch = new LuisRecognizer(recognizerOptions);
+            QnAMakerEndpoint qnaMakerEndpoint = new QnAMakerEndpoint();
+            qnaMakerEndpoint.setKnowledgeBaseId(configuration.getProperty("QnAKnowledgebaseId"));
+            qnaMakerEndpoint.setEndpointKey(configuration.getProperty("QnAEndpointKey"));
+            qnaMakerEndpoint.setHost(configuration.getProperty("QnAEndpointHostName"));
 
-        QnAMakerEndpoint qnaMakerEndpoint = new QnAMakerEndpoint();
-        qnaMakerEndpoint.setKnowledgeBaseId(configuration.getProperty("QnAKnowledgebaseId"));
-        qnaMakerEndpoint.setEndpointKey(configuration.getProperty("QnAEndpointKey"));
-        qnaMakerEndpoint.setHost(configuration.getProperty("QnAEndpointHostName"));
-
-        sampleQnA = new QnAMaker(qnaMakerEndpoint, null);
+            sampleQnA = new QnAMaker(qnaMakerEndpoint, null);
+        }
     }
 
     /**
