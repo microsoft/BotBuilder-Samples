98e5e8971eee86c0305aa9459a037d1886adddeb
tracyboehrer <tracyboehrer@users.noreply.github.com>
2021-05-04 14:07:29 -0500

    Composer Samples: Sample packages

---
+++ composer-samples/csharp_dotnetcore/packages/MultiplyDialog/MultiplyDialogBotComponent.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsofts.Bot.Component.Samples.MultiplyDialog
+{
+    public class MultiplyDialogBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<MultiplyDialog>(MultiplyDialog.Kind));
+        }
+    }
+}
