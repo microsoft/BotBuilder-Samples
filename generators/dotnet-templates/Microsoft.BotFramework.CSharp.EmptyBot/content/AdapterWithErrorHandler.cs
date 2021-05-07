2a798a3e803182de0ce4767e866a689a21fd140f
Eric Dahlvang <erdahlva@microsoft.com>
2021-05-07 11:48:20 -0700

    Update EmptyBot generator AdapterWithErrorHandler namespace

--- generators/dotnet-templates/Microsoft.BotFramework.CSharp.EmptyBot/content/AdapterWithErrorHandler.cs
+++ generators/dotnet-templates/Microsoft.BotFramework.CSharp.EmptyBot/content/AdapterWithErrorHandler.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 //
-// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v$templateversion$
+// Generated with EmptyBot .NET Template version __vX.X.X__
 
 using System.Net.Http;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
@@ -9,7 +9,7 @@ using Microsoft.Bot.Builder.TraceExtensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace $safeprojectname$
+namespace EmptyBot
 {
     public class AdapterWithErrorHandler : CloudAdapter
     {
