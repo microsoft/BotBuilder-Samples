83e9663b342177d1f09342e0079191a8bdef0d60
Martin Battaglino <martinbatta32@gmail.com>
2021-05-10 13:08:14 -0300

    Introduce BlobsStorage and CosmosDbPartitionedStorage in the sample

--- samples/java_springboot/45.state-management/src/main/java/com/microsoft/bot/sample/statemanagement/Application.java
+++ samples/java_springboot/45.state-management/src/main/java/com/microsoft/bot/sample/statemanagement/Application.java
@@ -3,9 +3,10 @@
 
 package com.microsoft.bot.sample.statemanagement;
 
-import com.microsoft.bot.builder.Bot;
-import com.microsoft.bot.builder.ConversationState;
-import com.microsoft.bot.builder.UserState;
+import com.microsoft.bot.azure.CosmosDbPartitionedStorage;
+import com.microsoft.bot.azure.CosmosDbPartitionedStorageOptions;
+import com.microsoft.bot.azure.blobs.BlobsStorage;
+import com.microsoft.bot.builder.*;
 import com.microsoft.bot.integration.AdapterWithErrorHandler;
 import com.microsoft.bot.integration.BotFrameworkHttpAdapter;
 import com.microsoft.bot.integration.Configuration;
@@ -66,4 +67,29 @@ public class Application extends BotDependencyConfiguration {
     public BotFrameworkHttpAdapter getBotFrameworkHttpAdaptor(Configuration configuration) {
         return new AdapterWithErrorHandler(configuration);
     }
+
+    /**
+     * AZURE BLOB STORAGE - Uncomment the code in this section to use Azure blob storage
+     * @return A BlobStorage
+     */
+    /*@Override
+    public Storage getStorage() {
+        // If using Blob Storage. Fill these connection details in from configuration.
+        return new BlobsStorage("<blob-storage-connection-string>", "bot-state");
+    }*/
+
+    /**
+     * COSMOSDB STORAGE - Uncomment the code in this section to use CosmosDB storage
+     * @return A CosmosDbPartitionedStorage
+     */
+    /*@Override
+    public Storage getStorage() {
+        // If using CosmosDbPartitionedStorage. Fill these connection details in from configuration.
+        CosmosDbPartitionedStorageOptions options = new CosmosDbPartitionedStorageOptions();
+        options.setCosmosDbEndpoint("<endpoint-for-your-cosmosdb-instance>");
+        options.setAuthKey("<your-cosmosdb-auth-key>");
+        options.setDatabaseId("<your-database-id>");
+        options.setContainerId("<cosmosdb-container-id>");
+        return new CosmosDbPartitionedStorage(options);
+    }*/
 }
