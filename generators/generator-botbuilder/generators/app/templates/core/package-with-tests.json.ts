7a1203771144d524ebc08a7f2efe7be14a9a0434
Josh Gummersall <jgummersall@microsoft.com>
2021-05-10 13:13:30 -0700

    4.13.1 -> 4.13.2

--- generators/generator-botbuilder/generators/app/templates/core/package-with-tests.json.ts
+++ generators/generator-botbuilder/generators/app/templates/core/package-with-tests.json.ts
@@ -38,10 +38,10 @@
     },
     "dependencies": {
       "@microsoft/recognizers-text-data-types-timex-expression": "1.1.4",
-      "botbuilder": "~4.13.1",
-      "botbuilder-ai": "~4.13.1",
-      "botbuilder-dialogs": "~4.13.1",
-      "botbuilder-testing": "~4.13.1",
+      "botbuilder": "~4.13.2",
+      "botbuilder-ai": "~4.13.2",
+      "botbuilder-dialogs": "~4.13.2",
+      "botbuilder-testing": "~4.13.2",
       "dotenv": "^8.2.0",
       "replace": "~1.2.0",
       "restify": "~8.5.1"
