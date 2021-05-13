bd80a4804b16862806c25bc06196f09d0f3efb03
Martin Battaglino <martinbatta32@gmail.com>
2021-05-13 17:54:19 -0300

    Remove models folder and relocate classes

--- samples/java_springboot/54.teams-task-module/src/main/java/com/microsoft/bot/sample/teamstaskmodule/UISettings.java
+++ samples/java_springboot/54.teams-task-module/src/main/java/com/microsoft/bot/sample/teamstaskmodule/UISettings.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.microsoft.bot.sample.teamstaskmodule.models;
+package com.microsoft.bot.sample.teamstaskmodule;
 
 public class UISettings {
     private int width;
--- samples/java_springboot/54.teams-task-module/src/main/java/com/microsoft/bot/sample/teamstaskmodule/models/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for
-// license information.
-
-/**
- * This package contains the classes for Teams Task Module Sample.
- */
-package com.microsoft.bot.sample.teamstaskmodule.models;
