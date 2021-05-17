26716f238c03d388f03392fbdbe5ddaeb875da8a
Martin Battaglino <martinbatta32@gmail.com>
2021-05-17 11:26:58 -0300

    [Samples][Java] Fix disparities of 56.teams-file-upload sample (#3278)
    * Remove autogenerated files
    
    * Update README
    
    * Add package-info file
    
    * Update index.html
    
    * Send configuration of application.properties to Bot
    
    * Add missing scenario to send an inline image download
    
    * Remove unnecesary and empty lines

--- samples/java_springboot/56.teams-file-upload/src/main/java/com/microsoft/bot/sample/teamsfileupload/TeamsFileUploadBot.java
+++ samples/java_springboot/56.teams-file-upload/src/main/java/com/microsoft/bot/sample/teamsfileupload/TeamsFileUploadBot.java
@@ -6,6 +6,8 @@ package com.microsoft.bot.sample.teamsfileupload;
 import com.microsoft.bot.builder.MessageFactory;
 import com.microsoft.bot.builder.TurnContext;
 import com.microsoft.bot.builder.teams.TeamsActivityHandler;
+import com.microsoft.bot.connector.authentication.MicrosoftAppCredentials;
+import com.microsoft.bot.integration.Configuration;
 import com.microsoft.bot.schema.Activity;
 import com.microsoft.bot.schema.Attachment;
 import com.microsoft.bot.schema.ResultPair;
@@ -15,15 +17,19 @@ import com.microsoft.bot.schema.teams.FileConsentCard;
 import com.microsoft.bot.schema.teams.FileConsentCardResponse;
 import com.microsoft.bot.schema.teams.FileDownloadInfo;
 import com.microsoft.bot.schema.teams.FileInfoCard;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -40,6 +46,16 @@ import java.util.concurrent.atomic.AtomicReference;
  * participants.</p>
  */
 public class TeamsFileUploadBot extends TeamsActivityHandler {
+    // You can install this bot at any scope. You can @mention the bot and it will present you with the file prompt. You can accept and
+    // the file will be uploaded, or you can decline and it won't.
+
+    private static String microsoftAppId;
+    private static String microsoftAppPassword;
+
+    public TeamsFileUploadBot(Configuration configuration) {
+        microsoftAppId = configuration.getProperty("MicrosoftAppId");
+        microsoftAppPassword = configuration.getProperty("MicrosoftAppPassword");
+    }
 
     @Override
     protected CompletableFuture<Void> onMessageActivity(TurnContext turnContext) {
@@ -50,6 +66,10 @@ public class TeamsFileUploadBot extends TeamsActivityHandler {
                     ? fileDownloadFailed(turnContext, result.value())
                     : fileDownloadCompleted(turnContext, attachment)
                 );
+        } else if (turnContext.getActivity().getAttachments() != null
+                && turnContext.getActivity().getAttachments().get(0).getContentType().contains("image/*")) {
+            // Inline image se.
+            return processInlineImage(turnContext);
         } else {
             File filePath = new File("files", "teams-logo.png");
             return sendFileCard(turnContext, filePath.getName(), filePath.length());
@@ -213,6 +233,62 @@ public class TeamsFileUploadBot extends TeamsActivityHandler {
             }
         })
             .thenApply(aVoid -> result.get());
+    }
+
+    private CompletableFuture<Void> processInlineImage(TurnContext turnContext) {
+        Attachment attachment = turnContext.getActivity().getAttachments().get(0);
+        final HttpURLConnection[] connection = {null};
+        // Get Bot's access token to fetch inline image.
+        return new MicrosoftAppCredentials(microsoftAppId, microsoftAppPassword).getToken().thenApply(token -> {
+            try {
+                // Save the inline image to Files directory.
+                String filePath = "files/imageFromUser.png";
+                URL url = new URL(attachment.getContentUrl());
+                connection[0] = (HttpURLConnection) url.openConnection();
+                connection[0].setRequestProperty("Authorization", "Bearer " + token);
+                try (
+                    FileOutputStream fileStream = new FileOutputStream(filePath);
+                    InputStream downloadStream = connection[0].getInputStream()
+                ) {
+                    byte[] buffer = new byte[4096];
+                    int bytes_read;
+                    while ((bytes_read = downloadStream.read(buffer)) != -1) {
+                        fileStream.write(buffer, 0, bytes_read);
+                    }
+                    // Create reply with image.
+                    byte[] fileSize = FileUtils.readFileToByteArray(new File(filePath));
+                    Activity reply = MessageFactory.text(
+                            String.format(
+                                    "Attachment of %s type and size of %d bytes received",
+                                    attachment.getContentType(),
+                                    fileSize.length));
+                    List<Attachment> attachments = new ArrayList<>();
+                    attachments.add(getInlineAttachment());
+                    reply.setAttachments(attachments);
+                    return turnContext.sendActivity(reply);
+                }
+            } catch (Throwable t) {
+            }  finally {
+                if (connection[0] != null) {
+                    connection[0].disconnect();
+                }
+            }
+            return null;
+        }).thenApply(aVoid -> null);
+    }
+
+    private static Attachment getInlineAttachment() {
+        String imagePath = "files/imageFromUser.png";
+        Attachment attachment = new Attachment();
+        File file = new File(imagePath);
+        try {
+            String imageData = Base64.encodeBase64String(FileUtils.readFileToByteArray(file));
+            attachment.setName("imageFromUser.png");
+            attachment.setContentType("image/png");
+            attachment.setContentUrl("data:image/png;base64," + imageData);
+        } catch (IOException e) {
+        }
+        return attachment;
     }
 
     private CompletableFuture<ResultPair<String>> downloadAttachment(Attachment attachment) {
