563e4d7b0c851f197c8ae75c83a9d2ef09a9ab11
Chris McConnell <chrimc>
2021-05-27 18:43:27 -0700

    Initial check-in of extensions including ControlForm.

---
+++ experimental/generation/Microsoft.Form.Extensions/Tests/TestUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Testing;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
+{
+    public class TestUtils
+    {
+        public static IConfiguration DefaultConfiguration { get; set; } = new ConfigurationBuilder().AddInMemoryCollection().Build();
+
+        public static string RootFolder { get; set; } = GetProjectPath();
+
+        public static IEnumerable<object[]> GetTestScripts(string relativeFolder)
+        {
+            string testFolder = Path.GetFullPath(Path.Combine(RootFolder, PathUtils.NormalizePath(relativeFolder)));
+            return Directory.EnumerateFiles(testFolder, "*.test.dialog", SearchOption.AllDirectories).Select(s => new object[] { Path.GetFileName(s) }).ToArray();
+        }
+
+        public static async Task RunTestScript(ResourceExplorer resourceExplorer, string resourceId = null, IConfiguration configuration = null, [CallerMemberName] string testName = null, IEnumerable<IMiddleware> middlweare = null, TestAdapter adapter = null)
+        {
+            var script = resourceExplorer.LoadType<TestScript>(resourceId ?? $"{testName}.test.dialog");
+            script.Configuration = configuration ?? new ConfigurationBuilder().AddInMemoryCollection().Build();
+            script.Description = script.Description ?? resourceId;
+            await script.ExecuteAsync(testName: testName, resourceExplorer: resourceExplorer, middlweare: middlweare, adapter: adapter).ConfigureAwait(false);
+        }
+
+        public static string GetProjectPath()
+        {
+            var parent = Environment.CurrentDirectory;
+            while (!string.IsNullOrEmpty(parent))
+            {
+                if (!parent.Contains("bin") && Directory.EnumerateFiles(parent, "*proj").Any())
+                {
+                    break;
+                }
+                else
+                {
+                    parent = Path.GetDirectoryName(parent);
+                }
+            }
+
+            return parent;
+        }
+        
+        public static DialogContext CreateContext(string text)
+        {
+            var activity = Activity.CreateMessageActivity();
+            activity.Text = text;
+            return new DialogContext(new DialogSet(), new TurnContext(new TestAdapter(), (Activity)activity), new DialogState());
+        }
+    }
+}
