2d706cea8d22d584dea49a6c509a33d241805145
Trinetra-MSFT <v-trkum@microsoft.com>
2021-06-03 10:56:01 +0530

    Eslint running

--- samples/javascript_nodejs/51.teams-messaging-extensions-action/.eslintrc.js
+++ samples/javascript_nodejs/51.teams-messaging-extensions-action/.eslintrc.js
@@ -1,15 +1,14 @@
-/* eslint-disable */
 module.exports = {
-    "extends": "standard",
+    "env": {
+        "browser": true,
+        "node":true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
     "rules": {
-        "semi": [2, "always"],
-        "indent": [2, 4],
-        "no-return-await": 0,
-        "space-before-function-paren": [2, {
-            "named": "never",
-            "anonymous": "never",
-            "asyncArrow": "always"
-        }],
-        "template-curly-spacing": [2, "always"]
     }
 };
