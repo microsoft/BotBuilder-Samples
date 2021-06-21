7ac3f8cc91f74d7248b8e40b57b3d5c2de73d3b6
Chris McConnell <chrimc>
2021-06-21 12:19:19 -0700

    Reorder triggers. Combine not understood into a single trigger.

--- experimental/generation/generator/packages/library/test/generate.test.ts
+++ experimental/generation/generator/packages/library/test/generate.test.ts
@@ -262,7 +262,7 @@ describe('dialog:generate library', async () => {
             await checkDirectory(ppath.join(output, 'language-understanding'), 0, 1)
             await checkDirectory(ppath.join(output, 'language-generation', 'en-us'), 1, 15)
             await checkDirectory(ppath.join(output, 'language-understanding', 'en-us'), 1, 10)
-            await checkPattern(ppath.join(output, '**'), 137)
+            await checkPattern(ppath.join(output, '**'), 136)
         } catch (e) {
             assert.fail(e.message)
         }
