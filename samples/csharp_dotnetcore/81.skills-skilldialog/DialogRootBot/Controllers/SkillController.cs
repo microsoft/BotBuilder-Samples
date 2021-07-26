0a98c90014f4cd08b5effa6e9a75f31b40204b16
msomanathan <msgoogol@gmail.com>
2021-07-26 14:18:27 -0700

    Refactor sample 81.

--- samples/csharp_dotnetcore/81.skills-skilldialog/DialogRootBot/Controllers/SkillController.cs
+++ samples/csharp_dotnetcore/81.skills-skilldialog/DialogRootBot/Controllers/SkillController.cs
@@ -4,19 +4,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Builder.Skills;
 
 namespace Microsoft.BotBuilderSamples.DialogRootBot.Controllers
 {
     /// <summary>
     /// A controller that handles skill replies to the bot.
-    /// This example uses the <see cref="SkillHandler"/> that is registered as a <see cref="ChannelServiceHandler"/> in startup.cs.
     /// </summary>
     [ApiController]
     [Route("api/skills")]
     public class SkillController : ChannelServiceController
     {
-        public SkillController(ChannelServiceHandler handler)
+        public SkillController(ChannelServiceHandlerBase handler)
             : base(handler)
         {
         }
--- samples/csharp_dotnetcore/81.skills-skilldialog/DialogSkillBot/Startup.cs
+++ samples/csharp_dotnetcore/81.skills-skilldialog/DialogSkillBot/Startup.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.BotFramework;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Runtime.Extensions;
 using Microsoft.BotBuilderSamples.DialogSkillBot.Bots;
 using Microsoft.BotBuilderSamples.DialogSkillBot.Dialogs;
 using Microsoft.Extensions.Configuration;
@@ -31,23 +28,6 @@ namespace Microsoft.BotBuilderSamples.DialogSkillBot
             services.AddControllers()
                 .AddNewtonsoftJson();
 
-            // Configure credentials.
-            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
-
-            // Register AuthConfiguration to enable custom claim validation.
-            services.AddSingleton(sp => new AuthenticationConfiguration
-            {
-                ClaimsValidator = new AllowedCallersClaimsValidator(new List<string>(sp.GetService<IConfiguration>().GetSection("AllowedCallers").Get<string[]>()))
-            });
-            
-            // Create the Bot Framework Adapter with error handling enabled.
-            services.AddSingleton<IBotFrameworkHttpAdapter, SkillAdapterWithErrorHandler>();
-
-            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
-            services.AddSingleton<IStorage, MemoryStorage>();
-
-            // Create the Conversation state. (Used by the Dialog system itself.)
-            services.AddSingleton<ConversationState>();
 
             // Register LUIS recognizer.
             services.AddSingleton<DialogSkillBotRecognizer>();
@@ -57,6 +37,9 @@ namespace Microsoft.BotBuilderSamples.DialogSkillBot
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, SkillBot<ActivityRouterDialog>>();
+
+            // Add Bot runtime components to register Bot Framework specific dependencies.
+            services.AddBotRuntime(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
