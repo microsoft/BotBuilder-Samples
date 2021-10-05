85dea496f9b993f34523c2524e20225967370509
CeciliaAvila <cecilia.avila@southworks.com>
2021-10-05 11:20:02 -0300

    Migrate JS samples to CloudAdapter (56-58)

--- samples/javascript_nodejs/58.teams-start-new-thread-in-channel/bots/teamsStartNewThreadInChannel.js
+++ samples/javascript_nodejs/58.teams-start-new-thread-in-channel/bots/teamsStartNewThreadInChannel.js
@@ -17,10 +17,9 @@ class TeamsStartNewThreadInChannel extends TeamsActivityHandler {
             const message = MessageFactory.text('This will be the first message in a new thread');
             const newConversation = await this.teamsCreateConversation(context, teamsChannelId, message);
 
-            await context.adapter.continueConversation(newConversation[0],
-                async (t) => {
-                    await t.sendActivity(MessageFactory.text('This will be the first response to the new thread'));
-                });
+            await context.adapter.continueConversationAsync(process.env.MicrosoftAppId, newConversation[0], async turnContext => {
+                await turnContext.sendActivity(MessageFactory.text('This will be the first response to the new thread'));
+            });
 
             await next();
         });
@@ -38,7 +37,9 @@ class TeamsStartNewThreadInChannel extends TeamsActivityHandler {
             activity: message
         };
 
-        const connectorClient = context.adapter.createConnectorClient(context.activity.serviceUrl);
+        const connectorFactory = context.turnState.get(context.adapter.ConnectorFactoryKey);
+        const connectorClient = await connectorFactory.create(context.activity.serviceUrl);
+
         const conversationResourceResponse = await connectorClient.conversations.createConversation(conversationParameters);
         const conversationReference = TurnContext.getConversationReference(context.activity);
         conversationReference.conversation.id = conversationResourceResponse.id;
