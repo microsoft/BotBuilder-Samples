c3ee9712ff9245786161d3358ee4706ecf26a7a5
CeciliaAvila <cecilia.avila@southworks.com>
2021-10-20 12:00:32 -0300

    Add MSI support to JS 05.multi-turn-prompt

--- samples/javascript_nodejs/05.multi-turn-prompt/index.js
+++ samples/javascript_nodejs/05.multi-turn-prompt/index.js
@@ -25,7 +25,9 @@ const { UserProfileDialog } = require('./dialogs/userProfileDialog');
 
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
     MicrosoftAppId: process.env.MicrosoftAppId,
-    MicrosoftAppPassword: process.env.MicrosoftAppPassword
+    MicrosoftAppPassword: process.env.MicrosoftAppPassword,
+    MicrosoftAppType: process.env.MicrosoftAppType,
+    MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 });
 
 const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
