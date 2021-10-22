c54d364d31fd5f8e56e97987e53efb51e303a079
Martin Luccanera <18757147+MartinLuccanera@users.noreply.github.com>
2021-10-22 17:20:44 -0300

    Applying feedback

--- samples/javascript_nodejs/11.qnamaker/index.js
+++ samples/javascript_nodejs/11.qnamaker/index.js
@@ -25,9 +25,9 @@ const {
 const { QnABot } = require('./bots/QnABot');
 
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
-    MicrosoftAppType: process.env.MicrosoftAppType,
     MicrosoftAppId: process.env.MicrosoftAppId,
     MicrosoftAppPassword: process.env.MicrosoftAppPassword,
+    MicrosoftAppType: process.env.MicrosoftAppType,
     MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 });
 
