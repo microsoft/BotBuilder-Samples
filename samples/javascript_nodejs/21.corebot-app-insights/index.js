cc31887281fcd6a1765e8ca2501df3a8e9706ce5
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2021-11-01 19:03:52 -0300

    [#3526] Add support for MSI to JS samples and generators - advanced bots (#3553)
    * Add MSI support in JS 16.proactive-messages
    
    * Add MSI support to JS 17.multilingual-bot
    
    * Add MSI support to JS 19.custom-dialogs
    
    * Add MSI support to JS 21.corebot-app-insights
    
    * Add MSI support to JS 23.facebook-events
    
    * Add MSI support to JS 44.prompt-for-user-input
    
    * Add MSI support to JS 47.inspection

--- samples/javascript_nodejs/21.corebot-app-insights/index.js
+++ samples/javascript_nodejs/21.corebot-app-insights/index.js
@@ -40,7 +40,9 @@ const BOOKING_DIALOG = 'bookingDialog';
 
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
     MicrosoftAppId: process.env.MicrosoftAppId,
-    MicrosoftAppPassword: process.env.MicrosoftAppPassword
+    MicrosoftAppPassword: process.env.MicrosoftAppPassword,
+    MicrosoftAppType: process.env.MicrosoftAppType,
+    MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 });
 
 const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
