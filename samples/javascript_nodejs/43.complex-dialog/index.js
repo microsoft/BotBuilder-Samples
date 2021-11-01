c0b0d424bb1a8d8bf0e8f017dde48d759daf0967
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2021-11-01 19:03:38 -0300

    [#3526] Add support for MSI to JS samples and generators - bot essentials (part 2/2) (#3552)
    * Add MSI support to JS 13.core-bot
    
    * Add MSI support to JS 14.nlp-with-orchestrator
    
    * Add MSI support to JS 15.handling-attachments
    
    * Add MSI support to JS 43.complex-dialog
    
    * Add MSI support to JS 45.state-management

--- samples/javascript_nodejs/43.complex-dialog/index.js
+++ samples/javascript_nodejs/43.complex-dialog/index.js
@@ -35,7 +35,9 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
     MicrosoftAppId: process.env.MicrosoftAppId,
-    MicrosoftAppPassword: process.env.MicrosoftAppPassword
+    MicrosoftAppPassword: process.env.MicrosoftAppPassword,
+    MicrosoftAppType: process.env.MicrosoftAppType,
+    MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 });
 
 const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfiguration(null, credentialsFactory);
