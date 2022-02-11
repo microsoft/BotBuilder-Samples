d2e9c9ff6d3cfe56d5c5db082a74f6a0e6aa8110
Bruce Haley <v-brucehaley@microsoft.com>
2022-02-10 18:34:18 -0800

    Add SendDirectLineMessage retries

--- samples/csharp_dotnetcore/tests/Samples.CoreBot.FunctionalTests/DirectLineClientTests.cs
+++ samples/csharp_dotnetcore/tests/Samples.CoreBot.FunctionalTests/DirectLineClientTests.cs
@@ -28,6 +28,15 @@ namespace Samples.CoreBot.FunctionalTests
             await StartBotConversationAsync(input);
 
             var botAnswer = await StartBotConversationAsync(input);
+
+            int retries = 4;
+            if (String.IsNullOrWhiteSpace(botAnswer) && retries-- > 0)
+            {
+                // Wait half a second before retrying.
+                await Task.Delay(TimeSpan.FromMilliseconds(500)).ConfigureAwait(false);
+                botAnswer = await StartBotConversationAsync(input);
+            }
+
             Assert.IsTrue(!String.IsNullOrWhiteSpace(botAnswer) && botAnswer.Contains("travel", StringComparison.OrdinalIgnoreCase),
                 $"Expected: A message containing 'travel'. Actual:<{botAnswer}>.");
         }
@@ -98,8 +107,8 @@ namespace Samples.CoreBot.FunctionalTests
 
                 if (answer.Equals(string.Empty))
                 {
-                    // Wait for one second before polling the bot again.
-                    await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+                    // Wait for half a second before polling the bot again.
+                    await Task.Delay(TimeSpan.FromMilliseconds(500)).ConfigureAwait(false);
                 }
             }
 
