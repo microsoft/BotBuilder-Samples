ac9f891a748bf3a15468ce4af36ff18ff14eb337
Bruce Haley <v-brucehaley@microsoft.com>
2022-05-26 12:31:59 -0700

    Fix Get-Location

--- composer-samples/csharp_dotnetcore/pipelines/CICDPipelineSample/build/yaml/templates/Build-Orchestrator.ps1
+++ composer-samples/csharp_dotnetcore/pipelines/CICDPipelineSample/build/yaml/templates/Build-Orchestrator.ps1
@@ -36,7 +36,7 @@ if ($models.Count -eq 0)
     Write-Host "No orchestrator models found."
     Write-Host $crossTrainedLUDirectory
     Write-Host $sourceDirectory
-    Write-Host Get-Location
+    Get-Location
     exit 0        
 }
 Write-Host "Orchestrator models"
