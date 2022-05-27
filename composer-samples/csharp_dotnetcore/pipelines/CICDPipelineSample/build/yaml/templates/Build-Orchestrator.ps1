b33eff18060893158a2f01e3a7cd92641b8fa114
Gabo Gilabert <ggilaber@microsoft.com>
2022-05-26 22:53:08 -0400

    Reverted some path changes and removed some diagnostics code

--- composer-samples/csharp_dotnetcore/pipelines/CICDPipelineSample/build/yaml/templates/Build-Orchestrator.ps1
+++ composer-samples/csharp_dotnetcore/pipelines/CICDPipelineSample/build/yaml/templates/Build-Orchestrator.ps1
@@ -34,9 +34,6 @@ $models = Get-LUModels -recognizerType "Microsoft.OrchestratorRecognizer" -cross
 if ($models.Count -eq 0)
 {
     Write-Host "No orchestrator models found."
-    Write-Host "-crossTrainedLUDirectory $crossTrainedLUDirectory"
-    Write-Host "-sourceDirectory $sourceDirectory"
-    Write-Host "Current directory: $(Get-Location)"
     exit 0        
 }
 Write-Host "Orchestrator models"
