b75911507ce84c53f2d46230f09414d915fe86c9
Hongyang Du <hond@microsoft.com>
2023-02-22 10:49:23 +0800

    remove linux deploy files

--- samples/csharp_dotnetcore/83.openai/Bots/OpenAIBot.cs
+++ samples/csharp_dotnetcore/83.openai/Bots/OpenAIBot.cs
@@ -27,7 +27,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var welcomeText = "Hello and welcome! Please say anything as the openai prompt.";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
