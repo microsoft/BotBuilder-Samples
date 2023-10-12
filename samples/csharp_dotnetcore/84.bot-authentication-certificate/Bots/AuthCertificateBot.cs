43b580cdb59d4bdcc987881c8aeecbe2cb58328c
JhontSouth <jhonatan.sandoval@southworks.com>
2023-10-12 10:44:20 -0500

    remove unused libraries

--- samples/csharp_dotnetcore/84.bot-authentication-certificate/Bots/AuthCertificateBot.cs
+++ samples/csharp_dotnetcore/84.bot-authentication-certificate/Bots/AuthCertificateBot.cs
@@ -5,9 +5,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
-using Microsoft.Extensions.Logging;
 
 namespace Microsoft.BotBuilderSamples
 {
